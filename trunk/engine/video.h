3ef704b06e80508f34b17f7b3ab25ffba9dd2b47
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-01-26 06:37:32 +0000

    video structure.

--- trunk/engine/video.h
+++ trunk/engine/video.h
@@ -1,4 +1,17 @@
-/*	$Csoft$	*/
+/*	$Csoft: video.h,v 1.1 2002/01/26 03:37:38 vedge Exp $	*/
 
-int	video_play(char *, SDL_Surface *);
+#include <smpeg.h>
+
+struct video {
+	int	flags;
+#define VIDEO_STREAM	0x01
+	int	fd;
+
+	SMPEG	  	*mpeg;
+	pthread_t	thread;	/* Playback thread */
+	SDL_mutex	*lock;	/* Lock on mpeg */
+};
+
+struct video	*video_create(char *, SDL_Surface *);
+void		 video_destroy(struct video *);
 
