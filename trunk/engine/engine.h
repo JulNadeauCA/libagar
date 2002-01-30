2d8121e587c1311e7cd02ff2722e79d126008dad
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-01-30 12:30:04 +0000

    engine initialization. this hides command-line settings and map edition switching from the script layer.

---
+++ trunk/engine/engine.h
@@ -0,0 +1,18 @@
+/*	$Csoft$	*/
+
+#include <libfobj/fobj.h>
+
+#include <engine/debug.h>
+#include <engine/view.h>
+#include <engine/object.h>
+#include <engine/xcf.h>
+#include <engine/world.h>
+#include <engine/map.h>
+#include <engine/event.h>
+#include <engine/char.h>
+
+int	engine_init(int, char **);
+int	engine_mapedit(void);
+void	engine_destroy(void);
+void	engine_join(void);
+
