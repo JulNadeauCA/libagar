e4df41e7d8b54f30bfe49a9eb9aef3a1b471b41c
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-02-03 11:08:08 +0000

    - avoid macro tricks that fail on, ie. MIPSpro.

--- trunk/engine/debug.h
+++ trunk/engine/debug.h
@@ -1,30 +1,37 @@
-/*	$Csoft: debug.h,v 1.1.1.1 2002/01/25 09:50:02 vedge Exp $	*/
+/*	$Csoft: debug.h,v 1.2 2002/01/26 06:37:18 vedge Exp $	*/
 
 #ifdef DEBUG
 extern int engine_debug;
-#define	dprintf(fmt, args...)					\
+
+# ifdef __GNUC__
+#  define dprintf(fmt, args...)					\
 	if (engine_debug)					\
 		printf("%s: " fmt, __FUNCTION__ , ##args) 
-#define	dprintfa(fmt, args...)					\
-	if (engine_debug)					\
-		printf(fmt, ##args) 
-#define	dprintfn(n, arg)					\
-	if (engine_debug > (n))					\
-n		printf("%s: " fmt, __FUNCTION__ , ##args) 
-
-#define	fatal(fmt, args...)					\
+#  define fatal(fmt, args...)					\
 	do {							\
 		printf("%s: " fmt, __FUNCTION__ , ##args);	\
 		abort();					\
 	} while (0)
+# else /* !__GNUC__ */
+#  define dprintf	printf
+#  define fatal 	printf
+# endif /* __GNUC__ */
 
 #else /* !DEBUG */
 
-#define	dprintf(arg...)		((void)0)
-#define	dprintfa(arg...)	((void)0)
-#define dprintfn(n, arg...)	((void)0)
-#define	fatal(fmt, args...)	printf("%s: " fmt, __FUNCTION__ , ##args)
+# ifdef __GNUC__
+#  define dprintf(arg...)		((void)0)
+#  define fatal(fmt, args...)	printf("%s: " fmt, __FUNCTION__ , ##args)
+# else
+#  define dprintf			((void)0)
+#  define fatal			printf
+# endif /* __GNUC__ */
+
 #endif /* DEBUG */
 
+#ifdef __GNUC__
 #define warning(fmt, args...)	printf("%s: " fmt, __FUNCTION__ , ##args)
+#else
+#define warning			printf
+#endif
 
