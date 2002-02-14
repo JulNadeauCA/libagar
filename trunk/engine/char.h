ca18008949187b9f9b9c7f49721ed5e741ea06b1
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-02-14 05:23:52 +0000

    - defines for sprites/anims. - use physics.

--- trunk/engine/char.h
+++ trunk/engine/char.h
@@ -1,4 +1,4 @@
-/*	$Csoft: char.h,v 1.6 2002/02/01 06:04:57 vedge Exp $	*/
+/*	$Csoft: char.h,v 1.7 2002/02/05 05:49:03 vedge Exp $	*/
 
 /*
  * Data shared by all character types.
@@ -16,8 +16,9 @@ struct character {
 #define EFFECT_POISON		0x0002
 #define EFFECT_DOOM		0x0004
 
-	struct	map *map;
-	int	x, y;
+	struct	map *map;	/* Map */
+	struct	mapdir dir;	/* Direction in map */
+	int	x, y;		/* Coordinates in map */
 
 	int	curoffs;	/* Current sprite or anim */
 	int	curspeed;	/* Current speed */
@@ -25,7 +26,8 @@ struct character {
 
 	int	flags;		/* Current state */
 #define CHAR_FOCUS	0x0001	/* Being controlled/viewed */
-#define CHAR_ANIM	0x0002	/* Animation in progress */
+#define CHAR_ONMAP	0x0002
+#define CHAR_ANIM	0x0004	/* Animation in progress */
 #define CHAR_DASH	0x0010	/* Boost timer temporarily */
 	
 	SDL_TimerID timer;
@@ -37,6 +39,21 @@ struct character {
 extern struct character *curchar;	/* Controlled character */
 
 struct character *char_create(char *, char *, int, int, int);
+
+/* Sprites */
+#define CHAR_ICON	0
+#define CHAR_IDLE	1
+#define CHAR_UP		2
+#define CHAR_DOWN	3
+#define CHAR_LEFT	4
+#define CHAR_RIGHT	5
+
+/* Animations */
+#define CHAR_WALKUP	0
+#define CHAR_WALKDOWN	1
+#define CHAR_WALKLEFT	2
+#define CHAR_WALKRIGHT	3
+
 
 int	char_setanim(struct character *, int);
 int	char_setsprite(struct character *, int);
