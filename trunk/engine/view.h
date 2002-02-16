03962f616775469c683207c8080acc77ec8c353b
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-02-16 04:55:46 +0000

    - view_setmode() -> view_setmap(). - no longer keep tile geo here.

--- trunk/engine/view.h
+++ trunk/engine/view.h
@@ -1,15 +1,13 @@
-/*	$Csoft: view.h,v 1.3 2002/02/10 01:38:40 vedge Exp $	*/
+/*	$Csoft: view.h,v 1.4 2002/02/15 10:49:55 vedge Exp $	*/
 
 struct viewport {
-	int	fps;			/* Refresh rate in FPS */
-	int	width, height, depth;	/* Viewport geometry */
-	int	tilew, tileh;		/* Tile geometry */
-	int	flags;			/* SDL surface flags */
-	int	mapw, maph;		/* Width/height in nodes */
-	int	mapx, mapy;		/* Map coordinates */
-	int	redraw;			/* Need redrawing */
-	struct	map *map;		/* Currently visible map */
-	SLIST_HEAD(, window) winsh;	/* Viewport sub-surfaces */
+	int	fps;				  /* Refresh rate in FPS */
+	int	width, height, depth;		  /* Viewport geometry */
+	struct	map *map;			  /* Currently visible map */
+	int	mapw, maph;			  /* Map coordinates */
+	int	mapx, mapy;			  /* Map coordinates */
+	int	flags;				  /* SDL surface flags */
+	SLIST_HEAD(, window) winsh;		  /* Viewport sub-surfaces */
 	SDL_Surface *v;
 };
 
@@ -24,8 +22,8 @@ struct window {
 
 extern struct viewport *mainview;	/* view.c */
 
-struct viewport *view_create(int, int, int, int, int, int);
-int		 view_setmode(struct viewport *);
+struct viewport *view_create(int, int, int, int);
+int		 view_setmap(struct viewport *, struct map *);
 void		 view_destroy(struct viewport *);
 int		 view_fullscreen(struct viewport *, int);
 void		 view_center(struct viewport *, int, int);
