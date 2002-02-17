43099797fd4087197fc84c7485a1a1e2c2c697d1
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-02-17 08:16:04 +0000

    - name clash.

--- trunk/engine/mapedit/command.h
+++ trunk/engine/mapedit/command.h
@@ -1,12 +1,12 @@
-/*	$Csoft: command.h,v 1.2 2002/02/16 09:14:15 vedge Exp $	*/
+/*	$Csoft: command.h,v 1.3 2002/02/17 01:03:15 vedge Exp $	*/
 
 void	mapedit_push(struct mapedit *, struct node *, int, int);
 void	mapedit_pop(struct mapedit *, struct node *);
 void	mapedit_clearmap(struct mapedit *);
 void	mapedit_fillmap(struct mapedit *);
 void	mapedit_setorigin(struct mapedit *, int *, int *);
-void	mapedit_load(struct mapedit *);
-void	mapedit_save(struct mapedit *);
+void	mapedit_loadmap(struct mapedit *);
+void	mapedit_savemap(struct mapedit *);
 void	mapedit_examine(struct map *, int, int);
 void	mapedit_editflags(struct mapedit *, int);
 void	mapedit_nodeflags(struct mapedit *, struct node *, int);
