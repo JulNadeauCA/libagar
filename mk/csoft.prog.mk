031cc653f675697956eb11629e0dcae63ba9aa46
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-02-18 02:52:41 +0000

    regen
    git-svn-id: https://dev.csoft.net/agar/trunk@348 86a84d31-9702-0410-8f1e-a60cab7bd424

--- mk/csoft.prog.mk
+++ mk/csoft.prog.mk
@@ -1,4 +1,4 @@
-# $Csoft: csoft.prog.mk,v 1.16 2002/02/18 02:07:36 vedge Exp $
+# $Csoft: csoft.prog.mk,v 1.17 2002/02/18 02:51:21 vedge Exp $
 
 # Copyright (c) 2001 CubeSoft Communications, Inc.
 # <http://www.csoft.org>
