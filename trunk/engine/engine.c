921177d021b37c90f4a1cc4d764cbbfc8bddf860
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-02-23 02:53:10 +0000

    - missing license.

--- trunk/engine/engine.c
+++ trunk/engine/engine.c
@@ -1,4 +1,32 @@
-/*	$Csoft: engine.c,v 1.15 2002/02/18 03:55:20 vedge Exp $	*/
+/*	$Csoft: engine.c,v 1.16 2002/02/21 02:18:10 vedge Exp $	*/
+
+/*
+ * Copyright (c) 2001 CubeSoft Communications, Inc.
+ * <http://www.csoft.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistribution of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistribution in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of CubeSoft Communications, nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <errno.h>
 #include <stdio.h>
@@ -112,7 +140,6 @@ engine_init(int argc, char *argv[], struct gameinfo *gameinfo, char *path)
 	if (SDL_InitSubSystem(SDL_INIT_JOYSTICK) != 0) {
 		njoy = -1;
 	}
-#if 0
 	if (njoy >= 0) {
 		joy = SDL_JoystickOpen(njoy);
 		if (joy != NULL) {
@@ -123,7 +150,6 @@ engine_init(int argc, char *argv[], struct gameinfo *gameinfo, char *path)
 			warning("no joystick at #%d\n", njoy);
 		}
 	}
-#endif
 	if (flags & SDL_FULLSCREEN) {
 		/* Give the new mode some time to take effect. */
 		SDL_Delay(1000);
