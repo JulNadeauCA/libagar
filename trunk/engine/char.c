3db5bd3fac25ce48dba338550bd37061b98cecda
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-02-25 08:55:08 +0000

    - assume multiple positions on the map using back references.

--- trunk/engine/char.c
+++ trunk/engine/char.c
@@ -1,4 +1,4 @@
-/*	$Csoft: char.c,v 1.17 2002/02/17 07:57:03 vedge Exp $	*/
+/*	$Csoft: char.c,v 1.18 2002/02/18 01:30:09 vedge Exp $	*/
 
 /*
  * Copyright (c) 2001 CubeSoft Communications, Inc.
@@ -37,21 +37,17 @@
 
 static struct obvec char_vec = {
 	char_destroy,
-	char_event,
 	char_load,
 	char_save,
 	char_link,
-	char_unlink
+	char_unlink,
+	char_dump
 };
 
 static Uint32	char_time(Uint32, void *);
-static void	char_joybutton(struct character *, SDL_Event *);
-static void	char_key(struct character *, SDL_Event *);
-
-struct character *curchar;
 
 struct character *
-char_create(char *name, char *desc, int maxhp, int maxmp, int flags)
+char_create(char *name, char *desc, Uint32 maxhp, Uint32 maxmp, Uint32 flags)
 {
 	struct character *ch;
 	
@@ -69,15 +65,7 @@ char_create(char *name, char *desc, int maxhp, int maxmp, int flags)
 	ch->maxmp = maxmp;
 	ch->hp = maxhp;
 	ch->mp = maxmp;
-
-	ch->map = NULL;
-	mapdir_init(&ch->dir, (struct object *)ch, NULL, 0, -1);
-	ch->x = -1;
-	ch->y = -1;
-
-	ch->curoffs = CHAR_UP;
-	ch->curspeed = -1;
-	ch->maxspeed = -1;
+	ch->maxspeed = 30;
 
 	dprintf("%s: new: hp %d/%d mp %d/%d\n",
 	    ch->obj.name, ch->hp, ch->maxhp, ch->mp, ch->maxmp);
@@ -89,6 +77,8 @@ int
 char_load(void *p, int fd)
 {
 	struct character *ch = (struct character *)p;
+	char *mname;
+	Uint32 i, ncoords;
 
 	if (version_read(fd, "agar char", 1, 0) != 0) {
 		return (-1);
@@ -101,6 +91,7 @@ char_load(void *p, int fd)
 	ch->exp = fobj_read_uint32(fd);
 	ch->age = fobj_read_uint32(fd);
 	ch->seed = fobj_read_uint64(fd);
+	ch->maxspeed = fobj_read_uint32(fd);
 
 	ch->maxhp = fobj_read_uint32(fd);
 	ch->hp = fobj_read_uint32(fd);
@@ -111,40 +102,38 @@ char_load(void *p, int fd)
 	    ch->flags, ch->level, ch->exp, ch->age,
 	    ch->hp, ch->maxhp, ch->mp, ch->maxhp);
 
-	if (ch->flags & CHAR_ONMAP) {
-		char *mname;
-		int i, ncoords;
-
-		/* Read single map coordinates. */
-		ncoords = fobj_read_uint32(fd);
-		for (i = 0; i < ncoords; i++) {
-			struct map *m;
-			int x, y;
+	/* Read single map coordinates. */
+	ncoords = fobj_read_uint32(fd);
+	for (i = 0; i < ncoords; i++) {
+		struct map *m;
+		Uint32 x, y, offs, flags, speed;
 		
-			mname = fobj_read_string(fd);
-			x = fobj_read_uint32(fd);
-			y = fobj_read_uint32(fd);
-			ch->curoffs = fobj_read_uint32(fd);
-			ch->curspeed = fobj_read_uint32(fd);
-			ch->maxspeed = fobj_read_uint32(fd);
+		mname = fobj_read_string(fd);
+		x = fobj_read_uint32(fd);
+		y = fobj_read_uint32(fd);
+		offs = fobj_read_uint32(fd);
+		flags = fobj_read_uint32(fd);
+		speed = fobj_read_uint32(fd);
 
-			m = (struct map *)object_strfind(mname);
-			if (m != NULL) {
-				struct node *node;
+		m = (struct map *)object_strfind(mname);
+		if (m != NULL) {
+			struct node *node;
+			struct map_bref *bref;
 
-				node = &m->map[x][y];
-				pthread_mutex_lock(&m->lock);
-				char_add(ch, m, x, y);
-				pthread_mutex_unlock(&m->lock);
-				dprintf("at %s:%d,%d\n", m->obj.name, x, y);
-			} else {
-				fatal("no such map: \"%s\"\n", mname);
-			}
-			free(mname);
+			pthread_mutex_lock(&m->lock);
+			node = &m->map[x][y];
+			bref = object_madd(ch, offs, flags, m, x, y);
+			bref->speed = speed;
+			pthread_mutex_unlock(&m->lock);
+
+			dprintf("at %s:%d,%d\n", m->obj.name, x, y);
+		} else {
+			fatal("no such map: \"%s\"\n", mname);
 		}
-		dprintf("%d coordinates\n", ncoords);
+		free(mname);
 	}
-
+	dprintf("%d coordinates\n", ncoords);
+	
 	return (0);
 }
 
@@ -153,6 +142,9 @@ char_save(void *p, int fd)
 {
 	struct character *ch = (struct character *)p;
 	struct fobj_buf *buf;
+	struct map_bref *bref;
+	size_t soffs;
+	Uint32 count;
 
 	buf = fobj_create_buf(128, 4);
 	if (buf == NULL) {
@@ -168,22 +160,31 @@ char_save(void *p, int fd)
 	fobj_bwrite_uint32(buf, ch->exp);
 	fobj_bwrite_uint32(buf, ch->age);
 	fobj_bwrite_uint64(buf, ch->seed);
+	fobj_bwrite_uint32(buf, ch->maxspeed);
 
 	fobj_bwrite_uint32(buf, ch->maxhp);
 	fobj_bwrite_uint32(buf, ch->hp);
 	fobj_bwrite_uint32(buf, ch->maxmp);
 	fobj_bwrite_uint32(buf, ch->mp);
-	
-	fobj_bwrite_uint32(buf, 1);
-	if (ch->flags & CHAR_ONMAP) {
-		/* Write single map coordinates. */
-		fobj_bwrite_string(buf, ch->map->obj.name);
-		fobj_bwrite_uint32(buf, ch->x);
-		fobj_bwrite_uint32(buf, ch->y);
-		fobj_bwrite_uint32(buf, ch->curoffs);
-		fobj_bwrite_uint32(buf, ch->curspeed);
-		fobj_bwrite_uint32(buf, ch->maxspeed);
+
+	/* Write map back references */
+	soffs = buf->offs;
+	fobj_bwrite_uint32(buf, 0);
+	count = 0;
+	SLIST_FOREACH(bref, &ch->obj.brefsh, brefs) {
+		dprintf("%s: reference %s:%d,%d offs=%d flags=0x%x\n",
+		    ch->obj.name, bref->map->obj.name, bref->x, bref->y,
+		    bref->nref->offs, bref->nref->flags);
+		fobj_bwrite_string(buf, bref->map->obj.name);
+		fobj_bwrite_uint32(buf, bref->x);
+		fobj_bwrite_uint32(buf, bref->y);
+		fobj_bwrite_uint32(buf, bref->nref->offs);
+		fobj_bwrite_uint32(buf, bref->nref->flags);
+		fobj_bwrite_uint32(buf, bref->speed);
+		count++;
 	}
+	fobj_bpwrite_uint32(buf, count, soffs);
+	dprintf("%s: %d back references\n", ch->obj.name, count);
 
 	fobj_flush_buf(buf, fd);
 	return (0);
@@ -202,7 +203,8 @@ char_link(void *ob)
 		return (-1);
 	}
 
-	ch->timer = SDL_AddTimer(ch->maxspeed - ch->curspeed, char_time, ch);
+	/* XXX speed */
+	ch->timer = SDL_AddTimer(ch->maxspeed / 2, char_time, ch);
 	if (ch->timer == NULL) {
 		fatal("SDL_AddTimer: %s\n", SDL_GetError());
 		return (-1);
@@ -239,21 +241,6 @@ char_destroy(void *p)
 
 	SDL_RemoveTimer(ch->timer);
 
-	if (ch->flags & CHAR_FOCUS) {
-		char_unfocus(ch);
-	}
-
-	pthread_mutex_lock(&ch->map->lock);
-	if (ch->map != NULL) {
-		struct node *node = &ch->map->map[ch->x][ch->y];
-		struct noderef *nref;
-
-		while ((nref = node_findref(node, ch, -1))) {
-			node_delref(node, nref);
-		}
-	}
-	pthread_mutex_unlock(&ch->map->lock);
-
 	pthread_mutex_lock(&world->lock);
 	SLIST_REMOVE(&world->wcharsh, ch, character, wchars);
 	pthread_mutex_unlock(&world->lock);
@@ -261,151 +248,12 @@ char_destroy(void *p)
 	return (0);
 }
 
-/* See if ch can move to nx:ny in its map. */
-int
-char_canmove(struct character *ch, int nx, int ny)
-{
-	struct node *me;
-
-	me = &ch->map->map[nx][ny];
-
-	return ((me->flags & NODE_WALK) ? 0 : -1);
-}
-
-void
-char_event(void *p, SDL_Event *ev)
-{
-	struct character *ch = (struct character *)p;
-
-	switch (ev->type) {
-	case SDL_JOYBUTTONDOWN:
-	case SDL_JOYBUTTONUP:
-		char_joybutton(ch, ev);
-		break;
-	case SDL_KEYDOWN:
-	case SDL_KEYUP:
-		char_key(ch, ev);
-		break;
-	}
-}
-
-/* Change current sprite. */
-int
-char_setsprite(struct character *ch, int soffs)
-{
-	struct noderef *nref;
-
-	nref = node_findref(&ch->map->map[ch->x][ch->y], ch, ch->curoffs);
-	node_delref(&ch->map->map[ch->x][ch->y], nref);
-	node_addref(&ch->map->map[ch->x][ch->y], ch, soffs, MAPREF_SPRITE);
-
-	ch->flags &= ~(CHAR_ANIM);
-	ch->curoffs = soffs;
-
-	return (0);
-}
-
-/* Change current animation. */
-int
-char_setanim(struct character *ch, int aoffs)
-{
-	struct noderef *nref;
-	
-	nref = node_findref(&ch->map->map[ch->x][ch->y], ch, ch->curoffs);
-	node_delref(&ch->map->map[ch->x][ch->y], nref);
-	node_addref(&ch->map->map[ch->x][ch->y], ch, aoffs, MAPREF_ANIM);
-
-	ch->flags |= CHAR_ANIM;
-	ch->curoffs = aoffs;
-
-	return (0);
-}
-
-/* Let the player control this character. */
-int
-char_focus(struct character *ch)
-{
-	ch->flags |= CHAR_FOCUS;
-	curchar = ch;
-
-	return (0);
-}
-
-int
-char_unfocus(struct character *ch)
-{
-	ch->flags &= CHAR_FOCUS;
-	curchar = NULL;
-	
-	return (0);
-}
-
-/* Position the character on m:x,y. */
-int
-char_add(struct character *ch, struct map *m, int x, int y)
-{
-	if (ch->flags & CHAR_ANIM) {
-		node_addref(&m->map[x][y], ch, ch->curoffs, MAPREF_ANIM);
-	} else {
-		node_addref(&m->map[x][y], ch, ch->curoffs, MAPREF_SPRITE);
-	}
-
-	ch->map = m;
-	if ((ch->flags & CHAR_ONMAP) == 0) {
-		mapdir_init(&ch->dir, (struct object *)ch, m,
-		    DIR_SCROLLVIEW|DIR_SOFTSCROLL, 5);
-		ch->flags |= CHAR_ONMAP;
-	}
-
-	ch->x = x;
-	ch->y = y;
-	
-	return (0);
-}
-
-/* Delete any reference to the character on m:x,y. */
-int
-char_del(struct character *ch, struct map *m, int x, int y)
-{
-	struct node *node = &m->map[x][y];
-	struct noderef *nref;
-
-	while ((nref = node_findref(node, ch, -1))) {
-		node_delref(node, nref);
-	}
-
-	return (0);
-}
-
-/* Move the character from its current position to nx,ny. */
-int
-char_move(struct character *ch, int nx, int ny)
-{
-	struct noderef *nref;
-	int oxoffs, oyoffs;
-
-	/* XXX recurse */
-	nref = node_findref(&ch->map->map[ch->x][ch->y], ch, -1);
-	oxoffs = nref->xoffs;
-	oyoffs = nref->yoffs;
-
-	char_del(ch, ch->map, ch->x, ch->y);
-	char_add(ch, ch->map, nx, ny);
-
-	/* XXX recurse */
-	nref = node_findref(&ch->map->map[nx][ny], ch, -1);
-	nref->xoffs = oxoffs;
-	nref->yoffs = oyoffs;
-
-	ch->x = nx;
-	ch->y = ny;
-
-	return (0);
-}
-
 void
 char_setspeed(struct character *ch, Uint32 speed)
 {
+#if 1
+	dprintf("not yet\n");
+#else
 	ch->curspeed = speed;
 
 	if (ch->curspeed >= ch->maxspeed) {
@@ -417,157 +265,61 @@ char_setspeed(struct character *ch, Uint32 speed)
 		ch->timer = SDL_AddTimer(ch->maxspeed - ch->curspeed,
 		    char_time, ch);
 	}
+#endif
 }
 
+/* XXX none of this is character-dependent */
 static Uint32
 char_time(Uint32 ival, void *obp)
 {
 	struct object *ob = (struct object *)obp;
-	struct character *ch = (struct character *)ob;
-	int moved = 0;
-	int x, y;
+	struct map_bref *bref;
+	Uint32 moved = 0;
+	Uint32 x, y;
 
-	if (ch->map == NULL) {
-		/* Nothing to do. age, perhaps. */
+	SLIST_FOREACH(bref, &ob->brefsh, brefs) {
+		x = bref->x;
+		y = bref->y;
+
+		/* Move a character. */
+		moved = mapdir_move(&bref->dir, &x, &y);
+		if (moved != 0) {
+			struct mapdir dir;
+			struct map_bref *nbref;
+
+			pthread_mutex_lock(&bref->map->lock);
+
+			dir = bref->dir;	/* Structure copy */
+
+			/* Remove old back reference. */
+			object_mdel(ob, 0, MAPREF_SPRITE,
+			    bref->map, bref->x, bref->y);
+
+			/* Insert new back reference. */
+			nbref = object_madd(ob, 0, MAPREF_SPRITE,
+			    bref->map, bref->x, bref->y);
+
+			nbref->dir = dir;	/* Structure copy */
+
+			/* Ensure movement continuation, if needed. */
+			mapdir_postmove(&nbref->dir, &x, &y, moved);
+
+			pthread_mutex_unlock(&bref->map->lock);
+
+			bref->map->redraw++;	/* XXX waste */
+		}
 	}
 
-	x = ch->x;
-	y = ch->y;
-	
-	/* Move the character. */
-	moved = mapdir_move(&ch->dir, &x, &y);
-	if (moved != 0) {
-#if 0
-		SDL_Event nev;
-		int i, nkeys;
-#endif
-		if (moved & DIR_UP)
-			char_setanim(ch, CHAR_WALKUP);
-		if (moved & DIR_DOWN)
-			char_setanim(ch, CHAR_WALKDOWN);
-		if (moved & DIR_LEFT)
-			char_setanim(ch, CHAR_WALKLEFT);
-		if (moved & DIR_RIGHT)
-			char_setanim(ch, CHAR_WALKRIGHT);
-
-		pthread_mutex_lock(&ch->map->lock);
-		char_move(ch, x, y);
-		mapdir_postmove(&ch->dir, &x, &y, moved);
-		pthread_mutex_unlock(&ch->map->lock);
-		ch->map->redraw++;
-	
-		
-
-#if 0
-		for (i = 0; i < sizeof(stickykeys) / sizeof(int); i++) {
-			if ((SDL_GetKeyState(&nkeys))[stickykeys[i]]) {
-				nev.type = SDL_KEYDOWN;
-				nev.key.keysym.sym = stickykeys[i];
-				SDL_PushEvent(&nev);
-			}
-		}
-#endif
-
-#if 0
-		/* Assume various status ailments. */
-
-		if (nme->flags & NODE_BIO) {
-			decrease(&ch->hp, 1, 1);
-			dprintf("bio. hp = %d/%d\n", ch->hp, ch->maxhp);
-		} else if (nme->flags & NODE_REGEN) {
-			increase(&ch->hp, 1, ch->maxhp);
-			dprintf("regen. hp = %d/%d\n", ch->hp, ch->maxhp);
-		}
-
-		if (nme->flags & NODE_SLOW) {
-			/* XXX rate */
-			nme->v1 = -10;
-			char_setspeed(ch, ch->curspeed + nme->v1);
-			dprintf("slow. speed = %d\n", ch->curspeed);
-		} else if (nme->flags & NODE_HASTE) {
-			/* XXX rate */
-			nme->v1 = 10;
-			char_setspeed(ch, ch->curspeed + nme->v1);
-			dprintf("haste. speed = %d\n", ch->curspeed);
-		}
-#endif
-	}
-	
 	return (ival);
 }
 
-static void
-char_joybutton(struct character *ch, SDL_Event *ev)
-{
-	static SDL_Event nev;
-	
-	dprintf("key %d\n", ev->jbutton.button);
-	
-	nev.type = (ev->type == SDL_JOYBUTTONUP) ?
-	    SDL_KEYUP : SDL_KEYDOWN;
-	
-	/* XXX remap */
-	switch (ev->jbutton.button) {
-	case 2:
-		nev.key.keysym.sym = SDLK_d;	/* Dash */
-		break;
-	/* ... */
-	}
-	SDL_PushEvent(&nev);
-}
-
-static void
-char_key(struct character *ch, SDL_Event *ev)
-{
-	int set;
-
-	set = (ev->type == SDL_KEYDOWN) ? 1 : 0;
-
-	pthread_mutex_lock(&ch->map->lock);
-
-	switch (ev->key.keysym.sym) {
-	case SDLK_d:
-		/* Dash */
-		if (ev->type == SDL_KEYDOWN) {
-			ch->flags |= CHAR_DASH;
-			char_setspeed(ch, 40);	/* XXX use v1 */
-		} else if (ev->type == SDL_KEYUP) {
-			ch->flags &= ~(CHAR_DASH);
-			char_setspeed(ch, 1);	/* XXX restore speed */
-		}
-		break;
-	case SDLK_UP:
-		mapdir_set(&ch->dir, DIR_UP, set);
-		break;
-	case SDLK_DOWN:
-		mapdir_set(&ch->dir, DIR_DOWN, set);
-		break;
-	case SDLK_LEFT:
-		mapdir_set(&ch->dir, DIR_LEFT, set);
-		break;
-	case SDLK_RIGHT:
-		mapdir_set(&ch->dir, DIR_RIGHT, set);
-		break;
-	default:
-		break;
-	}
-	
-	pthread_mutex_unlock(&ch->map->lock);
-}
-
-#ifdef DEBUG
-
 void
-char_dump(struct character *ch)
+char_dump(void *p)
 {
-	printf("%3d. %10s lvl %d (exp %d) hp %d/%d mp %d/%d at %s:%dx%d\n",
-	    ch->obj.id, ch->obj.name, ch->level, ch->exp, ch->hp, ch->maxhp,
-	    ch->mp, ch->maxmp, ch->map->obj.name, ch->x, ch->y);
-	printf("\t\t< ");
-	if (ch->flags & CHAR_FOCUS)
-		printf("focused ");
-	printf(">");
+	struct character *ch = (struct character *)p;
+	
+	printf("lvl %d (exp %d) hp %d/%d mp %d/%d\n",
+	    ch->level, ch->exp, ch->hp, ch->maxhp,
+	    ch->mp, ch->maxmp);
 }
-
-#endif /* DEBUG */
 
--- trunk/engine/char.h
+++ trunk/engine/char.h
@@ -1,7 +1,7 @@
-/*	$Csoft: char.h,v 1.9 2002/02/17 07:57:36 vedge Exp $	*/
+/*	$Csoft: char.h,v 1.10 2002/02/18 01:30:18 vedge Exp $	*/
 
 struct character {
-	struct	object obj;	/* Generic object */
+	struct	object obj;
 
 	Uint32	level;		/* Current level */
 	Uint32	exp;		/* Experience */
@@ -9,31 +9,19 @@ struct character {
 	Uint64	seed;		/* Random seed */
 	
 	Uint32	flags;
-#define CHAR_FOCUS	0x0001	/* Being controlled/viewed */
-#define CHAR_ONMAP	0x0002	/* Assume position on a map */
-#define CHAR_ANIM	0x0004	/* Animation in progress */
 #define CHAR_DASH	0x0010	/* Boost timer temporarily */
 #define CHAR_REGEN	0x0020	/* HP regeneration */
 #define CHAR_POISON	0x0040	/* HP decay */
 #define CHAR_DOOM	0x0080	/* Doom */
-#define CHAR_DONTSAVE	(CHAR_FOCUS|CHAR_ANIM|CHAR_DASH)
+#define CHAR_DONTSAVE	(CHAR_DASH)
 	
 	Uint32	maxhp, maxmp;	/* Maximum HP/MP */
 	Uint32	hp, mp;		/* Effective HP/MP */
-
-	struct	map *map;	/* Map */
-	struct	mapdir dir;	/* Direction in map */
-	Uint32	x, y;		/* Coordinates in map */
-
-	Uint32	curoffs;	/* Current sprite or anim */
-	Uint32	curspeed;	/* Current speed */
 	Uint32	maxspeed;	/* Max. speed in ms */
 
 	SDL_TimerID timer;
 	SLIST_ENTRY(character) wchars;	/* Active characters */
 };
-
-extern struct character *curchar;	/* Controlled character */
 
 /* Sprites */
 enum {
@@ -51,24 +39,13 @@ enum {
 	CHAR_WALKRIGHT
 };
 
-struct character *char_create(char *, char *, int, int, int);
+struct character *char_create(char *, char *, Uint32, Uint32, Uint32);
 int		  char_destroy(void *);
-void		  char_event(void *, SDL_Event *);
 int		  char_load(void *, int);
 int		  char_save(void *, int);
 int		  char_link(void *);
 int		  char_unlink(void *);
-#ifdef DEBUG
-void		  char_dump(struct character *);
-#endif
+void		  char_dump(void *);
 
-int		  char_focus(struct character *);
-int		  char_unfocus(struct character *);
-int		  char_setanim(struct character *, int);
-int		  char_setsprite(struct character *, int);
-int		  char_add(struct character *, struct map *, int, int);
-int		  char_canmove(struct character *, int, int);
-int		  char_move(struct character *, int, int);
-int		  char_del(struct character *, struct map *, int, int);
 void		  char_setspeed(struct character *, Uint32);
 
