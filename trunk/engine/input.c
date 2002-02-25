831a6346ed670457147ef88b31c43ecbd1dcf18a
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-02-25 08:57:29 +0000

    - multiplayer input device code; elements on the map are associated with   an input device structure (INPUT_{KEYBOARD,JOY,MOUSE}).

---
+++ trunk/engine/input.c
@@ -0,0 +1,268 @@
+/*	$Csoft$	*/
+
+/*
+ * Copyright (c) 2001 CubeSoft Communications, Inc.
+ * <http://www.csoft.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistribution of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistribution in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of CubeSoft Communications, nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdlib.h>
+
+#include <engine/engine.h>
+
+static struct obvec input_vec = {
+	input_destroy,
+	input_load,
+	input_save,
+	NULL,
+	NULL,
+	input_dump
+};
+
+static int	keyboard_init(struct input *, int);
+static int	joy_init(struct input *, int);
+static int	mouse_init(struct input *, int);
+
+struct input *
+input_create(int type, int index)
+{
+	struct input *input;
+	int rv = 0;
+	char name[64];
+	
+	switch (type) {
+	case INPUT_KEYBOARD:
+		sprintf(name, "keyboard%d", index);
+		break;
+	case INPUT_JOY:
+		sprintf(name, "joy%d:%s", index, SDL_JoystickName(index));
+		break;
+	case INPUT_MOUSE:
+		sprintf(name, "mouse%d", index);
+		break;
+	}
+
+	input = emalloc(sizeof(struct input));
+	object_init(&input->obj, name, OBJ_DEFERGC, &input_vec);
+	input->type = type;
+	input->index = index;
+	input->p = NULL;
+	input->bref = NULL;
+
+	switch (type) {
+	case INPUT_KEYBOARD:
+		rv = keyboard_init(input, index);
+		break;
+	case INPUT_JOY:
+		rv = joy_init(input, index);
+		break;
+	case INPUT_MOUSE:
+		rv = mouse_init(input, index);
+		break;
+	}
+	if (rv != 0) {
+		free(input);
+		return (NULL);
+	}
+
+	return (input);
+}
+
+static void
+input_key(struct input *in, SDL_Event *ev)
+{
+	int set;
+
+	set = (ev->type == SDL_KEYDOWN) ? 1 : 0;
+
+	switch (ev->key.keysym.sym) {
+	case SDLK_UP:
+		mapdir_set(&in->bref->dir, DIR_UP, set);
+		break;
+	case SDLK_DOWN:
+		mapdir_set(&in->bref->dir, DIR_DOWN, set);
+		break;
+	case SDLK_LEFT:
+		mapdir_set(&in->bref->dir, DIR_LEFT, set);
+		break;
+	case SDLK_RIGHT:
+		mapdir_set(&in->bref->dir, DIR_RIGHT, set);
+		break;
+	default:
+		/* XXX ... */
+		break;
+	}
+}
+
+static void
+input_joy(struct input *in, SDL_Event *ev)
+{
+	static int lastdir = 0;
+
+	switch (ev->jaxis.axis) {
+	case 0:	/* X */
+		if (ev->jaxis.value < 0) {
+			lastdir |= DIR_LEFT;
+			lastdir &= ~(DIR_RIGHT);
+			mapdir_set(&in->bref->dir, DIR_LEFT, 1);
+		} else if (ev->jaxis.value > 0) {
+			lastdir |= DIR_RIGHT;
+			lastdir &= ~(DIR_LEFT);
+			mapdir_set(&in->bref->dir, DIR_RIGHT, 1);
+		} else {
+			mapdir_set(&in->bref->dir, DIR_ALL, 0);
+		}
+		break;
+	case 1:	/* Y */
+		if (ev->jaxis.value < 0) {
+			lastdir |= DIR_UP;
+			lastdir &= ~(DIR_DOWN);
+			mapdir_set(&in->bref->dir, DIR_UP, 1);
+		} else if (ev->jaxis.value > 0) {
+			lastdir |= DIR_DOWN;
+			lastdir &= ~(DIR_UP);
+			mapdir_set(&in->bref->dir, DIR_DOWN, 1);
+		} else {
+			mapdir_set(&in->bref->dir, DIR_ALL, 0);
+		}
+		break;
+	}
+}	
+
+static void
+input_mouse(struct input *in, SDL_Event *ev)
+{
+	/* XXX ... */
+}
+
+int
+input_destroy(void *p)
+{
+	struct input *in = (struct input *)p;
+
+	switch (in->type) {
+	case INPUT_KEYBOARD:
+	case INPUT_MOUSE:
+		break;
+	case INPUT_JOY:
+		SDL_JoystickClose(in->p);
+		in->p = NULL;
+		break;
+	}
+	return (0);
+}
+
+void
+input_event(void *p, SDL_Event *ev)
+{
+	struct input *in = (struct input *)p;
+
+	if (in->bref == NULL) {
+		dprintf("%s: not controlling anything\n", in->obj.name);
+		return;
+	}
+
+	dprintf("input event: \n");
+	switch (in->type) {
+	case INPUT_KEYBOARD:
+		dprintf(" keyboard\n");
+		input_key(in, ev);
+		break;
+	case INPUT_JOY:
+		dprintf(" joy\n");
+		input_joy(in, ev);
+		break;
+	case INPUT_MOUSE:
+		dprintf(" mouse\n");
+		input_mouse(in, ev);
+		break;
+	}
+}
+
+int
+input_load(void *p, int fd)
+{
+	dprintf("todo\n");
+	return (0);
+}
+
+int
+input_save(void *p, int fd)
+{
+	dprintf("todo\n");
+	return (0);
+}
+
+static int
+keyboard_init(struct input *in, int index)
+{
+	/* XXX ... */
+	return (0);
+}
+
+static int
+joy_init(struct input *in, int index)
+{
+	if (index < 0) {
+		return (-1);
+	}
+
+	in->p = (SDL_Joystick *)SDL_JoystickOpen(index);
+	if (joy == NULL) {
+		warning("joy[%d]: %s\n", index, SDL_GetError());
+		return (-1);
+	}
+	SDL_JoystickEventState(SDL_ENABLE);
+
+	return (0);
+}
+
+static int
+mouse_init(struct input *in, int index)
+{
+	/* XXX ... */
+	return (0);
+}
+
+void
+input_dump(void *p)
+{
+	struct input *in = (struct input *)p;
+
+	switch (in->type) {
+	case INPUT_KEYBOARD:
+		printf("keyboard ");
+		break;
+	case INPUT_JOY:
+		printf("joy ");
+		break;
+	case INPUT_MOUSE:
+		printf("mouse ");
+		break;
+	}
+	printf("#%d: position=%s:%d,%d[offs %d]\n", in->index,
+	    in->bref->map->obj.name, in->bref->x, in->bref->y,
+	    in->bref->nref->offs);
+}
