28f930444bd73a1e1e354ac71be9d9d0985260a6
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-03-12 13:58:11 +0000

    MAPREF_ANIM_DELTA

--- trunk/engine/anim.h
+++ trunk/engine/anim.h
@@ -1,10 +1,10 @@
-/*	$Csoft: anim.h,v 1.2 2002/02/08 00:18:01 vedge Exp $	*/
+/*	$Csoft: anim.h,v 1.3 2002/02/08 00:37:40 vedge Exp $	*/
 
 struct anim {
-	SDL_Surface **frames;	/* Array of surfaces. */
-	int	maxframes;	/* Allocated surfaces. */
-	int	nframes;	/* Active surfaces. */
-	int	delay;		/* Interval in milliseconds */
+	SDL_Surface **frames;
+	Uint32	maxframes;
+	Uint32	frame, nframes;
+	Uint32	delta, delay;	/* For MAPREF_ANIM_DELTA */
 };
 
 struct anim	*anim_create(int);
