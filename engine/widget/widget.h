d50bc69362117aab7b98e70e77aa77af6f8b6c85
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-04-20 06:20:57 +0000

    WIDGET_DRAW().
    git-svn-id: https://dev.csoft.net/agar/trunk@621 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/widget/widget.h
+++ engine/widget/widget.h
@@ -1,4 +1,4 @@
-/*	$Csoft: widget.h,v 1.1 2002/04/18 03:57:28 vedge Exp $	*/
+/*	$Csoft: widget.h,v 1.2 2002/04/20 05:48:48 vedge Exp $	*/
 
 struct widvec {
 	struct	 obvec obvec;
@@ -10,16 +10,28 @@ struct widget {
 
 	Uint32	 flags;
 #define WIDGET_HIDE		0x01	/* Don't draw widget. */
+#define WIDGET_FOCUS		0x02	/* Catch keys and mouse motion. */
 
 	Uint32	*fgcolor;		/* Foreground color */
 
 	struct	window *win;		/* Parent window */
-	SDL_Rect rect;			/* Rectangle within parent window */
+	SDL_Rect rect;			/* Rectangle within parent window;
+					   width/height may be zero. */
 
 	TAILQ_ENTRY(widget) widgets;	/* Widgets within parent window */
 };
 
+#define WIDGET(wi)	((struct widget *)(wi))
 #define WIDVEC(ob)	((struct widvec *)OBJECT((ob))->vec)
+
+#define WIDGET_DRAW(wi, s) do {						\
+	static SDL_Rect rd;						\
+									\
+	rd = WIDGET((wi))->rect;					\
+	rd.x += WIDGET((wi))->win->rect.x;				\
+	rd.y += WIDGET((wi))->win->rect.y;				\
+	SDL_BlitSurface(s, NULL, WIDGET((wi))->win->view->v, &rd);	\
+} while (/*CONSTCOND*/0)
 
 struct widget	*widget_create(struct window *, char *, Uint32, SDL_Rect,
 		     Uint32 *);
