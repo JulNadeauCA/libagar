32e108cf7795bb37661988bec4f0178d2c52a8be
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-04-21 13:35:20 +0000

    - render; x/y margin - label justification
    git-svn-id: https://dev.csoft.net/agar/trunk@639 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/widget/button.c
+++ engine/widget/button.c
@@ -1,4 +1,4 @@
-/*	$Csoft$	*/
+/*	$Csoft: button.c,v 1.1 2002/04/20 11:23:52 vedge Exp $	*/
 
 /*
  * Copyright (c) 2002 CubeSoft Communications, Inc.
@@ -40,6 +40,7 @@
 
 #include "text.h"
 #include "widget.h"
+#include "widget_offs.h"
 #include "window.h"
 #include "button.h"
 
@@ -53,26 +54,37 @@ static struct widvec button_vec = {
 		button_link,	/* link */
 		button_unlink	/* unlink */
 	},
-	button_draw
+	button_draw,
+	button_event
 };
 
 struct button *
 button_create(struct window *win, char *name, char *caption, Uint32 flags,
-    Uint32 x, Uint32 y)
+    Uint32 x, Uint32 y, Uint32 w, Uint32 h)
 {
 	struct button *b;
+	struct fobj *fob;
 	SDL_Rect rd;
 
 	rd.x = x;
 	rd.y = y;
-	rd.w = 128;	/* variable */
-	rd.h = 32;	/* variable */
+	rd.w = w;	/* variable */
+	rd.h = h;	/* variable */
 
 	b = (struct button *)emalloc(sizeof(struct button));
 	widget_init(&b->wid, name, 0, &button_vec, win, rd);
 
 	b->caption = strdup(caption);
 	b->flags = flags;
+	b->justify = BUTTON_CENTER;
+	b->xmargin = 6;
+	b->ymargin = 6;
+
+	fob = fobj_load(savepath("widget", "fob"));
+	if (fob == NULL) {
+		return (NULL);
+	}
+	xcf_load(fob, BUTTON_XCF, OBJECT(b));
 
 	return (b);
 }
@@ -104,15 +116,45 @@ button_draw(void *p)
 {
 	static SDL_Color white = { 255, 255, 255 }; /* XXX fgcolor */
 	struct button *b = (struct button *)p;
-	SDL_Surface *s;
+	SDL_Surface *s, *bg;
+	Sint16 x = 0, y = 0;
 
+	/* Button */
+	bg = SPRITE(b, (b->flags & BUTTON_PRESSED) ? BUTTON_DOWN : BUTTON_UP);
+	WIDGET_DRAW(b, bg, 0, 0);
+
+	/* Label */
 	s = TTF_RenderText_Solid(font, b->caption, white);
-	SDL_FillRect(s, NULL, SDL_MapRGB(WIDGET(b)->win->view->v->format,
-	    127, 128, 127));
 	if (s == NULL) {
 		fatal("TTF_RenderTextSolid: %s\n", SDL_GetError());
 	}
-	WIDGET_DRAW(b, s);
+
+	switch (b->justify) {
+	case BUTTON_LEFT:
+		x = b->xmargin;
+		y = b->ymargin;
+		break;
+	case BUTTON_CENTER:
+		x = ((bg->w - s->w) >> 1) + (b->xmargin>>1);
+		y = ((bg->h - s->h) >> 1);
+		break;
+	case BUTTON_RIGHT:
+		x = bg->w - s->w - b->xmargin;
+		y = bg->h - s->h - b->ymargin;
+		break;
+	}
+	if (b->flags & BUTTON_PRESSED) {
+		x++;
+		y++;
+	}
+
+	WIDGET_DRAW(b, s, x, y);
 	SDL_FreeSurface(s);
+}
+
+void
+button_event(void *p, SDL_Event *ev, Uint32 flags)
+{
+	dprintf("button event\n");
 }
 
