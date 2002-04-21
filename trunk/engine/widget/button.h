56c73738ed25276395f4786c6fe44970f9c0ef60
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-04-21 13:35:20 +0000

    - render; x/y margin - label justification

--- trunk/engine/widget/button.h
+++ trunk/engine/widget/button.h
@@ -1,25 +1,34 @@
-/*	$Csoft$	*/
+/*	$Csoft: button.h,v 1.1 2002/04/20 11:23:52 vedge Exp $	*/
 
 struct button {
 	struct	 widget wid;
 
-	char	*caption;
-
 	Uint32	 flags;
 #define BUTTON_PRESSED	0x01
 
+	char	*caption;
+
 	enum {
-		LABEL_LEFT,
-		LABEL_CENTER,
-		LABEL_RIGHT
+		BUTTON_LEFT,
+		BUTTON_CENTER,
+		BUTTON_RIGHT
 	} justify;
+
+	Uint8	xmargin, ymargin;
+};
+
+/* Sprites */
+enum {
+	BUTTON_UP = 0,
+	BUTTON_DOWN
 };
 
 struct button	*button_create(struct window *, char *, char *, Uint32,
-		     Uint32, Uint32);
+		     Uint32, Uint32, Uint32, Uint32);
 int		 button_destroy(void *);
 int		 button_link(void *);
 int		 button_unlink(void *);
 
 void		 button_draw(void *);
+void		 button_event(void *, SDL_Event *, Uint32);
 
