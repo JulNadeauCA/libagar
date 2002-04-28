d3e09ed02179385d2ecac6dc085553afe40ab530
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-04-28 11:04:09 +0000

    process version information in const structs.
    git-svn-id: https://dev.csoft.net/agar/trunk@714 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/version.h
+++ engine/version.h
@@ -1,5 +1,10 @@
-/*	$Csoft$	*/
+/*	$Csoft: version.h,v 1.1 2002/02/18 00:54:37 vedge Exp $	*/
 
-int	version_read(int, char *, int, int);
-int	version_write(int, char *, int, int);
+struct version {
+	char	*name;
+	int	 vermaj, vermin;
+};
+
+int	version_read(int, const struct version *);
+int	version_write(int, const struct version *);
 
