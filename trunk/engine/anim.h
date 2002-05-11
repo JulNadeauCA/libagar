43457bc1806d0e5bbf236bdcecd35bf2a6551cb5
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-05-11 01:39:19 +0000

    ints

--- trunk/engine/anim.h
+++ trunk/engine/anim.h
@@ -1,10 +1,10 @@
-/*	$Csoft: anim.h,v 1.4 2002/03/12 13:58:11 vedge Exp $	*/
+/*	$Csoft: anim.h,v 1.5 2002/04/24 13:18:38 vedge Exp $	*/
 
 struct anim {
 	SDL_Surface **frames;
-	Uint32	maxframes;
-	Uint32	frame, nframes;
-	Uint32	delta, delay;	/* For MAPREF_ANIM_DELTA */
+	int	maxframes;
+	int	frame, nframes;
+	int	delta, delay;	/* For MAPREF_ANIM_DELTA */
 };
 
 void	 anim_init(struct anim *, int);
