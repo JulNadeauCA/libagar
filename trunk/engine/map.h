1e7c95c8a70f11a51efed9e0c76b0ee232751aae
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-05-11 03:57:40 +0000

    - hunt down bogus nodes and rectangles in debug mode. - lock when deferencing world->curmap. - more sanity checks. - nix node_reinit(). - plug a memleak in map_load(). - add a function to run a separate thread to scan the map for   inconsistencies/races in an insane manner. - int frame and fdelta, these are not saved.

--- trunk/engine/map.h
+++ trunk/engine/map.h
@@ -1,4 +1,4 @@
-/*	$Csoft: map.h,v 1.31 2002/04/24 13:18:38 vedge Exp $	*/
+/*	$Csoft: map.h,v 1.32 2002/04/28 11:05:52 vedge Exp $	*/
 
 enum {
 	MAP_MINWIDTH	= 10,
@@ -10,15 +10,15 @@ struct noderef {
 	Uint32	offs;		/* Sprite/anim within this object */
 	Uint32	flags;
 #define MAPREF_SAVE		0x0001	/* Saveable reference */
-#define MAPREF_SPRITE		0x0002	/* Sprite */
+#define MAPREF_SPRITE		0x0002	/* Single surface */
 #define MAPREF_ANIM		0x0010	/* Sequence of frames */
-#define MAPREF_ANIM_DELTA	0x0020	/* Increment per anim (default), or */
+#define MAPREF_ANIM_DELTA	0x0020	/* Increment per anim */
 #define MAPREF_ANIM_INDEPENDENT	0x0040	/* Increment per map reference */
 #define MAPREF_ANIM_STATIC	0x0080	/* Increment manually */
 #define MAPREF_WARP		0x1000	/* Another map */
 #define MAPREF_ANY		0xffff
 
-	Uint32	frame, fdelta;		/* For MAPREF_ANIM_INDEPENDENT */
+	int	frame, fdelta;	/* For MAPREF_ANIM_INDEPENDENT */
 
 	Sint32	xoffs, yoffs;	/* Incremented if > 0, decremented if < 0,
 				   used for direction and soft scroll. */
@@ -26,9 +26,11 @@ struct noderef {
 	TAILQ_ENTRY(noderef) nrefs;	/* Node reference list */
 };
 
+TAILQ_HEAD(nrefs_head, noderef);
+
 /* Coordinate within a map. */
 struct node {
-	TAILQ_HEAD(, noderef) nrefsh;
+	struct	nrefs_head nrefsh;
 	Uint32	nnrefs;
 	Uint32	flags;
 #define NODE_BLOCK	0x0001		/* Cannot walk through */
@@ -66,7 +68,8 @@ struct map {
 	struct	node **map;		/* Array of nodes */
 
 	pthread_t	draw_th;	/* Map rendering thread */
-	pthread_mutex_t lock;		/* Lock on all nodes */
+	pthread_mutex_t lock;		/* Lock on all nodes, and all
+					   references inside them */
 	
 	SLIST_ENTRY(map) wmaps;		/* Active maps */
 };
@@ -79,12 +82,14 @@ void	map_destroy(void *);
 void	map_draw(struct map *);
 void	map_animate(struct map *);
 
-int	map_focus(struct map *);
-int	map_unfocus(struct map *);
-void	map_clean(struct map *, struct object *, Uint32, Uint32,
-	    Uint32);
+void	map_focus(struct map *);
+void	map_unfocus(struct map *);
+void	map_clean(struct map *, struct object *, Uint32, Uint32, Uint32);
 void	map_allocnodes(struct map *, Uint32, Uint32, Uint32, Uint32);
 void	map_freenodes(struct map *);
+#ifdef DEBUG
+void	map_verify(struct map *);
+#endif
 
 struct noderef	*node_addref(struct node *, void *, Uint32, Uint32);
 struct noderef	*node_findref(struct node *, void *, Sint32, Uint32);
