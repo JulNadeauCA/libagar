2b102afd15caf698d3744653b6841d03e5d909a4
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-06-22 20:40:38 +0000

    obsolete, use widgets.

--- trunk/engine/mapedit/mouse.h
+++
@@ -1,7 +0,0 @@
-/*	$Csoft: mouse.h,v 1.1 2002/02/10 03:51:17 vedge Exp $	*/
-/*	Public domain	*/
-
-void	mouse_motion(struct mapedit *, SDL_Event *);
-void	mouse_button(struct mapedit *, SDL_Event *);
-
-
