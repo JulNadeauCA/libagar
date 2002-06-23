cebd3c58c22d9947f3420dbbfd916fbcbec3230d
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-06-23 02:38:21 +0000

    - clean up renderer. - center routine. - assign an edition cursor.
    git-svn-id: https://dev.csoft.net/agar/trunk@1036 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/mapedit/mapview.h
+++ engine/mapedit/mapview.h
@@ -1,26 +1,36 @@
-/*	$Csoft$	*/
+/*	$Csoft: mapview.h,v 1.1 2002/06/22 20:44:01 vedge Exp $	*/
 /*	Public domain	*/
+
+struct edcursor;
+struct mapedit;
 
 struct mapview {
 	struct	 widget wid;
 
 	int	flags;
 
-	struct {
+	struct {		/* For scrolling */
 		int	move;
 		Sint16	x;
 		Sint16	y;
 	} mouse;
 
+	struct	edcursor *cursor;
+
 	struct	map *map;
 	int	mx, my;		/* Map offset */
 	int	mw, mh;		/* Size in nodes */
+
+	struct	mapedit *med;	/* Back pointer to map editor */
+	struct	mapview *mv;	/* Back pointer to map view widget */
 };
 
-struct mapview	*mapview_new(struct region *, struct map *, int, int, int);
-void		 mapview_init(struct mapview *, struct map *, int, int, int);
+struct mapview	*mapview_new(struct region *, struct mapedit *, struct map *,
+		     int, int, int);
+void		 mapview_init(struct mapview *, struct mapedit *, struct map *,
+		     int, int, int);
 void		 mapview_destroy(void *);
 
 void	 mapview_draw(void *);
-void	 mapview_animate(void *);
+void	 mapview_center(struct mapview *, Uint32, Uint32);
 
