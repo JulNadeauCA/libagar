e095a1d4582d6ff5dd5b70eb5552189e64094ce2
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-07-07 06:26:57 +0000

    sync

--- trunk/engine/mapedit/fileops.h
+++ trunk/engine/mapedit/fileops.h
@@ -1,5 +1,8 @@
-/*	$Csoft: config.h,v 1.2 2002/06/09 10:08:07 vedge Exp $	*/
+/*	$Csoft: fileops.h,v 1.1 2002/06/22 20:43:04 vedge Exp $	*/
 /*	Public domain	*/
 
-void		 fileops_new_map(int, union evarg *);
+void	fileops_new_map(int, union evarg *);
+void	fileops_save_map(int, union evarg *);
+void	fileops_load_map(int, union evarg *);
+void	fileops_clear_map(int, union evarg *);
 
