6f5018bde470c8d169688f9bd758f4f53457269d
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-07-07 00:24:44 +0000

    protos (nonotice)

--- trunk/engine/widget/primitive.h
+++ trunk/engine/widget/primitive.h
@@ -1,5 +1,7 @@
-/*	$Csoft: primitive.h,v 1.2 2002/05/28 12:46:30 vedge Exp $	*/
+/*	$Csoft: primitive.h,v 1.3 2002/06/09 10:08:08 vedge Exp $	*/
 /*	Public domain	*/
 
 SDL_Surface	*primitive_box(void *, int, int, int);
+SDL_Surface	*primitive_frame(void *, int, int, int);
+SDL_Surface	*primitive_circle(void *, int, int, int, int);
 
