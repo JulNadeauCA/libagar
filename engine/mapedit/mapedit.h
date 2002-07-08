4533e84fe42cbaeafcd9e1b2641e0a568690428c
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-07-08 03:15:53 +0000

    load map window
    git-svn-id: https://dev.csoft.net/agar/trunk@1120 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/mapedit/mapedit.h
+++ engine/mapedit/mapedit.h
@@ -1,4 +1,4 @@
-/*	$Csoft: mapedit.h,v 1.44 2002/07/07 06:27:57 vedge Exp $	*/
+/*	$Csoft: mapedit.h,v 1.45 2002/07/07 06:30:21 vedge Exp $	*/
 /*	Public domain	*/
 
 struct editref {
@@ -40,6 +40,7 @@ struct mapedit {
 	struct	 window *objlist_win;
 	struct	 window *tileq_win;
 	struct	 window *new_map_win;
+	struct	 window *load_map_win;
 	struct	 window *settings_win;
 	struct	 window *coords_win;
 	struct	 label *coords_label;
