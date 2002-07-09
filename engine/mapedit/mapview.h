24e153af464c76cd5dcf3a3c07f01cf8c4d83ca1
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-07-09 09:28:45 +0000

    zoom; symmetric bitmap scaling algorithm.
    git-svn-id: https://dev.csoft.net/agar/trunk@1145 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/mapedit/mapview.h
+++ engine/mapedit/mapview.h
@@ -1,4 +1,4 @@
-/*	$Csoft: mapview.h,v 1.2 2002/06/23 02:38:21 vedge Exp $	*/
+/*	$Csoft: mapview.h,v 1.3 2002/07/06 23:56:14 vedge Exp $	*/
 /*	Public domain	*/
 
 struct edcursor;
@@ -9,6 +9,8 @@ struct mapview {
 
 	int	flags;
 #define MAPVIEW_CENTER	0x01	/* Center on cursor */
+#define MAPVIEW_EDIT	0x02	/* Mouse/keyboard edition */
+#define MAPVIEW_ZOOM	0x04	/* Allow zooming */
 
 	struct {		/* For scrolling */
 		int	move;
@@ -21,6 +23,8 @@ struct mapview {
 	struct	map *map;
 	int	mx, my;		/* Map offset */
 	int	mw, mh;		/* Size in nodes */
+	int	tilew, tileh;	/* Tile geometry */
+	int	zoom;		/* Zoom (%) */
 
 	struct	mapedit *med;	/* Back pointer to map editor */
 	struct	mapview *mv;	/* Back pointer to map view widget */
@@ -34,4 +38,5 @@ void		 mapview_destroy(void *);
 
 void	 mapview_draw(void *);
 void	 mapview_center(struct mapview *, int, int);
+void	 mapview_zoom(struct mapview *, int);
 
