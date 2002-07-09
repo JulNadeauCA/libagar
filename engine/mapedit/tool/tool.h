f0c363b2484712963c892738b1f13d2424858e7e
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-07-09 08:24:42 +0000

    - type check. - pass mapview to effect op. - function to return focused mapview.
    git-svn-id: https://dev.csoft.net/agar/trunk@1139 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/mapedit/tool/tool.h
+++ engine/mapedit/tool/tool.h
@@ -1,17 +1,20 @@
-/*	$Csoft: tool.h,v 1.1 2002/07/07 00:23:50 vedge Exp $	*/
+/*	$Csoft: tool.h,v 1.2 2002/07/07 06:32:44 vedge Exp $	*/
 /*	Public domain	*/
 
 struct window;
+struct mapview;
 
 struct tool_ops {
 	const	 struct object_ops obops;
 
 	struct	 window	*(*tool_window)(void *);
-	void	 	 (*tool_effect)(void *, struct map *, Uint32, Uint32);
+	void	 	 (*tool_effect)(void *, struct mapview *,
+			     Uint32, Uint32);
 };
 
 struct tool {
 	struct	 object obj;
+	char	*type;
 
 	Uint32	 flags;
 
@@ -23,7 +26,9 @@ struct tool {
 #define TOOL_OPS(t)	((struct tool_ops *)OBJECT((t))->ops)
 
 #define TOOL_DIALOG_X	16
-#define TOOL_DIALOG_Y	160	/* XXX */
+#define TOOL_DIALOG_Y	176	/* XXX */
 
-void	tool_init(struct tool *, char *, struct mapedit *, const void *);
+void		tool_init(struct tool *, char *, struct mapedit *,
+		     const void *);
+struct mapview *tool_mapview(void);
 
