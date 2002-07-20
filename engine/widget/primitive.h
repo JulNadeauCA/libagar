4bb2aa16907957b86935f99a68359a6401fe1f5c
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-07-20 19:03:19 +0000

    line
    git-svn-id: https://dev.csoft.net/agar/trunk@1180 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/widget/primitive.h
+++ engine/widget/primitive.h
@@ -1,10 +1,11 @@
-/*	$Csoft: primitive.h,v 1.4 2002/07/07 00:24:44 vedge Exp $	*/
+/*	$Csoft: primitive.h,v 1.5 2002/07/20 16:15:29 vedge Exp $	*/
 /*	Public domain	*/
 
 struct primitive_ops {
 	void	(*box)(void *, int, int, int, int, int);
-	void	(*frame)(void *, int, int, int);
-	void	(*circle)(void *, int, int, int, int);
+	void	(*frame)(void *, int, int, int, int, int);
+	void	(*circle)(void *, int, int, int, int, int, int);
+	void	(*line)(void *, int, int, int, int, Uint32);
 };
 
 extern struct primitive_ops primitives;
