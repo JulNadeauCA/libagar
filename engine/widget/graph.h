8a74f6a89faa0a6d162b87ee3a787934561bb1ba
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-07-22 05:22:08 +0000

    scale the graph; displace the origin with y mouse motion.
    git-svn-id: https://dev.csoft.net/agar/trunk@1187 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/widget/graph.h
+++ engine/widget/graph.h
@@ -1,4 +1,4 @@
-/*	$Csoft: graph.h,v 1.1 2002/07/20 18:55:59 vedge Exp $	*/
+/*	$Csoft: graph.h,v 1.2 2002/07/21 10:57:12 vedge Exp $	*/
 /*	Public domain	*/
 
 struct graph_item {
@@ -23,17 +23,23 @@ struct graph {
 
 	int	 flags;
 #define GRAPH_SCROLL	0x01	/* Scroll when a point is past visible area */
-	
+#define GRAPH_ORIGIN	0x02	/* Visible zero line */
+
+	int	 origin_y;		/* Current origin position */
+	Uint32	 origin_color[2];	/* Color for origin line */
+
 	int	 xinc;		/* X increment */
+	Sint32	 yrange;	/* Max. value */
+
 	int	 xoffs;		/* Display offset */
 	char	 *caption;	/* Graph description */
 	struct	 itemq items;	/* Lists of values */
 };
 
 struct graph	*graph_new(struct region *, const char *, enum graph_type,
-		     int, int, int);
+		     Sint32, int, int, int);
 void		 graph_init(struct graph *, const char *, enum graph_type,
-		     int, int, int);
+		     Sint32, int, int, int);
 struct graph_item *graph_add_item(struct graph *, char *, Uint32);
 void	 	 graph_destroy(void *);
 void		 graph_draw(void *);
