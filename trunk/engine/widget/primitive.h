316bb6aae28959f0a3fbe8bd8ab6992e30dad49a
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-07-24 04:54:53 +0000

    argument names in primitive ops.

--- trunk/engine/widget/primitive.h
+++ trunk/engine/widget/primitive.h
@@ -1,12 +1,14 @@
-/*	$Csoft: primitive.h,v 1.6 2002/07/20 19:03:19 vedge Exp $	*/
+/*	$Csoft: primitive.h,v 1.7 2002/07/21 10:56:44 vedge Exp $	*/
 /*	Public domain	*/
 
 struct primitive_ops {
-	void	(*box)(void *, int, int, int, int, int);
-	void	(*frame)(void *, int, int, int, int, int);
-	void	(*circle)(void *, int, int, int, int, int, int);
-	void	(*line)(void *, int, int, int, int, Uint32);
-	void	(*square)(void *, int, int, int, int, Uint32);
+	void	(*box)(void *p, int xoffs, int yoffs, int w, int h, int z);
+	void	(*frame)(void *p, int xoffs, int yoffs, int w, int h,
+		         Uint32 color);
+	void	(*circle)(void *p, int xoffs, int yoffs, int w, int h,
+		         int radius, Uint32 color);
+	void	(*line)(void *p, int x1, int y1, int x2, int y2, Uint32 color);
+	void	(*square)(void *p, int x, int y, int w, int h, Uint32 color);
 };
 
 extern struct primitive_ops primitives;
