d6fb4d1e9e3f8c19285b3130fa7ac1ee0d79d4b8
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-07-24 09:26:37 +0000

    - remove obsolete code.

--- trunk/engine/widget/text.h
+++ trunk/engine/widget/text.h
@@ -1,4 +1,4 @@
-/*	$Csoft: text.h,v 1.8 2002/06/12 20:40:09 vedge Exp $	*/
+/*	$Csoft: text.h,v 1.9 2002/07/05 01:13:09 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_TEXT_TEXT_H_
@@ -6,47 +6,12 @@
 
 #include <SDL_ttf.h>
 
-struct text {
-	struct	 object obj;
-
-	Uint32	 flags;
-#define TEXT_WAITKEY		0x01	/* Destroy on key press */
-#define TEXT_SLEEP		0x02	/* Destroy after timer has expired */
-#define TEXT_TRANSPARENT	0x04	/* Don't draw a background */
-#define TEXT_DEBUG		0x08	/* Show only in debug mode */
-
-	Uint16	 sleepms;	/* Sleep delay in ms */
-	Uint32	 w, h;		/* Window geometry */
-	Uint32	 x, y;		/* Window position */
-	Uint32	 tx, ty;	/* Current text offset */
-	Uint32	 nlines;	/* Lines in text window */
-	SDL_Rect mvmask;	/* Map view mask */
-
-	SDL_Color *fgcolor;
-	Uint32	 bgcolor;
-
-	SDL_Surface *surface;		/* TTF surface */
-	SDL_Surface *v;			/* XXX destination view */
-
-	TAILQ_ENTRY(text) texts;	/* Texts to display */
-};
-
 extern TTF_Font *font;
 extern int font_h;
 
-int	 text_engine_init(void);
-void	 text_engine_destroy(void);
-void	 text_init(struct text *, Sint16, Sint16, Uint16, Uint16, Uint32,
-	     Uint8);
-void	 text_destroy(void *);
-
-void	 text_clear(struct text *);
-void	 text_render(struct text *, char *);
-void	 text_drawall(void);
-void	 text_destroyall(void);
-
-void	 text_printf(struct text *, char *, ...);
-void	 text_msg(Uint8, Uint32, char *, ...);
-
+int		 text_engine_init(void);
+void		 text_engine_destroy(void);
+SDL_Surface	*text_render(char *, int, Uint32, char *);
+void		 text_msg(char *, ...);
 
 #endif	/* _AGAR_TEXT_TEXT_H_ */
