7f9b6eeac40a1a18eddd53177c6d0336b07c22ee
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-08-12 02:35:19 +0000

    obsolete functions.

--- trunk/engine/mapedit/command.h
+++ trunk/engine/mapedit/command.h
@@ -1,14 +1,11 @@
-/*	$Csoft: command.h,v 1.6 2002/05/08 09:42:28 vedge Exp $	*/
+/*	$Csoft: command.h,v 1.7 2002/06/09 10:08:07 vedge Exp $	*/
 /*	Public domain	*/
 
 void	mapedit_push(struct mapedit *, struct node *, int, int);
 void	mapedit_pop(struct mapedit *, struct node *, int);
 void	mapedit_clearmap(struct mapedit *);
 void	mapedit_fillmap(struct mapedit *);
-void	mapedit_setorigin(struct mapedit *, int *, int *);
-void	mapedit_loadmap(struct mapedit *);
-void	mapedit_savemap(struct mapedit *);
-void	mapedit_examine(struct map *, int, int);
+void	mapedit_set_origin(struct mapedit *, int *, int *);
 void	mapedit_editflags(struct mapedit *, int);
 void	mapedit_nodeflags(struct mapedit *, struct node *, Uint32);
 
