35d9b1d0e092639313a677f66b0f9ec732678711
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-08-13 00:01:24 +0000

    read/write/lseek error wrappers.
    git-svn-id: https://dev.csoft.net/agar/trunk@1266 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/error.h
+++ engine/error.h
@@ -1,4 +1,4 @@
-/*	$Csoft: error.h,v 1.2 2002/06/01 02:39:25 vedge Exp $	*/
+/*	$Csoft: error.h,v 1.3 2002/06/09 10:08:04 vedge Exp $	*/
 /*	Public domain	*/
 
 #define AGAR_GetError()    ((char *)pthread_getspecific(engine_errorkey))
@@ -27,6 +27,46 @@ extern pthread_key_t engine_errorkey;	/* engine.c */
 # define fatal		printf
 #endif
 
-void		*emalloc(size_t);
-void		*erealloc(void *, size_t);
+void	*emalloc(size_t);
+void	*erealloc(void *, size_t);
+
+#define eread(fd, buf, size) do {					\
+	ssize_t _eread_rv;						\
+									\
+	_eread_rv = read((fd), (buf), (size));				\
+	if (_eread_rv == -1) { 						\
+		fatal("read(%ld): %s\n", (long)size, strerror(errno));	\
+	}								\
+	if (_eread_rv != size) {					\
+		fatal("read(%ld): read %ld bytes\n", (long)size,	\
+		    (long)_eread_rv);					\
+	}								\
+} while (/*CONSTCOND*/0)
+
+#define ewrite(fd, buf, size) do {					\
+	ssize_t _ewrite_rv;						\
+									\
+	_ewrite_rv = write((fd), (buf), (size));			\
+	if (_ewrite_rv == -1) {						\
+		fatal("write(%ld): %s\n", (long)size, strerror(errno));	\
+	}								\
+	if (_ewrite_rv != (size)) {					\
+		fatal("write(%ld): wrote %ld bytes\n", (long)size,	\
+		    (long)_ewrite_rv);					\
+	}								\
+} while (/*CONSTCOND*/0)
+
+#define elseek(fd, off, whence) do {					\
+	off_t _elseek_rv;						\
+									\
+	_elseek_rv = lseek((fd), (off), (whence));			\
+	if (_elseek_rv == -1) {						\
+		fatal("lseek(%ld, %s): %s\n", (long)off,		\
+		    (whence == SEEK_SET) ? "SEEK_SET" :			\
+		    (whence == SEEK_CUR) ? "SEEK_CUR" :			\
+		    (whence == SEEK_END) ? "SEEK_END" :			\
+		     "???",						\
+		    strerror(errno));					\
+	}								\
+} while (/*CONSTCOND*/0)
 
