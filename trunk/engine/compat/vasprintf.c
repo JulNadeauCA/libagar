bae1e6d565d500af0cd1acc649e2e0fd0eddac4e
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-08-20 09:16:51 +0000

    vasprintf() variant.

---
+++ trunk/engine/compat/vasprintf.c
@@ -0,0 +1,60 @@
+/*	$Csoft$	*/
+
+/*
+ * Copyright (c) 2002 CubeSoft Communications, Inc.
+ * <http://www.csoft.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistribution of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistribution in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of CubeSoft Communications, nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+
+#include <engine/error.h>
+
+#include "vasprintf.h"
+
+int
+compat_vasprintf(char **ret, const char *fmt, va_list ap)
+{
+	char *buf;
+	int size;
+	size_t buflen;
+
+	/* Make a guess, might save one call. */
+	buflen = strlen(fmt) + 128;
+	buf = emalloc(buflen);
+	size = vsprintf(buf, fmt, ap);
+	if (size <= buflen) {
+		*ret = buf;
+		return (size);
+	}
+
+	/* Too large. */
+	buf = erealloc(buf, size);
+	size = vsprintf(buf, fmt, ap);
+	*ret = buf;
+	return (size);
+}
+
