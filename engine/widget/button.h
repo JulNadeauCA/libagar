7fda96df66e1eefd0dca463b7a915caf6cd20302
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-08-21 04:24:54 +0000

    test rien
    git-svn-id: https://dev.csoft.net/agar/trunk@1349 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/widget/button.h
+++ engine/widget/button.h
@@ -1,12 +1,12 @@
-/*	$Csoft: button.h,v 1.13 2002/06/20 16:35:47 vedge Exp $	*/
+/*	$Csoft: button.h,v 1.14 2002/07/29 02:16:19 vedge Exp $	*/
 /*	Public domain	*/
 
 struct button {
 	struct	 widget wid;
 
 	int	flags;
-#define BUTTON_PRESSED	0x01
-#define BUTTON_STICKY	0x02
+#define BUTTON_PRESSED	0x01		/* Button is pressed */
+#define BUTTON_STICKY	0x02		/* Button state is static */
 
 	char	*caption;		/* String, or NULL */
 	SDL_Surface	*label_s;	/* Label (or image) */
