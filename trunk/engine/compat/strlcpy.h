bba1d6ead1560301ce597dc1cd96d6fb08bf4f25
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-08-23 10:14:35 +0000

    compatibility functions; from OpenBSD.

---
+++ trunk/engine/compat/strlcpy.h
@@ -0,0 +1,9 @@
+/*	$Csoft$	*/
+
+#include <engine/mcconfig.h>
+
+#ifndef HAVE_STRLCPY
+#include <sys/types.h>
+size_t	strlcpy(char *, const char *, size_t);
+#endif
+
---
+++ trunk/engine/compat/strsep.h
@@ -0,0 +1,8 @@
+/*	$Csoft$	*/
+
+#include <engine/mcconfig.h>
+
+#ifndef HAVE_STRSEP
+char	*strsep(char **, const char *);
+#endif
+
