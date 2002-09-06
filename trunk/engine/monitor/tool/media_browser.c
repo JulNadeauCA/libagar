cb65c5ae8fc667e02e41762d675beecae2fa5470
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-09-06 01:30:13 +0000

    rien

--- trunk/engine/monitor/tool/media_browser.c
+++ trunk/engine/monitor/tool/media_browser.c
@@ -1,18 +1,15 @@
-/*	$Csoft: media_browser.c,v 1.1 2002/09/04 03:21:01 vedge Exp $	*/
+/*	$Csoft: media_browser.c,v 1.2 2002/09/06 01:29:17 vedge Exp $	*/
 
 /*
- * Copyright (c) 2002 CubeSoft Communications, Inc.
- * <http://www.csoft.org>
+ * Copyright (c) 2002 CubeSoft Communications, Inc. <http://www.csoft.org>
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
  * 1. Redistribution of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- * 2. Redistribution in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of CubeSoft Communications, nor the names of its
+ * 2. Neither the name of CubeSoft Communications, nor the names of its
  *    contributors may be used to endorse or promote products derived from
  *    this software without specific prior written permission.
  * 
--- trunk/engine/monitor/tool/monitor_tool.c
+++ trunk/engine/monitor/tool/monitor_tool.c
@@ -1,18 +1,15 @@
-/*	$Csoft: monitor_tool.c,v 1.1.1.1 2002/09/01 09:00:48 vedge Exp $	*/
+/*	$Csoft: monitor_tool.c,v 1.1.1.1 2002/09/01 09:03:06 vedge Exp $	*/
 
 /*
- * Copyright (c) 2002 CubeSoft Communications, Inc.
- * <http://www.csoft.org>
+ * Copyright (c) 2002 CubeSoft Communications, Inc. <http://www.csoft.org>
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
  * 1. Redistribution of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- * 2. Redistribution in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of CubeSoft Communications, nor the names of its
+ * 2. Neither the name of CubeSoft Communications, nor the names of its
  *    contributors may be used to endorse or promote products derived from
  *    this software without specific prior written permission.
  * 
