b02176fe235505edce25fe436b1a086765e3ff26
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-09-12 09:43:28 +0000

    - scrollbar. - allow multiple selections.

--- trunk/engine/widget/tlist.c
+++ trunk/engine/widget/tlist.c
@@ -1,4 +1,4 @@
-/*	$Csoft: tlist.c,v 1.4 2002/09/07 04:57:53 vedge Exp $	*/
+/*	$Csoft: tlist.c,v 1.5 2002/09/09 01:24:40 vedge Exp $	*/
 
 /*
  * Copyright (c) 2002 CubeSoft Communications, Inc. <http://www.csoft.org>
@@ -46,7 +46,7 @@ static struct widget_ops tlist_ops = {
 		NULL		/* save */
 	},
 	tlist_draw,
-	NULL		/* animate */
+	NULL		/* update */
 };
 
 enum {
@@ -65,6 +65,7 @@ static void	tlist_mouse_motion(int, union evarg *);
 static void	tlist_keydown(int, union evarg *);
 static void	tlist_scaled(int, union evarg *);
 static void	tlist_attached(int, union evarg *);
+static void	tlist_free_item(struct tlist_item *);
 
 struct tlist *
 tlist_new(struct region *reg, int rw, int rh, int flags)
@@ -100,20 +101,17 @@ tlist_init(struct tlist *tl, int rw, int rh, int flags)
 	tl->yspacing = 3;
 	tl->item_h = 16;
 	tl->ops.update = NULL;
-	tl->offs.start = 0;
-	tl->offs.soft_start = 0;
-	tl->offs.sel = -1;
 	tl->nitems = 0;
 	TAILQ_INIT(&tl->items);
 	pthread_mutex_init(&tl->items_lock, NULL);
+	
+	tl->vbar = emalloc(sizeof(struct scrollbar));
+	scrollbar_init(tl->vbar, -1, -1, tl->item_h, SCROLLBAR_VERTICAL);
 
 	event_new(tl, "window-mousemotion", 0, tlist_mouse_motion, NULL);
 	event_new(tl, "window-mousebuttondown", 0, tlist_mouse_button, NULL);
 	event_new(tl, "window-keydown", 0, tlist_keydown, NULL);
 	event_new(tl, "attached", 0, tlist_attached, NULL);
-
-	tl->vbar = emalloc(sizeof(struct scrollbar));
-	scrollbar_init(tl->vbar, -1, -1, SCROLLBAR_VERTICAL);
 	event_new(tl, "widget-scaled", 0, tlist_scaled, NULL);
 }
 
@@ -123,15 +121,12 @@ tlist_destroy(void *p)
 	struct tlist *tl = p;
 	struct tlist_item *it, *nextit;
 
-	for (it = TAILQ_FIRST(&tl->items);
-	     it != TAILQ_END(&tl->items);
-	     it = nextit) {
-		nextit = TAILQ_NEXT(it, items);
-		free(it);
-	}
-
-	object_destroy(tl->vbar);
+	tlist_clear_items(tl);
 	pthread_mutex_destroy(&tl->items_lock);
+	
+	object_destroy(tl->vbar);
+
+	widget_destroy(tl);
 }
 
 static void
@@ -146,14 +141,14 @@ static void
 tlist_scaled(int argc, union evarg *argv)
 {
 	struct tlist *tl = argv[0].p;
+	struct scrollbar *sb = tl->vbar;
 	int w = argv[1].i;
 	int h = argv[2].i;
-	struct scrollbar *sb = tl->vbar;
 
-	WIDGET(sb)->x = WIDGET(tl)->x + w - 19;
+	WIDGET(sb)->x = WIDGET(tl)->x + WIDGET(tl)->w - 20;
 	WIDGET(sb)->y = WIDGET(tl)->y;
 	WIDGET(sb)->w = 20;
-	WIDGET(sb)->h = h - 7;
+	WIDGET(sb)->h = WIDGET(tl)->h;
 }
 
 void
@@ -162,6 +157,8 @@ tlist_draw(void *p)
 	SDL_Rect rd;
 	struct tlist *tl = p;
 	struct tlist_item *it;
+	struct scrollbar *sb = tl->vbar;
+	int item_h;
 	int y, i = 0;
 
 	primitives.box(tl, 0, 0, WIDGET(tl)->w, WIDGET(tl)->h, -1,
@@ -173,12 +170,12 @@ tlist_draw(void *p)
 
 	pthread_mutex_lock(&tl->items_lock);
 
-	y = tl->offs.soft_start;
+	y = sb->range.soft_start;
 	TAILQ_FOREACH(it, &tl->items, items) {
-		int x = 2;
 		SDL_Surface *su;
+		int x = 2;
 
-		if (i++ < tl->offs.start) {
+		if (i++ < sb->range.start) {
 			continue;
 		}
 
@@ -186,10 +183,9 @@ tlist_draw(void *p)
 			goto out;
 		}
 
-		if (i - 1 == tl->offs.sel) {
+		if (it->selected) {
 			WIDGET_FILL(tl, x, y,
-			    WIDGET(tl)->w - WIDGET(tl->vbar)->w,
-			    tl->item_h,
+			    WIDGET(tl)->w - WIDGET(sb)->w, tl->item_h,
 			    WIDGET_COLOR(tl, SELECTION_COLOR));
 		}
 
@@ -209,52 +205,66 @@ tlist_draw(void *p)
 		SDL_FreeSurface(su);
 	}
 out:
+	scrollbar_draw(sb);
 	pthread_mutex_unlock(&tl->items_lock);
+}
 
-	scrollbar_draw(tl->vbar);
+static void
+tlist_free_item(struct tlist_item *it)
+{
+	if (it->icon != NULL) {
+		view_unused_surface(it->icon);
+	}
+	free(it->text);
+	free(it);
 }
 
 void
 tlist_remove_item(struct tlist_item *it)
 {
 	struct tlist *tl = it->tl_bp;
+	struct scrollbar *sb = tl->vbar;
 
 	pthread_mutex_lock(&tl->items_lock);
 	TAILQ_REMOVE(&tl->items, it, items);
-	scrollbar_set_range(tl->vbar, 0, --tl->nitems);
+	scrollbar_set_range(sb, --tl->nitems);
 	pthread_mutex_unlock(&tl->items_lock);
 
-	free(it->text);
-	free(it);
+	tlist_free_item(it);
 }
 
 void
 tlist_clear_items(struct tlist *tl)
 {
-	struct tlist_item *it;
+	struct tlist_item *it, *nit;
+	struct scrollbar *sb = tl->vbar;
 	
 	pthread_mutex_lock(&tl->items_lock);
 
-	TAILQ_FOREACH(it, &tl->items, items) {
-		free(it->text);
-		free(it);
+	for (it = TAILQ_FIRST(&tl->items);
+	     it != TAILQ_END(&tl->items);
+	     it = nit) {
+		nit = TAILQ_NEXT(it, items);
+		tlist_free_item(it);
 	}
 	TAILQ_INIT(&tl->items);
 	tl->nitems = 0;
-
-	pthread_mutex_unlock(&tl->items_lock);
 	
-	scrollbar_set_range(tl->vbar, 0, 0);
+	scrollbar_set_range(sb, 0);
+	
+	pthread_mutex_unlock(&tl->items_lock);
 }
 
 struct tlist_item *
 tlist_insert_item(struct tlist *tl, SDL_Surface *icon, char *text, void *p1)
 {
 	struct tlist_item *it;
+	struct scrollbar *sb = tl->vbar;
 
 	it = emalloc(sizeof(struct tlist_item));
 	it->icon_w = 16;
 	it->icon_h = 16;
+	it->selected = 0;
 	if (icon != NULL) {
 		it->icon = view_scale_surface(icon, it->icon_w, it->icon_h);
 	} else {
@@ -267,7 +277,7 @@ tlist_insert_item(struct tlist *tl, SDL_Surface *icon, char *text, void *p1)
 
 	pthread_mutex_lock(&tl->items_lock);
 	TAILQ_INSERT_HEAD(&tl->items, it, items);
-	scrollbar_set_range(tl->vbar, 0, ++tl->nitems);
+	scrollbar_set_range(sb, ++tl->nitems);
 	pthread_mutex_unlock(&tl->items_lock);
 
 	event_post(tl, "tlist-inserted-item", "%p", it);
@@ -275,10 +285,27 @@ tlist_insert_item(struct tlist *tl, SDL_Surface *icon, char *text, void *p1)
 	return (it);
 }
 
+/*
+ * Unset the selection flag on all items.
+ * Item list must not be locked by the caller thread.
+ */
+void
+tlist_unselect_items(struct tlist *tl)
+{
+	struct tlist_item *it;
+
+	pthread_mutex_lock(&tl->items_lock);
+	TAILQ_FOREACH(it, &tl->items, items) {
+		it->selected = 0;
+	}
+	pthread_mutex_unlock(&tl->items_lock);
+}
+
 static void
 tlist_mouse_motion(int argc, union evarg *argv)
 {
 	struct tlist *tl = argv[0].p;
+	struct scrollbar *sb = tl->vbar;
 	int x = argv[1].i;
 	int y = argv[2].i;
 	int xrel = argv[3].i;
@@ -286,24 +313,23 @@ tlist_mouse_motion(int argc, union evarg *argv)
 	Uint8 ms;
 	
 	if (x > WIDGET(tl)->w - WIDGET(tl->vbar)->w) {
-		event_post(tl->vbar, "window-mousemotion", "%i, %i, %i, %i",
-		    x, y, xrel, yrel);
+		event_forward(tl->vbar, "window-mousemotion", argc, argv);
 		return;
 	}
 
 	ms = SDL_GetMouseState(NULL, NULL);
 	if (ms & SDL_BUTTON_RMASK) {
-		if (yrel < 0 && (tl->offs.soft_start += yrel) < 0) {
-			tl->offs.soft_start = tl->item_h;
-			if (++tl->offs.start > tl->nitems) {
-				tl->offs.start = tl->nitems;
+		if (yrel < 0 && (sb->range.soft_start += yrel) < 0) {
+			sb->range.soft_start = tl->item_h;
+			if (++sb->range.start > tl->nitems) {
+				sb->range.start = tl->nitems;
 			}
 		}
-		if (yrel > 0 && tl->offs.start > 0 &&
-		    (tl->offs.soft_start += yrel) > tl->item_h) {
-			tl->offs.soft_start = 0;
-			if (--tl->offs.start < 0) {
-				tl->offs.start = 0;
+		if (yrel > 0 && sb->range.start > 0 &&
+		    (sb->range.soft_start += yrel) > tl->item_h) {
+			sb->range.soft_start = 0;
+			if (--sb->range.start < 0) {
+				sb->range.start = 0;
 			}
 		}
 	}
@@ -313,81 +339,160 @@ static void
 tlist_mouse_button(int argc, union evarg *argv)
 {
 	struct tlist *tl = argv[0].p;
+	struct scrollbar *sb = tl->vbar;
 	int button = argv[1].i;
 	int x = argv[2].i;
 	int y = argv[3].i;
-	int sy;
+	struct tlist_item *ti;
 
-	if (x > WIDGET(tl)->w - WIDGET(tl->vbar)->w) {
-		event_post(tl->vbar, "window-mousebuttondown", "%p, %p, %p",
-		    button, x, y);
+	if (x > WIDGET(tl)->w - WIDGET(sb)->w) {
+		event_forward(sb, "window-mousebuttondown", argc, argv);
 		return;
 	}
-
-	y -= tl->offs.soft_start;
-
+	
 	WIDGET_FOCUS(tl);
-
 	if (button != SELECTION_MOUSE_BUTTON) {
 		return;
 	}
-
-	sy = tl->offs.start + y/tl->item_h;
-	if ((tl->offs.sel = sy) > tl->nitems - 1) {
-		tl->offs.sel = tl->nitems - 1;
+	
+	pthread_mutex_lock(&tl->items_lock);
+	ti = _tlist_item_index(tl,
+	    (sb->range.start + (y - sb->range.soft_start) / tl->item_h), 0);
+	if (ti != NULL) {
+		if (tl->flags & TLIST_MULTI_STICKY ||
+		   ((tl->flags & TLIST_MULTI) &&
+		    (SDL_GetModState() & KMOD_CTRL))) {
+			if (ti->selected) {
+				ti->selected = 0;
+			} else {
+				ti->selected++;
+			}
+		} else {
+			tlist_unselect_items(tl);
+			ti->selected++;
+		}
+		event_post(tl, "tlist-changed", "%p, %i", ti, 1);
 	}
-
-	event_post(tl, "tlist-selection-changed", "%i", sy);
+	pthread_mutex_unlock(&tl->items_lock);
 }
 
 static void
 tlist_keydown(int argc, union evarg *argv)
 {
 	struct tlist *tl = argv[0].p;
+	struct scrollbar *sb = tl->vbar;
+	struct tlist_item *it, *pit;
 	int keysym = argv[1].i;
+	int sel;
 
 	pthread_mutex_lock(&tl->items_lock);
 	switch (keysym) {
 	case SDLK_UP:
-		if (--tl->offs.sel < 0) {
-			tl->offs.sel = 0;
-		}
-		if (tl->offs.sel <= tl->offs.start) {
-			if (--tl->offs.start < 0) {
-				tl->offs.start = 0;
+		TAILQ_FOREACH(it, &tl->items, items) {
+			if (it->selected) {
+				dprintf("to previous\n");
+				pit = TAILQ_PREV(it, tlist_itemq, items);
+				if (pit != NULL) {
+					/* Unselect current */
+					it->selected = 0;
+					event_post(tl, "tlist-changed",
+					    "%p, %i", it, 0);
+
+					/* Select previous */
+					pit->selected++;
+					event_post(tl, "tlist-changed",
+					    "%p, %i", pit, 1);
+					break;
+				} else {
+					dprintf("no previous item\n");
+				}
 			}
 		}
-		event_post(tl, "tlist-selection-changed", "%i", tl->offs.sel);
 		break;
 	case SDLK_DOWN:
-		if (++tl->offs.sel > tl->nitems) {
-			tl->offs.sel = tl->nitems;
+		TAILQ_FOREACH(it, &tl->items, items) {
+			if (it->selected) {
+				pit = TAILQ_NEXT(it, items);
+				if (pit != NULL) {
+					/* Unselect current */
+					it->selected = 0;
+					event_post(tl, "tlist-changed",
+					    "%p, %i", it, 0);
+					
+					/* Select next */
+					pit->selected++;
+					event_post(tl, "tlist-changed",
+					    "%p, %i", pit, 1);
+					break;
+				}
+			}
 		}
-		event_post(tl, "tlist-selection-changed", "%i", tl->offs.sel);
 		break;
 	case SDLK_LEFT:
-		if (--tl->offs.start < 0) {
-			tl->offs.start = 0;
+		if (--sb->range.start < 0) {
+			sb->range.start = 0;
 		}
 		break;
 	case SDLK_RIGHT:
-		if (++tl->offs.start > tl->nitems) {
-			tl->offs.start = tl->nitems;
+		if (++sb->range.start > tl->nitems) {
+			sb->range.start = tl->nitems;
 		}
 		break;
 	case SDLK_PAGEUP:
-		if ((tl->offs.start -= 4) < 0) {
-			tl->offs.start = 0;
+		if ((sb->range.start -= 4) < 0) {
+			sb->range.start = 0;
 		}
 		break;
 	case SDLK_PAGEDOWN:
-		if ((tl->offs.start += 4) > tl->nitems) {
-			tl->offs.start = tl->nitems;
+		if ((sb->range.start += 4) > tl->nitems) {
+			sb->range.start = tl->nitems;
 		}
 		break;
 	default:
 	}
 	
 	pthread_mutex_unlock(&tl->items_lock);
+}
+
+/*
+ * Return the item at the given index.
+ * Item list must not be locked by the caller thread.
+ */
+struct tlist_item *
+_tlist_item_index(struct tlist *tl, int index, int mustlock)
+{
+	struct tlist_item *it;
+	int i = 0;
+
+	if (mustlock) pthread_mutex_lock(&tl->items_lock);
+	TAILQ_FOREACH(it, &tl->items, items) {
+		if (++i == index) {
+			pthread_mutex_unlock(&tl->items_lock);
+			return (it);
+		}
+	}
+	if (mustlock) pthread_mutex_unlock(&tl->items_lock);
+	return (NULL);
+}
+
+/*
+ * Return the first item matching a text string.
+ * Item list must not be locked by the caller thread.
+ */
+struct tlist_item *
+_tlist_item_text(struct tlist *tl, char *text, int mustlock)
+{
+	struct tlist_item *it;
+	int i;
+
+	if (mustlock) pthread_mutex_lock(&tl->items_lock);
+	TAILQ_FOREACH(it, &tl->items, items) {
+		if (strncmp(it->text, text, it->text_len) == 0) {
+			pthread_mutex_unlock(&tl->items_lock);
+			return (it);
+		}
+	}
+	if (mustlock) pthread_mutex_unlock(&tl->items_lock);
+	return (NULL);
 }
 
