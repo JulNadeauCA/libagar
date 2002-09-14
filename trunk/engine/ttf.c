b9515647f1780d530ede500b7eaa3994b002c21c
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-09-14 00:39:35 +0000

    distrib; rien

--- trunk/engine/ttf.c
+++ trunk/engine/ttf.c
@@ -1,29 +1,29 @@
-/*	$Csoft: ttf.c,v 1.4 2002/08/20 09:35:28 vedge Exp $	*/
+/*	$Csoft: ttf.c,v 1.5 2002/09/06 01:29:12 vedge Exp $	*/
+/*	Id: SDL_ttf.c,v 1.6 2002/01/18 21:46:04 slouken Exp	*/
+
+/*	XXX rewrite	*/
 
 /*
- * Copyright (c) 2002 CubeSoft Communications, Inc. <http://www.csoft.org>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistribution of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Neither the name of the author, nor the names of its contributors may
- *    be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
- * USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+    SDL_ttf:  A companion library to SDL for working with TrueType (tm) fonts
+    Copyright (C) 1997, 1998, 1999, 2000, 2001  Sam Lantinga
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Sam Lantinga
+    slouken@libsdl.org
+*/
 
 #include <math.h>
 #include <stdio.h>
@@ -37,8 +37,6 @@
 
 #include "error.h"
 #include "ttf.h"
-
-#define UNICODE(c)	c
 
 /* FIXME: Right now we assume the gray-scale renderer Freetype is using
    supports 256 shades of gray, but we should instead key off of num_grays
@@ -96,8 +94,8 @@ TTF_Init(void)
 	FT_Error error;
 
 	status = 0;
-	error = FT_Init_FreeType( &library );
-	if ( error ) {
+	error = FT_Init_FreeType(&library);
+	if (error) {
 		error_set("cannot initialize FreeType");
 		status = -1;
 	} else {
@@ -214,7 +212,6 @@ static void Flush_Cache(TTF_Font *font)
 	if (font->scratch.cached) {
 		Flush_Glyph(&font->scratch);
 	}
-
 }
 
 static FT_Error
@@ -304,11 +301,13 @@ Load_Glyph(TTF_Font *font, Uint16 ch, struct cached_glyph *cached, int want)
 		/* Adjust for bold and italic text */
 		if(font->style & TTF_STYLE_BOLD) {
 			int bump = font->glyph_overhang;
+
 			dst->pitch += bump;
 			dst->width += bump;
 		}
 		if(font->style & TTF_STYLE_ITALIC) {
 			int bump = (int)ceil(font->glyph_italics);
+
 			dst->pitch += bump;
 			dst->width += bump;
 		}
