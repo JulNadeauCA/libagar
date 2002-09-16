735b8574c4393590ac57f66e79ee5df9f37569f4
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-09-16 16:03:19 +0000

    - implement world_exists() and world_find() operations. - turn world->lock into a recursive mutex.

--- trunk/engine/world.c
+++ trunk/engine/world.c
@@ -1,4 +1,4 @@
-/*	$Csoft: world.c,v 1.45 2002/09/02 08:12:14 vedge Exp $	*/
+/*	$Csoft: world.c,v 1.46 2002/09/06 01:29:12 vedge Exp $	*/
 
 /*
  * Copyright (c) 2001, 2002 CubeSoft Communications, Inc. <http://www.csoft.org>
@@ -46,11 +46,12 @@ world_init(struct world *wo, char *name)
 {
 	object_init(&wo->obj, "world", name, NULL, 0, &world_ops);
 	wo->nobjs = 0;
-	SLIST_INIT(&wo->wobjsh);
-	pthread_mutex_init(&wo->lock, NULL);
+	SLIST_INIT(&wo->wobjs);
+	pthread_mutexattr_init(&wo->lockattr);
+	pthread_mutexattr_settype(&wo->lockattr, PTHREAD_MUTEX_RECURSIVE);
+	pthread_mutex_init(&wo->lock, &wo->lockattr);
 }
 
-/* World must be locked. */
 int
 world_load(void *p, int fd)
 {
@@ -58,7 +59,10 @@ world_load(void *p, int fd)
 	struct object *ob;
 
 	/* XXX load the state map */
-	SLIST_FOREACH(ob, &world->wobjsh, wobjs) {
+
+	pthread_mutex_lock(&world->lock);
+
+	SLIST_FOREACH(ob, &world->wobjs, wobjs) {
 		dprintf("loading %s\n", ob->name);
 
 		if (curmapedit != NULL && !strcmp(ob->saveext, "m")) {
@@ -68,17 +72,20 @@ world_load(void *p, int fd)
 		object_load(ob);
 	}
 	printf("%s: loaded %d objects\n", OBJECT(wo)->name, wo->nobjs);
+	
+	pthread_mutex_unlock(&world->lock);
 	return (0);
 }
 
-/* World must be locked. */
 int
 world_save(void *p, int fd)
 {
 	struct world *wo = p;
 	struct object *ob;
 
-	SLIST_FOREACH(ob, &world->wobjsh, wobjs) {
+	pthread_mutex_lock(&world->lock);
+
+	SLIST_FOREACH(ob, &world->wobjs, wobjs) {
 		dprintf("saving %s\n", ob->name);
 
 		if (curmapedit != NULL && !strcmp(ob->saveext, "m")) {
@@ -89,8 +96,9 @@ world_save(void *p, int fd)
 		write_string(fd, (ob->desc != NULL) ? ob->desc : "");
 		object_save(ob);
 	}
-
 	dprintf("saved %d objects\n", wo->nobjs);
+
+	pthread_mutex_unlock(&world->lock);
 	return (0);
 }
 
@@ -100,13 +108,13 @@ world_destroy(void *p)
 {
 	struct world *wo = p;
 	struct object *ob, *nextob;
-	
+
 	pthread_mutex_lock(&wo->lock);
 
 	printf("freed:");
 	fflush(stdout);
-	for (ob = SLIST_FIRST(&wo->wobjsh);
-	     ob != SLIST_END(&wo->wobjsh);
+	for (ob = SLIST_FIRST(&wo->wobjs);
+	     ob != SLIST_END(&wo->wobjs);
 	     ob = nextob) {
 		nextob = SLIST_NEXT(ob, wobjs);
 		printf(" %s", ob->name);
@@ -114,36 +122,88 @@ world_destroy(void *p)
 		object_destroy(ob);
 	}
 	printf(".\n");
-
 	pthread_mutex_unlock(&wo->lock);
+
 	pthread_mutex_destroy(&wo->lock);
+	pthread_mutexattr_destroy(&wo->lockattr);
 }
 
-/* World must be locked. */
 void
 world_attach(void *parent, void *child)
 {
 	struct world *wo = parent;
 	struct object *ob = child;
 
-	SLIST_INSERT_HEAD(&wo->wobjsh, ob, wobjs);
+	pthread_mutex_lock(&wo->lock);
+
+	SLIST_INSERT_HEAD(&wo->wobjs, ob, wobjs);
 	wo->nobjs++;
-	
+	ob->state = OBJECT_CONSISTENT;
+
 	event_post(ob, "attached", "%p", wo);
+
+	pthread_mutex_unlock(&wo->lock);
 }
 
-/* World must be locked. */
 void
 world_detach(void *parent, void *child)
 {
 	struct world *wo = parent;
 	struct object *ob = child;
 	
-	event_post(ob, "detached", "%p", wo);
+	pthread_mutex_lock(&wo->lock);
 	
-	SLIST_REMOVE(&wo->wobjsh, ob, object, wobjs);
-	wo->nobjs--;
+	if (ob->state != OBJECT_CONSISTENT) {
+		fatal("inconsistent: %s\n", ob->name);
+	}
 
+	event_post(ob, "detached", "%p", wo);
+
+	SLIST_REMOVE(&wo->wobjs, ob, object, wobjs);
+	wo->nobjs--;
+	ob->state = OBJECT_ZOMBIE;
+
+	pthread_mutex_unlock(&wo->lock);
+
+	/* TODO defer */
 	object_destroy(ob);
+}
+
+/*
+ * See if an object exists; called from async event handlers to avoid
+ * a race between entry and acquisition of world->lock.
+ *
+ * World must be locked.
+ */
+int
+world_exists(void *p)
+{
+	struct object *ob = p;
+
+	pthread_mutex_lock(&world->lock);
+	SLIST_FOREACH(ob, &world->wobjs, wobjs) {
+		if (ob == (struct object *)p) {
+			pthread_mutex_unlock(&world->lock);
+			return (1);
+		}
+	}
+	pthread_mutex_unlock(&world->lock);
+	return (0);
+}
+
+void *
+world_find(char *s)
+{
+	struct object *ob;
+
+	pthread_mutex_lock(&world->lock);
+	SLIST_FOREACH(ob, &world->wobjs, wobjs) {
+		if (strcmp(ob->name, s) == 0) {
+			pthread_mutex_unlock(&world->lock);
+			return (ob);
+		}
+	}
+	pthread_mutex_unlock(&world->lock);
+	return (NULL);
 }
 
