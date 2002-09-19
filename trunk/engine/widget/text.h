8caf0e2e571a231e9c13203ba6fa138ed1562a09
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-09-19 21:14:59 +0000

    - multi-line text rendering; a bit complicated since the width of the   final surface is unpredictable. - simple message dialog. - include <errno.h>

--- trunk/engine/widget/text.h
+++ trunk/engine/widget/text.h
@@ -1,4 +1,4 @@
-/*	$Csoft: text.h,v 1.10 2002/07/24 09:26:37 vedge Exp $	*/
+/*	$Csoft: text.h,v 1.11 2002/08/22 23:04:36 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_TEXT_TEXT_H_
@@ -12,6 +12,6 @@ extern int font_h;
 int		 text_engine_init(void);
 void		 text_engine_destroy(void);
 SDL_Surface	*text_render(char *, int, Uint32, char *);
-void		 text_msg(char *, ...);
+void		 text_msg(char *, char *, ...);
 
 #endif	/* _AGAR_TEXT_TEXT_H_ */
