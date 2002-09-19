ac53fc16be94549c6717878a01bc6a068c8f8c77
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-09-19 22:08:07 +0000

    sync; rien
    git-svn-id: https://dev.csoft.net/agar/trunk@1605 86a84d31-9702-0410-8f1e-a60cab7bd424

--- mk/csoft.subdir.mk
+++ mk/csoft.subdir.mk
@@ -1,17 +1,14 @@
-# $Csoft: csoft.subdir.mk,v 1.15 2002/05/10 22:41:48 vedge Exp $
+# $Csoft: csoft.subdir.mk,v 1.16 2002/09/06 00:58:47 vedge Exp $
 
-# Copyright (c) 2001, 2002 CubeSoft Communications, Inc.
-# <http://www.csoft.org>
+# Copyright (c) 2001, 2002 CubeSoft Communications, Inc. <http://www.csoft.org>
+# All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
 # 1. Redistribution of source code must retain the above copyright
 #    notice, this list of conditions and the following disclaimer.
-# 2. Redistribution in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. Neither the name of CubeSoft Communications, nor the names of its
+# 2. Neither the name of CubeSoft Communications, nor the names of its
 #    contributors may be used to endorse or promote products derived from
 #    this software without specific prior written permission.
 # 
