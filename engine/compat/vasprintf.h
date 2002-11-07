5de2f72d98f079f76737e5734427178f408992d9
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-11-07 19:00:14 +0000

    another disgusting hack; rien
    git-svn-id: https://dev.csoft.net/agar/trunk@1651 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/compat/vasprintf.h
+++ engine/compat/vasprintf.h
@@ -1,9 +1,15 @@
-/*	$Csoft: vasprintf.h,v 1.2 2002/08/20 09:36:06 vedge Exp $	*/
+/*	$Csoft: vasprintf.h,v 1.3 2002/08/23 09:00:50 vedge Exp $	*/
 /*	Public domain	*/
 
 #include <engine/mcconfig.h>
 
-#ifndef HAVE_VASPRINTF
-int	vasprintf(char **, const char *, va_list);
+#ifdef HAVE_VASPRINTF
+# ifdef __linux__
+#  define _GNU_SOURCE
+# endif
+# include <stdio.h>
+#else
+# include <stdarg.h>
+extern int vasprintf(char **, const char *, va_list);
 #endif
 
