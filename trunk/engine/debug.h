9fd192b9abb7fa56989ec27c26fd5c3f2eb73a8d
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-11-12 01:58:13 +0000

    another debug printf macro

--- trunk/engine/debug.h
+++ trunk/engine/debug.h
@@ -1,4 +1,4 @@
-/*	$Csoft: debug.h,v 1.16 2002/09/13 04:53:48 vedge Exp $	*/
+/*	$Csoft: debug.h,v 1.17 2002/09/17 21:55:34 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_ENGINE_DEBUG_H_
@@ -12,8 +12,14 @@ extern int engine_debug;
 		if (engine_debug)					\
 			printf("%s: " fmt, __FUNCTION__ , ##args);	\
 	} while (/*CONSTCOND*/0)
+#  define deprintf(fmt, args...)					\
+	do {								\
+		if (engine_debug)					\
+			fprintf(stderr, ##args);			\
+	} while (/*CONSTCOND*/0)
 # else	/* !__GNUC__ */
 #  define dprintf	printf
+#  define deprintf	printf
 # endif	/* __GNUC__ */
 # define dprintrect(name, rect)						\
 	dprintf("%s: %dx%d at [%d,%d]\n", (name),			\
@@ -23,8 +29,10 @@ extern int engine_debug;
 #else	/* !DEBUG */
 # if defined(__GNUC__)
 #  define dprintf(arg...) ((void)0)
+#  define deprintf(arg...) ((void)0)
 # else
 #  define dprintf	  printf
+#  define deprintf	  printf
 # endif
 # define dprintrect(name, rect)
 # define dprintnode(m, x, y, str)
@@ -64,25 +72,25 @@ extern int engine_debug;
 } while (/*CONSTCOND*/0)
 
 #if 0
-#define sem_init(wsem, pshared, value) do {				\
+#define Sem_init(wsem, pshared, value) do {				\
 	int _rv;							\
 	if ((_rv = sem_init((wsem), (pshared), (value))) != 0) {	\
 		fatal("sem_init(%p): %s\n", strerror(_rv));		\
 	}								\
 } while (/*CONSTCOND*/0)
-#define sem_destroy(wsem) do {						\
+#define Sem_destroy(wsem) do {						\
 	int _rv;							\
 	if ((_rv = sem_destroy((wsem))) != 0) {				\
 		fatal("sem_destroy(%p): %s\n", (wsem), strerror(_rv));	\
 	}								\
 } while (/*CONSTCOND*/0)
-#define sem_wait(wsem) do {						\
+#define Sem_wait(wsem) do {						\
 	int _rv;							\
 	if ((_rv = sem_wait((wsem))) != 0) {				\
 		fatal("sem_wait(%p): %s\n", (wsem), strerror(_rv));	\
 	}								\
 } while (/*CONSTCOND*/0)
-#define sem_post(wsem) do {						\
+#define Sem_post(wsem) do {						\
 	int _rv;							\
 	if ((_rv = sem_post((wsem))) != 0) {				\
 		fatal("sem_post(%p): %s\n", (wsem), strerror(_rv));	\
@@ -90,14 +98,14 @@ extern int engine_debug;
 } while (/*CONSTCOND*/0)
 #endif
 
-#define pthread_create(thread, attr, func, arg) do {			\
+#define Pthread_create(thread, attr, func, arg) do {			\
 	int _rv;							\
 	if ((_rv = pthread_create((thread), (attr), (func), (arg)))	\
 	    != 0) {							\
 		fatal("pthread_create: %s\n", strerror(_rv));		\
 	}								\
 } while (/*CONSTCOND*/0)
-#define pthread_join(thread, valptr) do {				\
+#define Pthread_join(thread, valptr) do {				\
 	int _rv;							\
 	if ((_rv = pthread_join((thread), (valptr))) != 0) {		\
 		fatal("pthread_join: %s\n", strerror(_rv));		\
