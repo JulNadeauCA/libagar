1216c23567ccc1fbb39201961274248bba0cb71e
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-11-15 01:14:59 +0000

    use polling to update the event handler lists.
    git-svn-id: https://dev.csoft.net/agar/trunk@1731 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/monitor/object_browser.h
+++ engine/monitor/object_browser.h
@@ -1,16 +1,15 @@
-/*	$Csoft: object_browser.h,v 1.2 2002/11/14 05:59:02 vedge Exp $	*/
+/*	$Csoft: object_browser.h,v 1.3 2002/11/14 07:59:28 vedge Exp $	*/
 /*	Public domain	*/
 
 struct object_browser {
 	struct monitor_tool	 tool;
 	struct tlist		*objlist;
-
-	int	flags;
+	int			 flags;
 };
 
 struct object_browser	*object_browser_new(struct monitor *, int);
 void			 object_browser_init(struct object_browser *,
 			     struct monitor *, int);
 struct window		*object_browser_window(void *);
-void			 object_browser_attached(int, union evarg *);
-void			 object_browser_detached(int, union evarg *);
+void			 object_browser_attached_object(int, union evarg *);
+void			 object_browser_detached_object(int, union evarg *);
