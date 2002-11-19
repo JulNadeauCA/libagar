e09f91a36167110c60d5cea5fd4fc76d2e289a63
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-11-19 05:06:32 +0000

    implement a LABEL_POLLED type, to dereference values each time the label is drawn, acquiring an optional mutex accordingly.
    git-svn-id: https://dev.csoft.net/agar/trunk@1750 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/widget/label.c
+++ engine/widget/label.c
@@ -1,4 +1,4 @@
-/*	$Csoft: label.c,v 1.31 2002/11/14 07:18:33 vedge Exp $	*/
+/*	$Csoft: label.c,v 1.32 2002/11/17 23:13:59 vedge Exp $	*/
 
 /*
  * Copyright (c) 2002 CubeSoft Communications, Inc. <http://www.csoft.org>
@@ -70,7 +70,7 @@ label_new(struct region *reg, int w, int h, const char *fmt, ...)
 	va_end(args);
 
 	label = emalloc(sizeof(struct label));
-	label_init(label, buf, w, h);
+	label_init(label, LABEL_STATIC, buf, w, h);
 
 	free(buf);
 
@@ -80,24 +80,80 @@ label_new(struct region *reg, int w, int h, const char *fmt, ...)
 }
 
 void
-label_init(struct label *label, const char *caption, int rw, int rh)
+label_init(struct label *label, enum label_type type, const char *s,
+    int rw, int rh)
 {
 	widget_init(&label->wid, "label", "widget", &label_ops, rw, rh);
 	widget_map_color(label, TEXT_COLOR, "label-text", 250, 250, 250);
 
-	label->flags = 0;
-	label->text.caption = strdup(caption);
-	label->text.surface = text_render(NULL, -1,
-	    WIDGET_COLOR(label, TEXT_COLOR), label->text.caption);
-	pthread_mutex_init(&label->text.lock, NULL);
+	label->type = type;
+	switch (type) {
+	case LABEL_STATIC:
+		label->text.caption = strdup(s);
+		label->text.surface = text_render(NULL, -1,
+		    WIDGET_COLOR(label, TEXT_COLOR), label->text.caption);
+		pthread_mutex_init(&label->text.lock, NULL);
+		break;
+	case LABEL_POLLED:
+		label->text.caption = NULL;
+		label->text.surface = NULL;
+		label->poll.fmt = strdup(s);
+
+		memset(label->poll.ptrs, NULL,
+		    sizeof(void *) * LABEL_MAX_POLLITEMS);
+		label->poll.nptrs = 0;
+		break;
+	}
 }
 
-/* Window must be locked. */
+struct label *
+label_polled_new(struct region *reg, int w, int h, pthread_mutex_t *mutex,
+    const char *fmt, ...)
+{
+	struct label *label;
+	va_list args;
+	const char *p;
+
+	label = emalloc(sizeof(struct label));
+	label_init(label, LABEL_POLLED, fmt, w, h);
+
+	label->poll.lock = mutex;
+
+	va_start(args, fmt);
+	for (p = fmt; *p != '\0'; p++) {
+		if (*p == '%' && *(p+1) != '\0') {
+			switch (*(p+1)) {
+			case ' ':
+			case '(':
+			case ')':
+			case '%':
+				break;
+			default:
+				label->poll.ptrs[label->poll.nptrs++] =
+				    va_arg(args, void *);
+				break;
+			}
+		}
+	}
+	va_end(args);
+
+	region_attach(reg, label);
+
+	return (label);
+}
+
+/* Window must be locked. XXX */
 void
 label_printf(struct label *label, const char *fmt, ...)
 {
 	va_list args;
 	char *buf;
+
+#ifdef DEBUG
+	if (label->type != LABEL_STATIC) {
+		fatal("not a static label\n");
+	}
+#endif
 
 	va_start(args, fmt);
 	if (vasprintf(&buf, fmt, args) == -1) {
@@ -125,11 +181,156 @@ void
 label_draw(void *p)
 {
 	struct label *label = p;
+	
+	/* XXX justify */
 
-	/* XXX justify, etc */
-	pthread_mutex_lock(&label->text.lock);
-	WIDGET_DRAW(label, label->text.surface, 0, 0);
-	pthread_mutex_unlock(&label->text.lock);
+	switch (label->type) {
+	case LABEL_STATIC:
+		pthread_mutex_lock(&label->text.lock);
+		WIDGET_DRAW(label, label->text.surface, 0, 0);
+		pthread_mutex_unlock(&label->text.lock);
+		break;
+	case LABEL_POLLED:
+		{
+			SDL_Surface *ts;
+			char s[LABEL_MAX_LENGTH], *s2, *fmtp, *sp;
+			int ri = 0;
+			
+			s[0] = '\0';
+
+			if (label->poll.lock != NULL)
+				pthread_mutex_lock(label->poll.lock);
+
+			for (sp = s, fmtp = label->poll.fmt;
+			     *fmtp != '\0';
+			     fmtp++) {
+
+#define LABEL_ARG(fmt, _la_type)					\
+	asprintf(&s2, (fmt), *(_la_type *)label->poll.ptrs[ri++])
+
+				if (*fmtp == '%' && *(fmtp+1) != '\0') {
+					switch (*(fmtp+1)) {
+					case 'd':
+					case 'i':
+						LABEL_ARG("%d", int);
+						break;
+					case 'o':
+						LABEL_ARG("%o", unsigned int);
+						break;
+					case 'u':
+						LABEL_ARG("%u", unsigned int);
+						break;
+					case 'x':
+						LABEL_ARG("%x", int);
+						break;
+					case 'X':
+						LABEL_ARG("%X", unsigned int);
+						break;
+					case 'c':
+						LABEL_ARG("%c", char);
+						break;
+					case 's':
+						LABEL_ARG("%s", char *);
+						break;
+					case 'p':
+						LABEL_ARG("%p", void *);
+						break;
+					case '[':
+						dprintf("fmtp: %s\n", fmtp+2);
+						if (strncmp("u8", fmtp+2, 2)
+						    == 0) {
+							LABEL_ARG("%d", Uint8);
+							fmtp += 3;
+						} else if (strncmp("s8",
+						           fmtp+2, 2) == 0) {
+							LABEL_ARG("%d", Sint8);
+							fmtp += 3;
+						} else if (strncmp("u16",
+						           fmtp+2, 3) == 0) {
+							LABEL_ARG("%d", Uint16);
+							fmtp += 4;
+						} else if (strncmp("s16",
+						           fmtp+2, 3) == 0) {
+							LABEL_ARG("%d", Sint16);
+							fmtp += 4;
+						} else if (strncmp("u32",
+						           fmtp+2, 3) == 0) {
+							LABEL_ARG("%d", Uint32);
+							fmtp += 4;
+						} else if (strncmp("s32",
+						           fmtp+2, 3) == 0) {
+							LABEL_ARG("%d", Sint32);
+							fmtp += 4;
+						} else if (strncmp("wxh",
+						           fmtp+2, 3) == 0) {
+							SDL_Rect *rd =
+							    label->poll.ptrs
+							    [ri++];
+
+							asprintf(&s2,
+							    "%dx%d",
+							    rd->w, rd->h);
+							fmtp += 4;
+						} else if (strncmp("x,y",
+						           fmtp+2, 3) == 0) {
+							SDL_Rect *rd =
+							    label->poll.ptrs
+							    [ri++];
+
+							asprintf(&s2,
+							    "[%d,%d]",
+							    rd->x, rd->y);
+							fmtp += 4;
+						} else if (strncmp("rect",
+						           fmtp+2, 4) == 0) {
+							SDL_Rect *rd =
+							    label->poll.ptrs
+							    [ri++];
+
+							asprintf(&s2,
+							    "%dx%d at %d,%d",
+							    rd->w, rd->h,
+							    rd->x, rd->y);
+							fmtp += 5;
+						} else {
+							fatal("bad cast\n");
+						}
+						break;
+					case '%':
+						asprintf(&s2, "%%");
+						break;
+					default:
+						fatal("bad format: %c\n",
+						    *(fmtp+1));
+					}
+					fmtp++;
+				} else {
+					asprintf(&s2, "%c", *fmtp);
+				}
+				
+				if (s2 != NULL) {
+					size_t scl;
+
+					scl = strlcat(sp, s2, LABEL_MAX_LENGTH);
+					if (scl > LABEL_MAX_LENGTH) {
+						fatal("overflow\n");
+					}
+					sp += scl;
+					free(s2);
+				}
+			}
+			*sp = '\0';
+
+			if (label->poll.lock != NULL)
+				pthread_mutex_unlock(label->poll.lock);
+
+			ts = text_render(NULL, -1,
+			    WIDGET_COLOR(label, TEXT_COLOR), s);
+			WIDGET_DRAW(label, ts, 0, 0);
+			SDL_FreeSurface(ts);
+		}
+		break;
+	}
 }
 
 void
@@ -137,7 +338,14 @@ label_destroy(void *p)
 {
 	struct label *label = p;
 
-	free(label->text.caption);
-	SDL_FreeSurface(label->text.surface);
-	pthread_mutex_destroy(&label->text.lock);
+	switch (label->type) {
+	case LABEL_STATIC:
+		free(label->text.caption);
+		SDL_FreeSurface(label->text.surface);
+		pthread_mutex_destroy(&label->text.lock);
+		break;
+	case LABEL_POLLED:
+		free(label->poll.fmt);
+		break;
+	}
 }
