38c82948f945690e4f459289a189efdde1295260
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-11-21 22:55:34 +0000

    - rename. - get rid of the status ailment queue, generic properties can be used instead. - use object_get_pos() when updating the position.
    git-svn-id: https://dev.csoft.net/agar/trunk@1765 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/perso.c
+++ engine/perso.c
@@ -1,4 +1,4 @@
-/*	$Csoft: char.c,v 1.61 2002/10/30 17:15:16 vedge Exp $	*/
+/*	$Csoft: char.c,v 1.62 2002/11/14 05:58:59 vedge Exp $	*/
 
 /*
  * Copyright (c) 2001, 2002 CubeSoft Communications, Inc. <http://www.csoft.org>
@@ -36,7 +36,7 @@
 #include "engine.h"
 #include "map.h"
 #include "rootmap.h"
-#include "char.h"
+#include "perso.h"
 #include "physics.h"
 #include "input.h"
 #include "version.h"
@@ -48,42 +48,36 @@ enum {
 	DEFAULT_ZUARS	= 0
 };
 
-static const struct version char_ver = {
-	"agar char",
-	1, 1
+static const struct version perso_ver = {
+	"agar personage",
+	1, 0
 };
 
-static const struct object_ops char_ops = {
-	char_destroy,	/* destroy */
-	char_load,
-	char_save
+static const struct object_ops perso_ops = {
+	NULL,		/* destroy */
+	perso_load,
+	perso_save
 };
 
-static Uint32	char_time(Uint32, void *);
+static Uint32	perso_time(Uint32, void *);
 
-struct character *
-char_new(char *name, char *media)
+struct perso *
+perso_new(char *name, char *media)
 {
-	struct character *ch;
+	struct perso *ch;
 
-	ch = emalloc(sizeof(struct character));
-	char_init(ch, name, media);
+	ch = emalloc(sizeof(struct perso));
+	perso_init(ch, name, media);
 
-	pthread_mutex_lock(&world->lock);
 	world_attach(world, ch);
-	pthread_mutex_unlock(&world->lock);
-
 	return (ch);
 }
 
 void
-char_init(struct character *ch, char *name, char *media)
+perso_init(struct perso *ch, char *name, char *media)
 {
-	struct ailment_alive *alive;
-
-	/* XXX audio */
-	object_init(&ch->obj, "character", name, media,
-	    OBJECT_ART|OBJECT_BLOCK, &char_ops);
+	object_init(&ch->obj, "perso", name, media, OBJECT_ART|OBJECT_BLOCK,
+	    &perso_ops);
 
 	ch->flags = 0;
 	ch->level = 0;
@@ -98,47 +92,26 @@ char_init(struct character *ch, char *name, char *media)
 	ch->maxspeed = DEFAULT_SPEED;
 	ch->nzuars = DEFAULT_ZUARS;
 
-	TAILQ_INIT(&ch->ailments);
-
-	alive = emalloc(sizeof(struct ailment_alive));
-	ailment_init(&alive->ail, AILMENT_ALIVE, NULL, AILMENT_SAVE);
-	alive->ticks = 0;
-	TAILQ_INSERT_HEAD(&ch->ailments, AILMENT(alive), ailments);
-
 	pthread_mutex_init(&ch->lock, NULL);
 
-	event_new(ch, "attached", char_attached, NULL);
-	event_new(ch, "detached", char_detached, NULL);
-}
-
-void
-char_destroy(void *p)
-{
-	struct character *ch = p;
-	struct ailment *ail, *nail;
-
-	for (ail = TAILQ_FIRST(&ch->ailments);
-	     ail != TAILQ_END(&ch->ailments);
-	     ail = nail) {
-		nail = TAILQ_NEXT(ail, ailments);
-		free(ail);
-	}
+	event_new(ch, "attached", perso_attached, NULL);
+	event_new(ch, "detached", perso_detached, NULL);
 }
 
 int
-char_load(void *p, int fd)
+perso_load(void *p, int fd)
 {
-	struct character *ch = p;
+	struct perso *ch = p;
 	struct input *input = NULL;
 	struct map *m;
 
-	if (version_read(fd, &char_ver) != 0) {
+	if (version_read(fd, &perso_ver) != 0) {
 		return (-1);
 	}
 
 	pthread_mutex_lock(&ch->lock);
 
-	/* Read character properties. */
+	/* Read perso properties. */
 	free(read_string(fd));		/* Ignore name */
 	ch->flags = read_uint32(fd);
 	ch->level = read_uint32(fd);
@@ -186,7 +159,7 @@ char_load(void *p, int fd)
 			npos = object_addpos(ch, offs, flags, input, m, x, y);
 			npos->speed = speed;
 			if (view->gfx_engine == GFX_ENGINE_TILEBASED &&
-			    ch->flags & CHAR_FOCUS) {
+			    ch->flags & PERSO_FOCUSED) {
 				rootmap_center(m, x, y);
 			}
 			pthread_mutex_unlock(&m->lock);
@@ -208,9 +181,9 @@ char_load(void *p, int fd)
 }
 
 int
-char_save(void *p, int fd)
+perso_save(void *p, int fd)
 {
-	struct character *ch = (struct character *)p;
+	struct perso *ch = (struct perso *)p;
 	struct fobj_buf *buf;
 	struct mappos *pos = OBJECT(ch)->pos;
 
@@ -219,13 +192,13 @@ char_save(void *p, int fd)
 		return (-1);
 	}
 
-	version_write(fd, &char_ver);
+	version_write(fd, &perso_ver);
 	
 	pthread_mutex_lock(&ch->lock);
 
-	/* Write character properties. */
+	/* Write perso properties. */
 	buf_write_string(buf, ch->obj.name);
-	buf_write_uint32(buf, ch->flags & ~(CHAR_DONTSAVE));
+	buf_write_uint32(buf, ch->flags & ~(PERSO_DONTSAVE));
 	buf_write_uint32(buf, ch->level);
 	buf_write_uint32(buf, ch->exp);
 	buf_write_uint32(buf, ch->age);
@@ -262,19 +235,19 @@ char_save(void *p, int fd)
 }
 
 void
-char_attached(int argc, union evarg *argv)
+perso_attached(int argc, union evarg *argv)
 {
-	struct character *ch = argv[0].p;
+	struct perso *ch = argv[0].p;
 
 	pthread_mutex_lock(&ch->lock);
-	ch->timer = SDL_AddTimer(ch->maxspeed, char_time, ch);
+	ch->timer = SDL_AddTimer(ch->maxspeed, perso_time, ch);
 	pthread_mutex_unlock(&ch->lock);
 }
 
 void
-char_detached(int argc, union evarg *argv)
+perso_detached(int argc, union evarg *argv)
 {
-	struct character *ch = argv[0].p;
+	struct perso *ch = argv[0].p;
 
 	pthread_mutex_lock(&ch->lock);
 	SDL_RemoveTimer(ch->timer);
@@ -282,29 +255,25 @@ char_detached(int argc, union evarg *argv)
 }
 
 static Uint32
-char_time(Uint32 ival, void *p)
+perso_time(Uint32 ival, void *p)
 {
 	struct object *ob = p;
 	struct map *m;
 	struct mappos *pos;
 	Uint32 x, y, moved = 0;
 
-	if (ob->pos == NULL) {
+	pos = object_get_pos(ob);
+
+	if (pos == NULL) {
+		/* Character is nowhere. */
 		return (ival);
 	}
-
-	/* Obtain the position. XXX array */
-	pthread_mutex_lock(&ob->pos_lock);
-	pos = ob->pos;
-	pthread_mutex_unlock(&ob->pos_lock);
 
 	m = pos->map;
 	x = pos->x;
 	y = pos->y;
 	
 	pthread_mutex_lock(&m->lock);
-
-	/* Move the character if necessary. */
 	moved = mapdir_move(&pos->dir, &x, &y);
 	if (moved != 0) {
 		struct mappos *newpos;
@@ -312,7 +281,6 @@ char_time(Uint32 ival, void *p)
 		newpos = object_movepos(ob, m, x, y);
 		mapdir_postmove(&newpos->dir, &x, &y, moved);
 	}
-
 	pthread_mutex_unlock(&m->lock);
 
 	return (ival);
