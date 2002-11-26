88f7c2f897f1c0cf9aedecf336a41e8d3a520f78
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-11-26 11:01:00 +0000

    viewport parameters tool.

--- trunk/engine/monitor/monitor.h
+++ trunk/engine/monitor/monitor.h
@@ -1,4 +1,4 @@
-/*	$Csoft: monitor.h,v 1.8 2002/11/16 00:57:40 vedge Exp $	*/
+/*	$Csoft: monitor.h,v 1.9 2002/11/17 23:13:11 vedge Exp $	*/
 /*	Public domain	*/
 
 struct object;
@@ -14,7 +14,8 @@ enum {
 	MONITOR_OBJECT_BROWSER,
 	MONITOR_SPRITE_BROWSER,
 	MONITOR_LEVEL_BROWSER,
-	MONITOR_WIDGET_BROWSER
+	MONITOR_WIDGET_BROWSER,
+	MONITOR_VIEW_PARAMS
 };
 
 extern struct monitor	monitor;
@@ -26,3 +27,4 @@ struct window	*object_browser_window(void);
 struct window	*sprite_browser_window(void);
 struct window	*level_browser_window(void);
 struct window	*widget_browser_window(void);
+struct window	*view_params_window(void);
