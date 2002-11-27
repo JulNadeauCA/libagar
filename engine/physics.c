ae0828bf03fc4a23a23d1cba49cf77537d66fd87
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-11-27 05:14:15 +0000

    more debug flags.
    git-svn-id: https://dev.csoft.net/agar/trunk@1826 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/physics.c
+++ engine/physics.c
@@ -1,4 +1,4 @@
-/*	$Csoft: physics.c,v 1.43 2002/11/22 08:56:49 vedge Exp $	    */
+/*	$Csoft: physics.c,v 1.44 2002/11/23 22:40:46 vedge Exp $	    */
 
 /*
  * Copyright (c) 2001, 2002 CubeSoft Communications, Inc. <http://www.csoft.org>
@@ -45,6 +45,14 @@ enum {
 static void	mapdir_change(struct mapdir *, struct noderef *);
 static void	mapdir_setsprite(struct mapdir *, Uint32, int);
 static int	mapdir_canmove(struct mapdir *, struct map *, Uint32, Uint32);
+
+#ifdef DEBUG
+#define DEBUG_MOVE	0x01
+#define DEBUG_BLOCKED	0x02
+
+int	physics_debug = DEBUG_MOVE|DEBUG_BLOCKED;
+#define engine_debug physics_debug
+#endif
 
 void
 gendir_init(struct gendir *dir)
@@ -273,193 +281,184 @@ mapdir_move(struct mapdir *dir, Uint32 *mapx, Uint32 *mapy)
 	struct node *node;
 	Uint32 moved = 0;
 
+	/* Make sure the coordinates are right. */
 	node = &m->map[*mapy][*mapx];
 	nref = node_findref(m, node, dir->ob, -1, MAPREF_ANY);
 	if (nref == NULL) {
-		dprintf("%s not at %s:%dx%d\n", dir->ob->name,
+		debug(DEBUG_MOVE, "%s not at %s:%dx%d\n", dir->ob->name,
 		    OBJECT(m)->name, *mapx, *mapy);
 		return (0);
 	}
 
-	if (nref->yoffs == 0 && nref->xoffs == 0) {
+	if (nref->yoffs == 0 && nref->xoffs == 0) {	/* Not moving? */
 		mapdir_change(dir, nref);
-	} else {
-		/* Up */
-		if (nref->yoffs < 0) {
-			if (nref->yoffs == -1) {	/* Once */
-				if (*mapy > 2) {
-					if (!mapdir_canmove(dir, m, *mapx,
-					    (*mapy)-1)) {
-						nref->yoffs = 0;
-#if 0
-						mapdir_setsprite(dir,
-						    DIR_ANIM_IDLEUP, 1);
-#endif
-						return (0);
-					}
-					m->map[(*mapy)-1][*mapx].overlap++;
-					m->map[(*mapy)-2][*mapx].overlap++;
-				} else {
+		return (0);
+	}
+
+	/* XXX this is insane */
+
+	if (nref->yoffs < 0) {					/* Move up */
+		debug(DEBUG_MOVE, "%s moving up (yoffs=%d)\n", dir->ob->name,
+		    nref->yoffs);
+		if (nref->yoffs == -1) {
+			if (*mapy > 2) {
+				if (!mapdir_canmove(dir, m, *mapx, (*mapy)-1)) {
+					debug(DEBUG_BLOCKED, "%s is blocked\n",
+					    dir->ob->name);
 					nref->yoffs = 0;
-					mapdir_unset(dir, DIR_UP);
+					return (0);
 				}
-			}
-			if (nref->yoffs <= (-TILEW + dir->speed)) {
-				node->flags &= ~(NODE_ANIM);
-				dir->moved |= DIR_UP;
-				moved |= DIR_UP;
-
-				if ((*mapy)-- < 1) {
-					*mapy = 1;
-				}
-
-				if ((dir->flags & DIR_SCROLLVIEW) &&
-				    (view->rootmap != NULL) &&
-				    (view->rootmap->y - *mapy) <= 0) {
-				    	rootmap_scroll(m, DIR_UP);
-				}
-				m->map[*mapy][*mapx].overlap--;
-				m->map[(*mapy)-1][*mapx].overlap--;
+				m->map[(*mapy)-1][*mapx].overlap++;
+				m->map[(*mapy)-2][*mapx].overlap++;
 			} else {
-				if (dir->flags & DIR_SOFTSCROLL) {
-					nref->yoffs -= dir->speed;
-				} else {
-					nref->yoffs = -TILEH;
-				}
+				nref->yoffs = 0;
+				mapdir_unset(dir, DIR_UP);
 			}
 		}
-		/* Down */
-		if (nref->yoffs > 0) {
-			if (nref->yoffs == 1) {		/* Once */
-				if ((*mapy)+1 < m->maph - 2) {
-					if (!mapdir_canmove(dir, m, *mapx,
-					    (*mapy)+1)) {
-						nref->yoffs = 0;
-#if 0
-						mapdir_setsprite(dir,
-						    DIR_ANIM_IDLEDOWN, 1);
-#endif
-						return (0);
-					}
-					m->map[(*mapy)+1][*mapx].overlap++;
-				} else {
-					nref->yoffs = 0;
-					mapdir_unset(dir, DIR_DOWN);
-				}
+		if ((dir->flags & DIR_SCROLLVIEW) && (view->rootmap != NULL)) {
+		    	rootmap_scroll(m, DIR_UP);
+		}
+		if (nref->yoffs <= (-TILEW + dir->speed)) {
+			node->flags &= ~(NODE_ANIM);
+			dir->moved |= DIR_UP;
+			moved |= DIR_UP;
+			if ((*mapy)-- < 1) {
+				*mapy = 1;
 			}
-			if (nref->yoffs >= TILEW - dir->speed) {
-				nref->yoffs = 1;
-				node->flags &= ~(NODE_ANIM);
-				dir->moved |= DIR_DOWN;
-				moved |= DIR_DOWN;
-				if (++(*mapy) > m->maph - 1) {
-					*mapy = m->maph - 1;
-				}
-				if ((dir->flags & DIR_SCROLLVIEW) &&
-				    (view->rootmap != NULL) &&
-				    (view->rootmap->y - *mapy) <=
-				     -view->rootmap->h + 2) {
-					rootmap_scroll(m, DIR_DOWN);
-				}
-				m->map[*mapy][*mapx].overlap--;
+			m->map[*mapy][*mapx].overlap--;
+			m->map[(*mapy)-1][*mapx].overlap--;
+		} else {
+			if (dir->flags & DIR_SOFTSCROLL) {
+				nref->yoffs -= dir->speed;
 			} else {
-				if (dir->flags & DIR_SOFTSCROLL) {
-					nref->yoffs += dir->speed;
-				} else {
-					nref->yoffs = TILEH;
-				}
+				nref->yoffs = -TILEH;
 			}
 		}
+	}
+
+	if (nref->yoffs > 0) {				/* Move down */
+		debug(DEBUG_MOVE, "%s moving down (yoffs=%d)\n", dir->ob->name,
+		    nref->yoffs);
+		if (nref->yoffs == 1) {
+			if ((*mapy)+1 < m->maph - 2) {
+				/* Make sure the object can move to the node. */
+				if (!mapdir_canmove(dir, m, *mapx, (*mapy)+1)) {
+					debug(DEBUG_BLOCKED, "%s is blocked\n",
+					    dir->ob->name);
+					nref->yoffs = 0;
+					return (0);
+				}
+				m->map[(*mapy)+1][*mapx].overlap++;
+			} else {
+				nref->yoffs = 0;
+				mapdir_unset(dir, DIR_DOWN);
+			}
+		}
+		if (nref->yoffs >= TILEW - dir->speed) {
+			nref->yoffs = 1;
+			node->flags &= ~(NODE_ANIM);
+			dir->moved |= DIR_DOWN;
+			moved |= DIR_DOWN;
+			if (++(*mapy) > m->maph - 1) {
+				*mapy = m->maph - 1;
+			}
+			if ((dir->flags & DIR_SCROLLVIEW) &&
+			    (view->rootmap != NULL) &&
+			    (view->rootmap->y - *mapy) <= -view->rootmap->h+2) {
+				rootmap_scroll(m, DIR_DOWN);
+			}
+			m->map[*mapy][*mapx].overlap--;
+		} else {
+			if (dir->flags & DIR_SOFTSCROLL) {
+				nref->yoffs += dir->speed;
+			} else {
+				nref->yoffs = TILEH;
+			}
+		}
+	}
 		
-		/* Left */
-		if (nref->xoffs < 0) {
-			if (nref->xoffs == -1) {	/* Once */
-				if (*mapx > 1) {
-					if (!mapdir_canmove(dir, m, (*mapx)-1,
-					    *mapy)) {
-						nref->xoffs = 0;
-#if 0
-						mapdir_setsprite(dir,
-						    DIR_ANIM_IDLELEFT, 1);
-#endif
-						return (0);
-					}
-					m->map[*mapy][(*mapx)-1].overlap++;
-					m->map[(*mapy)-1][(*mapx)-1].overlap++;
-				} else {
+	if (nref->xoffs < 0) {				/* Move left */
+		debug(DEBUG_MOVE, "%s moving left (xoffs=%d)\n", dir->ob->name,
+		    nref->xoffs);
+		if (nref->xoffs == -1) {
+			if (*mapx > 1) {
+				if (!mapdir_canmove(dir, m, (*mapx)-1, *mapy)) {
+					debug(DEBUG_BLOCKED, "%s is blocked\n",
+					    dir->ob->name);
 					nref->xoffs = 0;
-					mapdir_unset(dir, DIR_LEFT);
+					return (0);
 				}
-			}
-			if (nref->xoffs <= (-TILEW + dir->speed)) {
-				nref->xoffs = -1;
-				node->flags &= ~(NODE_ANIM);
-				dir->moved |= DIR_LEFT;
-				moved |= DIR_LEFT;
-
-				if ((*mapx)-- < 1) {
-					*mapx = 1;
-				}
-
-				if (view->gfx_engine == GFX_ENGINE_TILEBASED &&
-				    (dir->flags & DIR_SCROLLVIEW) &&
-				    (view->rootmap != NULL) &&
-				    (view->rootmap->x - *mapx) <= 0) {
-					rootmap_scroll(m, DIR_LEFT);
-				}
-				m->map[*mapy][*mapx].overlap--;
-				m->map[(*mapy)-1][*mapx].overlap--;
+				m->map[*mapy][(*mapx)-1].overlap++;
+				m->map[(*mapy)-1][(*mapx)-1].overlap++;
 			} else {
-				if (dir->flags & DIR_SOFTSCROLL) {
-					nref->xoffs -= dir->speed;
-				} else {
-					nref->xoffs = -TILEW;
-				}
+				nref->xoffs = 0;
+				mapdir_unset(dir, DIR_LEFT);
 			}
 		}
-		/* Right */
-		if (nref->xoffs > 0) {
-			if (nref->xoffs == 1) {		/* Once */
-				if ((*mapx) + 1 < m->mapw - 2) {
-					if (!mapdir_canmove(dir, m, (*mapx)+1,
-					    *mapy)) {
-						nref->xoffs = 0;
-#if 0
-						mapdir_setsprite(dir,
-						    DIR_ANIM_IDLERIGHT, 1);
-#endif
-						return (0);
-					}
-					m->map[*mapy][(*mapx)+1].overlap++;
-					m->map[(*mapy)-1][(*mapx)+1].overlap++;
-				} else {
-					nref->xoffs = 0;
-					mapdir_unset(dir, DIR_RIGHT);
-				}
+		if (nref->xoffs <= (-TILEW + dir->speed)) {
+			nref->xoffs = -1;
+			node->flags &= ~(NODE_ANIM);
+			dir->moved |= DIR_LEFT;
+			moved |= DIR_LEFT;
+			if ((*mapx)-- < 1) {
+				*mapx = 1;
 			}
-			if (nref->xoffs >= (TILEW - dir->speed)) {
-				nref->xoffs = 1;
-				node->flags &= ~(NODE_ANIM);
-				dir->moved |= DIR_RIGHT;
-				moved |= DIR_RIGHT;
-				if (++(*mapx) > m->mapw-1) {
-					*mapx = m->mapw-1;
-				}
-				if (view->gfx_engine == GFX_ENGINE_TILEBASED &&
-				    (dir->flags & DIR_SCROLLVIEW) &&
-				    (view->rootmap != NULL) &&
-				    (view->rootmap->x - *mapx) <=
-				     -view->rootmap->w + 2) {
-					rootmap_scroll(m, DIR_RIGHT);
-				}
-				m->map[*mapy][*mapx].overlap--;
-				m->map[(*mapy)-1][*mapx].overlap--;
+			if (view->gfx_engine == GFX_ENGINE_TILEBASED &&
+			    (dir->flags & DIR_SCROLLVIEW) &&
+			    (view->rootmap != NULL) &&
+			    (view->rootmap->x - *mapx) <= 0) {
+				rootmap_scroll(m, DIR_LEFT);
+			}
+			m->map[*mapy][*mapx].overlap--;
+			m->map[(*mapy)-1][*mapx].overlap--;
+		} else {
+			if (dir->flags & DIR_SOFTSCROLL) {
+				nref->xoffs -= dir->speed;
 			} else {
-				if (dir->flags & DIR_SOFTSCROLL) {
-					nref->xoffs += dir->speed;
-				} else {
-					nref->xoffs = TILEW;
+				nref->xoffs = -TILEW;
+			}
+		}
+	}
+
+	if (nref->xoffs > 0) {				/* Move right */
+		debug(DEBUG_MOVE, "%s moving right (xoffs=%d)\n", dir->ob->name,
+		    nref->xoffs);
+		if (nref->xoffs == 1) {
+			if ((*mapx) + 1 < m->mapw - 2) {
+				if (!mapdir_canmove(dir, m, (*mapx)+1, *mapy)) {
+					debug(DEBUG_BLOCKED, "%s is blocked\n",
+					    dir->ob->name);
+					nref->xoffs = 0;
+					return (0);
 				}
+				m->map[*mapy][(*mapx)+1].overlap++;
+				m->map[(*mapy)-1][(*mapx)+1].overlap++;
+			} else {
+				nref->xoffs = 0;
+				mapdir_unset(dir, DIR_RIGHT);
+			}
+		}
+		if (nref->xoffs >= (TILEW - dir->speed)) {
+			nref->xoffs = 1;
+			node->flags &= ~(NODE_ANIM);
+			dir->moved |= DIR_RIGHT;
+			moved |= DIR_RIGHT;
+			if (++(*mapx) > m->mapw-1) {
+				*mapx = m->mapw-1;
+			}
+			if (view->gfx_engine == GFX_ENGINE_TILEBASED &&
+			    (dir->flags & DIR_SCROLLVIEW) &&
+			    (view->rootmap != NULL) &&
+			    (view->rootmap->x - *mapx) <= -view->rootmap->w+2) {
+				rootmap_scroll(m, DIR_RIGHT);
+			}
+			m->map[*mapy][*mapx].overlap--;
+			m->map[(*mapy)-1][*mapx].overlap--;
+		} else {
+			if (dir->flags & DIR_SOFTSCROLL) {
+				nref->xoffs += dir->speed;
+			} else {
+				nref->xoffs = TILEW;
 			}
 		}
 	}
