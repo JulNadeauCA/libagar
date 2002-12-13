e46e046cf5166b38b3df21cc2fb751c21a2a2851
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-12-13 11:14:17 +0000

    - reimplement noderefs to accomodate new types without breaking map   compatibility. noderef data is now stored in an union of structs. - node_addref() replaced by node_add_{sprite,anim,warp}(). implemented   node_move_ref() and node_copy_ref(). - implement warp points (map id, origin and direction args). - add centering offsets to sprite/anim noderefs. - use debug flags. - store x/y coordinates in nodes in debug mode and a signature in noderefs. - add a transform queue to noderef structures. - replace node_free() by node_destroy() and don't let it free the node. - thread safe map_shrink() and map_grow(). - implement zooming; adapted from the mapview widget. - break the load/save routines into {node,noderef}_{save,load}(), and   export them, move the object table code out of here. - get rid of RLE node compression, it would be too ugly with the new   noderef structures and transforms. - save the zoom factor as well as tile geometry.
    git-svn-id: https://dev.csoft.net/agar/trunk@1900 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/map.h
+++ engine/map.h
@@ -1,4 +1,4 @@
-/*	$Csoft: map.h,v 1.48 2002/11/30 02:09:47 vedge Exp $	*/
+/*	$Csoft: map.h,v 1.49 2002/12/01 14:31:02 vedge Exp $	*/
 /*	Public domain	*/
 
 #define TILEW		32
@@ -6,46 +6,50 @@
 #define TILEW_SHIFT	5
 #define TILEH_SHIFT	5
 
-#ifndef CHARW
-#define CHARW	32
-#endif
-#ifndef CHARH
-#define CHARH	64
-#endif
+#include <engine/transform.h>
 
-enum {
-	MAP_MINWIDTH	= 10,
-	MAP_MINHEIGHT	= 10
+#define MAP_MINWIDTH	10	/* XXX */
+#define MAP_MINHEIGHT	8
+
+enum noderef_type {
+	NODEREF_SPRITE,
+	NODEREF_ANIM,
+	NODEREF_WARP
 };
 
-/* XXX use unions */
+/* Reference within a node. */
 struct noderef {
 #ifdef DEBUG
 	char	magic[18];
 #define NODEREF_MAGIC "agar map noderef"
 #endif
-	struct	object *pobj;	/* Object pointer */
-	Uint32	flags;
-#define MAPREF_SAVE		0x0001	/* Saveable reference */
-#define MAPREF_SPRITE		0x0002	/* Single surface */
-#define MAPREF_ANIM		0x0010	/* Sequence of frames */
-#define MAPREF_ANIM_DELTA	0x0020	/* Increment per anim */
-#define MAPREF_ANIM_INDEPENDENT	0x0040	/* Increment per map reference */
-#define MAPREF_ANIM_STATIC	0x0080	/* Increment manually */
-#define MAPREF_MAP_WARP		0x1000	/* Jump to another map */
-#define MAPREF_MAP_NODE		0x2000	/* Node of another map */
-#define MAPREF_ANY		0xffff
-#define MAPREF_DONTSAVE		0
-	Uint32	offs;
-	
-	Sint32	xoffs;		/* X motion offset */
-	Sint32	yoffs;		/* Y motion offset */
+	enum noderef_type	 type;	/* Type of reference */
+	Uint32			 flags;
+#define NODEREF_SAVEABLE	0x01	/* Saveable reference */
+#define NODEREF_BLOCK		0x04	/* Block other objects on maps */
+#define NODEREF_EPHEMERAL	0x00
 
-	/* Animation variables */
-	int	frame;		/* Animation frame # */
-	int	fdelta;		/* For MAPREF_ANIM_INDEPENDENT */
+	struct object	*pobj;		/* Object pointer */
+	Uint32		 offs;		/* Sprite/anim array offset */
 
-	TAILQ_ENTRY(noderef) nrefs;	/* Node reference list */
+	Sint16	xcenter, ycenter;	/* Sprite/anim centering offsets */
+	Sint16	xmotion, ymotion;	/* Sprite/anim motion offsets */
+
+	union {
+		struct {
+			Uint32	flags;
+#define NODEREF_ANIM_AUTO	0x01		/* Auto increment frame# */
+			Uint32	frame;		/* Current frame# */
+		} anim;
+		struct {
+			char	*map;		/* Map identifier */
+			Uint32	 x, y;		/* Origin override */
+			Uint8	 dir;		/* Default direction */
+		} warp;
+	} data;
+
+	SLIST_HEAD(, transform) transforms;	/* Run-time tile transforms */
+	TAILQ_ENTRY(noderef) nrefs;		/* Node reference list */
 };
 
 TAILQ_HEAD(noderefq, noderef);
@@ -57,9 +61,8 @@ struct node {
 #define NODE_MAGIC "agar map node"
 	int	x, y;
 #endif
-	struct	noderefq nrefsh;
-	Uint32	nnrefs;
-	Uint32	flags;
+	struct noderefq	 nrefs;		/* Items on this node */
+	Uint32	 flags;
 #define NODE_BLOCK	0x0001		/* Cannot walk through */
 #define NODE_ORIGIN	0x0002		/* Origin of this map */
 #define NODE_WALK	0x0004		/* Can walk through */
@@ -69,53 +72,64 @@ struct node {
 #define NODE_REGEN	0x0200		/* Cause HP Regeneration */
 #define NODE_SLOW	0x0400		/* Decrease speed by v1 */
 #define NODE_HASTE	0x0800		/* Increase speed by v1 */
-#define NODE_DONTSAVE	0x0000
+#define NODE_EPHEMERAL	0x0000
 
-	Uint32	v1, v2;			/* Extra properties */
-	Uint32	nanims;			/* Animation count */
+	Uint32	 v1;			/* Extra property */
+	Uint32	 nanims;		/* Animation count (optimization) */
 };
 
 /* Region within the world. */
 struct map {
 	struct object	  obj;
 
+	/* Read-only once attached */
 	Uint32		  flags;
 #define MAP_RLE_COMPRESSION	0x01	/* RLE-compress the nodes */
 #define MAP_2D			0x20	/* Two-dimensional */
 
+	/* Read-write, thread-safe */
 	int		  redraw;	/* Redraw at next tick. XXX */
-	Uint32		  fps;		/* Maximum fps */
 	Uint32		  mapw, maph;	/* Map geometry */
 	int		  tilew, tileh;	/* Tile geometry */
+	Uint16		  zoom;		/* Zoom (%) */
 	Uint32		  defx, defy;	/* Map origin */
 	struct node	**map;		/* Array of nodes */
-	int		  zoom;		/* Zoom (%) */
 
-	pthread_t	  draw_th;	/* Map rendering thread */
-	pthread_mutex_t	  lock;		/* Recursive lock on all nodes, and all
-					   references inside them */
-	pthread_mutexattr_t lockattr;
+	pthread_mutex_t		lock;	/* Recursive lock on the map. */
+	pthread_mutexattr_t	lockattr;
 };
 
-void	map_init(struct map *, char *, char *, Uint32);
-int	map_load(void *, int);
-int	map_save(void *, int);
-void	map_destroy(void *);
-
-void	map_clean(struct map *, struct object *, Uint32, Uint32, Uint32);
-void	map_allocnodes(struct map *, Uint32, Uint32);
-void	map_freenodes(struct map *);
-void	map_shrink(struct map *, Uint32, Uint32);
-void	map_grow(struct map *, Uint32, Uint32);
-void	map_adjust(struct map *, Uint32, Uint32);
+void		 map_init(struct map *, char *, char *, Uint32);
+int		 map_load(void *, int);
+int		 map_save(void *, int);
+void		 map_destroy(void *);
+void		 map_alloc_nodes(struct map *, Uint32, Uint32);
+void		 map_free_nodes(struct map *);
+void		 map_shrink(struct map *, Uint32, Uint32);
+void		 map_grow(struct map *, Uint32, Uint32);
+void		 map_adjust(struct map *, Uint32, Uint32);
+void		 map_set_zoom(struct map *, Uint16);
 #ifdef DEBUG
-void	map_verify(struct map *);
+void		 map_verify(struct map *);
 #endif
-void	map_set_zoom(struct map *, int);
 
-struct noderef	*node_addref(struct node *, void *, Uint32, Uint32);
-struct noderef	*node_findref(struct map *, struct node *, void *, Sint32,
-		     Uint32);
-int		 node_delref(struct node *, struct noderef *);
+void		 noderef_init(struct noderef *);
+void		 noderef_destroy(struct noderef *);
+void		 noderef_load(int, struct object **, Uint32, struct node *,
+		     struct noderef **);
+void		 noderef_save(struct fobj_buf *, struct object **, Uint32,
+		     struct noderef *);
+
+void		 node_init(struct node *, int, int);
+void		 node_load(int, struct object **, Uint32, struct node *);
+void		 node_save(struct fobj_buf *, struct object **, Uint32,
+		     struct node *);
+void		 node_destroy(struct node *, int, int);
+struct noderef	*node_add_sprite(struct node *, void *, Uint32);
+struct noderef	*node_add_anim(struct node *, void *, Uint32, Uint32);
+struct noderef	*node_add_warp(struct node *, char *, Uint32, Uint32, Uint8);
+void		 node_move_ref(struct noderef *, struct node *, struct node *);
+void		 node_copy_ref(struct noderef *, struct node *);
+void		 node_del_ref(struct node *, struct noderef *);
 void		 node_draw(struct map *, struct node *, Uint32, Uint32);
 
