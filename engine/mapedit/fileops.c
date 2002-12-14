44f40d1f4dee7307c7e27fcbb97416b2f53c7bcc
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-12-14 11:28:56 +0000

    clean up the load/create map dialogs, check the existence of maps, add cancel buttons.
    git-svn-id: https://dev.csoft.net/agar/trunk@1923 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/mapedit/fileops.c
+++ engine/mapedit/fileops.c
@@ -1,4 +1,4 @@
-/*	$Csoft: fileops.c,v 1.18 2002/12/13 07:47:33 vedge Exp $	*/
+/*	$Csoft: fileops.c,v 1.19 2002/12/13 12:33:29 vedge Exp $	*/
 
 /*
  * Copyright (c) 2002 CubeSoft Communications, Inc <http://www.csoft.org>
@@ -44,6 +44,88 @@
 #include "mapwin.h"
 #include "mapview.h"
 
+/* Create the `new map' dialog. */
+struct window *
+fileops_new_map_window(struct mapedit *med)
+{
+	struct window *win;
+	struct region *reg;
+	struct textbox *name_tbox, *w_tbox, *h_tbox;
+	struct button *button;
+
+	win = window_new("mapedit-new-map-dialog", WINDOW_CENTER,
+	     0, 0, 380, 162, 187, 162);
+	window_set_caption(win, "Create a map");
+
+	reg = region_new(win, REGION_VALIGN, 0, 0, 100, 30);
+	{
+		name_tbox = textbox_new(reg, "Name: ", 0, 100, 100);
+		win->focus = WIDGET(name_tbox);
+	}
+
+	reg = region_new(win, REGION_HALIGN, 0, 30, 100, 40);
+	{
+		w_tbox = textbox_new(reg, "W: ", 0, 50, 100);
+		textbox_printf(w_tbox, "64");
+		h_tbox = textbox_new(reg, "H: ", 0, 50, 100);
+		textbox_printf(h_tbox, "64");
+	}
+
+	reg = region_new(win, REGION_HALIGN, 0, 70, 100, 30);
+	{
+		button = button_new(reg, "OK", NULL, 0, 50, 100);
+		event_new(button, "button-pushed", fileops_new_map,
+		    "%p, %p, %p, %p",
+		    med, name_tbox, w_tbox, h_tbox);
+		event_new(name_tbox, "textbox-return", fileops_new_map,
+		    "%p, %p, %p, %p",
+		    med, name_tbox, w_tbox, h_tbox);
+		
+		button = button_new(reg, "Cancel", NULL, 0, 50, 100);
+		event_new(button, "button-pushed", window_generic_hide,
+		    "%p",  win);
+	}
+
+	return (win);
+}
+
+/* Create the `load map' dialog. */
+struct window *
+fileops_load_map_window(struct mapedit *med)
+{
+	struct window *win;
+	struct region *reg;
+	struct textbox *name_tbox;
+	struct button *button;
+
+	win = window_new("mapedit-load-map-dialog", WINDOW_CENTER, -1, -1,
+	    356, 105, 100, 105);
+	window_set_caption(win, "Load a map");
+
+	reg = region_new(win, REGION_VALIGN, 0, 0, 100, 50);
+	{
+		name_tbox = textbox_new(reg, "Name: ", 0, 100, 100);
+		win->focus = WIDGET(name_tbox);
+	}
+	
+	reg = region_new(win, REGION_HALIGN, 0, 50, 100, 50);
+	{
+		button = button_new(reg, "OK", NULL, 0, 50, 100);
+		WIDGET(button)->flags |=
+		    WIDGET_NO_FOCUS|WIDGET_UNFOCUSED_BUTTONUP;
+		event_new(button, "button-pushed", fileops_load_map,
+		    "%p, %p", med, name_tbox);
+		event_new(name_tbox, "textbox-return", fileops_load_map,
+		    "%p, %p", med, name_tbox);
+		
+		button = button_new(reg, "Cancel", NULL, 0, 50, 100);
+		event_new(button, "button-pushed", window_generic_hide,
+		    "%p",  win);
+	}
+
+	return (win);
+}
+
 /* Create a new map. */
 void
 fileops_new_map(int argc, union evarg *argv)
@@ -51,27 +133,31 @@ fileops_new_map(int argc, union evarg *argv)
 	struct widget *wid = argv[0].p;
 	struct mapedit *med = argv[1].p;
 	struct textbox *name_tbox = argv[2].p;
-	struct textbox *media_tbox = argv[3].p;
-	struct textbox *w_tbox = argv[4].p;
-	struct textbox *h_tbox = argv[5].p;
+	struct textbox *w_tbox = argv[3].p;
+	struct textbox *h_tbox = argv[4].p;
 	struct window *win;
 	struct node *origin;
 	struct map *m;
-	char *name, *media;
+	char *name, *path;
 	Uint32 w, h;
 
 	name = textbox_string(name_tbox);
-	media = textbox_string(media_tbox);
 
-	if (strcmp(name, "") == 0) {
+	if (strcmp(name, "") == 0) {				/* Not empty? */
 		text_msg("Error", "No map name given.");
+		goto out;
+	}
+	path = object_path(name, "map");			/* Existing? */
+	if (path != NULL) {
+		free(path);
+		text_msg("Error", "Existing map (%s).", path);
 		goto out;
 	}
 	w = (Uint32)textbox_int(w_tbox);
 	h = (Uint32)textbox_int(h_tbox);
 
 	m = emalloc(sizeof(struct map));
-	map_init(m, name, strcmp(media, "") == 0 ? NULL : media, MAP_2D);
+	map_init(m, name, NULL, MAP_2D);
 	map_alloc_nodes(m, w, h);
 
 	m->defx = w / 2;
@@ -85,12 +171,9 @@ fileops_new_map(int argc, union evarg *argv)
 	window_show(win);
 
 	textbox_printf(name_tbox, "");
-	textbox_printf(media_tbox, "");
-
 	window_hide(wid->win);
 out:
 	free(name);
-	free(media);
 }
 
 /* Save the map to the default location. */
@@ -112,14 +195,22 @@ fileops_load_map(int argc, union evarg *argv)
 	struct window *win;
 	struct map *m;
 	char *name;
+	char *path;
 
 	name = textbox_string(name_tbox);
 
-	if (strcmp(name, "") == 0) {
+	if (strcmp(name, "") == 0) {				/* Not empty? */
 		text_msg("Error", "No map name given.");
 		goto out;
 	}
-
+	path = object_path(name, "map");			/* Existing? */
+	if (path == NULL) {
+		text_msg("Error", "%s.", error_get());
+		goto out;
+	} else {
+		free(path);
+	}
+	
 	m = emalloc(sizeof(struct map));
 	map_init(m, name, NULL, MAP_2D);
 
@@ -156,7 +247,7 @@ fileops_revert_map(int argc, union evarg *argv)
 	free(path);
 }
 
-/* Clear all nodes. */
+/* Clear all nodes on a map. */
 void
 fileops_clear_map(int argc, union evarg *argv)
 {
