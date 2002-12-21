696b11c94cef9ee17c83ac50471ea0fcff4daa5e
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-12-21 10:25:34 +0000

    box/frame variants that take a SDL_Rect argument.

--- trunk/engine/widget/primitive.h
+++ trunk/engine/widget/primitive.h
@@ -1,11 +1,13 @@
-/*	$Csoft: primitive.h,v 1.12 2002/09/11 23:55:08 vedge Exp $	*/
+/*	$Csoft: primitive.h,v 1.13 2002/11/14 02:17:45 vedge Exp $	*/
 /*	Public domain	*/
 
 struct primitive_ops {
 	void	(*box)(void *p, int xoffs, int yoffs, int w, int h, int z,
 		       Uint32 color);
+	void	(*box_rect)(void *p, SDL_Rect *rd, int z, Uint32 color);
 	void	(*frame)(void *p, int xoffs, int yoffs, int w, int h,
 		         Uint32 color);
+	void	(*frame_rect)(void *p, SDL_Rect *rd, Uint32 color);
 	void	(*circle)(void *p, int xoffs, int yoffs, int w, int h,
 		         int radius, Uint32 color);
 	void	(*line)(void *p, int x1, int y1, int x2, int y2, Uint32 color);
