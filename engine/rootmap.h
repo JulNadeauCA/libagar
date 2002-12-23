10c61a2b04f38bcd217f3bb3a94ed93c99fc2c75
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-12-23 03:05:04 +0000

    - clean up video initialization code. - create an OpenGL rendering context with HAVE_OPENGL.
    git-svn-id: https://dev.csoft.net/agar/trunk@1982 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/rootmap.h
+++ engine/rootmap.h
@@ -1,13 +1,15 @@
-/*	$Csoft: rootmap.h,v 1.5 2002/11/22 08:56:49 vedge Exp $	*/
+/*	$Csoft: rootmap.h,v 1.6 2002/11/28 01:06:50 vedge Exp $	*/
 /*	Public domain	*/
 
+struct viewmap;
+struct viewport;
+
+void	 rootmap_init(struct viewmap *, int, int);
 void	 rootmap_animate(void);
 void	 rootmap_draw(void);
 void	 rootmap_focus(struct map *);
 void	 rootmap_center(struct map *, int, int);
 void	 rootmap_scroll(struct map *, int, int);
-
-struct viewport;
 
 SDL_Rect	**rootmap_alloc_maprects(int, int);
 void		  rootmap_free_maprects(struct viewport *);
