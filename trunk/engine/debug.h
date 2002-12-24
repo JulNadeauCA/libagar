533b63b9704461b6916d925dca7d29eb21485ede
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-12-24 14:22:17 +0000

    style

--- trunk/engine/debug.h
+++ trunk/engine/debug.h
@@ -1,4 +1,4 @@
-/*	$Csoft: debug.h,v 1.23 2002/11/26 05:03:11 vedge Exp $	*/
+/*	$Csoft: debug.h,v 1.24 2002/12/24 10:28:37 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_ENGINE_DEBUG_H_
@@ -57,51 +57,50 @@ extern int engine_debug;
 # endif
 #endif	/* DEBUG */
 
-#if defined(LOCKDEBUG) && defined(SERIALIZATION)
-#include <stdlib.h>
-#include <string.h>
-
-#define pthread_mutex_lock(mutex) do {					\
+#ifdef SERIALIZATION
+# ifdef LOCKDEBUG
+#  include <stdlib.h>
+#  include <string.h>
+#  define pthread_mutex_lock(mutex) do {				\
 	int _rv;							\
 	if ((_rv = pthread_mutex_lock((mutex))) != 0) {			\
 		fatal("mutex_lock(%p): %s\n", (mutex), strerror(_rv));	\
 	}								\
-} while (/*CONSTCOND*/0)
-#define pthread_mutex_unlock(mutex) do {				\
+} while (0)
+#  define pthread_mutex_unlock(mutex) do {				\
 	int _rv;							\
 	if ((_rv = pthread_mutex_unlock((mutex))) != 0) {		\
 		fatal("mutex_unlock(%p): %s\n", (mutex), strerror(_rv));\
 	}								\
-} while (/*CONSTCOND*/0)
-#define pthread_mutex_init(mutex, attr)	do {				\
+} while (0)
+#  define pthread_mutex_init(mutex, attr)	do {			\
 	int _rv;							\
 	if ((_rv = pthread_mutex_init((mutex), (attr))) != 0) {		\
 		fatal("pthread_mutex_init: %s\n", strerror(_rv));	\
 	}								\
-} while (/*CONSTCOND*/0)
-#define pthread_mutex_destroy(mutex) do {				\
+} while (0)
+#  define pthread_mutex_destroy(mutex) do {				\
 	int _rv;							\
 	if ((_rv = pthread_mutex_destroy((mutex))) != 0) {		\
 		fatal("pthread_mutex_destroy: %s\n", strerror(_rv));	\
 	}								\
-} while (/*CONSTCOND*/0)
+} while (0)
 
-#endif	/* LOCKDEBUG and SERIALIZATION */
+# endif /* LOCKDEBUG */
 
-#ifdef SERIALIZATION
-#define Pthread_create(thread, attr, func, arg) do {			\
+# define Pthread_create(thread, attr, func, arg) do {			\
 	int _rv;							\
 	if ((_rv = pthread_create((thread), (attr), (func), (arg)))	\
 	    != 0) {							\
 		fatal("pthread_create: %s\n", strerror(_rv));		\
 	}								\
-} while (/*CONSTCOND*/0)
-#define Pthread_join(thread, valptr) do {				\
+} while (0)
+# define Pthread_join(thread, valptr) do {				\
 	int _rv;							\
 	if ((_rv = pthread_join((thread), (valptr))) != 0) {		\
 		fatal("pthread_join: %s\n", strerror(_rv));		\
 	}								\
-} while (/*CONSTCOND*/0)
-#endif
+} while (0)
+#endif /* SERIALIZATION */
 
-#endif	/* !_AGAR_ENGINE_DEBUG_H_ */
+#endif /* !_AGAR_ENGINE_DEBUG_H_ */
