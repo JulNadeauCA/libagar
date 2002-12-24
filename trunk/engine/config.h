15e537df2e7b294be0a272b774b4e6bc186083ab
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2002-12-24 10:28:23 +0000

    unused

--- trunk/engine/config.h
+++ trunk/engine/config.h
@@ -1,21 +1,9 @@
-/*	$Csoft: config.h,v 1.9 2002/09/06 01:22:52 vedge Exp $	*/
+/*	$Csoft: config.h,v 1.10 2002/09/07 04:31:54 vedge Exp $	*/
 /*	Public domain	*/
 
-/* flags */
-#define CONFIG_FONT_CACHE	0x01	/* Cache common glyphs */
-#define CONFIG_FULLSCREEN	0x02	/* Try full-screen mode */
-#define CONFIG_ASYNCBLIT	0x04	/* Asynchronous screen updates */
-
-/* widgets.flags */
-#define CONFIG_REGION_BORDERS	0x01	/* Region borders always visible */
-#define CONFIG_WINDOW_ANYSIZE	0x02	/* Show resolution on resize */
-
 struct config {
-	struct	 object obj;
-	struct {
-		struct	window *settings;
-		struct	window *algorithm_sw;
-	} windows;
+	struct object	obj;
+	struct window	*settings;
 };
 
 extern struct config *config;
@@ -26,8 +14,5 @@ int		 config_load(void *, int);
 int		 config_save(void *, int);
 void		 config_destroy(void *);
 
-void		 config_init_wins(struct config *);
-
-int		 config_int(const char *);
-char		*config_string(const char *);
+void		 config_window(struct config *);
 
