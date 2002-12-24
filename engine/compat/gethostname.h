031b0ba99f435f85309a60638b87084e0ea9e9e3
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-12-24 14:24:15 +0000

    pull in size_t definition from <sys/types.h>
    git-svn-id: https://dev.csoft.net/agar/trunk@2049 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/compat/gethostname.h
+++ engine/compat/gethostname.h
@@ -1,5 +1,7 @@
-/*	$Csoft$	*/
+/*	$Csoft: gethostname.h,v 1.1 2002/11/22 23:24:59 vedge Exp $	*/
 /*	Public domain	*/
+
+#include <sys/types.h>
 
 #if defined(__GNUC__) && defined(_SGI_SOURCE)
 /* IRIX gcc 2.95 hack. */
