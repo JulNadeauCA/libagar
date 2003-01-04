90d97832c0ed942d16da435eab239e82b66dccd9
Julien Nadeau Carriere <vedge@hypertriton.com>
2003-01-04 13:57:00 +0000

    (c) 2003
    git-svn-id: https://dev.csoft.net/agar/trunk@2196 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/widget/keymaps/us.h
+++ engine/widget/keymaps/us.h
@@ -1,7 +1,8 @@
-/*	$Csoft$	*/
+/*	$Csoft: us.h,v 1.4 2002/08/19 05:33:05 vedge Exp $	*/
 
 /*
- * Copyright (c) 2002 CubeSoft Communications <http://www.csoft.org>
+ * Copyright (c) 2002, 2003 CubeSoft Communications, Inc.
+ * <http://www.csoft.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -9,9 +10,9 @@
  * are met:
  * 1. Redistribution of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- * 2. Neither the name of CubeSoft Communications, nor the names of its
- *    contributors may be used to endorse or promote products derived from
- *    this software without specific prior written permission.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
