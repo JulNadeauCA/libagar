7f827c4974551a9ba15d56a9f3cd4a4fe37b6e37
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2003-01-20 12:05:45 +0000

    - use a boolean binding for the button state. - don't scale labels to insane sizes.

--- trunk/engine/widget/button.h
+++ trunk/engine/widget/button.h
@@ -1,28 +1,26 @@
-/*	$Csoft: button.h,v 1.16 2002/08/29 07:15:43 vedge Exp $	*/
+/*	$Csoft: button.h,v 1.17 2002/12/21 10:26:33 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_WIDGET_BUTTON_H_
 #define _AGAR_WIDGET_BUTTON_H_
 
 struct button {
-	struct	 widget wid;
+	struct widget	 wid;
 
-	int	flags;
-#define BUTTON_PRESSED	0x01		/* Button is pressed */
-#define BUTTON_STICKY	0x02		/* Button state is static */
+	int		 flags;
+#define BUTTON_STICKY	0x01
 
-	char	*caption;		/* String, or NULL */
-
+	char		*caption;	/* String, or NULL */
 	SDL_Surface	*label_s;	/* Label (or image) */
 	SDL_Surface	*slabel_s;	/* Scaled label surface */
-
 	enum {
 		BUTTON_LEFT,
 		BUTTON_CENTER,
 		BUTTON_RIGHT
 	} justify;
-
-	Uint8	xmargin, ymargin;
+	struct {			/* Default binding */
+		int	value;
+	} def;
 };
 
 struct button	*button_new(struct region *, char *, SDL_Surface *, int, int,
@@ -30,7 +28,6 @@ struct button	*button_new(struct region *, char *, SDL_Surface *, int, int,
 void		 button_init(struct button *, char *, SDL_Surface *, int, int,
 		     int);
 void		 button_destroy(void *);
-
-void	 button_draw(void *);
+void		 button_draw(void *);
 
 #endif /* _AGAR_WIDGET_BUTTON_H_ */
