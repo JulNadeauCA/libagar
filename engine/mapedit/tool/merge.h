22555fe558a824fce978c8d0f6d3f34bfe28bc03
Julien Nadeau Carriere <vedge@hypertriton.com>
2003-02-10 04:44:23 +0000

    - implement multiple brushes. - save the brush sets.
    git-svn-id: https://dev.csoft.net/agar/trunk@2371 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/mapedit/tool/merge.h
+++ engine/mapedit/tool/merge.h
@@ -1,4 +1,4 @@
-/*	$Csoft: merge.h,v 1.1 2003/02/02 21:14:02 vedge Exp $	*/
+/*	$Csoft: merge.h,v 1.2 2003/02/08 00:33:54 vedge Exp $	*/
 /*	Public domain	*/
 
 enum merge_mode {
@@ -7,16 +7,23 @@ enum merge_mode {
 	MERGE_INSERT_EMPTY
 };
 
+SLIST_HEAD(brushq, object);
+
 struct merge {
 	struct tool	 tool;
 	int		 mode;
 	int		 flags;
 #define MERGE_RANDOMIZE_SHIFT	0x01
-	struct map	 brush;
+
+	struct tlist	*brushes_tl;
+	struct brushq	 brushes;
 };
 
 void		 merge_init(void *);
 struct window	*merge_window(void *);
 void		 merge_effect(void *, struct mapview *, struct node *);
+void		 merge_apply(struct merge *, struct mapview *, struct map *);
 void		 merge_destroy(void *);
+int		 merge_load(void *, int);
+int		 merge_save(void *, int);
 
