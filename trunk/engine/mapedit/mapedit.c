9a6db317356d2a1fec90f9063e108cd2ca4c1ff0
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2003-02-20 03:25:55 +0000

    default 5x5 brushes

--- trunk/engine/mapedit/mapedit.c
+++ trunk/engine/mapedit/mapedit.c
@@ -1,4 +1,4 @@
-/*	$Csoft: mapedit.c,v 1.140 2003/02/10 04:46:05 vedge Exp $	*/
+/*	$Csoft: mapedit.c,v 1.141 2003/02/13 11:30:12 vedge Exp $	*/
 
 /*
  * Copyright (c) 2001, 2002, 2003 CubeSoft Communications, Inc.
@@ -134,8 +134,8 @@ mapedit_init(void)
 	prop_set_int(med, "tilemap-bg-square-size", 16);
 	prop_set_uint32(med, "default-map-width", 64);
 	prop_set_uint32(med, "default-map-height", 32);
-	prop_set_uint32(med, "default-brush-width", 4);
-	prop_set_uint32(med, "default-brush-height", 4);
+	prop_set_uint32(med, "default-brush-width", 5);
+	prop_set_uint32(med, "default-brush-height", 5);
 
 	mapedition = 1;
 
@@ -143,9 +143,7 @@ mapedit_init(void)
 	for (i = 0; i < ntools; i++) {
 		const struct tools_ent *toolent = &tools[i];
 
-		toolent = &tools[i];
 		*toolent->p = emalloc(toolent->size);
-
 		toolent->init(*toolent->p);
 		world_attach(*toolent->p);
 	}
