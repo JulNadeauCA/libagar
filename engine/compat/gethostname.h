fd61358ac13bc833e026627cf973a1d302c7c827
Julien Nadeau Carriere <vedge@hypertriton.com>
2003-03-02 07:29:13 +0000

    remove unnecessary irix hack.
    git-svn-id: https://dev.csoft.net/agar/trunk@2477 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/compat/gethostname.h
+++ engine/compat/gethostname.h
@@ -1,12 +1,6 @@
-/*	$Csoft: gethostname.h,v 1.1 2002/11/22 23:24:59 vedge Exp $	*/
+/*	$Csoft: gethostname.h,v 1.2 2002/12/24 14:24:15 vedge Exp $	*/
 /*	Public domain	*/
 
 #include <sys/types.h>
-
-#if defined(__GNUC__) && defined(_SGI_SOURCE)
-/* IRIX gcc 2.95 hack. */
-extern int gethostname(char *, size_t);
-#else
-# include <unistd.h>
-#endif
+#include <unistd.h>
 
