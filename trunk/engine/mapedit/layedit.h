1812b556e00fa000449f298304de514aef8678cc
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2003-03-11 03:40:01 +0000

    layer rename.

--- trunk/engine/mapedit/layedit.h
+++ trunk/engine/mapedit/layedit.h
@@ -1,4 +1,4 @@
-/*	$Csoft: nodeedit.h,v 1.2 2003/03/02 07:29:53 vedge Exp $	*/
+/*	$Csoft: layedit.h,v 1.1 2003/03/05 02:16:32 vedge Exp $	*/
 /*	Public domain	*/
 
 struct mapview;
@@ -7,6 +7,7 @@ struct layedit {
 	struct window	*win;
 	struct button	*trigger;
 	struct tlist	*layers_tl;
+	struct textbox	*rename_tb;
 };
 
 void	layedit_init(struct mapview *);
