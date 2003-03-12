5ef0bac2ad5ce6d2917ae6675675f1d4458de35a
Julien Nadeau Carriere <vedge@hypertriton.com>
2003-03-12 23:59:25 +0000

    dead
    git-svn-id: https://dev.csoft.net/agar/trunk@2552 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/mapedit/tool/select.h
+++ engine/mapedit/tool/select.h
@@ -1,4 +1,4 @@
-/*	$Csoft: select.h,v 1.3 2003/02/02 21:14:02 vedge Exp $	*/
+/*	$Csoft: select.h,v 1.4 2003/03/11 02:46:53 vedge Exp $	*/
 /*	Public domain	*/
 
 struct select {
@@ -6,5 +6,4 @@ struct select {
 };
 
 void		 select_init(void *);
-struct window	*select_window(void *);
 
