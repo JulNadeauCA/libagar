a00cbd015c3510bd3771df40e7253a20efaf2f95
Julien Nadeau Carriere <vedge@hypertriton.com>
2003-03-13 22:43:55 +0000

    use our pread/pwrite emulation everywhere. should fix build under linux; reported by Joel Carlbark <joel@lnxscene.org>.
    git-svn-id: https://dev.csoft.net/agar/trunk@2575 86a84d31-9702-0410-8f1e-a60cab7bd424

---
+++ engine/compat/pread.c
@@ -0,0 +1,63 @@
+/*	$Csoft$	*/
+
+/*
+ * Copyright (c) 2002, 2003 CubeSoft Communications, Inc.
+ * <http://www.csoft.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/types.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include <engine/error.h>
+
+#include "pread.h"
+
+ssize_t
+Pread(int fd, void *buf, size_t size, off_t offs)
+{
+	off_t offs_save;
+	ssize_t rv;
+
+	if ((offs_save = lseek(fd, 0, SEEK_CUR)) == -1)
+		fatal("lseek: %s", strerror(errno));
+
+	if (lseek(fd, offs, SEEK_SET) == -1)
+		fatal("lseek: %s", strerror(errno));
+	rv = read(fd, buf, size);
+	if (rv == -1) {
+		fatal("write(%lu): %s", (unsigned long)size, strerror(errno));
+	} else if (rv != size) {
+		fatal("short write: %lu/%lu", (unsigned long)rv,
+		    (unsigned long)size);
+	}
+
+	if (lseek(fd, offs_save, SEEK_SET) == -1)
+		fatal("lseek: %s", strerror(errno));
+	return (rv);
+}
+
---
+++ engine/compat/pread.h
@@ -0,0 +1,7 @@
+/*	$Csoft$	*/
+/*	Public domain	*/
+
+#include <sys/types.h>
+
+ssize_t	Pread(int, void *, size_t, off_t);
+
