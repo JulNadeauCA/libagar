22552104802949e00184cd0e4a2c47b139b4aabb
Julien Nadeau Carriere <vedge@hypertriton.com>
2003-03-14 04:16:12 +0000

    getenv.h -> setenv.h
    git-svn-id: https://dev.csoft.net/agar/trunk@2587 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/compat/setenv.h
+++ engine/compat/setenv.h
@@ -1,7 +1,7 @@
-/*	$Csoft: setenv.h,v 1.2 2002/09/06 00:50:43 vedge Exp $	*/
+/*	$Csoft: setenv.h,v 1.3 2002/12/24 10:29:21 vedge Exp $	*/
 /*	Public domain	*/
 
-#include <config/have_getenv.h>
+#include <config/have_setenv.h>
 
 #ifndef HAVE_SETENV
 int	setenv(const char *, const char *, int);
