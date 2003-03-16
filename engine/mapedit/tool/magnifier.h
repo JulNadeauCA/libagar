6048a8c0f81e2f3b485910ef01f71a1701954a36
Julien Nadeau Carriere <vedge@hypertriton.com>
2003-03-16 02:58:20 +0000

    simplify, fix mouse zooming.
    git-svn-id: https://dev.csoft.net/agar/trunk@2607 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/mapedit/tool/magnifier.h
+++ engine/mapedit/tool/magnifier.h
@@ -1,20 +1,13 @@
-/*	$Csoft: magnifier.h,v 1.6 2003/02/02 21:14:02 vedge Exp $	*/
+/*	$Csoft: magnifier.h,v 1.7 2003/02/22 11:44:05 vedge Exp $	*/
 /*	Public domain	*/
-
-enum magnifier_mode {
-	MAGNIFIER_ZOOM_IN,
-	MAGNIFIER_ZOOM_OUT
-};
 
 struct magnifier {
 	struct tool	tool;
-	int		mode;
 	int		increment;
 };
 
 void		 magnifier_init(void *);
 struct window	*magnifier_window(void *);
-void		 magnifier_effect(void *, struct mapview *, struct node *);
 void		 magnifier_mouse(void *, struct mapview *, Sint16, Sint16,
 		     Uint8);
 
