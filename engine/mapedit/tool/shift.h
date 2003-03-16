637088fa0996721338f8d04e6a3e768dbecd58d9
Julien Nadeau Carriere <vedge@hypertriton.com>
2003-03-16 02:39:14 +0000

    shift all the selected tiles.
    git-svn-id: https://dev.csoft.net/agar/trunk@2602 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/mapedit/tool/shift.h
+++ engine/mapedit/tool/shift.h
@@ -1,17 +1,14 @@
-/*	$Csoft: shift.h,v 1.3 2003/02/02 21:14:02 vedge Exp $	*/
+/*	$Csoft: shift.h,v 1.4 2003/03/07 03:24:49 vedge Exp $	*/
 /*	Public domain	*/
 
 enum shift_mode {
 	SHIFT_HIGHEST,
-	SHIFT_LOWEST,
-	SHIFT_SELECTIVE,
 	SHIFT_ALL
 };
 
 struct shift {
 	struct tool	tool;
 	int		mode;
-	int		all_layers;
 };
 
 void		 shift_init(void *);
