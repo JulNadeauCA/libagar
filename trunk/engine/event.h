242f12bbe81daf46c49f86e6cb92a36604311bc1
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2003-03-20 01:17:33 +0000

    event_new() should return a struct event *.

--- trunk/engine/event.h
+++ trunk/engine/event.h
@@ -1,5 +1,7 @@
-/*	$Csoft: event.h,v 1.13 2002/12/31 02:20:53 vedge Exp $	*/
+/*	$Csoft: event.h,v 1.14 2003/03/02 01:00:13 vedge Exp $	*/
 /*	Public domain	*/
+
+#include <config/floating_point.h>
 
 typedef union evarg {
 	void	*p;
@@ -7,35 +9,31 @@ typedef union evarg {
 	int	 i;
 	char	 c;
 	long int li;
+#ifdef FLOATING_POINT
 	double	 f;
+#endif
 } *evargs;
 
-#define EVENT_MAXARGS	16
+#define EVENT_MAX_ARGS	16
 
 struct event {
-	char	*name;
-	int	 flags;
+	char		*name;
+	int		 flags;
 #define	EVENT_ASYNC	0x01	/* Event handler runs in own thread */
-	union	 evarg argv[EVENT_MAXARGS];
-	int	 argc;
-	void	(*handler)(int, union evarg *);
+	union evarg	 argv[EVENT_MAX_ARGS];
+	int		 argc;
+	void		(*handler)(int, union evarg *);
 	TAILQ_ENTRY(event) events;
 };
 
-/* SDL user events */
-enum {
-	USER_WINDOW_EVENT
-};
-
-void	 event_loop(void);
-void	 event_new(void *, char *, void (*)(int, union evarg *),
-	     const char *, ...);
-void	 event_post(void *, char *, const char *fmt, ...);
-void	 event_forward(void *, char *, int, union evarg *);
-
+void		 event_loop(void);
+struct event	*event_new(void *, char *, void (*)(int, union evarg *),
+		     const char *, ...);
+void		 event_post(void *, char *, const char *fmt, ...);
+void		 event_forward(void *, char *, int, union evarg *);
 #ifdef DEBUG
 struct window	*event_show_fps_counter(void);
 #endif
 
-extern int event_idle;
+extern int	 event_idle;
 
