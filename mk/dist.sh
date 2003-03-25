2c2889fdb3e5c1db5e4fe88beb32a87f7cc078b3
Julien Nadeau Carriere <vedge@hypertriton.com>
2003-03-25 10:45:17 +0000

    sign the source snapshots.
    git-svn-id: https://dev.csoft.net/agar/trunk@2681 86a84d31-9702-0410-8f1e-a60cab7bd424

--- mk/dist.sh
+++ mk/dist.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #
-#	$Csoft: dist.sh,v 1.7 2003/03/13 23:18:34 vedge Exp $
+#	$Csoft: dist.sh,v 1.8 2003/03/14 07:53:59 vedge Exp $
 
 DATE=`date +%m%d%Y`
 DISTFILE=agar-${DATE}
 
 cd ..
-echo "copying agar onto agar-${DATE}"
+echo "snapshot: agar-${DATE}"
 rm -fr agar-${DATE}
 cp -fRp agar agar-${DATE}
 rm -fR `find agar-${DATE} \( -name CVS \
@@ -24,8 +24,9 @@ gzip -f9 ${DISTFILE}.tar
 md5 ${DISTFILE}.tar.gz > ${DISTFILE}.tar.gz.md5
 rmd160 ${DISTFILE}.tar.gz >> ${DISTFILE}.tar.gz.md5
 sha1 ${DISTFILE}.tar.gz >> ${DISTFILE}.tar.gz.md5
+gpg -ab ${DISTFILE}.tar.gz
 
 echo "uploading"
-scp -C ${DISTFILE}.tar.{gz,gz.md5} vedge@resin:www/snap
-ssh vedge@resin "cp -f www/snap/${DISTFILE}.tar.{gz,gz.md5} www/beta.csoft.org/agar && ls -l www/beta.csoft.org/agar/${DISTFILE}.tar.{gz,gz.md5}"
+scp -C ${DISTFILE}.tar.{gz,gz.md5,gz.asc} vedge@resin:www/snap
+ssh vedge@resin "cp -f www/snap/${DISTFILE}.tar.{gz,gz.md5,gz.asc} www/beta.csoft.org/agar && ls -l www/beta.csoft.org/agar/${DISTFILE}.tar.{gz,gz.md5,gz.asc}"
 
