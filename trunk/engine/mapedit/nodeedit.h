f404418875e7fb1d2104c9aeb1565647b9c7bfd3
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2003-03-28 00:23:23 +0000

    cosmetic

--- trunk/engine/mapedit/nodeedit.h
+++ trunk/engine/mapedit/nodeedit.h
@@ -1,4 +1,4 @@
-/*	$Csoft: nodeedit.h,v 1.1 2003/03/02 04:08:54 vedge Exp $	*/
+/*	$Csoft: nodeedit.h,v 1.2 2003/03/02 07:29:53 vedge Exp $	*/
 /*	Public domain	*/
 
 struct mapview;
@@ -6,7 +6,7 @@ struct mapview;
 struct nodeedit {
 	struct window	*win;
 	struct button	*trigger;
-	struct label	*node_flags_lab, *node_size_lab;
+	struct label	*node_flags_lab;
 	struct label	*noderef_type_lab, *noderef_flags_lab;
 	struct label	*noderef_center_lab;
 	struct tlist	*refs_tl, *transforms_tl;
