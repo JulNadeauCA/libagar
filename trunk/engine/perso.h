64fdda9b1d20656594291db7e7494d67767529ef
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2003-04-12 01:28:43 +0000

    - switch to netbufs. - position saving is now handled by object-generic layer.

--- trunk/engine/perso.h
+++ trunk/engine/perso.h
@@ -1,39 +1,33 @@
-/*	$Csoft: perso.h,v 1.2 2002/11/22 04:43:45 vedge Exp $	*/
+/*	$Csoft: perso.h,v 1.3 2002/12/13 12:33:16 vedge Exp $	*/
 /*	Public domain	*/
 
 struct perso {
 	struct object	obj;
 
-	char	*name;		/* Character name */
-	Uint32	 level;		/* Current level */
-	Uint32	 exp;		/* Experience */
-	Uint32	 age;		/* Age */
-	Uint32	 seed;		/* Random seed */
-	
-	Uint32	 flags;
-#define PERSO_FOCUSED	0x01		/* Focused */
+	pthread_mutex_t	 lock;
+	char		*name;		/* Character name set by user */
+	Uint32		 flags;
+#define PERSO_FOCUSED	0x01
 #define PERSO_EPHEMERAL	0x00
-
-	Uint32	 maxhp, maxmp;	/* Maximum HP/MP */
-	Uint32	 hp, mp;	/* Effective HP/MP */
-	Uint32	 maxspeed;	/* Max. speed in ms (saved) */
-	Uint32	 nzuars;	/* Money */
-
+	int	 level;			/* Current level */
+	int	 exp;			/* Experience */
+	int	 age;			/* Age */
+	Uint32	 seed;			/* Random seed */
+	int	 maxhp, maxmp;		/* Maximum HP/MP */
+	int	 hp, mp;		/* Effective HP/MP */
+	Uint32	 nzuars;		/* Money */
 	SDL_TimerID	timer;
-
-	pthread_mutex_t	lock;	/* Lock on whole structure */
 };
 
 #define PERSO(ob)	((struct perso *)(ob))
 
-struct perso	*perso_new(char *, char *, Uint32, Uint32);
-void		 perso_init(struct perso *, char *, char *, Uint32, Uint32);
+struct perso	*perso_new(char *, char *, int, int);
+void		 perso_init(struct perso *, char *, char *, int, int);
 void		 perso_destroy(void *);
 void		 perso_destroy(void *);
-int		 perso_load(void *, int);
-int		 perso_save(void *, int);
+int		 perso_load(void *, struct netbuf *);
+int		 perso_save(void *, struct netbuf *);
 void		 perso_attached(int, union evarg *);
 void		 perso_detached(int, union evarg *);
-
 void		 perso_say(struct perso *, const char *, ...);
 
