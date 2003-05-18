1f57e3737e232c762285b7c147cc707e9632e57f
Julien Nadeau Carriere <vedge@hypertriton.com>
2003-05-18 00:20:05 +0000

    typesw[] array
    git-svn-id: https://dev.csoft.net/agar/trunk@2853 86a84d31-9702-0410-8f1e-a60cab7bd424

---
+++ engine/typesw.c
@@ -0,0 +1,47 @@
+/*	$Csoft$	*/
+
+/*
+ * Copyright (c) 2003 CubeSoft Communications, Inc.
+ * <http://www.csoft.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <engine/engine.h>
+#include <engine/typesw.h>
+
+#include <engine/object.h>
+#include <engine/map.h>
+#include <engine/perso.h>
+
+extern const struct object_ops
+	object_ops,
+	map_ops,
+	perso_ops;
+
+const struct object_type typesw[] = {
+	{ "object", "Generic object", sizeof(struct object), &object_ops },
+	{ "map", "Area", sizeof(struct map), &map_ops },
+	{ "perso", "Character", sizeof(struct perso), &perso_ops }
+};
+const int ntypesw = sizeof(typesw) / sizeof(typesw[0]);
+
