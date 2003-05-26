152102b84ab33ebf0c1993ae049d2311095858db
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2003-05-26 03:02:55 +0000

    - size bounded caption string. - get rid of the rh argument to init.

--- trunk/engine/widget/checkbox.h
+++ trunk/engine/widget/checkbox.h
@@ -1,4 +1,4 @@
-/*	$Csoft: checkbox.h,v 1.12 2003/02/02 21:16:15 vedge Exp $	*/
+/*	$Csoft: checkbox.h,v 1.13 2003/04/25 09:47:10 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_WIDGET_CHECKBOX_H_
@@ -8,20 +8,21 @@
 
 #include "begin_code.h"
 
+#define CHECKBOX_CAPTION_MAX	32
+
 struct checkbox {
 	struct widget	wid;
-	char		*caption;	/* Label string */
-	SDL_Surface	*label_s;	/* Label surface */
-	int	 	 cbox_w;	/* Checkbox width */
+	char		 caption[CHECKBOX_CAPTION_MAX];
+	SDL_Surface	*label_s;
 	struct {
 		int	 state;
 	} def;
 };
 
 __BEGIN_DECLS
-extern DECLSPEC struct checkbox	*checkbox_new(struct region *, int,
-				              const char *, ...);
-extern DECLSPEC void		 checkbox_init(struct checkbox *, int, char *);
+extern DECLSPEC struct checkbox	*checkbox_new(struct region *, const char *,
+				              ...);
+extern DECLSPEC void		 checkbox_init(struct checkbox *, char *);
 extern DECLSPEC void		 checkbox_destroy(void *);
 extern DECLSPEC void		 checkbox_draw(void *);
 __END_DECLS
