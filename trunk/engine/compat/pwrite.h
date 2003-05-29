ce724b4a65ab6b4f6c86f028045d288ac86eaf3f
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2003-05-29 01:52:32 +0000

    fix

--- trunk/engine/compat/pwrite.h
+++ trunk/engine/compat/pwrite.h
@@ -1,7 +1,11 @@
-/*	$Csoft$	*/
+/*	$Csoft: pwrite.h,v 1.1 2003/03/13 22:43:54 vedge Exp $	*/
 /*	Public domain	*/
 
 #include <sys/types.h>
 
-ssize_t	Pwrite(int, const void *, size_t, off_t);
+#ifdef HAVE_PWRITE
+#include <unistd.h>
+#else
+ssize_t	pwrite(int, const void *, size_t, off_t);
+#endif
 
