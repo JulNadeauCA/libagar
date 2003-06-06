fd5595339f743fed89f46ffde1ce71b484d669c0
Julien Nadeau Carriere <vedge@hypertriton.com>
2003-06-06 02:50:19 +0000

    rootmap_redraw() inline function.
    git-svn-id: https://dev.csoft.net/agar/trunk@2935 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/rootmap.h
+++ engine/rootmap.h
@@ -1,4 +1,4 @@
-/*	$Csoft: rootmap.h,v 1.7 2002/12/23 03:05:04 vedge Exp $	*/
+/*	$Csoft: rootmap.h,v 1.8 2003/04/25 09:47:05 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_ROOTMAP_H_
@@ -15,6 +15,7 @@ extern DECLSPEC void	 rootmap_draw(void);
 extern DECLSPEC void	 rootmap_focus(struct map *);
 extern DECLSPEC void	 rootmap_center(struct map *, int, int);
 extern DECLSPEC void	 rootmap_scroll(struct map *, int, int);
+extern __inline__ void	 rootmap_redraw(void);
 
 extern DECLSPEC SDL_Rect **rootmap_alloc_maprects(int, int);
 extern DECLSPEC void	   rootmap_free_maprects(struct viewport *);
