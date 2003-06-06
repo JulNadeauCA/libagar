0cd3abdcf1b7f09cc4d6fc4873e0cef6c722934c
Julien Nadeau Carriere <vedge@hypertriton.com>
2003-06-06 02:16:42 +0000

    fixed-size string bufs.
    git-svn-id: https://dev.csoft.net/agar/trunk@2922 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/widget/graph.h
+++ engine/widget/graph.h
@@ -1,4 +1,4 @@
-/*	$Csoft: graph.h,v 1.9 2003/04/12 01:45:49 vedge Exp $	*/
+/*	$Csoft: graph.h,v 1.10 2003/04/25 09:47:10 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_WIDGET_GRAPH_H_
@@ -8,59 +8,61 @@
 
 #include "begin_code.h"
 
+#define GRAPH_CAPTION_MAX	32
+#define GRAPH_ITEM_NAME_MAX	32
+
 struct graph;
 
 struct graph_item {
-	char	*name;		/* Identifier */
-	Uint32	color;		/* Foreground color */
+	char	 name[GRAPH_ITEM_NAME_MAX];	/* Description */
+	Uint32	 color;				/* Line color */
+	Sint32	*vals;				/* Value array */
+	Uint32	 nvals;
+	Uint32	 maxvals;
 
-	Sint32	*vals;		/* Relative values */
-	Uint32	nvals, maxvals;
-
-	struct	graph *graph;	/* Back pointer */
-	TAILQ_ENTRY(graph_item) items;
+	struct graph		*graph;		/* Back pointer */
+	TAILQ_ENTRY(graph_item)	 items;
 };
 
 TAILQ_HEAD(itemq, graph_item);
 
+enum graph_type {
+	GRAPH_POINTS,
+	GRAPH_LINES
+};
+
 struct graph {
-	struct widget	wid;
+	struct widget	 wid;
+	enum graph_type	 type;
+	char		 caption[GRAPH_CAPTION_MAX];
+	int		 flags;
+#define GRAPH_SCROLL	0x01	/* Scroll if the end is not visible */
+#define GRAPH_ORIGIN	0x02	/* Visible origin */
 
-	enum graph_type {
-		GRAPH_POINTS,
-		GRAPH_LINES
-	} type;
-
-	Uint32	 flags;
-#define GRAPH_SCROLL	0x01	/* Scroll when a point is past visible area */
-#define GRAPH_ORIGIN	0x02	/* Visible zero line */
-
-	Sint32	 yrange;	/* Max. value */
-
-	Sint32	 xoffs;		/* Display offset */
-	Uint8	 xinc;		/* X increment */
-	Uint8	 origin_y;	/* Current origin position (%) */
-
-	char	 *caption;	/* Graph description */
-
-	struct itemq	items;	/* Lists of values */
+	Sint32		 yrange;	/* Max. value */
+	Sint32		 xoffs;		/* Display offset */
+	int		 xinc;		/* X increment */
+	int		 origin_y;	/* Origin position (%) */
+	struct itemq	 items;		/* Lists of values */
 };
 
 __BEGIN_DECLS
-extern DECLSPEC struct graph	  *graph_new(struct region *, const char *,
-				             enum graph_type, Uint32, Sint32,
-					     int, int);
-extern DECLSPEC void		   graph_init(struct graph *, const char *,
-				              enum graph_type, Uint32, Sint32,
-					      int, int);
-extern DECLSPEC struct graph_item *graph_add_item(struct graph *, char *,
-				                  Uint32);
+extern DECLSPEC struct graph	  *graph_new(void *, const char *,
+				             enum graph_type, int, Sint32);
+extern DECLSPEC struct graph_item *graph_add_item(struct graph *, const char *,
+				                  Uint8, Uint8, Uint8);
+
+extern DECLSPEC void	 graph_init(struct graph *, const char *,
+			            enum graph_type, int, Sint32);
 extern DECLSPEC void	 graph_destroy(void *);
-extern DECLSPEC void	 graph_draw(void *);
-extern DECLSPEC void	 graph_plot(struct graph_item *, Sint32);
 extern DECLSPEC int	 graph_load(void *, struct netbuf *);
 extern DECLSPEC int	 graph_save(void *, struct netbuf *);
-extern DECLSPEC void	 graph_scroll(struct graph *, int);
+extern DECLSPEC void	 graph_draw(void *);
+extern DECLSPEC void	 graph_scale(void *, int, int);
+
+extern DECLSPEC void	 graph_plot(struct graph_item *, Sint32);
+extern __inline__ void	 graph_scroll(struct graph *, int);
+extern DECLSPEC void	 graph_free_items(struct graph *);
 __END_DECLS
 
 #include "close_code.h"
