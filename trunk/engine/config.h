8d1669aafa7030ca8cf7d6b75d33f30badc4fd89
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2003-06-06 03:20:29 +0000

    use new widget code.

--- trunk/engine/config.h
+++ trunk/engine/config.h
@@ -1,21 +1,24 @@
-/*	$Csoft: config.h,v 1.12 2003/04/12 01:45:31 vedge Exp $	*/
+/*	$Csoft: config.h,v 1.13 2003/04/25 09:47:05 vedge Exp $	*/
 /*	Public domain	*/
 
 #include "begin_code.h"
 
 struct config {
-	struct object	obj;
+	struct object	 obj;
 	struct window	*settings;
+
+	pthread_mutex_t	lock;
+	char		*save_path;		/* Data file save path */
 };
 extern struct config *config;
 
 __BEGIN_DECLS
 extern DECLSPEC struct config	*config_new(void);
 extern DECLSPEC void		 config_init(struct config *);
-extern DECLSPEC int		 config_load(void *, struct netbuf *);
-extern DECLSPEC int		 config_save(void *, struct netbuf *);
 extern DECLSPEC void		 config_destroy(void *);
 extern DECLSPEC void		 config_window(struct config *);
+extern DECLSPEC char		*config_search_file(const char *, const char *,
+				                    const char *);
 __END_DECLS
 
 #include "close_code.h"
