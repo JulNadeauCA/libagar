8fa48dc72a3599d3680bb3ecc70d5437130ab4ca
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2003-06-10 06:58:17 +0000

    document the [hv]box derivates.

--- trunk/engine/widget/box.h
+++ trunk/engine/widget/box.h
@@ -1,4 +1,4 @@
-/*	$Csoft$	*/
+/*	$Csoft: box.h,v 1.1 2003/06/06 02:57:44 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_WIDGET_BOX_H_
@@ -28,14 +28,6 @@ struct box {
 	int		 spacing;	/* Spacing between widgets */
 };
 
-struct hbox {
-	struct box box;
-};
-
-struct vbox {
-	struct box box;
-};
-
 __BEGIN_DECLS
 extern DECLSPEC struct box	*box_new(void *, enum box_type, int);
 extern DECLSPEC void		 box_init(struct box *, enum box_type, int);
@@ -47,24 +39,6 @@ extern DECLSPEC void		 box_set_homogenous(struct box *, int);
 extern DECLSPEC void		 box_set_padding(struct box *, int);
 extern DECLSPEC void		 box_set_spacing(struct box *, int);
 __END_DECLS
-
-#define VBOX_HOMOGENOUS	BOX_HOMOGENOUS
-#define VBOX_WFILL	BOX_WFILL
-#define VBOX_HFILL	BOX_HFILL
-#define HBOX_HOMOGENOUS	BOX_HOMOGENOUS
-#define HBOX_WFILL	BOX_WFILL
-#define HBOX_HFILL	BOX_HFILL
-
-#define hbox_new(p, fl)	(struct hbox *)box_new((p), BOX_HORIZ, (fl))
-#define vbox_new(p, fl)	(struct vbox *)box_new((p), BOX_VERT, (fl))
-#define hbox_init(b, fl) box_init((struct box *)(b), BOX_HORIZ, (fl))
-#define vbox_init(b, fl) box_init((struct box *)(b), BOX_VERT, (fl))
-#define hbox_set_homogenous(b, fl) box_set_homogenous((struct box *)(b), (fl))
-#define vbox_set_homogenous(b, fl) box_set_homogenous((struct box *)(b), (fl))
-#define hbox_set_padding(b, pad) box_set_padding((struct box *)(b), (pad))
-#define vbox_set_padding(b, pad) box_set_padding((struct box *)(b), (pad))
-#define hbox_set_spacing(b, sp) box_set_spacing((struct box *)(b), (sp))
-#define vbox_set_spacing(b, sp) box_set_spacing((struct box *)(b), (sp))
 
 #include "close_code.h"
 #endif /* _AGAR_WIDGET_BOX_H_ */
--- trunk/engine/widget/hbox.h
+++ trunk/engine/widget/hbox.h
@@ -1,4 +1,23 @@
-/*	$Csoft$	*/
+/*	$Csoft: hbox.h,v 1.1 2003/06/06 02:59:06 vedge Exp $	*/
 /*	Public domain	*/
 
+#ifndef _AGAR_WIDGET_HBOX_H_
+#define _AGAR_WIDGET_HBOX_H_
+
 #include <engine/widget/box.h>
+
+struct hbox {
+	struct box box;
+};
+
+#define HBOX_HOMOGENOUS	BOX_HOMOGENOUS
+#define HBOX_WFILL	BOX_WFILL
+#define HBOX_HFILL	BOX_HFILL
+
+#define hbox_new(p, fl)	(struct hbox *)box_new((p), BOX_HORIZ, (fl))
+#define hbox_init(b, fl) box_init((struct box *)(b), BOX_HORIZ, (fl))
+#define hbox_set_homogenous(b, fl) box_set_homogenous((struct box *)(b), (fl))
+#define hbox_set_padding(b, pad) box_set_padding((struct box *)(b), (pad))
+#define hbox_set_spacing(b, sp) box_set_spacing((struct box *)(b), (sp))
+
+#endif /* _AGAR_WIDGET_HBOX_H_ */
