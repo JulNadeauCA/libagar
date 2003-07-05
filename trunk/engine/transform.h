7887786494ee34387e2caf0aa3a9165df7951cd5
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2003-07-05 12:20:56 +0000

    constification

--- trunk/engine/transform.h
+++ trunk/engine/transform.h
@@ -1,4 +1,4 @@
-/*	$Csoft: transform.h,v 1.6 2003/04/25 09:47:05 vedge Exp $	*/
+/*	$Csoft: transform.h,v 1.7 2003/06/18 00:46:58 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_TRANSFORM_H_
@@ -23,9 +23,10 @@ struct transform *transform_new(enum transform_type, int, Uint32 *);
 int		  transform_init(struct transform *, enum transform_type, int,
 		                 Uint32 *);
 int		  transform_load(struct netbuf *, struct transform *);
-void		  transform_save(struct netbuf *, struct transform *);
+void		  transform_save(struct netbuf *, const struct transform *);
 void		  transform_destroy(struct transform *);
-__inline__ int	  transform_compare(struct transform *, struct transform *);
+__inline__ int	  transform_compare(const struct transform *,
+		                    const struct transform *);
 __END_DECLS
 
 #include "close_code.h"
