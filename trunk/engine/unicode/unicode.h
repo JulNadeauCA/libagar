b6d28cf8eee9fbd21389fde68ff3c01b195555a7
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2003-09-05 20:57:43 +0000

    ASCII -> US_ASCII

--- trunk/engine/unicode/unicode.h
+++ trunk/engine/unicode/unicode.h
@@ -1,4 +1,4 @@
-/*	$Csoft: unicode.h,v 1.5 2003/08/31 11:58:09 vedge Exp $	*/
+/*	$Csoft: unicode.h,v 1.6 2003/09/01 07:42:35 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_UNICODE_H_
@@ -6,14 +6,12 @@
 #include "begin_code.h"
 
 enum unicode_conv {
-	UNICODE_FROM_ASCII,		/* ASCII -> UCS-4 */
+	UNICODE_FROM_US_ASCII,		/* US-ASCII -> UCS-4 */
 	UNICODE_FROM_UTF8,		/* UTF-8 -> UCS-4 */
 	UNICODE_TO_UTF8			/* UCS-4 -> UTF-8 */
 };
 
 __BEGIN_DECLS
-void	 unicode_init(void);
-void	 unicode_destroy(void);
 Uint32	*unicode_import(enum unicode_conv, const char *);
 ssize_t	 unicode_export(enum unicode_conv, char *, const Uint32 *, size_t);
 size_t	 ucs4_len(const Uint32 *);
