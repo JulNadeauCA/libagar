b005dbc6a29357483fcc9dd670138f827233fffd
Julien Nadeau Carriere <vedge@hypertriton.com>
2003-09-12 03:09:04 +0000

    allow setting the limit explicitely for dynamically allocated strings.
    git-svn-id: https://dev.csoft.net/agar/trunk@3300 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/loader/string.h
+++ engine/loader/string.h
@@ -1,10 +1,11 @@
-/*	$Csoft$	*/
+/*	$Csoft: string.h,v 1.1 2003/06/19 01:53:38 vedge Exp $	*/
 /*	Public domain	*/
 
 #include "begin_code.h"
 
 __BEGIN_DECLS
-char	*read_string(struct netbuf *);
+char	*read_string_len(struct netbuf *, size_t);
+#define	 read_string(nb) read_string_len((nb), 32767)
 void	 write_string(struct netbuf *, const char *);
 size_t	 copy_string(char *, struct netbuf *, size_t);
 __END_DECLS
