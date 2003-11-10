2084d291f7c0eb9b448c068721cf4becea2e6f1c
Julien Nadeau Carriere <vedge@hypertriton.com>
2003-11-10 22:39:20 +0000

    ucombo: combo widget without textbox.
    git-svn-id: https://dev.csoft.net/agar/trunk@3339 86a84d31-9702-0410-8f1e-a60cab7bd424

---
+++ engine/widget/ucombo.h
@@ -0,0 +1,32 @@
+/*	$Csoft: combo.h,v 1.5 2003/10/14 02:16:29 vedge Exp $	*/
+/*	Public domain	*/
+
+#ifndef _AGAR_WIDGET_UCOMBO_H_
+#define _AGAR_WIDGET_UCOMBO_H_
+
+#include <engine/widget/widget.h>
+#include <engine/widget/button.h>
+#include <engine/widget/window.h>
+#include <engine/widget/tlist.h>
+
+#include "begin_code.h"
+
+struct ucombo {
+	struct widget wid;
+
+	struct button *button;		/* Selection button */
+	struct tlist *list;		/* Item list */
+	struct window *win;		/* Pop-up window */
+	int saved_h;			/* Saved pop-up window height */
+};
+
+__BEGIN_DECLS
+struct ucombo	*ucombo_new(void *);
+
+void	 ucombo_init(struct ucombo *);
+void	 ucombo_scale(void *, int, int);
+void	 ucombo_destroy(void *);
+__END_DECLS
+
+#include "close_code.h"
+#endif /* _AGAR_WIDGET_UCOMBO_H_ */
