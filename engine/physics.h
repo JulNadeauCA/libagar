e3ee4ffa7762e4fd1068ad562309d814d94d77e0
Julien Nadeau Carriere <vedge@hypertriton.com>
2003-11-15 03:54:35 +0000

    so long generic directions. you are of no use now.
    git-svn-id: https://dev.csoft.net/agar/trunk@3349 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/physics.h
+++ engine/physics.h
@@ -1,55 +1,39 @@
-/*	$Csoft: physics.h,v 1.21 2003/04/25 09:47:05 vedge Exp $	    */
+/*	$Csoft: physics.h,v 1.22 2003/06/18 00:46:58 vedge Exp $	    */
 /*	Public domain	*/
 
 #ifndef _AGAR_PHYSICS_H_
 #define _AGAR_PHYSICS_H_
 #include "begin_code.h"
 
-#define DIR_UP		0x01
-#define DIR_DOWN	0x02
-#define DIR_LEFT	0x04
-#define DIR_RIGHT	0x08
-#define DIR_ALL		0xff
+#define DIR_N	0x01
+#define DIR_S	0x02
+#define DIR_W	0x04
+#define DIR_E	0x08
+#define DIR_NW	(DIR_N|DIR_W)
+#define DIR_NE	(DIR_N|DIR_E)
+#define DIR_SW	(DIR_S|DIR_W)
+#define DIR_SE	(DIR_S|DIR_E)
+#define DIR_ALL	0xff
 
 struct object;
-struct map;
 
-/* 2D movement of undefined constructs in an infinite area. */
-struct gendir {
-	int	set;		/* Set direction mask (move) */
-	int	current;	/* Current direction mask (moving) */
-	int	clear;		/* Clear direction mask (stop move) */
-	int	moved;		/* Post direction mask (moved) */
-	int	offs;		/* Timing for repeat */
-	int	noffs;		/* Timing for delay */
-};
-
-/* 2D movement of map references between adjacent map nodes. */
 struct mapdir {
-	int	speed;		/* Soft-scroll increments */
-
 	struct object	*ob;	/* Object back pointer */
-	struct map	*map;	/* Map back pointer */
 
+	int	speed;		/* Increment for object shift and scrolling */
 	int	set;		/* Set direction mask (move) */
 	int	current;	/* Current direction mask (moving) */
 	int	clear;		/* Clear direction mask (stop move) */
 	int	moved;		/* Post direction mask (moved) */
 
 	int	flags;
-#define DIR_SCROLLVIEW	0x01	/* Perform centering/soft-scrolling. */
-#define DIR_SOFTSCROLL	0x02	/* Animate all node->node displacements. */
-#define DIR_PASSTHROUGH	0x08	/* Pass through all nodes. */
+#define DIR_CENTER_VIEW  0x01	/* Center view around object and scroll. */
+#define DIR_SOFT_MOTION  0x02	/* Animate node->node displacements. */
+#define DIR_PASS_THROUGH 0x04	/* Ignore node surface settings. */
 };
 
 __BEGIN_DECLS
-void	gendir_init(struct gendir *);
-void	gendir_set(struct gendir *, int);
-void	gendir_unset(struct gendir *, int);
-int	gendir_move(struct gendir *);
-void	gendir_postmove(struct gendir *, int);
-
-void	mapdir_init(struct mapdir *, struct object *, struct map *, int, int);
+void	mapdir_init(struct mapdir *, struct object *, int, int);
 void	mapdir_set(struct mapdir *, int);
 void	mapdir_unset(struct mapdir *, int);
 int	mapdir_move(struct mapdir *);
