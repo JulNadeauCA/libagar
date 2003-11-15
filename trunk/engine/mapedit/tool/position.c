8fc66b72ae88a8ed1e44596dd3cf77d779395cd1
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2003-11-15 03:58:07 +0000

    beat the positionment system into shape.

--- trunk/engine/mapedit/tool/position.c
+++ trunk/engine/mapedit/tool/position.c
@@ -1,4 +1,4 @@
-/*	$Csoft: position.c,v 1.3 2003/10/13 23:49:00 vedge Exp $	*/
+/*	$Csoft: position.c,v 1.4 2003/10/14 03:15:26 vedge Exp $	*/
 
 /*
  * Copyright (c) 2002, 2003 CubeSoft Communications, Inc.
@@ -34,6 +34,7 @@
 #include <engine/widget/tlist.h>
 #include <engine/widget/checkbox.h>
 #include <engine/widget/combo.h>
+#include <engine/widget/spinbutton.h>
 
 static int	position_cursor(void *, struct mapview *, SDL_Rect *);
 static void	position_effect(void *, struct mapview *, struct map *,
@@ -111,14 +112,15 @@ poll_input_devs(int argc, union evarg *argv)
 static void
 poll_submaps(int argc, union evarg *argv)
 {
-	struct tlist *objs_tl = argv[1].p;
-	struct tlist_item *it;
-	struct object *ob;
+	struct tlist *tl = argv[0].p;
+	struct position *po = argv[1].p;
+	struct object *ob = po->obj;
 
-	if ((it = tlist_item_selected(objs_tl)) == NULL) {
+	if (ob == NULL)
 		return;
-	}
-	ob = it->p1;
+
+	tlist_clear_items(tl);
+	tlist_restore_selections(tl);
 }
 
 void
@@ -127,50 +129,85 @@ position_init(void *p)
 	struct position *po = p;
 	struct window *win;
 	struct box *bo;
-	struct tlist *objs_tl;
+	struct tlist *tl;
 
 	tool_init(&po->tool, "position", &position_ops, MAPEDIT_TOOL_POSITION);
+	po->speed = 1;
+	po->center_view = 0;
+	po->soft_motion = 1;
+	po->pass_through = 0;
 
 	win = TOOL(po)->win = window_new("mapedit-tool-position");
 	window_set_position(win, WINDOW_MIDDLE_LEFT, 0);
 	window_set_caption(win, _("Position"));
 	event_new(win, "window-close", tool_window_close, "%p", po);
 
-	objs_tl = po->objs_tl = tlist_new(win, TLIST_POLL|TLIST_TREE);
-	WIDGET(objs_tl)->flags |= WIDGET_HFILL;
-	tlist_prescale(objs_tl, "XXXXXXXXXXXXXXXX", 5);
-	event_new(objs_tl, "tlist-poll", poll_objs, "%p", world);
+	tl = tlist_new(win, TLIST_POLL|TLIST_TREE);
+	WIDGET(tl)->flags |= WIDGET_HFILL;
+	tlist_prescale(tl, "XXXXXXXXXXXXXXXXX", 5);
+	widget_bind(tl, "selected", WIDGET_POINTER, &po->obj);
+	event_new(tl, "tlist-poll", poll_objs, "%p", world);
 
 	bo = box_new(win, BOX_VERT, BOX_WFILL);
 	{
 		struct checkbox *cb;
+		struct spinbutton *sb;
 		struct combo *com;
 
 		com = combo_new(bo, COMBO_POLL, _("Submap: "));
-		event_new(com->list, "tlist-poll", poll_submaps, "%p", objs_tl);
-		po->submaps_tl = com->list;
+		event_new(com->list, "tlist-poll", poll_submaps, "%p", po);
+		widget_bind(com->list, "selected", WIDGET_POINTER, &po->submap);
 
 		com = combo_new(bo, COMBO_POLL, _("Input device: "));
 		event_new(com->list, "tlist-poll", poll_input_devs, NULL);
-		po->inputs_tl = com->list;
+		widget_bind(com->list, "selected", WIDGET_POINTER,
+		    &po->input_dev);
+	
+		sb = spinbutton_new(bo, _("Speed: "));
+		widget_bind(sb, "value", WIDGET_INT, &po->speed);
+		spinbutton_set_min(sb, 1);
+		spinbutton_set_max(sb, 16);
+		spinbutton_set_increment(sb, 1);
 		
-		cb = checkbox_new(bo, _("Center view"));
+		cb = checkbox_new(bo, _("Center view around"));
 		widget_bind(cb, "state", WIDGET_BOOL, &po->center_view);
+		
+		cb = checkbox_new(bo, _("Soft motion"));
+		widget_bind(cb, "state", WIDGET_BOOL, &po->soft_motion);
+		
+		cb = checkbox_new(bo, _("Pass through"));
+		widget_bind(cb, "state", WIDGET_BOOL, &po->pass_through);
 	}
 }
 
 static void
 position_effect(void *p, struct mapview *mv, struct map *m, struct node *dn)
 {
-//	struct position *po = p;
+	struct position *po = p;
+	struct object *ob = po->obj;
+	int dirflags = 0;
 
-	
+	if (ob == NULL)
+		return;
+
+	dprintf("position `%s'\n", ob->name);
+	object_set_position(ob, mv->map, mv->cx, mv->cy, mv->map->cur_layer);
+	object_set_submap(ob, po->submap);
+	ob->pos->input = po->input_dev;
+
+	if (po->center_view)
+		dirflags |= DIR_CENTER_VIEW;
+	if (po->soft_motion)
+		dirflags |= DIR_SOFT_MOTION;
+	if (po->pass_through)
+		dirflags |= DIR_PASS_THROUGH;
+
+	object_set_direction(ob, DIR_S, dirflags, po->speed);
 }
 
 static int
 position_cursor(void *p, struct mapview *mv, SDL_Rect *rd)
 {
-	/* xxx */
 	return (-1);
 }
 
