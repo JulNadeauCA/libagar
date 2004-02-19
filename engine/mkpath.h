866db3b17c3949142e3155aee5039ebe55276970
Julien Nadeau Carriere <vedge@hypertriton.com>
2004-02-19 03:53:55 +0000

    fix comment
    git-svn-id: https://dev.csoft.net/agar/trunk@3445 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/mkpath.h
+++ engine/mkpath.h
@@ -1,4 +1,4 @@
-/*	$Csoft: mkpath.h,v 1.1 2003/06/02 00:10:55 vedge Exp $	*/
+/*	$Csoft: mkpath.h,v 1.2 2003/06/18 00:46:58 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_MKPATH_H_
@@ -10,4 +10,4 @@ int	mkpath(char *, mode_t, mode_t);
 __END_DECLS
 
 #include "close_code.h"
-#endif /* _AGAR_PHYSICS_H_ */
+#endif /* _AGAR_MKPATH_H_ */
