b787b1a6beb2590b9477b8d8e14c31bf0860bab7
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2004-02-26 09:19:10 +0000

    move

--- trunk/engine/compat/strsep.h
+++
@@ -1,9 +0,0 @@
-/*	$Csoft: strsep.h,v 1.2 2002/09/06 00:50:43 vedge Exp $	*/
-/*	Public domain	*/
-
-#include <config/have_strsep.h>
-
-#ifndef HAVE_STRSEP
-char	*strsep(char **, const char *);
-#endif
-
