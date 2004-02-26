96d9533da5eaddeed5a3a13870db56e01225a7fb
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2004-02-26 10:14:36 +0000

    use a velocity vector.

--- trunk/engine/position.c
+++ trunk/engine/position.c
@@ -1,4 +1,4 @@
-/*	$Csoft$	*/
+/*	$Csoft: position.c,v 1.1 2004/02/20 04:20:33 vedge Exp $	*/
 
 /*
  * Copyright (c) 2004 CubeSoft Communications, Inc.
@@ -32,14 +32,18 @@
 #include <engine/input.h>
 #include <engine/view.h>
 
+#include <compat/math.h>
+
 #ifdef DEBUG
 #define DEBUG_STATE	0x01
 #define DEBUG_POSITION	0x02
 #define DEBUG_CONTROL	0x04
 #define DEBUG_MOVE	0x08
-#define DEBUG_BLOCKS	0x10
+#define DEBUG_COLLISION	0x10
+#define DEBUG_VELVEC	0x20
+#define DEBUG_PROJECTION 0x40
 int	position_debug = DEBUG_STATE|DEBUG_POSITION|DEBUG_CONTROL|DEBUG_MOVE|
-			 DEBUG_BLOCKS;
+			 DEBUG_COLLISION|DEBUG_VELVEC|DEBUG_PROJECTION;
 #define engine_debug position_debug
 #endif
 
@@ -54,7 +58,9 @@ position_init(struct position *pos)
 	pos->projmap = NULL;
 	pos->projmap_name = NULL;
 	pos->input = NULL;
-	mapdir_init(&pos->dir, NULL, 0, 0);
+	pos->flags = 0;
+	pos->dir = 0;
+	pos->vel = 0;
 }
 
 void
@@ -63,51 +69,52 @@ position_destroy(struct position *pos)
 	position_unproject(pos);
 	Free(pos->map_name);
 	Free(pos->projmap_name);
-	mapdir_destroy(&pos->dir);
 }
 
-/* Assign a position to an object. */
+/* Assign position m:[x,y] to an object and select the projection map. */
 int
-position_set(void *p, struct map *dstmap, int x, int y, int z,
-    struct map *projmap)
+position_set(void *p, struct map *m, int x, int y, int z, struct map *projmap)
 {
 	struct object *ob = p;
+	struct position *pos = ob->pos;
 
 	pthread_mutex_lock(&ob->lock);
-	dprintf("%s: position -> %s:[%d,%d,%d]\n", ob->name,
-	    OBJECT(dstmap)->name, x, y, z);
-
-	if (ob->pos != NULL) {
-		debug(DEBUG_POSITION, "%s: updating position\n", ob->name);
-		position_unproject(ob->pos);
-	} else {
-		debug(DEBUG_POSITION, "%s: creating position\n", ob->name);
-		ob->pos = Malloc(sizeof(struct position));
-		position_init(ob->pos);
-		ob->pos->projmap = projmap;
-		mapdir_init(&ob->pos->dir, ob, DIR_SOFT_MOTION, 1);
-	}
-
-	pthread_mutex_lock(&dstmap->lock);
-	if (ob->pos->x >= 0 && ob->pos->x < dstmap->mapw &&
-	    ob->pos->y >= 0 && ob->pos->y < dstmap->maph &&
-	    z >= 0 && z < dstmap->nlayers) {
-		ob->pos->map = dstmap;
-		ob->pos->x = x;
-		ob->pos->y = y;
-		ob->pos->z = z;
-		position_project(ob->pos);
-	} else {
+	pthread_mutex_lock(&m->lock);
+	if (x < 0 || x > m->mapw ||
+	    y < 0 || y > m->maph ||
+	    z < 0 || z > m->nlayers) {
 		error_set(_("Illegal coordinates: %s[%d,%d,%d]"),
-		    OBJECT(dstmap)->name, x, y, z);
+		    OBJECT(m)->name, x, y, z);
 		goto fail;
 	}
-	pthread_mutex_unlock(&dstmap->lock);
+	
+	if (pos == NULL) {
+		debug(DEBUG_POSITION, "%s: creating position (proj=%s)\n",
+		    ob->name, OBJECT(projmap)->name);
+		pos = ob->pos = Malloc(sizeof(struct position));
+		position_init(pos);
+	} else {
+		debug(DEBUG_POSITION, "%s: updating position\n", ob->name);
+		position_unproject(pos);
+	}
+	
+
+	debug(DEBUG_POSITION, "%s: position -> %s:[%d,%d,%d]\n", ob->name,
+	    OBJECT(m)->name, x, y, z);
+	pos->map = m;
+	pos->map_name = Strdup(OBJECT(m)->name);
+	pos->projmap = projmap;
+	pos->projmap_name = Strdup(OBJECT(projmap)->name);
+	pos->x = x;
+	pos->y = y;
+	pos->z = z;
+	position_project(pos);
+	pthread_mutex_unlock(&m->lock);
 	pthread_mutex_unlock(&ob->lock);
 	return (0);
 fail:
 	position_unset(ob);
-	pthread_mutex_unlock(&dstmap->lock);
+	pthread_mutex_unlock(&m->lock);
 	pthread_mutex_unlock(&ob->lock);
 	return (-1);
 }
@@ -156,7 +163,7 @@ fail:
 	return (-1);
 }
 
-/* Define the projection of an object. */
+/* Select the projection map of an object by name. */
 int
 position_set_projmap(void *p, const char *name)
 {
@@ -165,7 +172,7 @@ position_set_projmap(void *p, const char *name)
 
 	pthread_mutex_lock(&ob->lock);
 	
-	dprintf("%s: %s -> %s\n", ob->name,
+	debug(DEBUG_PROJECTION, "%s: %s -> %s\n", ob->name,
 	    ob->pos != NULL ? OBJECT(ob->pos->projmap)->name : "none", name);
 
 	TAILQ_FOREACH(projmap, &ob->children, cobjs) {
@@ -180,6 +187,7 @@ position_set_projmap(void *p, const char *name)
 		position_unproject(ob->pos);
 	}
 	ob->pos->projmap = (struct map *)projmap;
+	ob->pos->projmap_name = Strdup(name);
 
 	pthread_mutex_unlock(&ob->lock);
 	return (0);
@@ -196,9 +204,6 @@ position_project(struct position *pos)
 	struct map *dm = pos->map;
 	int sx, sy, dx, dy;
 
-	dprintf("[%d,%d,%d]+%dx%d\n", pos->x, pos->y, pos->z, sm->mapw,
-	    sm->maph);
-	
 	object_add_dep(dm, sm);
 	object_page_in(sm, OBJECT_DATA);
 	object_page_in(dm, OBJECT_DATA);
@@ -224,9 +229,6 @@ position_unproject(struct position *pos)
 {
 	int x, y;
 	
-	dprintf("[%d,%d,%d]+%dx%d\n", pos->x, pos->y, pos->z,
-	    pos->projmap->mapw, pos->projmap->maph);
-	
 	object_page_in(pos->map, OBJECT_DATA);
 	for (y = pos->y; y < pos->y+pos->projmap->maph; y++) {
 		for (x = pos->x; x < pos->x+pos->projmap->mapw; x++)
@@ -236,45 +238,46 @@ position_unproject(struct position *pos)
 }
 
 /*
- * Set the direction of an object inside a map.
- * A position must exist.
+ * Set the velocity vector for an object.
+ * An object position must exist.
  */
-int
-position_set_direction(void *p, int dir, int dirflags, int speed)
+void
+position_set_velvec(void *p, int dir, int vel)
 {
 	struct object *ob = p;
+	struct position *pos = ob->pos;
 
 	pthread_mutex_lock(&ob->lock);
-
-	if (ob->pos == NULL) {
-		error_set(_("Object `%s' has no position."), ob->name);
-		goto fail;
+	if (dir >= 0) {
+		debug(DEBUG_VELVEC, "%s: direction -> %d\n", ob->name, dir);
+		pos->dir = (Uint8)dir;
 	}
-	debug(DEBUG_POSITION, "%s: direction -> %d/%d\n", ob->name, dir, speed);
-	mapdir_init(&ob->pos->dir, ob, dirflags, speed);
-
+	if (vel >= 0) {
+		debug(DEBUG_VELVEC, "%s: velocity -> %d\n", ob->name, vel);
+		pos->vel = (Uint8)vel;
+	}
 	pthread_mutex_unlock(&ob->lock);
-	return (0);
-fail:
-	pthread_mutex_unlock(&ob->lock);
-	return (-1);
 }
 
 int
 position_save(struct position *pos, struct netbuf *buf)
 {
 	char map_id[OBJECT_NAME_MAX];
+	char projmap_id[OBJECT_NAME_MAX];
 
 	object_copy_name(pos->map, map_id, sizeof(map_id));
+	object_copy_name(pos->projmap, projmap_id, sizeof(projmap_id));
+
 	write_string(buf, map_id);
-	write_string(buf, OBJECT(pos->projmap)->name);
-	write_string(buf, (pos->input != NULL) ?
-	    OBJECT(pos->input)->name : "");
+	write_string(buf, projmap_id);
+	write_string(buf, (pos->input != NULL) ? OBJECT(pos->input)->name : "");
+
 	write_uint32(buf, (Uint32)pos->x);
 	write_uint32(buf, (Uint32)pos->y);
-	write_uint8(buf, (Uint8)pos->z);
-
-	/* XXX save direction */
+	write_uint32(buf, (Uint32)pos->z);
+	write_uint32(buf, (Uint32)pos->dir);
+	write_uint32(buf, (Uint32)pos->vel);
+	write_uint32(buf, (Uint32)pos->flags);
 }
 
 int
@@ -282,9 +285,10 @@ position_load(struct object *ob, struct position *pos, struct netbuf *buf)
 {
 	char map_name[OBJECT_PATH_MAX];
 	char projmap_name[OBJECT_PATH_MAX];
-	char input_name[OBJECT_PATH_MAX];
+	char input_name[OBJECT_NAME_MAX];
 	struct map *map, *projmap;
 	int x, y, z;
+	int dir, vel, flags;
 
 	if (ob->pos != NULL)
 		position_unset(ob);
@@ -295,10 +299,14 @@ position_load(struct object *ob, struct position *pos, struct netbuf *buf)
 
 	x = (int)read_uint32(buf);
 	y = (int)read_uint32(buf);
-	z = (int)read_uint8(buf);
+	z = (int)read_uint32(buf);
+	dir = (int)read_uint32(buf);
+	vel = (int)read_uint32(buf);
+	flags = (int)read_uint32(buf);
 		
-	debug(DEBUG_STATE, "%s: at %s:[%d,%d,%d], as %s\n", ob->name,
-	    map_name, x, y, z, projmap_name);
+	debug(DEBUG_STATE,
+	    "%s: at %s:[%d,%d,%d] proj %s dir %d vel %d flags 0x%x\n",
+	    ob->name, map_name, x, y, z, projmap_name, dir, vel, flags);
 
 	if ((map = object_find(map_name)) == NULL) {
 		error_set(_("No such level map: `%s'"), map_name);
@@ -310,7 +318,7 @@ position_load(struct object *ob, struct position *pos, struct netbuf *buf)
 	}
 
 	pthread_mutex_lock(&map->lock);
-	if (position_set(ob, map, x, y, z, projmap) == NULL) {
+	if (position_set(ob, map, x, y, z, projmap) == -1) {
 		goto fail;
 	}
 	if (input_name != NULL &&
@@ -322,332 +330,5 @@ position_load(struct object *ob, struct position *pos, struct netbuf *buf)
 fail:
 	pthread_mutex_unlock(&map->lock);
 	return (-1);
-}
-
-void
-mapdir_init(struct mapdir *dir, struct object *ob, int flags, int speed)
-{
-	dir->set = 0;
-	dir->current = 0;
-	dir->clear = 0;
-	dir->moved = 0;
-	dir->flags = flags;
-	dir->speed = speed;
-	dir->ob = ob;
-}
-
-void
-mapdir_destroy(struct mapdir *dir)
-{
-}
-
-void
-mapdir_set(struct mapdir *dir, int direction)
-{
-	dir->clear &= ~direction;
-	dir->set   |=  direction;
-}
-
-void
-mapdir_unset(struct mapdir *dir, int direction)
-{
-	dir->clear |=  direction;
-	dir->set   &= ~direction;
-}
-
-static void
-mapdir_set_motion(struct mapdir *dir, int xmotion, int ymotion)
-{
-	struct position *pos = dir->ob->pos;
-	int x, y;
-
-	for (y = pos->y; y < pos->y+pos->projmap->maph; y++) {
-		for (x = pos->x; x < pos->x+pos->projmap->mapw; x++) {
-			struct node *node = &pos->map->map[y][x];
-			struct noderef *r;
-
-			TAILQ_FOREACH(r, &node->nrefs, nrefs) {
-				if (r->layer != pos->z) {
-					continue;
-				}
-				r->r_gfx.xmotion = xmotion;
-				r->r_gfx.ymotion = ymotion;
-			}
-		}
-	}
-}
-
-static void
-mapdir_add_motion(struct mapdir *dir, int xmotion, int ymotion)
-{
-	struct position *pos = dir->ob->pos;
-	int x, y;
-
-	for (y = pos->y; y < pos->y+pos->projmap->maph; y++) {
-		for (x = pos->x; x < pos->x+pos->projmap->mapw; x++) {
-			struct node *node = &pos->map->map[y][x];
-			struct noderef *r;
-
-			TAILQ_FOREACH(r, &node->nrefs, nrefs) {
-				if (r->layer != pos->z) {
-					continue;
-				}
-				r->r_gfx.xmotion += xmotion;
-				r->r_gfx.ymotion += ymotion;
-			}
-		}
-	}
-}
-
-/* Update directions assuming the object is not already moving. */
-static void
-mapdir_update_idle(struct mapdir *dir)
-{
-	if (dir->set == 0)
-		return;
-
-	if (dir->set & DIR_N) {
-		position_set_projmap(dir->ob, "n-move");
-		mapdir_set_motion(dir, 0, -1);
-		dir->current |= DIR_N;
-		dir->current &= ~(DIR_S|DIR_W|DIR_W);
-	}
-	if (dir->set & DIR_S) {
-		position_set_projmap(dir->ob, "s-move");
-		mapdir_set_motion(dir, 0, 1);
-		dir->current |= DIR_S;
-		dir->current &= ~(DIR_N|DIR_W|DIR_W);
-	}
-	if (dir->set & DIR_W) {
-		position_set_projmap(dir->ob, "w-move");
-		mapdir_set_motion(dir, -1, 0);
-		dir->current |= DIR_W;
-		dir->current &= ~(DIR_W|DIR_N|DIR_S);
-	}
-	if (dir->set & DIR_E) {
-		position_set_projmap(dir->ob, "e-move");
-		mapdir_set_motion(dir, 1, 0);
-		dir->current |= DIR_W;
-		dir->current &= ~(DIR_W|DIR_N|DIR_S);
-	}
-	dir->set = 0;
-}
-
-/* See if the object can move to m:x,y. */
-static int
-mapdir_can_move(struct mapdir *dir, struct map *dstmap, int x, int y)
-{
-	struct object *ob = dir->ob;
-	struct map *projmap = ob->pos->projmap;
-	struct node *node = &dstmap->map[y][x];
-	struct noderef *r;
-
-	if (x < 0 || y < 0 ||
-	    x+projmap->mapw >= dstmap->mapw ||
-	    y+projmap->maph >= dstmap->maph) {
-		error_set("map boundary");
-		return (0);
-	}
-
-	if (dir->flags & DIR_PASS_THROUGH) {
-		return (1);
-	}
-	TAILQ_FOREACH(r, &node->nrefs, nrefs) {
-		if (ob->pos->z == r->layer &&
-		   ((r->flags & NODEREF_WALK)) == 0) {
-			error_set("nref block");
-			return (0);
-		}
-	}
-	return (1);
-}
-
-static void
-mapdir_update_moving(struct mapdir *dir, int xmotion, int ymotion)
-{
-	struct object *ob = dir->ob;
-	struct position *pos = ob->pos;
-	struct map *map = pos->map;
-	int moved = 0;
-	
-	debug(DEBUG_MOVE, "%s: motion %d,%d\n", ob->name, xmotion, ymotion);
-
-	if (!mapdir_can_move(dir, map, pos->x+xmotion, pos->y+ymotion)) {
-		debug(DEBUG_BLOCKS, "%s: block (%s)\n", ob->name, error_get());
-		mapdir_set_motion(dir, 0, 0);
-		mapdir_unset(dir, DIR_ALL);
-		return;
-	}
-
-	if (ymotion < 0) {						/* Up */
-		if ((dir->flags & DIR_CENTER_VIEW) && (view->rootmap != NULL))
-		    	rootmap_scroll(map, DIR_N, dir->speed);
-		if (ymotion <= -TILEW+dir->speed) {
-			dir->moved |= DIR_N;
-			moved |= DIR_N;
-			if (--pos->y < 0)
-				pos->y = 0;
-		} else {
-			if (dir->flags & DIR_SOFT_MOTION) {
-				mapdir_add_motion(dir, 0, -dir->speed);
-			} else {
-				mapdir_set_motion(dir, 0, -TILEH);
-			}
-		}
-	} else if (ymotion > 0) {				     /* Down */
-		if ((dir->flags & DIR_CENTER_VIEW) && (view->rootmap != NULL))
-		    	rootmap_scroll(map, DIR_S, dir->speed);
-		if (ymotion >= TILEW-dir->speed) {
-			mapdir_set_motion(dir, 0, 1);
-			dir->moved |= DIR_S;
-			moved |= DIR_S;
-			if (++pos->y >= map->maph)
-				pos->y = map->maph-1;
-		} else {
-			if (dir->flags & DIR_SOFT_MOTION) {
-				mapdir_add_motion(dir, 0, dir->speed);
-			} else {
-				mapdir_set_motion(dir, 0, TILEH);
-			}
-		}
-	}
-
-	if (xmotion < 0) {					    /* Left */
-		if ((dir->flags & DIR_CENTER_VIEW) && (view->rootmap != NULL))
-		    	rootmap_scroll(map, DIR_W, dir->speed);
-		if (xmotion <= -TILEW+dir->speed) {
-			mapdir_set_motion(dir, -1, 0);
-			dir->moved |= DIR_W;
-			moved |= DIR_W;
-			if (--pos->x < 0)
-				pos->x = 0;
-		} else {
-			if (dir->flags & DIR_SOFT_MOTION) {
-				mapdir_add_motion(dir, -dir->speed, 0);
-			} else {
-				mapdir_set_motion(dir, -TILEW, 0);
-			}
-		}
-	} else if (xmotion > 0) {				   /* Right */
-		if ((dir->flags & DIR_CENTER_VIEW) && (view->rootmap != NULL))
-		    	rootmap_scroll(map, DIR_W, dir->speed);
-		if (xmotion >= TILEW-dir->speed) {
-			mapdir_set_motion(dir, 1, 0);
-			dir->moved |= DIR_W;
-			moved |= DIR_W;
-			if (++pos->x >= map->mapw)
-				pos->x = map->mapw-1;
-		} else {
-			if (dir->flags & DIR_SOFT_MOTION) {
-				mapdir_add_motion(dir, dir->speed, 0);
-			} else {
-				mapdir_set_motion(dir, TILEW, 0);
-			}
-		}
-	}
-}
-
-/* Update a map direction and move its associated object if necessary. */
-int
-mapdir_move(struct mapdir *dir)
-{
-	struct position *pos;
-	int x, y;
-
-	pthread_mutex_lock(&dir->ob->lock);
-	pos = dir->ob->pos;
-
-	pthread_mutex_lock(&pos->map->lock);
-	pthread_mutex_lock(&pos->projmap->lock);
-
-	dprintf("%s at %s:[%d,%d]+%dx%d\n", dir->ob->name,
-	    OBJECT(pos->projmap)->name, pos->x, pos->y,
-	    pos->projmap->mapw, pos->projmap->maph);
-
-	if (pos->x < 0 || pos->y < 0 ||
-	    pos->x+pos->projmap->mapw >= pos->map->mapw ||
-	    pos->y+pos->projmap->maph >= pos->map->maph ||
-	    pos->z >= pos->map->nlayers) {
-		error_set("bad coords");
-		goto fail;
-	}
-
-	for (y = pos->y; y < pos->y+pos->projmap->maph; y++) {
-		for (x = pos->x; x < pos->x+pos->projmap->mapw; x++) {
-			struct node *node = &pos->map->map[y][x];
-			struct noderef *r;
-			
-			TAILQ_FOREACH(r, &node->nrefs, nrefs) {
-				if (r->layer != pos->z)
-					continue;
-
-				if (r->r_gfx.ymotion == 0 &&
-				    r->r_gfx.xmotion == 0) {
-					mapdir_update_idle(dir);
-				} else {
-					mapdir_update_moving(dir,
-					    r->r_gfx.xmotion,
-					    r->r_gfx.ymotion);
-				}
-				/*
-				 * Other nrefs in this rectangle/layer are
-				 * assumed to have the same [xy]motion.
-				 */
-				goto out;
-			}
-		}
-	}
-out:
-	pthread_mutex_unlock(&pos->projmap->lock);
-	pthread_mutex_unlock(&pos->map->lock);
-	pthread_mutex_unlock(&dir->ob->lock);
-	return (0);
-fail:
-	pthread_mutex_unlock(&pos->projmap->lock);
-	pthread_mutex_unlock(&pos->map->lock);
-	pthread_mutex_unlock(&dir->ob->lock);
-	return (-1);
-}
-
-/*
- * Called after a movement, to ensure continuation if necessary, or
- * stop moving.
- */
-void
-mapdir_postmove(struct mapdir *dir, int *mapx, int *mapy, int moved)
-{
-	if (dir->clear & DIR_N) {
-		dir->current &= ~DIR_N;
-		position_set_projmap(dir->ob, "n-idle");
-	}
-	if (dir->clear & DIR_S) {
-		dir->current &= ~DIR_S;
-		position_set_projmap(dir->ob, "s-idle");
-	}
-	if (dir->clear & DIR_W) {
-		dir->current &= ~DIR_W;
-		position_set_projmap(dir->ob, "w-idle");
-	}
-	if (dir->clear & DIR_W) {
-		dir->current &= ~DIR_W;
-		position_set_projmap(dir->ob, "e-idle");
-	}
-	dir->clear = 0;
-	mapdir_set_motion(dir, 0, 0);
-
-	if (dir->current != 0) {
-		if (dir->set == 0) {
-			/* Continue moving in the same direction. */
-			dir->set = moved;
-		} else {
-			/* Change directions. */
-		}
-
-		/* Apply direction changes. */
-		mapdir_update_idle(dir);
-
-		dir->current |= dir->set;
-		dir->set = 0;
-	}
 }
 
--- trunk/engine/position.h
+++ trunk/engine/position.h
@@ -1,50 +1,27 @@
-/*	$Csoft: object.h,v 1.101 2004/01/22 09:58:42 vedge Exp $	*/
+/*	$Csoft: position.h,v 1.1 2004/02/20 04:20:33 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_POSITION_H_
 #define _AGAR_POSITION_H_
 #include "begin_code.h"
 
-#define DIR_N	0x01
-#define DIR_S	0x02
-#define DIR_W	0x04
-#define DIR_E	0x08
-#define DIR_NW	(DIR_N|DIR_W)
-#define DIR_NE	(DIR_N|DIR_E)
-#define DIR_SW	(DIR_S|DIR_W)
-#define DIR_SE	(DIR_S|DIR_E)
-#define DIR_ALL	0xff
-
 struct object;
 struct map;
 struct input;
 
-/* Object direction on a map */
-struct mapdir {
-	struct object	*ob;	/* Object back pointer */
-
-	int	speed;		/* Increment for object shift and scrolling */
-	int	set;		/* Set direction mask (move) */
-	int	current;	/* Current direction mask (moving) */
-	int	clear;		/* Clear direction mask (stop move) */
-	int	moved;		/* Post direction mask (moved) */
-
-	int	flags;
-#define DIR_CENTER_VIEW  0x01	/* Center view around object and scroll. */
-#define DIR_SOFT_MOTION  0x02	/* Animate node->node displacements. */
-#define DIR_PASS_THROUGH 0x04	/* Ignore node surface settings. */
-};
-
 /* Unique object position on a map. */
 struct position {
 	struct map	*map;		/* Map (NULL if unresolved) */
-	char		*map_name;	/* Map reference to resolve */
+	char		*map_name;	/* Map reference */
+	struct map	*projmap;	/* Projection map (NULL if invisible) */
+	char		*projmap_name;	/* Projection map reference */
+	struct input	*input;		/* Controller (NULL if uncontrolled) */
 	int		 x, y, z;	/* Map coordinates */
-	struct map	*projmap;	/* Current projection map */
-	char		*projmap_name;	/* Submap reference to resolve */
-	struct input	*input;		/* Controller (or NULL) */
-	struct mapdir	 dir;		/* Direction */
-	int		 vel;		/* Velocity factor */
+	Uint8		 dir, vel;	/* Velocity vector */
+	Uint8		 flags;
+#define POSITION_CENTER_VIEW  0x01	/* Center view around object/scroll. */
+#define POSITION_SOFT_MOTION  0x02	/* Animate projection displacements. */
+#define POSITION_PASS_THROUGH 0x04	/* Ignore node friction attribute. */
 };
 
 __BEGIN_DECLS
@@ -55,19 +32,12 @@ int	 position_load(struct object *, struct position *, struct netbuf *);
 
 int	 position_set_input(void *, const char *);
 int	 position_set_projmap(void *, const char *);
-int	 position_set_direction(void *, int, int, int);
+void	 position_set_velvec(void *, int, int);
 
 int	 position_set(void *, struct map *, int, int, int, struct map *);
 void	 position_unset(void *);
 void	 position_project(struct position *);
 void	 position_unproject(struct position *);
-
-void	mapdir_init(struct mapdir *, struct object *, int, int);
-void	mapdir_destroy(struct mapdir *);
-void	mapdir_set(struct mapdir *, int);
-void	mapdir_unset(struct mapdir *, int);
-int	mapdir_move(struct mapdir *);
-void	mapdir_postmove(struct mapdir *, int *, int *, int);
 __END_DECLS
 
 #include "close_code.h"
