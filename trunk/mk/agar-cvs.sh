bbb3fdcc5f5e769eddb9aecae399d5b1bb8e1671
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2004-03-13 09:26:44 +0000

    rien

--- trunk/mk/agar-cvs.sh
+++ trunk/mk/agar-cvs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#	$Csoft$
+#	$Csoft: agar-cvs.sh,v 1.1 2003/03/14 07:54:30 vedge Exp $
 
 TMPDIR=/hack/vedge/.tmp
 CVSREPO=/home/cvs/agar
@@ -9,5 +9,5 @@ CVSREPO=/home/cvs/agar
  tar -f- -c . | gzip -9 - > $TMPDIR/agar-cvs.tar.gz &&
  scp -qC $TMPDIR/agar-cvs.tar.gz vedge@resin:www/agar/cvs &&
  rm -f $TMPDIR/agar-cvs.tar.gz &&
- ssh vedge@resin 'cd ~/www/agar/cvs && tar -xzf agar-cvs.tar.gz && rm -f agar-cvs.tar.gz')
+ ssh vedge@resin 'cd ~/www/agar/cvs && tar -xzf agar-cvs.tar.gz && rm -f agar-cvs.tar.gz; /bin/sh ~vedge/bin/agar.sh')
 
