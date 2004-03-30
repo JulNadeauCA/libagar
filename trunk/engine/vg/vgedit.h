d61fb53714d28ed3e7d7a31b1c4cb4b141813301
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2004-03-30 16:05:42 +0000

    simple vector drawing object with edition.

---
+++ trunk/engine/vg/vgedit.h
@@ -0,0 +1,25 @@
+/*	$Csoft: vgedit.h,v 1.16 2003/09/02 02:07:48 vedge Exp $	*/
+/*	Public domain	*/
+
+#ifndef _AGAR_VGEDIT_H_
+#define _AGAR_VGEDIT_H_
+
+#include <engine/vg/vg.h>
+
+#include "begin_code.h"
+
+struct vgedit {
+	struct object obj;
+	struct vg *vg;
+};
+
+__BEGIN_DECLS
+void		 vgedit_init(void *, const char *);
+void		 vgedit_destroy(void *);
+struct window	*vgedit_edit(void *);
+int		 vgedit_load(void *, struct netbuf *);
+int		 vgedit_save(void *, struct netbuf *);
+__END_DECLS
+
+#include "close_code.h"
+#endif	/* _AGAR_VGEDIT_H_ */
