eb0bfda419eada463ea2c8897b57413278326ca8
Julien Nadeau Carriere <vedge@hypertriton.com>
2004-04-19 02:15:17 +0000

    VG_NORIGINS
    git-svn-id: https://dev.csoft.net/agar/trunk@3703 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/vg/vg_origin.h
+++ engine/vg/vg_origin.h
@@ -1,9 +1,11 @@
-/*	$Csoft$	*/
+/*	$Csoft: vg_origin.h,v 1.1 2004/04/17 00:43:39 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_VG_ORIGIN_H_
 #define _AGAR_VG_ORIGIN_H_
 #include "begin_code.h"
+
+#define VG_NORIGINS	3
 
 __BEGIN_DECLS
 void		vg_origin2(struct vg *, int, double, double);
