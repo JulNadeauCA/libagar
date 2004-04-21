377a3272542edd774e2c14609f44e11f13100315
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2004-04-21 00:15:25 +0000

    use portable types; spotted by Bluelive.

--- trunk/compat/arc4random.h
+++ trunk/compat/arc4random.h
@@ -1,13 +1,13 @@
-/*	$Csoft: arc4random.h,v 1.1 2003/12/31 01:55:15 vedge Exp $	*/
+/*	$Csoft: arc4random.h,v 1.1 2004/02/26 09:19:37 vedge Exp $	*/
 /*	Public domain	*/
 
 #include <config/have_arc4random.h>
 
 #ifndef HAVE_ARC4RANDOM
 #include <sys/types.h>
-u_int32_t	arc4random(void);
-void		arc4random_stir(void);
-void		arc4random_addrandom(u_char *, int);
+Uint32	arc4random(void);
+void	arc4random_stir(void);
+void	arc4random_addrandom(unsigned char *, int);
 #else
 #include <sys/types.h>
 #include <stdlib.h>
