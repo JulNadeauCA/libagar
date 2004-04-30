427b04278ad02557f05870e3aaa501c8ef41becd
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2004-04-30 12:18:14 +0000

    unusedunusedunusedunused

--- trunk/engine/vg/vg_point.h
+++ trunk/engine/vg/vg_point.h
@@ -1,4 +1,4 @@
-/*	$Csoft: vg_point.h,v 1.2 2004/04/22 01:45:46 vedge Exp $	*/
+/*	$Csoft: vg_point.h,v 1.3 2004/04/22 12:36:09 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_VG_POINTS_H_
@@ -7,8 +7,6 @@
 
 __BEGIN_DECLS
 void		vg_point_init(struct vg *, struct vg_element *);
-__inline__ void	vg_point_radius(struct vg *, double);
-__inline__ void	vg_point_diameter(struct vg *, double);
 void		vg_draw_points(struct vg *, struct vg_element *);
 void		vg_points_bbox(struct vg *, struct vg_element *,
 		               struct vg_rect *);
