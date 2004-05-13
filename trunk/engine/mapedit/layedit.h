48644f3129b0d368b298e638e5f64eb4f51b3f45
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2004-05-13 02:48:00 +0000

    - toolify the layer editor and media selector. - get rid of the node editor altogether.

--- trunk/engine/mapedit/layedit.h
+++ trunk/engine/mapedit/layedit.h
@@ -1,21 +1,10 @@
-/*	$Csoft: layedit.h,v 1.6 2004/03/18 03:07:53 vedge Exp $	*/
+/*	$Csoft: layedit.h,v 1.7 2004/03/30 15:56:51 vedge Exp $	*/
 /*	Public domain	*/
 
 #include "begin_code.h"
 
-struct mapview;
-
-struct layedit {
-	struct window	*win;
-	struct button	*trigger;
-	struct tlist	*layers_tl;
-	struct textbox	*rename_tb;
-};
-
 __BEGIN_DECLS
-void	layedit_init(struct mapview *, struct window *);
 void	layedit_poll(int, union evarg *);
-void	layedit_destroy(struct mapview *);
 __END_DECLS
 
 #include "close_code.h"
