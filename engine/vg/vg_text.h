ec2ac688337d2e854fb7862b6559112f628ad8f1
Julien Nadeau Carriere <vedge@hypertriton.com>
2004-05-28 22:31:58 +0000

    sampling
    git-svn-id: https://dev.csoft.net/agar/trunk@3915 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/vg/vg_text.h
+++ engine/vg/vg_text.h
@@ -1,17 +1,20 @@
-/*	$Csoft: vg_text.h,v 1.4 2004/05/12 04:51:31 vedge Exp $	*/
+/*	$Csoft: vg_text.h,v 1.5 2004/05/24 03:32:22 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_VG_TEXT_H_
 #define _AGAR_VG_TEXT_H_
 #include "begin_code.h"
 
-#define VG_TEXT_MAX		256
-		
+#define VG_TEXT_MAX	 256
+#define VG_TEXT_MAX_PTRS 32
+
 struct vg_text_args {
 	SDL_Surface *su;		/* Text surface */
 	char text[VG_TEXT_MAX];		/* Text buffer */
 	double angle;			/* Angle of rotation (deg) */
 	enum vg_alignment align;	/* Alignment around vertex */
+	void *ptrs[VG_TEXT_MAX_PTRS];	/* Pointers (for polling) */
+	int  nptrs;
 };
 
 __BEGIN_DECLS
@@ -20,6 +23,7 @@ void	vg_text_destroy(struct vg *, struct vg_element *);
 void	vg_text_align(struct vg *, enum vg_alignment);
 void	vg_text_angle(struct vg *, double);
 void	vg_printf(struct vg *, const char *, ...);
+void	vg_pprintf(struct vg *, const char *, ...);
 void	vg_draw_text(struct vg *, struct vg_element *);
 void	vg_text_bbox(struct vg *, struct vg_element *, struct vg_rect *);
 __END_DECLS
