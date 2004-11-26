23b62303eb6cc5c7bb6fc54537fbc49fbfae74c3
Julien Nadeau Carriere <vedge@hypertriton.com>
2004-11-26 06:10:55 +0000

    inline the one-liners
    git-svn-id: https://dev.csoft.net/agar/trunk@4107 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/loader/ttf.h
+++ engine/loader/ttf.h
@@ -1,4 +1,4 @@
-/*	$Csoft: ttf.h,v 1.2 2003/08/31 11:58:08 vedge Exp $	*/
+/*	$Csoft: ttf.h,v 1.3 2004/05/24 03:28:09 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_MEDIA_TTF_H_
@@ -11,20 +11,20 @@ void		 ttf_destroy(void);
 ttf_font	*ttf_open_font(const char *, int);
 void		 ttf_close_font(ttf_font *);
 
-int		 ttf_get_font_style(ttf_font *);
-void		 ttf_set_font_style(ttf_font *, int);
+__inline__ int	 ttf_get_font_style(ttf_font *);
+__inline__ void	 ttf_set_font_style(ttf_font *, int);
 #define TTF_STYLE_NORMAL	0x00
 #define TTF_STYLE_BOLD		0x01
 #define TTF_STYLE_ITALIC	0x02
 #define TTF_STYLE_UNDERLINE	0x04
 
-int		 ttf_font_height(ttf_font *);
-int		 ttf_font_ascent(ttf_font *);
-int		 ttf_font_descent(ttf_font *);
-int		 ttf_font_line_skip(ttf_font *);
-int		 ttf_font_face_fixed_width(ttf_font *);
-char		*ttf_font_face_family(ttf_font *);
-char		*ttf_font_face_style(ttf_font *);
+__inline__ int	 ttf_font_height(ttf_font *);
+__inline__ int	 ttf_font_ascent(ttf_font *);
+__inline__ int	 ttf_font_descent(ttf_font *);
+__inline__ int	 ttf_font_line_skip(ttf_font *);
+__inline__ int	 ttf_font_face_fixed_width(ttf_font *);
+__inline__ char	*ttf_font_face_family(ttf_font *);
+__inline__ char	*ttf_font_face_style(ttf_font *);
 
 int  ttf_glyph_metrics(ttf_font *, Uint32 , int *, int *, int *, int *, int *);
 int  ttf_size_text(ttf_font *, const char *, int *, int *);
