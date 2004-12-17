867a039c4f98dbeadf00797f23389af0b8ee0fed
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2004-12-17 03:17:51 +0000

    prepare for texture mgmt.

--- trunk/engine/gfx.h
+++ trunk/engine/gfx.h
@@ -1,4 +1,4 @@
-/*	$Csoft: gfx.h,v 1.20 2004/05/10 05:17:34 vedge Exp $	*/
+/*	$Csoft: gfx.h,v 1.21 2004/10/27 10:43:14 vedge Exp $	*/
 /*	Public domain	*/
 
 #include <engine/transform.h>
@@ -9,23 +9,32 @@ struct object;
 struct noderef;
 
 struct gfx_anim {
-	SDL_Surface	 **frames;
-	Uint32		  nframes;
-	Uint32		maxframes;
-	Uint32		    frame;		/* Shared frame# */
+	SDL_Surface **frames;
+#ifdef HAVE_OPENGL
+	GLuint *textures;
+#endif
+	Uint32 nframes;
+	Uint32 maxframes;
+	Uint32 frame;		/* Shared frame# */
 };
 
 struct gfx_cached_sprite {
-	SDL_Surface	 *su;			/* Modified sprite */
-	Uint32		  last_drawn;		/* Time last draw occured */
+	SDL_Surface *su;
+#ifdef HAVE_OPENGL
+	GLuint texture;
+#endif
+	Uint32 last_drawn;			/* Time last draw occured */
 	struct transformq transforms;		/* Applied transforms */
 	SLIST_ENTRY(gfx_cached_sprite) sprites;
 };
 
 struct gfx_cached_anim {
-	struct gfx_anim	  *anim;		/* Modified anim */
-	Uint32		   last_drawn;		/* Time last draw occured */
-	struct transformq  transforms;		/* Applied transforms */
+	struct gfx_anim	*anim;			/* Modified anim */
+#ifdef HAVE_OPENGL
+	GLuint texture;
+#endif
+	Uint32 last_drawn;			/* Time last draw occured */
+	struct transformq transforms;		/* Applied transforms */
 	SLIST_ENTRY(gfx_cached_anim) anims;
 };
 
@@ -41,13 +50,17 @@ struct gfx {
 	char *name;
 	enum {
 		GFX_SHARED,			/* Managed, shared graphics */
-		GFX_PRIVATE			/* Object private graphics */
+		GFX_PRIVATE			/* Object-managed graphics */
 	} type;
 
-	SDL_Surface		 **sprites;	/* Static images */
+	SDL_Surface		 **sprites;
+#ifdef HAVE_OPENGL
+	GLuint			  *textures;
+#endif
 	struct gfx_spritecl	 *csprites;	/* Sprite transform cache */
 	Uint32			  nsprites;
 	Uint32			maxsprites;
+
 	struct gfx_anim		 **anims;	/* Animations */
 	struct gfx_animcl	 *canims;	/* Anim transform cache */
 	Uint32			  nanims;
@@ -59,12 +72,14 @@ struct gfx {
 
 	pthread_mutex_t	 used_lock;
 	Uint32		 used;			/* Reference count */
-#define GFX_MAX_USED	 (0xffffffff-1)
+#define GFX_MAX_USED	 (0xffffffff-1)		/* Maximum #references */
+
 	TAILQ_ENTRY(gfx) gfxs;			/* Art pool */
 };
 
-#define SPRITE(ob, i)	((struct object *)(ob))->gfx->sprites[(i)]
-#define ANIM(ob, i)	((struct object *)(ob))->gfx->anims[(i)]
+#define SPRITE(ob, i)		((struct object *)(ob))->gfx->sprites[(i)]
+#define SPRITE_TEX(ob, i)	((struct object *)(ob))->gfx->textures[(i)]
+#define ANIM(ob, i)		((struct object *)(ob))->gfx->anims[(i)]
 
 #define GFX_ANIM_FRAME(r, an) \
     (an)->frames[((r)->r_anim.flags & NODEREF_PVT_FRAME) ? \
