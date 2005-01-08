12cdb0c95e800ac2b76c9978d7611ff6968cc999
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2005-01-08 03:35:55 +0000

    cosmetic parameters

--- trunk/engine/widget/box.h
+++ trunk/engine/widget/box.h
@@ -1,4 +1,4 @@
-/*	$Csoft: box.h,v 1.2 2003/06/10 06:58:17 vedge Exp $	*/
+/*	$Csoft: box.h,v 1.3 2003/06/18 00:47:04 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_WIDGET_BOX_H_
@@ -11,6 +11,7 @@
 #define BOX_HOMOGENOUS	0x01	/* Divide space evenly */
 #define BOX_WFILL	0x02	/* Expand to fill available width */
 #define BOX_HFILL	0x04	/* Expand to fill available height */
+#define BOX_FRAME	0x08	/* Display a frame */
 
 enum box_type {
 	BOX_HORIZ,
@@ -19,13 +20,13 @@ enum box_type {
 
 struct box {
 	struct widget wid;
+	enum box_type type;
 
-	enum box_type	 type;
-
-	pthread_mutex_t	 lock;
-	int		 homogenous;	/* Divide space evenly */
-	int		 padding;	/* Padding around widgets */
-	int		 spacing;	/* Spacing between widgets */
+	pthread_mutex_t	lock;
+	int homogenous;			/* Divide space evenly */
+	int padding;			/* Padding around widgets */
+	int spacing;			/* Spacing between widgets */
+	int depth;			/* Depth of frame (for BOX_FRAME) */
 };
 
 __BEGIN_DECLS
@@ -39,6 +40,8 @@ void	 box_scale(void *, int, int);
 void	 box_set_homogenous(struct box *, int);
 void	 box_set_padding(struct box *, int);
 void	 box_set_spacing(struct box *, int);
+void	 box_set_color(struct box *, Uint8, Uint8, Uint8);
+void	 box_set_depth(struct box *, int);
 __END_DECLS
 
 #include "close_code.h"
