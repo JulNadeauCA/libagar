ec2771a3e85bdbef6223b2e632bd7272a7681098
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2005-02-15 04:05:23 +0000

    HSV/RGB color conversion; based on formulas from http://www.EasyRGB.com.

--- trunk/engine/rg/prim.h
+++ trunk/engine/rg/prim.h
@@ -1,4 +1,4 @@
-/*	$Csoft$	*/
+/*	$Csoft: prim.h,v 1.1 2005/02/08 15:50:29 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_RG_PRIM_H_
@@ -12,13 +12,18 @@ enum prim_blend_mode {
 };
 
 __BEGIN_DECLS
+void prim_rgb2hsv(Uint8, Uint8, Uint8, float *, float *, float *);
+void prim_hsv2rgb(float, float, float, Uint8 *, Uint8 *, Uint8 *);
+
 void prim_color_rgb(struct tile *, Uint8, Uint8, Uint8);
 void prim_color_rgba(struct tile *, Uint8, Uint8, Uint8, Uint8);
+void prim_color_hsv(struct tile *, float, float, float);
+void prim_color_hsva(struct tile *, float, float, float, Uint8);
 void prim_color_u32(struct tile *, Uint32);
 void prim_line_style(struct tile *, int);
 
-void prim_put_pixel(struct tile *, int, int, Uint32);
-void prim_blend_rgb(struct tile *, int, int, enum prim_blend_mode,
+void prim_put_pixel(SDL_Surface *, int, int, Uint32);
+void prim_blend_rgb(SDL_Surface *, int, int, enum prim_blend_mode,
 	            Uint8, Uint8, Uint8, Uint8);
 void prim_circle2(struct tile *, int, int, int);
 void prim_line(struct tile *, int, int, int, int);
