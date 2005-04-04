541097f7eb1d89920ee4e3970301c7ef2ee89aa6
Julien Nadeau Carriere <vedge@hypertriton.com>
2005-04-04 01:06:13 +0000

    add artwork list as source.
    git-svn-id: https://dev.csoft.net/agar/trunk@4383 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/mapedit/tool/stamp.c
+++ engine/mapedit/tool/stamp.c
@@ -1,4 +1,4 @@
-/*	$Csoft: stamp.c,v 1.61 2004/12/17 03:19:41 vedge Exp $	*/
+/*	$Csoft: stamp.c,v 1.62 2005/01/05 04:44:04 vedge Exp $	*/
 
 /*
  * Copyright (c) 2002, 2003, 2004, 2005 CubeSoft Communications, Inc.
@@ -28,114 +28,162 @@
 
 #include <engine/engine.h>
 #include <engine/mapedit/mapedit.h>
+#include <engine/rg/tileset.h>
 
 #include <engine/widget/radio.h>
+#include <engine/widget/checkbox.h>
+#include <engine/widget/label.h>
+#include <engine/widget/tlist.h>
 
-static void stamp_init(struct tool *);
-static int stamp_cursor(struct tool *, SDL_Rect *);
-static void stamp_effect(struct tool *, struct node *);
+static enum {
+	STAMP_SRC_ARTWORK,	/* From artwork list */
+	STAMP_SRC_COPYBUF	/* From copy/paste buffer */
+} source = STAMP_SRC_ARTWORK;
+
+static int replace = 0;
+
+static void
+init(struct tool *t)
+{
+	static const char *source_items[] = {
+		N_("Artwork list"),
+		N_("Map buffer"),
+		NULL
+	};
+	struct window *win;
+	struct radio *rad;
+	struct checkbox *cb;
+
+	win = tool_window(t, "mapedit-tool-stamp");
+
+	label_new(win, LABEL_STATIC, _("Source: "));
+	rad = radio_new(win, source_items);
+	widget_bind(rad, "value", WIDGET_INT, &source);
+
+	cb = checkbox_new(win, _("Replace mode"));
+	widget_bind(cb, "state", WIDGET_INT, &replace);
+
+	tool_push_status(t, _("Specify the destination node."));
+}
+
+static void
+effect(struct tool *t, struct node *n)
+{
+	struct mapview *mv = t->mv;
+	struct map *m = mv->map;
+
+	if (source == STAMP_SRC_COPYBUF) {
+		struct map *copybuf = &mapedit.copybuf;
+		int sx, sy, dx, dy;
+
+		for (sy = 0, dy = mv->cy;
+		     sy < copybuf->maph && dy < m->maph;
+		     sy++, dy++) {
+			for (sx = 0, dx = mv->cx;
+			     sx < copybuf->mapw && dx < m->mapw;
+			     sx++, dx++) {
+				struct node *sn = &copybuf->map[sy][sx];
+				struct node *dn = &m->map[dy][dx];
+				struct noderef *r;
+
+				if (replace) {
+					node_clear(m, dn, m->cur_layer);
+				}
+				TAILQ_FOREACH(r, &sn->nrefs, nrefs)
+					node_copy_ref(r, m, dn, m->cur_layer);
+			}
+		}
+	} else {
+		struct tlist_item *it;
+
+		if (mv->art_tl != NULL &&
+		    (it = tlist_item_selected(mv->art_tl)) != 0 &&
+		    strcmp(it->class, "tile") == 0) {
+			struct tile *t = it->p1;
+			struct noderef *r;
+			
+			if (replace) {
+				node_clear(m, n, m->cur_layer);
+			}
+			r = node_add_sprite(m, n, t->ts, t->sprite);
+			r->layer = m->cur_layer;
+		}
+	}
+}
+
+static int
+cursor(struct tool *t, SDL_Rect *rd)
+{
+	struct mapview *mv = t->mv;
+	struct map *m = mv->map;
+	struct tlist_item *it;
+	int rv = -1;
+
+	if (source == STAMP_SRC_COPYBUF) {
+		struct map *copybuf = &mapedit.copybuf;
+		struct noderef *r;
+		int sx, sy, dx, dy;
+	
+		/* Avoid circular references when viewing the copy buffer. */
+		if (mv->map == copybuf) {
+			return (-1);
+		}
+		for (sy = 0, dy = rd->y;
+		     sy < copybuf->maph;
+		     sy++, dy += mv->tilesz) {
+			for (sx = 0, dx = rd->x;
+			     sx < copybuf->mapw;
+			     sx++, dx += mv->tilesz) {
+				struct node *sn = &copybuf->map[sy][sx];
+	
+				TAILQ_FOREACH(r, &sn->nrefs, nrefs) {
+					noderef_draw(m, r,
+					    WIDGET(mv)->cx+dx,
+					    WIDGET(mv)->cy+dy,
+					    mv->tilesz);
+					rv = 0;
+				}
+				if (mv->flags & MAPVIEW_PROPS)
+					mapview_draw_props(mv, sn, dx, dy,
+					    -1, -1);
+			}
+		}
+	} else if (mv->art_tl != NULL &&
+	   (it = tlist_item_selected(mv->art_tl)) != NULL &&
+	   strcmp(it->class, "tile") == 0) {
+		struct tile *t = it->p1;
+		int dx, dy;
+
+		if (t->su != NULL) {
+			struct noderef rtmp;
+
+			noderef_init(&rtmp, NODEREF_SPRITE);
+			rtmp.r_sprite.obj = (void *)t->ts;
+			rtmp.r_sprite.offs = t->sprite;
+			noderef_draw(m, &rtmp,
+			    WIDGET(mv)->cx + rd->x,
+			    WIDGET(mv)->cy + rd->y,
+			    mv->tilesz);
+			rv = 0;
+		}
+	}
+	return (rv);
+}
 
 const struct tool stamp_tool = {
 	N_("Stamp"),
 	N_("Insert the contents of the copy/paste buffer."),
 	STAMP_TOOL_ICON,
 	-1,
-	stamp_init,
+	init,
 	NULL,			/* destroy */
 	NULL,			/* load */
 	NULL,			/* save */
-	stamp_cursor,
-	stamp_effect,
+	cursor,
+	effect,
 	NULL,			/* mousemotion */
 	NULL,			/* mousebuttondown */
 	NULL,			/* mousebuttonup */
 	NULL,			/* keydown */
 	NULL			/* keyup */
 };
-
-static enum {
-	STAMP_REPLACE,		/* Replace other refs on layer */
-	STAMP_INSERT_HIGHEST	/* Insert on top of the stack */
-} mode = STAMP_REPLACE;
-
-static void
-stamp_init(struct tool *t)
-{
-	static const char *mode_items[] = {
-		N_("Replace"),
-		N_("Insert"),
-		NULL
-	};
-	struct window *win;
-	struct radio *rad;
-
-	win = tool_window(t, "mapedit-tool-stamp");
-
-	rad = radio_new(win, mode_items);
-	widget_bind(rad, "value", WIDGET_INT, &mode);
-
-	tool_push_status(t, _("Specify the destination node."));
-}
-
-static void
-stamp_effect(struct tool *t, struct node *n)
-{
-	struct mapview *mv = t->mv;
-	struct map *m = mv->map;
-	struct map *copybuf = &mapedit.copybuf;
-	int sx, sy, dx, dy;
-	
-	for (sy = 0, dy = mv->cy;
-	     sy < copybuf->maph && dy < m->maph;
-	     sy++, dy++) {
-		for (sx = 0, dx = mv->cx;
-		     sx < copybuf->mapw && dx < m->mapw;
-		     sx++, dx++) {
-			struct node *sn = &copybuf->map[sy][sx];
-			struct node *dn = &m->map[dy][dx];
-			struct noderef *r;
-
-			if (mode == STAMP_REPLACE)
-				node_clear(m, dn, m->cur_layer);
-
-			TAILQ_FOREACH(r, &sn->nrefs, nrefs)
-				node_copy_ref(r, m, dn, m->cur_layer);
-		}
-	}
-}
-
-static int
-stamp_cursor(struct tool *t, SDL_Rect *rd)
-{
-	struct mapview *mv = t->mv;
-	struct map *copybuf = &mapedit.copybuf;
-	struct noderef *r;
-	int sx, sy, dx, dy;
-	int rv = -1;
-	
-	/* Avoid circular references when viewing the copy buffer. */
-	if (mv->map == copybuf)
-		return (-1);
-
-	for (sy = 0, dy = rd->y;
-	     sy < copybuf->maph;
-	     sy++, dy += mv->tilesz) {
-		for (sx = 0, dx = rd->x;
-		     sx < copybuf->mapw;
-		     sx++, dx += mv->tilesz) {
-			struct node *sn = &copybuf->map[sy][sx];
-
-			TAILQ_FOREACH(r, &sn->nrefs, nrefs) {
-				noderef_draw(mv->map, r,
-				    WIDGET(mv)->cx+dx,
-				    WIDGET(mv)->cy+dy,
-				    mv->tilesz);
-				rv = 0;
-			}
-			if (mv->flags & MAPVIEW_PROPS)
-				mapview_draw_props(mv, sn, dx, dy, -1, -1);
-		}
-	}
-	return (rv);
-}
-
