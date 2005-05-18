d83358f369f012a163408ff29e4ebb87a70be16f
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2005-05-18 09:07:03 +0000

    antialiased line algorithm

--- trunk/engine/rg/prim.h
+++ trunk/engine/rg/prim.h
@@ -1,4 +1,4 @@
-/*	$Csoft: prim.h,v 1.3 2005/02/16 14:48:24 vedge Exp $	*/
+/*	$Csoft: prim.h,v 1.4 2005/04/10 09:09:02 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_RG_PRIM_H_
@@ -23,13 +23,13 @@ void prim_color_rgba(struct tile *, Uint8, Uint8, Uint8, Uint8);
 void prim_color_hsv(struct tile *, float, float, float);
 void prim_color_hsva(struct tile *, float, float, float, Uint8);
 void prim_color_u32(struct tile *, Uint32);
-void prim_line_style(struct tile *, int);
 
 void prim_put_pixel(SDL_Surface *, int, int, Uint32);
 void prim_blend_rgb(SDL_Surface *, int, int, enum prim_blend_mode,
 	            Uint8, Uint8, Uint8, Uint8);
 void prim_circle2(struct tile *, int, int, int);
 void prim_line(struct tile *, int, int, int, int);
+void prim_wuline(struct tile *, double, double, double, double);
 __END_DECLS
 
 #include "close_code.h"
