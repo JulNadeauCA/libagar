63e3468da8e4eca7975ea41e6f1c1942c33b6c19
Julien Nadeau Carriere <vedge@hypertriton.com>
2005-07-24 06:49:53 +0000

    sync
    git-svn-id: https://dev.csoft.net/agar/trunk@4764 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/map/tools.h
+++ engine/map/tools.h
@@ -1,7 +1,10 @@
-/*	$Csoft$	*/
+/*	$Csoft: tools.h,v 1.1 2005/07/23 17:54:20 vedge Exp $	*/
 /*	Public domain	*/
 
 extern const struct tool nodesel_tool;
 extern const struct tool refsel_tool;
 extern const struct tool fill_tool;
+extern const struct tool insert_tool;
+extern const struct tool flip_tool;
+extern const struct tool invert_tool;
 
