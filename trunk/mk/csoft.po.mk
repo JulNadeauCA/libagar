957b03bc8ab0a873a547992b076b3bfff259f51a
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2005-08-02 01:55:47 +0000

    rien

--- trunk/mk/csoft.po.mk
+++ trunk/mk/csoft.po.mk
@@ -1,4 +1,4 @@
-# $Csoft: csoft.po.mk,v 1.15 2004/04/25 05:24:48 vedge Exp $
+# $Csoft: csoft.po.mk,v 1.18 2004/10/19 16:08:47 vedge Exp $
 
 # Copyright (c) 2003, 2004 CubeSoft Communications, Inc.
 # <http://www.csoft.org>
@@ -78,10 +78,13 @@ ${DOMAIN}.pot: ${POTFILES}
 		    --directory=${SRC} --add-comments \
 		    --keyword=_ --keyword=N_ \
 		    --files-from=${POTFILES} ${XGETTEXT_FLAGS} -o $@"; \
-		${XGETTEXT} --default-domain=${DOMAIN} \
+		if [ "${XGETTEXT} --default-domain=${DOMAIN} \
 		    --directory=${SRC} --add-comments \
 		    --keyword=_ --keyword=N_ \
-		    --files-from=${POTFILES} ${XGETTEXT_FLAGS} -o $@; \
+		    --files-from=${POTFILES} \
+		    ${XGETTEXT_FLAGS} -o $@" != 0 ]; then \
+			echo "skipping $@ (xgettext failed); "; \
+		fi; \
 	else \
 		echo "skipping $@ (no gettext)"; \
 	fi
