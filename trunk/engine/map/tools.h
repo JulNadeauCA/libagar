cc5257448253fdbe2ebe0961debb6cf3472e95d6
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2005-08-22 02:11:50 +0000

    bring back the eraser

--- trunk/engine/map/tools.h
+++ trunk/engine/map/tools.h
@@ -1,4 +1,4 @@
-/*	$Csoft: tools.h,v 1.3 2005/07/30 05:01:34 vedge Exp $	*/
+/*	$Csoft: tools.h,v 1.4 2005/08/10 06:59:24 vedge Exp $	*/
 /*	Public domain	*/
 
 extern const struct tool_ops nodesel_ops;
@@ -8,4 +8,5 @@ extern const struct tool_ops insert_ops;
 extern const struct tool_ops ginsert_ops;
 extern const struct tool_ops flip_ops;
 extern const struct tool_ops invert_ops;
+extern const struct tool_ops eraser_ops;
 
