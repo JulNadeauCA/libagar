db663d870c4b9cca3fe005b14d1ff99af6fc4727
Julien Nadeau Carriere <vedge@hypertriton.com>
2005-09-27 03:49:00 +0000

    move from tests to agar-bench
    git-svn-id: https://dev.csoft.net/agar/trunk@4976 86a84d31-9702-0410-8f1e-a60cab7bd424

---
+++ agar-bench/test.h
@@ -0,0 +1,29 @@
+/*	$Csoft: test.h,v 1.4 2005/09/27 00:46:11 vedge Exp $	*/
+/*	Public domain	*/
+
+#include <engine/engine.h>
+#include <engine/view.h>
+#include <engine/map/map.h>
+#include <engine/widget/gui.h>
+
+struct testfn_ops {
+	char *name;
+	u_int runs;
+	u_int iterations;
+	int flags;
+#define TEST_SDL	0x01		/* SDL-only */
+#define TEST_GL		0x02		/* OpenGL-only */
+
+	void (*init)(void);
+	void (*destroy)(void);
+	void (*run)(void);
+	double last_result;
+};
+
+struct test_ops {
+	char *name;
+	void (*edit)(AG_Window *);
+	struct testfn_ops *funcs;
+	u_int nfuncs;
+};
+
