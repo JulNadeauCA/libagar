ef804bd83f1bfef05f53458c029d7d6ecc32851d
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2005-10-01 14:16:53 +0000

    *** empty log message ***

--- trunk/demos/gamemenu/gamemenu.c
+++ trunk/demos/gamemenu/gamemenu.c
@@ -1,4 +1,4 @@
-/*	$Csoft: tests.c,v 1.2 2005/09/27 04:11:01 vedge Exp $	*/
+/*	$Csoft: gamemenu.c,v 1.1 2005/09/28 15:46:13 vedge Exp $	*/
 /*	Public domain	*/
 
 /*
@@ -19,6 +19,20 @@
 #include <unistd.h>
 
 static void
+PollItems(int argc, union evarg *argv)
+{
+	AG_Table *t = argv[0].p;
+
+	AG_TableBegin(t);
+	AG_TableAddRow(t, "%s:%s", "Foo", "Bar");
+	AG_TableAddRow(t, "%d:%d", 10, 2);
+	AG_TableAddRow(t, "%s:%d", "Baz", 3);
+	AG_TableAddRow(t, "%s:%d", "Bezo", 4);
+	AG_TableAddRow(t, "%s:%d", "Fez", 5);
+	AG_TableEnd(t);
+}
+
+static void
 CreateGameMenu(void)
 {
 	AG_Window *win;
@@ -26,6 +40,7 @@ CreateGameMenu(void)
 	AG_Label *lb1, *lb2;
 	AG_Button *btn;
 	AG_Pixmap *px;
+	AG_Table *t;
 
 	/* Create a fixed-size window with no titlebar or border. */
 	win = AG_WindowNew(AG_WINDOW_NO_TITLEBAR|AG_WINDOW_NO_DECORATIONS,
@@ -55,8 +70,8 @@ CreateGameMenu(void)
 	 */
 	lb1 = AG_LabelNew(NULL, AG_LABEL_STATIC, "Foo");
 	lb2 = AG_LabelNew(NULL, AG_LABEL_STATIC, "Bar");
-	AG_FixedPut(fx, lb1, 10, 10);
-	AG_FixedPut(fx, lb2, 10, AGWIDGET(lb1)->h+5);
+	AG_FixedPut(fx, lb1, 16, 32);
+	AG_FixedPut(fx, lb2, 16, 32+AGWIDGET(lb1)->h);
 
 	/*
 	 * Create a series of 32x32 buttons at the right. We initially attach
@@ -74,6 +89,13 @@ CreateGameMenu(void)
 	btn = AG_ButtonNew(fx, "D");
 	AG_FixedMove(fx, btn, 204+192, 48);
 	AG_FixedSize(fx, btn, 32, 32);
+
+	/* Create a polled table. */
+	t = AG_TablePolled(fx, 0, PollItems, NULL);
+	AG_FixedMove(fx, t, 640-128, 32);
+	AG_FixedSize(fx, t, 128-16, 128-64);
+	AG_TableAddCol(t, "Name", "<Name>", NULL);
+	AG_TableAddCol(t, "Num", "<88>", NULL);
 
 	/*
 	 * Set the window geometry manually. This must be called after the
