51572b966c249230fdac31d01892e7c85d4d20a3
Julien Nadeau Carriere <vedge@hypertriton.com>
2005-10-06 10:41:50 +0000

    - save the modelview, projection, color and texture matrices to avoid   recomputation when switching context. - use glViewport() instead of projection transformations for the widget offset. - add handlers for low-level widget operations.
    git-svn-id: https://dev.csoft.net/agar/trunk@5083 86a84d31-9702-0410-8f1e-a60cab7bd424

--- engine/widget/glview.h
+++ engine/widget/glview.h
@@ -1,11 +1,14 @@
-/*	$Csoft: button.h,v 1.30 2004/09/12 05:51:44 vedge Exp $	*/
+/*	$Csoft: glview.h,v 1.1 2005/10/04 18:04:47 vedge Exp $	*/
 /*	Public domain	*/
 
 #ifndef _AGAR_WIDGET_GLVIEW_H_
 #define _AGAR_WIDGET_GLVIEW_H_
 
-#include <engine/widget/widget.h>
+#include <config/have_opengl.h>
 
+#ifdef HAVE_OPENGL
+
+#include <engine/widget/widget.h>
 #include "begin_code.h"
 
 typedef struct ag_glview {
@@ -13,8 +16,20 @@ typedef struct ag_glview {
 	u_int flags;
 #define AG_GLVIEW_WFILL		0x01
 #define AG_GLVIEW_HFILL		0x02
-	AG_Event *draw_ev;
-	AG_Event *reshape_ev;
+#define AG_GLVIEW_NOMODELVIEW	0x04	/* Don't preserve modelview matrix */
+#define AG_GLVIEW_NOTEXTURE	0x08	/* Don't preserve texture matrix */
+#define AG_GLVIEW_NOCOLOR	0x10	/* Don't preserve color matrix */
+
+	AG_Event *draw_ev;			/* Draw callback */
+	AG_Event *scale_ev;			/* Scaling/movement event */
+	AG_Event *keydown_ev, *keyup_ev;	/* Keyboard events */
+	AG_Event *btndown_ev, *btnup_ev;	/* Mouse button events */
+	AG_Event *motion_ev;			/* Mouse motion event */
+
+	GLdouble mProjection[16];		/* Projection matrix to load */
+	GLdouble mModelview[16];		/* Modelview matrix to load */
+	GLdouble mTexture[16];			/* Texture matrix to load */
+	GLdouble mColor[16];			/* Color matrix to load */
 } AG_GLView;
 
 __BEGIN_DECLS
@@ -23,9 +38,16 @@ void	   AG_GLViewInit(AG_GLView *, u_int);
 void	   AG_GLViewDestroy(void *);
 void	   AG_GLViewDraw(void *);
 void	   AG_GLViewScale(void *, int, int);
-void	   AG_GLViewDrawFn(AG_GLView *, void (*)(int, union evarg *),
-		           const char *, ...);
+void	   AG_GLViewReshape(AG_GLView *);
+void	   AG_GLViewDrawFn(AG_GLView *, AG_EventFn, const char *, ...);
+void	   AG_GLViewScaleFn(AG_GLView *, AG_EventFn, const char *, ...);
+void	   AG_GLViewKeydownFn(AG_GLView *, AG_EventFn, const char *, ...);
+void	   AG_GLViewKeyupFn(AG_GLView *, AG_EventFn, const char *, ...);
+void	   AG_GLViewButtondownFn(AG_GLView *, AG_EventFn, const char *, ...);
+void	   AG_GLViewButtonupFn(AG_GLView *, AG_EventFn, const char *, ...);
+void	   AG_GLViewMotionFn(AG_GLView *, AG_EventFn, const char *, ...);
 __END_DECLS
 
+#endif /* HAVE_OPENGL */
 #include "close_code.h"
 #endif /* _AGAR_WIDGET_GLVIEW_H_ */
