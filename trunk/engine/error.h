4624a07221ec3e2a55678a815012db1c606c5bc5
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2005-10-08 10:27:08 +0000

    merge with core.

--- trunk/engine/error.h
+++ trunk/engine/error.h
@@ -71,9 +71,43 @@
 	va_end((args));						\
 } while (0)
 
-#include <engine/error/malloc.h>
-
 #include "begin_code.h"
+
+enum {
+	M_GENERIC,	/* generic */
+	M_OBJECT,	/* object */
+	M_POSITION,	/* object position */
+	M_DEP,		/* dependency table entry */
+	M_PROP,		/* property table entry */
+	M_EVENT,	/* event queue entry */
+	M_GFX,		/* gfx structure */
+	M_AUDIO,	/* audio structure */
+	M_MAP,		/* map nodes and layers */
+	M_MAP_NITEM,	/* map noderefs */
+	M_MAPEDIT,	/* map edition */
+	M_NODEXFORM,	/* map node transform */
+	M_NODEMASK,	/* map node mask */
+	M_WIDGET,	/* widget */
+	M_VG,		/* vector graphics */
+	M_RG,		/* raster graphics */
+	M_VIEW,		/* view interface */
+	M_NETBUF,	/* network i/o */
+	M_LOADER,	/* file loaders */
+	M_TEXT,		/* text rendering */
+	M_TYPESW,	/* type switch */
+	M_INPUT,	/* input devices */
+	M_CAD,		/* cad applications */
+	M_EDA,		/* eda applications */
+	M_GAME,		/* game applications */
+	M_MATH,		/* math routines */
+	M_LAST
+};
+
+struct ag_malloc_type {
+	size_t msize;			/* Total allocated memory */
+	u_int nallocs;			/* Total number of allocations */
+	u_int nfrees;			/* Total number of frees */
+};
 
 #ifdef DEBUG
 extern int agDebugLvl;
@@ -109,6 +143,9 @@ void		*AG_PtrMismatch(void);
 void		*AG_ObjectMismatch(const char *, const char *);
 int		 AG_IntMismatch(void);
 float		 AG_FloatMismatch(void);
+__inline__ void	*AG_Malloc(size_t, int);
+__inline__ void	*AG_Realloc(void *, size_t);
+void		 AG_Free(void *, int);
 __END_DECLS
 
 #include "close_code.h"
