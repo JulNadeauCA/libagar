6173c642bb498b17a577cccf6569261cb81f4106
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2005-10-09 03:47:35 +0000

    removed fixed-point stuff and unneeded irix workaround.

--- trunk/compat/math.c
+++ trunk/compat/math.c
@@ -5,29 +5,6 @@
 
 #include "math.h"
 
-fix30
-fix30sqrt(fix30 f)
-{
-	Uint32 root = 0;
-	Uint32 remHi = 0;
-	Uint32 remLo = f;
-	Uint32 count = 30;	/* 15 + (30 >> 1) */
-	Uint32 testDiv;
-
-	do {
-		remHi = (remHi << 2) | (remLo >> 30);
-		remLo <<= 2;
-		root <<= 1;
-		testDiv = (root << 1) + 1;
-		if (remHi >= testDiv) {
-			remHi -= testDiv;
-			root += 1;
-		}
-	} while (count-- != 0);
-
-	return (root);
-}
-
 int
 ftrunc(double d)
 {
