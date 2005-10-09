7b599e95cbdd4dd78f452f61827f2c56852e197e
Julien Nadeau Carriere <vedge@hypertriton.com>
2005-10-09 10:41:02 +0000

    math utility functions
    git-svn-id: https://dev.csoft.net/agar/trunk@5165 86a84d31-9702-0410-8f1e-a60cab7bd424

---
+++ core/math.h
@@ -0,0 +1,14 @@
+/*	$Csoft: view.h,v 1.107 2005/10/03 17:37:30 vedge Exp $	*/
+/*	Public domain	*/
+
+#ifndef _AGAR_CORE_MATH_H_
+#define _AGAR_CORE_MATH_H_
+#include "begin_code.h"
+
+__BEGIN_DECLS
+__inline__ int AG_PowOf2i(int);
+
+__END_DECLS
+
+#include "close_code.h"
+#endif /* _AGAR_CORE_MATH_H_ */
