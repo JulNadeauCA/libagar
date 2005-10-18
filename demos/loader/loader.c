c361cde30b5b190b95bac7003273536e810b6b38
Julien Nadeau Carriere <vedge@hypertriton.com>
2005-10-18 02:51:57 +0000

    AG_FileDlg demo
    git-svn-id: https://dev.csoft.net/agar/trunk@5272 86a84d31-9702-0410-8f1e-a60cab7bd424

---
+++ demos/loader/loader.c
@@ -0,0 +1,102 @@
+/*	$Csoft: gamemenu.c,v 1.6 2005/10/07 07:09:35 vedge Exp $	*/
+/*	Public domain	*/
+
+#include <agar/core/core.h>
+#include <agar/core/config.h>
+#include <agar/core/view.h>
+
+#include <agar/gui/gui.h>
+
+#include <string.h>
+#include <unistd.h>
+
+static void
+LoadFile(AG_Event *event)
+{
+	char *file = AG_STRING(1);
+
+	AG_TextMsg(AG_MSG_INFO, "File: %s", file);
+	AG_TextMsg(AG_MSG_INFO, "%d args, a[0]=%d, a[1]=%d", event->argc,
+	    event->argt[0], event->argt[1]);
+}
+
+static void
+CreateWindow(void)
+{
+	AG_Window *win;
+	AG_FileDlg *fd;
+
+	win = AG_WindowNew(0);
+
+	fd = AG_FileDlgNew(win, AG_FILEDLG_CLOSEWIN);
+	AG_FileDlgSetDirectory(fd, "/");
+	AG_FileDlgSetFilename(fd, "foo.foo");
+	AG_FileDlgAddType(fd, "Foo file", "*.foo", LoadFile, NULL);
+	
+	AG_WindowMaximize(win);
+	AG_WindowShow(win);
+}
+
+int
+main(int argc, char *argv[])
+{
+	int c, i, fps = -1;
+	char *s;
+
+	if (AG_InitCore("focusing-demo", 0) == -1) {
+		fprintf(stderr, "%s\n", AG_GetError());
+		return (1);
+	}
+
+	while ((c = getopt(argc, argv, "?vfFgGr:")) != -1) {
+		extern char *optarg;
+
+		switch (c) {
+		case 'v':
+			exit(0);
+		case 'f':
+			AG_SetBool(agConfig, "view.full-screen", 1);
+			break;
+		case 'F':
+			AG_SetBool(agConfig, "view.full-screen", 0);
+			break;
+#ifdef HAVE_OPENGL
+		case 'g':
+			AG_SetBool(agConfig, "view.opengl", 1);
+			break;
+		case 'G':
+			AG_SetBool(agConfig, "view.opengl", 0);
+			break;
+#endif
+		case 'r':
+			fps = atoi(optarg);
+			break;
+		case '?':
+		default:
+			printf("%s [-vfFgG] [-r fps]\n", agProgName);
+			exit(0);
+		}
+	}
+
+	/* Initialize a 640x480x32 display. Respond to keyboard/mouse events. */
+	if (AG_InitVideo(640, 480, 32, 0) == -1 ||
+	    AG_InitInput(0) == -1) {
+		fprintf(stderr, "%s\n", AG_GetError());
+		return (-1);
+	}
+	AG_InitConfigWin(0);
+	AG_SetRefreshRate(fps);
+	AG_BindGlobalKey(SDLK_ESCAPE, KMOD_NONE, AG_Quit);
+	AG_BindGlobalKey(SDLK_F1, KMOD_NONE, AG_ShowSettings);
+	AG_BindGlobalKey(SDLK_F8, KMOD_NONE, AG_ViewCapture);
+	
+	CreateWindow();
+
+	AG_EventLoop();
+	AG_Destroy();
+	return (0);
+fail:
+	AG_Destroy();
+	return (1);
+}
+
