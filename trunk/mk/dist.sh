40f2d5c19506f85b3a554bbe057e558b9e71335f
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2005-10-20 05:20:30 +0000

    use openssl

--- trunk/mk/dist.sh
+++ trunk/mk/dist.sh
@@ -22,9 +22,9 @@ rm -fR `find agar-${VER} \( -name .svn \
 echo "packaging"
 tar -f ${DISTFILE}.tar -c agar-${VER}
 gzip -f9 ${DISTFILE}.tar
-md5 ${DISTFILE}.tar.gz > ${DISTFILE}.tar.gz.md5
-rmd160 ${DISTFILE}.tar.gz >> ${DISTFILE}.tar.gz.md5
-sha1 ${DISTFILE}.tar.gz >> ${DISTFILE}.tar.gz.md5
+openssl md5 ${DISTFILE}.tar.gz > ${DISTFILE}.tar.gz.md5
+openssl rmd160 ${DISTFILE}.tar.gz >> ${DISTFILE}.tar.gz.md5
+openssl sha1 ${DISTFILE}.tar.gz >> ${DISTFILE}.tar.gz.md5
 gpg -ab ${DISTFILE}.tar.gz
 
 echo "uploading"
