38386b4612d9eed71958a602305b8288ff6fbe9e
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2005-10-21 08:47:18 +0000

    public vg and rg headers

---
+++ trunk/rg/rg_pub.h
@@ -0,0 +1,10 @@
+/*	$Csoft: gui.h,v 1.6 2005/10/01 09:51:02 vedge Exp $	*/
+/*	Public domain	*/
+
+#ifndef _AGAR_RG_PUBLIC_H_
+#define _AGAR_RG_PUBLIC_H_
+#include <agar/core/core_begin.h>
+#include <agar/rg/tileset.h>
+#include <agar/rg/tileview.h>
+#include <agar/core/core_close.h>
+#endif
---
+++ trunk/vg/vg_pub.h
@@ -0,0 +1,9 @@
+/*	$Csoft: gui.h,v 1.6 2005/10/01 09:51:02 vedge Exp $	*/
+/*	Public domain	*/
+
+#ifndef _AGAR_VG_PUBLIC_H_
+#define _AGAR_VG_PUBLIC_H_
+#include <agar/core/core_begin.h>
+#include <agar/vg/vg.h>
+#include <agar/core/core_close.h>
+#endif
