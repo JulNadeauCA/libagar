7b0c47ed58aba90ed045fa8542b96c175c7fb63e
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2006-10-11 01:55:19 +0000

    fix scaled mode

--- trunk/gui/pixmap.h
+++ trunk/gui/pixmap.h
@@ -11,28 +11,34 @@
 typedef struct ag_pixmap {
 	struct ag_widget wid;
 	Uint flags;
-#define AG_PIXMAP_HFILL	0x01
-#define AG_PIXMAP_VFILL	0x02
+#define AG_PIXMAP_HFILL		0x01
+#define AG_PIXMAP_VFILL		0x02
+#define AG_PIXMAP_FORCE_SIZE	0x04	/* Always override image size */
 #define AG_PIXMAP_EXPAND (AG_PIXMAP_HFILL|AG_PIXMAP_VFILL)
-	int n;				/* Current surface */
-	int s, t;			/* Source coordinates */
+
+	int n;			/* Current surface (or -1) */
+	int s, t;		/* Source coordinates */
+	int pre_w, pre_h;	/* Geometry to use if there is no surface */
 } AG_Pixmap;
 
 __BEGIN_DECLS
+AG_Pixmap *AG_PixmapNew(void *, Uint, Uint, Uint);
 AG_Pixmap *AG_PixmapFromSurface(void *, Uint, SDL_Surface *);
 AG_Pixmap *AG_PixmapFromSurfaceCopy(void *, Uint, SDL_Surface *);
 AG_Pixmap *AG_PixmapFromSurfaceScaled(void *, Uint, SDL_Surface *, Uint, Uint);
 AG_Pixmap *AG_PixmapFromBMP(void *, Uint, const char *);
 AG_Pixmap *AG_PixmapFromXCF(void *, Uint, const char *);
 
-int	   AG_PixmapAddSurface(AG_Pixmap *, SDL_Surface *);
-int	   AG_PixmapAddSurfaceCopy(AG_Pixmap *, SDL_Surface *);
-int	   AG_PixmapAddSurfaceScaled(AG_Pixmap *, SDL_Surface *, Uint, Uint);
-
 void	   AG_PixmapInit(AG_Pixmap *, Uint);
 void	   AG_PixmapDestroy(void *);
 void	   AG_PixmapDraw(void *);
 void	   AG_PixmapScale(void *, int, int);
+
+int	   AG_PixmapAddSurface(AG_Pixmap *, SDL_Surface *);
+int	   AG_PixmapAddSurfaceCopy(AG_Pixmap *, SDL_Surface *);
+int	   AG_PixmapAddSurfaceScaled(AG_Pixmap *, SDL_Surface *, Uint, Uint);
+void	   AG_PixmapReplaceSurfaceScaled(AG_Pixmap *, SDL_Surface *, Uint,
+		                         Uint);
 
 __inline__ void AG_PixmapSetSurface(AG_Pixmap *, int);
 __inline__ void AG_PixmapSetCoords(AG_Pixmap *, int, int);
