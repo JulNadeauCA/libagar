e2d400a0c44c450ba0f21866dd830b3f4661cb35
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2007-05-01 07:08:02 +0000

    sync

--- trunk/map/transform.h
+++ trunk/map/transform.h
@@ -1,50 +1,50 @@
 /*	$Csoft: transform.h,v 1.2 2005/04/16 05:58:03 vedge Exp $	*/
 /*	Public domain	*/
 
-#ifndef _AGAR_MAP_GFX_TRANSFORM_H_
-#define _AGAR_MAP_GFX_TRANSFORM_H_
+#ifndef _AGAR_MAP_TRANSFORM_H_
+#define _AGAR_MAP_TRANSFORM_H_
 #include "begin_code.h"
 
-#define AG_TRANSFORM_MAX_ARGS	64
+#define MAP_TRANSFORM_MAX_ARGS	64
 
-enum ag_transform_type {
-	AG_TRANSFORM_MIRROR,
-	AG_TRANSFORM_FLIP,
-	AG_TRANSFORM_ROTATE,
-	AG_TRANSFORM_RGB_INVERT
+enum map_transform_type {
+	MAP_TRANSFORM_MIRROR,
+	MAP_TRANSFORM_FLIP,
+	MAP_TRANSFORM_ROTATE,
+	MAP_TRANSFORM_RGB_INVERT
 };
 
-typedef TAILQ_HEAD(ag_transformq, ag_transform) AG_TransformQ;
+TAILQ_HEAD(map_transformq, map_transform);
 
-typedef struct ag_transform {
-	enum ag_transform_type	  type;
-	SDL_Surface		*(*func)(SDL_Surface *, int, Uint32 *);
-	Uint32			 *args;
-	int			  nargs;
-	TAILQ_ENTRY(ag_transform) transforms;
-} AG_Transform;
+typedef struct map_transform {
+	enum map_transform_type type;
+	SDL_Surface *(*func)(SDL_Surface *, int, Uint32 *);
+	Uint32 *args;
+	int nargs;
+	TAILQ_ENTRY(map_transform) transforms;
+} MAP_Transform;
 
-struct ag_transform_ent {
-	char			 *name;
-	enum ag_transform_type	  type;
-	SDL_Surface		*(*func)(SDL_Surface *, int, Uint32 *);
+struct map_transform_ent {
+	char *name;
+	enum map_transform_type type;
+	SDL_Surface *(*func)(SDL_Surface *, int, Uint32 *);
 };
 
-struct ag_nitem;
+struct map_item;
 
 __BEGIN_DECLS
-AG_Transform   *AG_TransformNew(enum ag_transform_type, int, Uint32 *);
-int		AG_TransformInit(AG_Transform *, enum ag_transform_type, int,
-		                   Uint32 *);
-int		AG_TransformLoad(AG_Netbuf *, AG_Transform *);
-void		AG_TransformSave(AG_Netbuf *, const AG_Transform *);
-void		AG_TransformDestroy(AG_Transform *);
-__inline__ int	AG_TransformCompare(const AG_Transform *,
-		                      const AG_Transform *);
-void		AG_TransformPrint(const struct ag_transformq *, char *, size_t)
+MAP_Transform *MAP_TransformNew(enum map_transform_type, int, Uint32 *);
+int	       MAP_TransformInit(MAP_Transform *, enum map_transform_type, int,
+		                  Uint32 *);
+int	       MAP_TransformLoad(AG_Netbuf *, MAP_Transform *);
+void	       MAP_TransformSave(AG_Netbuf *, const MAP_Transform *);
+void	       MAP_TransformDestroy(MAP_Transform *);
+__inline__ int MAP_TransformCompare(const MAP_Transform *,
+		                    const MAP_Transform *);
+void	       MAP_TransformPrint(const struct map_transformq *, char *, size_t)
 		                  BOUNDED_ATTRIBUTE(__string__, 2, 3);
-AG_Transform   *AG_TransformRotate(struct ag_nitem *, int);
+MAP_Transform *MAP_TransformRotate(struct map_item *, int);
 __END_DECLS
 
 #include "close_code.h"
-#endif	/* _AGAR_MAP_GFX_TRANSFORM_H_ */
+#endif	/* _AGAR_MAP_TRANSFORM_H_ */
