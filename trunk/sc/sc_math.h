1b2a123167a8a5caf2736b143b58f8b88798ebbd
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2007-05-28 05:10:11 +0000

    math constants

--- trunk/sc/sc_math.h
+++ trunk/sc/sc_math.h
@@ -32,6 +32,21 @@ typedef double SC_Real;
 typedef float SC_Real;
 #endif
 
+/* Constants */
+#define	SC_E		2.7182818284590452354	/* e */
+#define	SC_LOG2E	1.4426950408889634074	/* log 2e */
+#define	SC_LOG10E	0.43429448190325182765	/* log 10e */
+#define	SC_LN2		0.69314718055994530942	/* log e2 */
+#define	SC_LN10		2.30258509299404568402	/* log e10 */
+#define	SC_PI		3.14159265358979323846	/* pi */
+#define	SC_PI_2		1.57079632679489661923	/* pi/2 */
+#define	SC_PI_4		0.78539816339744830962	/* pi/4 */
+#define	SC_1_PI		0.31830988618379067154	/* 1/pi */
+#define	SC_2_PI		0.63661977236758134308	/* 2/pi */
+#define	SC_2_SQRTPI	1.12837916709551257390	/* 2/sqrt(pi) */
+#define	SC_SQRT2	1.41421356237309504880	/* sqrt(2) */
+#define	SC_SQRT1_2	0.70710678118654752440	/* 1/sqrt(2) */
+
 typedef Uint32 SC_QTime;
 typedef SC_Real SC_Time;
 
