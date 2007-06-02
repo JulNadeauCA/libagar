9d66b5ae1d90c60aac40cfd2b602c5a5e9c2a250
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2007-06-02 08:03:56 +0000

    use map_pub.h

---
+++ trunk/map/map_pub.h
@@ -0,0 +1,11 @@
+/*	Public domain	*/
+
+#ifndef _AGAR_MAP_PUBLIC_H_
+#define _AGAR_MAP_PUBLIC_H_
+#define _AGAR_MAP_PUBLIC
+#include <agar/core/core_begin.h>
+#include <agar/map/map.h>
+#include <agar/map/mapview.h>
+#include <agar/map/actor.h>
+#include <agar/core/core_close.h>
+#endif
