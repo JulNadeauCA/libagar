b40bd5d7e9dec710ad7290a5eeef3958d83c6f39
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2007-06-02 23:51:05 +0000

    fix compile on MacOS X.

--- trunk/sg/sg_pub.h
+++ trunk/sg/sg_pub.h
@@ -7,6 +7,15 @@
 #include <agar/sg/sg.h>
 #include <agar/core/core_close.h>
 
-#include <GL/gl.h>
-#include <GL/glu.h>
+#include <agar/config/have_opengl.h>
+#ifdef HAVE_OPENGL
+# ifdef __APPLE__
+#  include <OpenGL/gl.h>
+#  include <OpenGL/glu.h>
+# else
+#  include <GL/gl.h>
+#  include <GL/glu.h>
+# endif
+#endif
+
 #endif
