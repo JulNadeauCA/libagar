df2d8a247acf1817ed7c5d94fe5b8dc3979e6eeb
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2007-06-03 01:22:38 +0000

    include sg_gui.h

--- trunk/sg/sg_pub.h
+++ trunk/sg/sg_pub.h
@@ -5,6 +5,7 @@
 #define _AGAR_SG_PUBLIC
 #include <agar/core/core_begin.h>
 #include <agar/sg/sg.h>
+#include <agar/sg/sg_gui.h>
 #include <agar/core/core_close.h>
 
 #include <agar/config/have_opengl.h>
