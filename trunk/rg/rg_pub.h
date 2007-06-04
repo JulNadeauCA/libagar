5c9adba9c1ec4ad00f752f237751a94e53a41d57
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2007-06-04 00:57:30 +0000

    need gui/window.h.

--- trunk/rg/rg_pub.h
+++ trunk/rg/rg_pub.h
@@ -5,6 +5,7 @@
 #define _AGAR_RG_PUBLIC_H_
 #define _AGAR_RG_PUBLIC
 #include <agar/core/core_begin.h>
+#include <agar/gui/window.h>
 #include <agar/rg/tileset.h>
 #include <agar/rg/tileview.h>
 #include <agar/core/core_close.h>
