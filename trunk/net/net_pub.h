af5a05ec6333f13323b594bc7560e7aa6d246965
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2007-06-05 15:01:14 +0000

    add core prologue and epilogue

--- trunk/net/net_pub.h
+++ trunk/net/net_pub.h
@@ -3,8 +3,10 @@
 #ifndef _AGAR_NET_PUBLIC_H_
 #define _AGAR_NET_PUBLIC_H_
 #define _AGAR_NET_PUBLIC
+#include <agar/core/core_begin.h>
 #include <agar/net/command.h>
 #include <agar/net/client.h>
 #include <agar/net/server.h>
 #include <agar/net/error.h>
+#include <agar/core/core_close.h>
 #endif
