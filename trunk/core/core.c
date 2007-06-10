a17e1dd9750171eac28f0955e5ec119a43c99fa8
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2007-06-10 16:52:02 +0000

    - add agVerbose flag. - fix AG_Destroy() behavior when video is not used.

--- trunk/core/core.c
+++ trunk/core/core.c
@@ -67,10 +67,14 @@ void (*agAtexitFunc)(void) = NULL;
 AG_Mutex agLinkageLock;
 AG_Mutex agTimingLock;
 int agServerMode = 0;
+int agVerbose = 0;
 
 int
 AG_InitCore(const char *progname, Uint flags)
 {
+	if (flags & AG_CORE_VERBOSE)
+		agVerbose = 1;
+
 	agProgName = progname;
 
 #ifdef HAVE_SETLOCALE
@@ -108,22 +112,28 @@ AG_InitCore(const char *progname, Uint flags)
 	AG_ConfigInit(agConfig);
 	AG_ObjectLoad(agConfig);
 
-	printf("\n");
+	if (agVerbose) {
+		printf("\n");
+	}
 #ifdef HAVE_SDL_CPUINFO
-	printf(_("Architecture extensions: "));
-	if (SDL_HasRDTSC()) { printf("RDTSC "); }
-	if (SDL_HasMMX()) { printf("MMX "); }
-	if (SDL_HasMMXExt()) { printf("MMXExt "); }
-	if (SDL_Has3DNow()) { printf("3DNow "); }
-	if (SDL_Has3DNowExt()) { printf("3DNowExt "); }
-	if (SDL_HasSSE()) { printf("SSE "); }
-	if (SDL_HasSSE2()) { printf("SSE2 "); }
-	if (SDL_HasAltiVec()) { printf("AltiVec "); }
-	printf(".\n");
+	if (agVerbose) {
+		printf(_("Architecture extensions: "));
+		if (SDL_HasRDTSC()) { printf("RDTSC "); }
+		if (SDL_HasMMX()) { printf("MMX "); }
+		if (SDL_HasMMXExt()) { printf("MMXExt "); }
+		if (SDL_Has3DNow()) { printf("3DNow "); }
+		if (SDL_Has3DNowExt()) { printf("3DNowExt "); }
+		if (SDL_HasSSE()) { printf("SSE "); }
+		if (SDL_HasSSE2()) { printf("SSE2 "); }
+		if (SDL_HasAltiVec()) { printf("AltiVec "); }
+		printf(".\n");
+	}
 #endif
-
 	agWorld = AG_ObjectNew(NULL, "world");
 	AG_ObjectRemain(agWorld, AG_OBJECT_REMAIN_DATA);
+#ifdef NETWORK
+	AG_InitNetwork(0);
+#endif
 	return (0);
 }
 
@@ -140,24 +150,20 @@ AG_InitVideo(int w, int h, int bpp, Uint flags)
 	}
 	SDL_WM_SetCaption(agProgName, agProgName);
 
-	if ((agVideoInfo = SDL_GetVideoInfo()) != NULL) {
+#ifdef DEBUG
+	if (agVerbose && (agVideoInfo = SDL_GetVideoInfo()) != NULL) {
 		char accel[2048];
 		char unaccel[2048];
 		size_t size = 2048;
 
 		agVideoFmt = agVideoInfo->vfmt;
 
-		if (agVideoInfo->wm_available) {
-			printf(_("Window manager is available.\n"));
-		} else {
-			printf(_("Window manager is unavailable.\n"));
-		}
-			
-		if (agVideoInfo->hw_available) {
-			printf(_("Hardware surfaces are available.\n"));
-		} else {
-			printf(_("Hardware surfaces are unavailable.\n"));
-		}
+		printf(_("Window manager is %s.\n"),
+		    agVideoInfo->wm_available ?
+		    _("available") : _("unavailable"));
+		printf(_("Hardware surfaces are %s.\n"),
+		    agVideoInfo->hw_available ?
+		    _("available") : _("unavailable"));
 		
 		accel[0] = '\0';
 		unaccel[0] = '\0';
@@ -184,6 +190,7 @@ AG_InitVideo(int w, int h, int bpp, Uint flags)
 
 		printf("\n");
 	}
+#endif
 
 	if (flags & (AG_VIDEO_OPENGL|AG_VIDEO_OPENGL_OR_SDL)) {
 #ifdef HAVE_OPENGL
@@ -258,21 +265,20 @@ AG_InitInput(Uint flags)
 	return (0);
 }
 
+#ifdef NETWORK
 int
 AG_InitNetwork(Uint flags)
 {
-#if defined(DEBUG) && defined(NETWORK) && defined(THREADS)
-	if ((flags & AG_INIT_DEBUG_SERVER) && agServerMode) {
+#if defined(DEBUG) && defined(THREADS)
+	if (agServerMode) {
 		extern int AG_DebugServerStart(void);
 		AG_DebugServerStart();
 	}
 #endif
-#if defined(NETWORK)
-	if (flags & AG_INIT_RCS)
-		AG_RcsInit();
-#endif
+	AG_RcsInit();
 	return (0);
 }
+#endif /* NETWORK */
 
 /* Register a function to invoke in AG_Quit(). */
 void
@@ -303,16 +309,17 @@ AG_Destroy(void)
 #endif
 
 	AG_ObjectDestroy(agWorld);
-	AG_TextDestroy();
-	AG_ViewDestroy();
-	
-	AG_ColorsDestroy();
-	AG_CursorsDestroy();
+
+	if (agView != NULL) {
+		AG_TextDestroy();
+		AG_ViewDestroy();
+		AG_ColorsDestroy();
+		AG_CursorsDestroy();
+		AG_ObjectDestroy(&agIconMgr);
+	}
 
 	AG_ObjectDestroy(agConfig);
 	Free(agConfig, M_OBJECT);
-	
-	AG_ObjectDestroy(&agIconMgr);
 
 /*	AG_MutexDestroy(&agLinkageLock); */
 	AG_MutexDestroy(&agTimingLock);
