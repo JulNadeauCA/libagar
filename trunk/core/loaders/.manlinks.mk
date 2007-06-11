4134e6f90ee11893cffc2b1751afd7d080692e4c
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2007-06-11 13:45:51 +0000

    sync

--- trunk/core/loaders/.manlinks.mk
+++ trunk/core/loaders/.manlinks.mk
@@ -2,3 +2,7 @@ MANLINKS+=AG_Version.3:AG_ReadVersion.3
 CATLINKS+=AG_Version.cat3:AG_ReadVersion.cat3
 MANLINKS+=AG_Version.3:AG_WriteVersion.3
 CATLINKS+=AG_Version.cat3:AG_WriteVersion.cat3
+MANLINKS+=AG_Version.3:AG_ReadObjectVersion.3
+CATLINKS+=AG_Version.cat3:AG_ReadObjectVersion.cat3
+MANLINKS+=AG_Version.3:AG_WriteObjectVersion.3
+CATLINKS+=AG_Version.cat3:AG_WriteObjectVersion.cat3
