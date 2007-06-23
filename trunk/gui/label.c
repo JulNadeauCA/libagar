dcead4047b5df8fe7d883b9d7700d8faf5490cbe
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2007-06-23 12:02:05 +0000

    - implement LABEL_NOMINSIZE flag; display "..." for flags that are   too wide to display fully. - implement flags,flags8,flags16 and flags32 format specifiers, to   display bitmasks/flags in text format.

--- trunk/gui/label.c
+++ trunk/gui/label.c
@@ -154,80 +154,104 @@ AG_LabelNewStatic(void *parent, Uint flags, const char *fmt, ...)
 AG_Label *
 AG_LabelNewStaticString(void *parent, Uint flags, const char *text)
 {
-	AG_Label *label;
+	AG_Label *lbl;
 	
-	label = Malloc(sizeof(AG_Label), M_OBJECT);
-	AG_LabelInit(label, AG_LABEL_STATIC, flags, text);
-	AG_ObjectAttach(parent, label);
-	return (label);
+	lbl = Malloc(sizeof(AG_Label), M_OBJECT);
+	AG_LabelInit(lbl, AG_LABEL_STATIC, flags, text);
+	AG_ObjectAttach(parent, lbl);
+	return (lbl);
 }
 
 void
 AG_LabelScale(void *p, int rw, int rh)
 {
-	AG_Label *lab = p;
+	AG_Label *lbl = p;
 
 	if (rw == -1 && rh == -1) {
-		switch (lab->type) {
+		if (lbl->flags & AG_LABEL_NOMINSIZE) {
+			AGWIDGET(lbl)->w = lbl->lPad + lbl->rPad;
+			AGWIDGET(lbl)->h = agTextFontHeight +
+			                   lbl->tPad + lbl->bPad;
+			return;
+		}
+		switch (lbl->type) {
 		case AG_LABEL_STATIC:
-			AG_MutexLock(&lab->lock);
-			if (lab->wPre != -1) {
-				
+			AG_MutexLock(&lbl->lock);
+			if (lbl->surface != -1) {
+				AGWIDGET(lbl)->w =
+				    AGWIDGET_SURFACE(lbl,lbl->surface)->w +
+				    lbl->lPad + lbl->rPad;
+				AGWIDGET(lbl)->h =
+				    AGWIDGET_SURFACE(lbl,lbl->surface)->h +
+				    lbl->tPad + lbl->bPad;
 			}
-			if (lab->surface != -1) {
-				AGWIDGET(lab)->w =
-				    AGWIDGET_SURFACE(lab,lab->surface)->w +
-				    lab->lPad + lab->rPad;
-				AGWIDGET(lab)->h =
-				    AGWIDGET_SURFACE(lab,lab->surface)->h +
-				    lab->tPad + lab->bPad;
-			}
-			AG_MutexUnlock(&lab->lock);
+			AG_MutexUnlock(&lbl->lock);
 			break;
 		case AG_LABEL_POLLED:
 		case AG_LABEL_POLLED_MT:
 			if (rh == -1 && rh == -1) {
-				AGWIDGET(lab)->w = lab->wPre + lab->lPad + lab->rPad;
-				AGWIDGET(lab)->h = lab->hPre + lab->tPad + lab->bPad;
+				AGWIDGET(lbl)->w = lbl->wPre + lbl->lPad +
+				                   lbl->rPad;
+				AGWIDGET(lbl)->h = lbl->hPre + lbl->tPad +
+				                   lbl->bPad;
 			}
 			break;
 		}
 	}
+	if ((lbl->flags & AG_LABEL_NOMINSIZE) && lbl->surface != -1) {
+		SDL_Surface *su = AGWIDGET_SURFACE(lbl,lbl->surface);
+
+		if ((su->w + lbl->lPad+lbl->rPad) > rw ||
+		    (su->h + lbl->tPad+lbl->bPad) > rh) {
+			lbl->flags |= AG_LABEL_PARTIAL;
+			if (lbl->surfaceCont == -1) {
+				/* XXX this should be shared between widgets */
+				lbl->surfaceCont = AG_WidgetMapSurface(lbl,
+				    AG_TextRender(NULL, -1,
+				    AG_COLOR(TEXT_COLOR), " ... "));
+			}
+		} else {
+			lbl->flags &= ~AG_LABEL_PARTIAL;
+		}
+		AGWIDGET(lbl)->w = rw;
+		AGWIDGET(lbl)->h = rh;
+	}
 }
 
 void
-AG_LabelInit(AG_Label *label, enum ag_label_type type, Uint flags,
+AG_LabelInit(AG_Label *lbl, enum ag_label_type type, Uint flags,
     const char *s)
 {
-	int wflags = AG_WIDGET_CLIPPING;
+	int wflags = 0;
 
 	if (flags & AG_LABEL_HFILL) { wflags |= AG_WIDGET_HFILL; }
 	if (flags & AG_LABEL_VFILL) { wflags |= AG_WIDGET_VFILL; }
 
-	AG_WidgetInit(label, "label", &agLabelOps, wflags);
-	label->type = type;
-	label->flags = flags;
-	label->lPad = 2;
-	label->rPad = 2;
-	label->tPad = 0;
-	label->bPad = 1;
-	label->wPre = -1;
-	label->hPre = -1;
+	AG_WidgetInit(lbl, "label", &agLabelOps, wflags);
+	lbl->type = type;
+	lbl->flags = flags;
+	lbl->lPad = 2;
+	lbl->rPad = 2;
+	lbl->tPad = 0;
+	lbl->bPad = 1;
+	lbl->wPre = -1;
+	lbl->hPre = -1;
+	lbl->surfaceCont = -1;
+	SLIST_INIT(&lbl->lflags);
 
 	switch (type) {
 	case AG_LABEL_STATIC:
-		label->surface = (s == NULL) ? -1 : AG_WidgetMapSurface(label,
+		lbl->surface = (s == NULL) ? -1 : AG_WidgetMapSurface(lbl,
 		    AG_TextRender(NULL, -1, AG_COLOR(TEXT_COLOR), s));
-		AG_MutexInit(&label->lock);
+		AG_MutexInit(&lbl->lock);
 		break;
 	case AG_LABEL_POLLED:
 	case AG_LABEL_POLLED_MT:
-		AG_LabelPrescale(label, 1, "XXXXXXXXXXXXXXXXX");
-		label->surface = -1;
-		strlcpy(label->poll.fmt, s, sizeof(label->poll.fmt));
-		memset(label->poll.ptrs, 0,
-		    sizeof(void *)*AG_LABEL_MAX_POLLPTRS);
-		label->poll.nptrs = 0;
+		AG_LabelPrescale(lbl, 1, "XXXXXXXXXXXXXXXXX");
+		lbl->surface = -1;
+		strlcpy(lbl->poll.fmt, s, sizeof(lbl->poll.fmt));
+		memset(lbl->poll.ptrs, 0, sizeof(void *)*AG_LABEL_MAX_POLLPTRS);
+		lbl->poll.nptrs = 0;
 		break;
 	}
 }
@@ -245,41 +269,41 @@ AG_LabelPrescale(AG_Label *lab, Uint nlines, const char *text)
 }
 
 void
-AG_LabelSetSurface(AG_Label *label, SDL_Surface *su)
+AG_LabelSetSurface(AG_Label *lbl, SDL_Surface *su)
 {
 #ifdef DEBUG
-	if (label->type != AG_LABEL_STATIC)
+	if (lbl->type != AG_LABEL_STATIC)
 		fatal("label is not static");
 #endif
-	AG_MutexLock(&label->lock);
-	AG_WidgetReplaceSurface(label, 0, su);
-	AG_MutexUnlock(&label->lock);
+	AG_MutexLock(&lbl->lock);
+	AG_WidgetReplaceSurface(lbl, 0, su);
+	AG_MutexUnlock(&lbl->lock);
 }
 
 void
-AG_LabelSetPadding(AG_Label *label, int lPad, int rPad, int tPad, int bPad)
+AG_LabelSetPadding(AG_Label *lbl, int lPad, int rPad, int tPad, int bPad)
 {
-	if (lPad != -1) { label->lPad = lPad; }
-	if (rPad != -1) { label->rPad = rPad; }
-	if (tPad != -1) { label->tPad = tPad; }
-	if (bPad != -1) { label->bPad = bPad; }
+	if (lPad != -1) { lbl->lPad = lPad; }
+	if (rPad != -1) { lbl->rPad = rPad; }
+	if (tPad != -1) { lbl->tPad = tPad; }
+	if (bPad != -1) { lbl->bPad = bPad; }
 }
 
 void
-AG_LabelPrintf(AG_Label *label, const char *fmt, ...)
+AG_LabelPrintf(AG_Label *lbl, const char *fmt, ...)
 {
 	char s[AG_LABEL_MAX];
 	va_list args;
 
 #ifdef DEBUG
-	if (label->type != AG_LABEL_STATIC)
+	if (lbl->type != AG_LABEL_STATIC)
 		fatal("label is not static");
 #endif
 	va_start(args, fmt);
 	vsnprintf(s, sizeof(s), fmt, args);
 	va_end(args);
 	
-	AG_LabelSetSurface(label, (s[0]=='\0') ? NULL :
+	AG_LabelSetSurface(lbl, (s[0]=='\0') ? NULL :
 	    AG_TextRender(NULL, -1, AG_COLOR(TEXT_COLOR), s));
 }
 
@@ -333,7 +357,6 @@ static void
 label_objt(AG_Label *label, char *s, size_t len, int ri)
 {
 	AG_Object *ob = LABEL_ARG(AG_Object *);
-
 	snprintf(s, len, "%s", ob->ops->type);
 }
 
@@ -341,7 +364,6 @@ static void
 label_wxh(AG_Label *label, char *s, size_t len, int ri)
 {
 	SDL_Rect *rd = &LABEL_ARG(SDL_Rect);
-
 	snprintf(s, len, "%ux%u", rd->w, rd->h);
 }
 
@@ -349,7 +371,6 @@ static void
 label_xy(AG_Label *label, char *s, size_t len, int ri)
 {
 	SDL_Rect *rd = &LABEL_ARG(SDL_Rect);
-
 	snprintf(s, len, "%d,%d", rd->x, rd->y);
 }
 
@@ -357,7 +378,6 @@ static void
 label_rect(AG_Label *label, char *s, size_t len, int ri)
 {
 	SDL_Rect *rd = &LABEL_ARG(SDL_Rect);
-
 	snprintf(s, len, "%ux%u at %d,%d", rd->w, rd->h, rd->x, rd->y);
 }
 
@@ -365,8 +385,68 @@ static void
 label_int_bool(AG_Label *label, char *s, size_t len, int ri)
 {
 	int *flag = &LABEL_ARG(int);
-
 	snprintf(s, len, "%s", *flag ? _("yes") : _("no"));
+}
+
+static void
+label_flags(AG_Label *label, char *s, size_t len, int ri)
+{
+	Uint *flags = &LABEL_ARG(Uint);
+	AG_LabelFlag *lfl;
+
+	s[0] = '\0';
+	SLIST_FOREACH(lfl, &label->lflags, lflags) {
+		if (lfl->idx == ri && *flags & (Uint)lfl->v) {
+			if (s[0] != '\0') { strlcat(s, ", ", len); }
+			strlcat(s, lfl->text, len);
+		}
+	}
+}
+
+
+static void
+label_flags8(AG_Label *label, char *s, size_t len, int ri)
+{
+	Uint8 *flags = &LABEL_ARG(Uint8);
+	AG_LabelFlag *lfl;
+
+	s[0] = '\0';
+	SLIST_FOREACH(lfl, &label->lflags, lflags) {
+		if (lfl->idx == ri && *flags & (Uint8)lfl->v) {
+			if (s[0] != '\0') { strlcat(s, ", ", len); }
+			strlcat(s, lfl->text, len);
+		}
+	}
+}
+
+static void
+label_flags16(AG_Label *label, char *s, size_t len, int ri)
+{
+	Uint16 *flags = &LABEL_ARG(Uint16);
+	AG_LabelFlag *lfl;
+
+	s[0] = '\0';
+	SLIST_FOREACH(lfl, &label->lflags, lflags) {
+		if (lfl->idx == ri && *flags & (Uint16)lfl->v) {
+			if (s[0] != '\0') { strlcat(s, ", ", len); }
+			strlcat(s, lfl->text, len);
+		}
+	}
+}
+
+static void
+label_flags32(AG_Label *label, char *s, size_t len, int ri)
+{
+	Uint32 *flags = &LABEL_ARG(Uint32);
+	AG_LabelFlag *lfl;
+
+	s[0] = '\0';
+	SLIST_FOREACH(lfl, &label->lflags, lflags) {
+		if (lfl->idx == ri && *flags & (Uint32)lfl->v) {
+			if (s[0] != '\0') { strlcat(s, ", ", len); }
+			strlcat(s, lfl->text, len);
+		}
+	}
 }
 
 static const struct {
@@ -386,6 +466,10 @@ static const struct {
 	{ "x,y", sizeof("x,y"),		label_xy },
 	{ "rect", sizeof("rect"),	label_rect },
 	{ "ibool", sizeof("ibool"),	label_int_bool },
+	{ "flags", sizeof("flags"),	label_flags },
+	{ "flags8", sizeof("flags8"),	label_flags8 },
+	{ "flags16", sizeof("flags16"),	label_flags16 },
+	{ "flags32", sizeof("flags32"),	label_flags32 },
 };
 static const int nfmts = sizeof(fmts) / sizeof(fmts[0]);
 
@@ -394,7 +478,7 @@ static void
 AG_LabelDrawPolled(AG_Label *label)
 {
 	char s[AG_LABEL_MAX];
-	char s2[32];
+	char s2[AG_LABEL_MAX];
 	SDL_Surface *ts;
 	char *fmtp;
 	int i, ri = 0;
@@ -565,34 +649,68 @@ AG_LabelDrawPolled(AG_Label *label)
 void
 AG_LabelDraw(void *p)
 {
-	AG_Label *label = p;
-	
-	switch (label->type) {
+	AG_Label *lbl = p;
+	int wClip;
+
+	if (AGWIDGET(lbl)->w == 0 || AGWIDGET(lbl)->h == 0)
+		return;
+
+	wClip = AGWIDGET(lbl)->w;
+	if (lbl->flags & AG_LABEL_PARTIAL) {
+		wClip -= AGWIDGET_SURFACE(lbl,lbl->surfaceCont)->w;
+	}
+	AG_WidgetPushClipRect(lbl, 0, 0, wClip, AGWIDGET(lbl)->h);
+
+	switch (lbl->type) {
 	case AG_LABEL_STATIC:
-		AG_MutexLock(&label->lock);
-		AG_WidgetBlitSurface(label, label->surface,
-		    label->lPad, label->tPad);
-		AG_MutexUnlock(&label->lock);
+		AG_MutexLock(&lbl->lock);
+		AG_WidgetBlitSurface(lbl, lbl->surface, lbl->lPad, lbl->tPad);
+		AG_MutexUnlock(&lbl->lock);
 		break;
 	case AG_LABEL_POLLED:
-		AG_LabelDrawPolled(label);
+		AG_LabelDrawPolled(lbl);
 		break;
 	case AG_LABEL_POLLED_MT:
-		AG_MutexLock(label->poll.lock);
-		AG_LabelDrawPolled(label);
-		AG_MutexUnlock(label->poll.lock);
+		AG_MutexLock(lbl->poll.lock);
+		AG_LabelDrawPolled(lbl);
+		AG_MutexUnlock(lbl->poll.lock);
 		break;
 	}
+
+	AG_WidgetPopClipRect(lbl);
+	if (lbl->flags & AG_LABEL_PARTIAL)
+		AG_WidgetBlitSurface(lbl, lbl->surfaceCont, wClip, lbl->tPad);
 }
 
 void
 AG_LabelDestroy(void *p)
 {
-	AG_Label *label = p;
+	AG_Label *lbl = p;
+	AG_LabelFlag *lfl, *lflNext;
 
-	if (label->type == AG_LABEL_STATIC) {
-		AG_MutexDestroy(&label->lock);
+	if (lbl->type == AG_LABEL_STATIC) {
+		AG_MutexDestroy(&lbl->lock);
 	}
-	AG_WidgetDestroy(label);
+	for (lfl = SLIST_FIRST(&lbl->lflags);
+	     lfl != SLIST_END(&lbl->lflags);
+	     lfl = lflNext) {
+		lflNext = SLIST_NEXT(lfl, lflags);
+		Free(lfl, M_WIDGET);
+	}
+	AG_WidgetDestroy(lbl);
 }
 
+/* Register a flag description text. */
+void
+AG_LabelFlagNew(AG_Label *lbl, Uint idx, const char *text,
+    enum ag_widget_binding_type type, Uint32 v)
+{
+	AG_LabelFlag *lfl;
+
+	lfl = Malloc(sizeof(AG_LabelFlag), M_WIDGET);
+	lfl->idx = idx;
+	lfl->text = text;
+	lfl->type = type;
+	lfl->v = v;
+	SLIST_INSERT_HEAD(&lbl->lflags, lfl, lflags);
+}
--- trunk/gui/label.h
+++ trunk/gui/label.h
@@ -20,23 +20,35 @@ enum ag_label_type {
 	AG_LABEL_POLLED_MT		/* Polling (thread safe) */
 };
 
+typedef struct ag_label_flag {
+	Uint idx;			/* Flag arg in format string */
+	const char *text;		/* Label text */
+	Uint32 v;			/* Bitmask */
+	enum ag_widget_binding_type type;
+	SLIST_ENTRY(ag_label_flag) lflags;
+} AG_LabelFlag;
+
 typedef struct ag_label {
 	struct ag_widget wid;
 	enum ag_label_type type;
 	Uint flags;
-#define AG_LABEL_HFILL		0x01		/* Fill horizontal space */
-#define AG_LABEL_VFILL		0x02		/* Fill vertical space */
+#define AG_LABEL_HFILL		0x01	/* Fill horizontal space */
+#define AG_LABEL_VFILL		0x02	/* Fill vertical space */
+#define AG_LABEL_NOMINSIZE	0x04	/* No minimum enforced size */
+#define AG_LABEL_PARTIAL	0x10	/* Partial mode (RO) */
 #define AG_LABEL_EXPAND		(AG_LABEL_HFILL|AG_LABEL_VFILL)
 	AG_Mutex lock;
-	int surface;
-	int wPre, hPre;				/* Prescale dimensions */
-	int lPad, rPad, tPad, bPad;		/* Label padding */
+	int surface;			/* Label surface */
+	int surfaceCont;		/* [...] surface */
+	int wPre, hPre;			/* Prescale dimensions */
+	int lPad, rPad, tPad, bPad;	/* Label padding */
 	struct {
-		char fmt[AG_LABEL_MAX];
-		void *ptrs[AG_LABEL_MAX_POLLPTRS];
+		char fmt[AG_LABEL_MAX];		   /* Poll format string */
+		void *ptrs[AG_LABEL_MAX_POLLPTRS]; /* Polled data ptrs */
 		int nptrs;
-		AG_Mutex *lock;
+		AG_Mutex *lock;			   /* Lock protecting data */
 	} poll;
+	SLIST_HEAD(,ag_label_flag) lflags;	/* Label flag descriptions */
 } AG_Label;
 
 __BEGIN_DECLS
@@ -63,6 +75,15 @@ void	 AG_LabelSetPadding(AG_Label *, int, int, int, int);
 #define	 AG_LabelSetPaddingTop(lbl,v)    AG_LabelSetPadding(lbl,-1,-1,(v),-1)
 #define	 AG_LabelSetPaddingBottom(lbl,v) AG_LabelSetPadding(lbl,-1,-1,-1,(v))
 void	 AG_LabelPrescale(AG_Label *, Uint, const char *);
+
+void	 AG_LabelFlagNew(AG_Label *, Uint, const char *,
+	                 enum ag_widget_binding_type, Uint32);
+#define	 AG_LabelFlag8(lbl,i,s,v) \
+	 AG_LabelFlagNew((lbl),(i),(s),AG_WIDGET_FLAG8,(Uint8)(v))
+#define	 AG_LabelFlag16(lbl,i,s,v) \
+	 AG_LabelFlagNew((lbl),(i),(s),AG_WIDGET_FLAG16,(Uint16)(v))
+#define	 AG_LabelFlag32(lbl,i,s,v) \
+	 AG_LabelFlagNew((lbl),(i),(s),AG_WIDGET_FLAG32,(Uint32)(v))
 __END_DECLS
 
 #include "close_code.h"
