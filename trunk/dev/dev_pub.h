25cce713d81065a64bfd0d3ddc20ef10ae036f32
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2007-08-04 09:34:50 +0000

    public dev.h

---
+++ trunk/dev/dev_pub.h
@@ -0,0 +1,8 @@
+/*	Public domain	*/
+
+#ifndef _AGAR_DEV_PUBLIC_H_
+#define _AGAR_DEV_PUBLIC_H_
+#include <agar/core/core_begin.h>
+#include <agar/dev/dev.h>
+#include <agar/core/core_close.h>
+#endif
