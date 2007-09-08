c24825457c336d179903fa9084adeaf4ba52924f
Julien Nadeau Carriere <vedge@hypertriton.com>
2007-09-08 16:21:42 +0000

    add angle dimensions
    git-svn-id: https://dev.csoft.net/agar/trunk@6301 86a84d31-9702-0410-8f1e-a60cab7bd424

--- sg/sk_dimension.h
+++ sg/sk_dimension.h
@@ -2,8 +2,15 @@
 
 #define SK_DIMENSION_TEXT_MAX	256
 
+enum sk_dimension_type {
+	SK_DIMENSION_DISTANCE,		/* Linear distance */
+	SK_DIMENSION_ANGLE_ENDPOINT,	/* Angle at shared endpoint */
+	SK_DIMENSION_ANGLE_INTERSECT	/* Angle at line intersection */
+};
+
 typedef struct sk_dimension {
 	struct sk_annot annot;
+	enum sk_dimension_type type;	/* Type of dimension */
 	const AG_Unit *unit;		/* Length/angle unit (NULL = default) */
 	SK_Node *n1, *n2;		/* Dimension between nodes */
 	SG_Color cLbl;			/* Color of label text */
