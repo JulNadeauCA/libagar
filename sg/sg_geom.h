80a6dc16ecb83cdba1378e09fd9e4b6570d9c262
Julien Nadeau Carriere <vedge@hypertriton.com>
2007-09-08 16:24:41 +0000

    use the origin/direction/scalar representation for line segments/halflines.
    git-svn-id: https://dev.csoft.net/agar/trunk@6304 86a84d31-9702-0410-8f1e-a60cab7bd424

--- sg/sg_geom.h
+++ sg/sg_geom.h
@@ -1,9 +1,8 @@
 /*	Public domain	*/
 
 __BEGIN_DECLS
-SG_Line2	SG_Line2From2Pts(SG_Vector, SG_Vector);
-SG_Line		SG_LineFrom2Pts(SG_Vector, SG_Vector);
-SG_Real		SG_DistanceFromLine2Pts(SG_Vector, SG_Vector, SG_Vector);
+SG_Line2	SG_LineFromPts2(SG_Vector2, SG_Vector2);
+SG_Line		SG_LineFromPts(SG_Vector, SG_Vector);
 
 SG_Plane		SG_PlaneFromNormal(SG_Vector, SG_Real);
 SG_Plane		SG_PlaneFrom3Pts(SG_Vector, SG_Vector, SG_Vector);
@@ -14,4 +13,8 @@ int	 		SG_PlaneIsValid(SG_Plane);
 __inline__ SG_Vector	SG_PlaneNorm(SG_Plane);
 __inline__ SG_Vector	SG_PlaneNormp(const SG_Plane *);
 __inline__ SG_Real	SG_VectorPlaneAngle(SG_Vector, SG_Plane);
+
+SG_Intersect2	SG_IntersectLineLine2(SG_Line2, SG_Line2);
+SG_Real		SG_PointLineDistance2(SG_Vector2, SG_Line2);
+SG_Real		SG_LineLineAngle2(SG_Line2, SG_Line2);
 __END_DECLS
