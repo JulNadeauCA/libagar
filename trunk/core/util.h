47af409904218dcaa447a8eeb236c75fa07ee781
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2007-10-10 07:58:30 +0000

    rename

---
+++ trunk/core/util.h
@@ -0,0 +1,15 @@
+/*	Public domain	*/
+
+#ifndef _AGAR_CORE_UTIL_H_
+#define _AGAR_CORE_UTIL_H_
+#include "begin_code.h"
+
+__BEGIN_DECLS
+__inline__ int AG_PowOf2i(int);
+__inline__ int AG_Truncf(double);
+__inline__ double AG_Fracf(double);
+__inline__ double AG_FracInvf(double);
+__END_DECLS
+
+#include "close_code.h"
+#endif /* _AGAR_CORE_UTIL_H_ */
