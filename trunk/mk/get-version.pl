1c03e9c5d41b0a1b158aa435d1aadefc731d0b13
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2007-10-14 13:58:29 +0000

    fix

--- trunk/mk/get-version.pl
+++ trunk/mk/get-version.pl
@@ -5,7 +5,7 @@ my $found = 0;
 open(CONFIG, "configure.in") || die "configure.in: $!";
 foreach $_ (<CONFIG>) {
 	chop;
-	if (/^HDEFINE\(VERSION,\s*"\\"([\w+\.\-]+)\\""\)$/) {
+	if (/^\s*HDEFINE\s*\(VERSION,\s*\"([\w+\.\-]+)\"\s*\)\s*$/) {
 		print $1, "\n";
 		$found++;
 	}
