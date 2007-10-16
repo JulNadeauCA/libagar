d8d5c1ba52a20a73f7220d7aca768a775a799640
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2007-10-16 23:32:49 +0000

    CameraNewDuplicate()

--- trunk/sg/sg_camera.h
+++ trunk/sg/sg_camera.h
@@ -44,6 +44,7 @@ __BEGIN_DECLS
 extern SG_NodeOps sgCameraOps;
 
 SG_Camera	*SG_CameraNew(void *, const char *);
+SG_Camera	*SG_CameraNewDuplicate(void *, const char *, const SG_Camera *);
 void		 SG_CameraInit(void *, const char *);
 int		 SG_CameraLoad(void *, AG_Netbuf *);
 int		 SG_CameraSave(void *, AG_Netbuf *);
