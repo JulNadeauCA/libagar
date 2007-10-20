caf5cd93a5dc9215ffdfde06a03df6f89a06aea7
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2007-10-20 10:36:35 +0000

    allow normals/colors/texcoords to be ignored. only detect duplicates if requested.

--- trunk/sg/sg_load_ply.h
+++ trunk/sg/sg_load_ply.h
@@ -1,6 +1,11 @@
 /*	$Csoft$	*/
 /*	Public domain	*/
 
+#define SG_PLY_LOAD_VTX_NORMALS	0x01	/* Load vertex normals */
+#define SG_PLY_LOAD_VTX_COLORS	0x02	/* Load vertex colors */
+#define SG_PLY_LOAD_TEXCOORDS	0x04	/* Load texture coordinates */
+#define SG_PLY_DUP_VERTICES	0x08	/* Eliminate duplicate vertices */
+
 __BEGIN_DECLS
-int SG_ObjectLoadPLY(void *, const char *);
+int SG_ObjectLoadPLY(void *, const char *, Uint);
 __END_DECLS
