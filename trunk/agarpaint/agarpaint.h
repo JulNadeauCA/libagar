27270b03b967a0324a97abfb17739851d5ed1d8c
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2007-10-23 11:04:10 +0000

    i18n

---
+++ trunk/agarpaint/agarpaint.h
@@ -0,0 +1,35 @@
+#ifndef _AGARPAINT_H_
+#define _AGARPAINT_H_
+
+#include <agar/compat/strlcpy.h>
+#include <agar/compat/strlcat.h>
+
+#include <agar/config/_mk_have_unsigned_typedefs.h>
+#ifndef _MK_HAVE_UNSIGNED_TYPEDEFS
+#define _MK_HAVE_UNSIGNED_TYPEDEFS
+typedef unsigned int Uint;
+typedef unsigned int Uchar;
+typedef unsigned long Ulong;
+#endif
+
+#include <agar/config/enable_nls.h>
+#ifdef ENABLE_NLS
+# include <agar/libintl/libintl.h>
+# define _(String) gettext(String)
+# define gettext_noop(String) (String)
+# define N_(String) gettext_noop(String)
+#else
+# undef _
+# undef N_
+# undef textdomain
+# undef bindtextdomain
+# define _(s) (s)
+# define N_(s) (s)
+# define textdomain(d)
+# define bindtextdomain(p, d)
+#endif
+
+#define Malloc AG_Malloc
+#define Free AG_Free
+
+#endif /* _AGARPAINT_H_ */
