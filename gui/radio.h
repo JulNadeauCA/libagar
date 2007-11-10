d5409f57deee7e1cea549c40e35e25294317154f
Julien Nadeau Carriere <vedge@hypertriton.com>
2007-11-10 04:22:51 +0000

    rework
    git-svn-id: https://dev.csoft.net/agar/trunk@6722 86a84d31-9702-0410-8f1e-a60cab7bd424

--- gui/radio.h
+++ gui/radio.h
@@ -11,6 +11,12 @@
 
 #include "begin_code.h"
 
+typedef struct ag_radio_item {
+	char text[128];
+	int surface;
+	SDLKey hotkey;
+} AG_RadioItem;
+
 typedef struct ag_radio {
 	struct ag_widget wid;
 	Uint flags;
@@ -18,9 +24,8 @@ typedef struct ag_radio {
 #define AG_RADIO_VFILL	0x02
 #define AG_RADIO_EXPAND (AG_RADIO_HFILL|AG_RADIO_VFILL)
 	int value;		/* Default value binding */
-	int *labels;		/* Label surface IDs */
-	const char **itemText;
-	int nitems;
+	AG_RadioItem *items;
+	int nItems;
 	int selitem;		/* Index of selected item */
 	int max_w;		/* Width of widest label */
 	int oversel;		/* Overlapping selection */
@@ -31,7 +36,12 @@ typedef struct ag_radio {
 
 __BEGIN_DECLS
 extern const AG_WidgetOps agRadioOps;
+
 AG_Radio *AG_RadioNew(void *, Uint, const char *[]);
+void      AG_RadioItemsFromArray(AG_Radio *, const char **);
+int       AG_RadioAddItem(AG_Radio *, const char *, ...);
+int       AG_RadioAddItemHK(AG_Radio *, SDLKey, const char *, ...);
+void      AG_RadioClearItems(AG_Radio *);
 __END_DECLS
 
 #include "close_code.h"
