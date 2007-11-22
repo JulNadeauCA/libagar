4b0a1ee53ef4401625ddc95259c35123986c4fe6
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2007-11-22 16:14:17 +0000

    consistency; avoid the need for _GNU_SOURCE.

--- trunk/core/vasprintf.h
+++ trunk/core/vasprintf.h
@@ -1,30 +1,7 @@
 /*	Public domain	*/
 
-#ifdef _AGAR_INTERNAL
-#include <config/have_format_attribute.h>
-#include <config/have_nonnull_attribute.h>
-#else
-#include <agar/config/have_format_attribute.h>
-#include <agar/config/have_nonnull_attribute.h>
-#endif
-
-#ifndef FORMAT_ATTRIBUTE
-# ifdef HAVE_FORMAT_ATTRIBUTE
-#  define FORMAT_ATTRIBUTE(t, a, b) __attribute__((__format__ (t,a,b)))
-# else
-#  define FORMAT_ATTRIBUTE(t, a, b)
-# endif
-#endif
-#ifndef NONNULL_ATTRIBUTE
-# ifdef HAVE_NONNULL_ATTRIBUTE
-#  define NONNULL_ATTRIBUTE(a) __attribute__((__nonnull__ (a)))
-# else
-#  define NONNULL_ATTRIBUTE(a)
-# endif
-#endif
-
 #include <stdarg.h>
 
-int AG_Vasprintf(char **, const char *, va_list)
-    FORMAT_ATTRIBUTE(printf, 2, 0)
-    NONNULL_ATTRIBUTE(2);
+__BEGIN_DECLS
+int AG_Vasprintf(char **, const char *, va_list);
+__END_DECLS
