6f35ae9bb962532435f0c0fc459f029a10a02b18
Julien Nadeau Carriere <vedge@hypertriton.com>
2007-12-06 06:31:33 +0000

    Linux/i386 package flavors
    git-svn-id: https://dev.csoft.net/agar/trunk@6969 86a84d31-9702-0410-8f1e-a60cab7bd424

---
+++ Packages/linux-i386-nofreetype/agar-config.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# Static agar-config for Linux/i386 binary package.
+# Public domain
+#
+
+PREFIX="%PREFIX%"
+
+if [ "$1" = "--version" ]; then
+	echo "%INSTALLED_VERSION%"
+fi
+if [ "$1" = "--release" ]; then
+	echo "%INSTALLED_RELEASE%"
+fi
+if [ "$1" = "--prefix" ]; then
+	echo "${PREFIX}"
+fi
+if [ "$1" = "--cflags" ]; then
+	echo "-I${PREFIX}/include/agar -I${PREFIX}/include/SDL -I/usr/include/SDL -I/usr/local/include/SDL -I/usr/X11R6/include/SDL -D_GNU_SOURCE=1 -D_REENTRANT -I/usr/X11R6/include"
+fi
+if [ "$1" = "--libs" ]; then
+	echo "-L${PREFIX}/lib -L/usr/local/lib -L/usr/X11R6/lib -L/usr/lib -lag_gui -lag_core -lag_net -lSDL -lGL -lm -lpthread"
+fi
