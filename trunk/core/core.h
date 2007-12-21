133fd15d683f6ba380d45a2c5bb26b2eaee3bdf0
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2007-12-21 10:33:47 +0000

    AG_BYTEORDER must be defined prior to load_*.h inclusion!

--- trunk/core/core.h
+++ trunk/core/core.h
@@ -70,6 +70,20 @@ typedef unsigned long Ulong;
 # endif
 #endif
 
+#define AG_BIG_ENDIAN 4321
+#define AG_LITTLE_ENDIAN 1234
+#include <config/_mk_big_endian.h>
+#include <config/_mk_little_endian.h>
+#if defined(_MK_BIG_ENDIAN)
+# define AG_BYTEORDER AG_BIG_ENDIAN
+#elif defined(_MK_LITTLE_ENDIAN)
+# define AG_BYTEORDER AG_LITTLE_ENDIAN
+#else
+# error "Byte order is unknown"
+#endif
+#undef _MK_BIG_ENDIAN
+#undef _MK_LITTLE_ENDIAN
+
 #include <core/error.h>
 #include <core/queue.h>
 
@@ -135,20 +149,6 @@ typedef unsigned long Ulong;
 #ifndef MAX3
 #define	MAX3(a,b,c) MAX((a),MAX((b),(c)))
 #endif
-
-#define AG_BIG_ENDIAN 4321
-#define AG_LITTLE_ENDIAN 1234
-#include <config/_mk_big_endian.h>
-#include <config/_mk_little_endian.h>
-#if defined(_MK_BIG_ENDIAN)
-# define AG_BYTEORDER AG_BIG_ENDIAN
-#elif defined(_MK_LITTLE_ENDIAN)
-# define AG_BYTEORDER AG_LITTLE_ENDIAN
-#else
-# error "Byte order is unknown"
-#endif
-#undef _MK_BIG_ENDIAN
-#undef _MK_LITTLE_ENDIAN
 
 #ifdef _MSC_VER
 #pragma warning(disable: 4018)
