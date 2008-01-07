920372b456e19ce1bb2843e15f6e87a84de7dc9b
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2008-01-07 14:40:10 +0000

    - scale properly to very small sizes. - add effective and requested button width parameters. - implement ScrollbarSetIncFn() and ScrollbarSetDecFn() - add scrollbar-drag-{begin,end} events.

--- trunk/gui/scrollbar.h
+++ trunk/gui/scrollbar.h
@@ -16,6 +16,13 @@ enum ag_scrollbar_type {
 	AG_SCROLLBAR_VERT
 };
 
+enum ag_scrollbar_button {
+	AG_SCROLLBAR_BUTTON_NONE,
+	AG_SCROLLBAR_BUTTON_DEC,
+	AG_SCROLLBAR_BUTTON_INC,
+	AG_SCROLLBAR_BUTTON_SCROLL
+};
+
 typedef struct ag_scrollbar {
 	struct ag_widget wid;
 	Uint flags;
@@ -27,10 +34,13 @@ typedef struct ag_scrollbar {
 	int min, max;			/* Default range binding */
 	int visible;			/* Number of visible items */
 	enum ag_scrollbar_type type;
-	int bw;				/* Scroll button size */
-	int curbutton;			/* Button held */
+	int bw;				/* Effective button size */
+	int bwDefault;			/* Default button size */
+	enum ag_scrollbar_button curBtn; /* Active button */
 	int barSz;			/* Scroll bar size */
 	int arrowSz;			/* Arrow height */
+	AG_Event *buttonIncFn;		/* Alt. handler for increment btns */
+	AG_Event *buttonDecFn;		/* Alt. handler for decrement btns */
 } AG_Scrollbar;
 
 #define AGSCROLLBAR(p) ((AG_Scrollbar *)p)
@@ -40,16 +50,43 @@ extern AG_WidgetClass agScrollbarClass;
 
 AG_Scrollbar *AG_ScrollbarNew(void *, enum ag_scrollbar_type, Uint);
 int           AG_ScrollbarVisible(AG_Scrollbar *);
+void          AG_ScrollbarSetIncFn(AG_Scrollbar *, AG_EventFn, const char *,
+                                   ...);
+void          AG_ScrollbarSetDecFn(AG_Scrollbar *, AG_EventFn, const char *,
+                                   ...);
 
 static __inline__ void
 AG_ScrollbarSetBarSize(AG_Scrollbar *sb, int bsize)
 {
+	AG_ObjectLock(sb);
 	sb->barSz = (bsize > 10 || bsize == -1) ? bsize : 10;
+	AG_ObjectUnlock(sb);
+}
+static __inline__ int
+AG_ScrollbarGetBarSize(AG_Scrollbar *sb)
+{
+	int rv;
+	AG_ObjectLock(sb);
+	rv = sb->barSz;
+	AG_ObjectUnlock(sb);
+	return (rv);
 }
 static __inline__ void
-AG_ScrollbarGetBarSize(AG_Scrollbar *sb, int *bsize)
+AG_ScrollbarSetButtonSize(AG_Scrollbar *sb, int bw)
 {
-	*bsize = sb->barSz;
+	AG_ObjectLock(sb);
+	sb->bwDefault = bw;
+	sb->bw = bw;
+	AG_ObjectUnlock(sb);
+}
+static __inline__ int
+AG_ScrollbarGetButtonSize(AG_Scrollbar *sb)
+{
+	int rv;
+	AG_ObjectLock(sb);
+	rv = sb->bw;
+	AG_ObjectUnlock(sb);
+	return (rv);
 }
 __END_DECLS
 
