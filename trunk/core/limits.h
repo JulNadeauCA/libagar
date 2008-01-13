2ca56f707febe988ac5aa021e9aa3668f4cfd5d5
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2008-01-13 11:29:39 +0000

    - use the compiler __FOO_MIN__ and __FOO_MAX__ if available. - add long double limits.

--- trunk/core/limits.h
+++ trunk/core/limits.h
@@ -3,13 +3,15 @@
 #ifdef _AGAR_INTERNAL
 #include <config/_mk_have_limits_h.h>
 #include <config/have_long_long.h>
+#include <config/have_long_double.h>
 #else
 #include <agar/config/_mk_have_limits_h.h>
 #include <agar/config/have_long_long.h>
+#include <agar/config/have_long_double.h>
 #endif
 
-#ifndef _COMPAT_LIMITS_H
-#define _COMPAT_LIMITS_H
+#ifndef _AGAR_CORE_LIMITS_H
+#define _AGAR_CORE_LIMITS_H
 
 #ifdef _MK_HAVE_LIMITS_H
 # include <limits.h>
@@ -39,9 +41,37 @@
 # define AG_LLONG_MAX	0x7fffffffffffffffLL	
 #endif
 
-#define AG_FLT_MIN	1.175494351e-38F
-#define AG_FLT_MAX	3.402823466e+38F
-#define AG_DBL_MIN	2.2250738585072014e-308
-#define AG_DBL_MAX	1.7976931348623158e+308
+#ifdef __FLT_MIN__
+# define AG_FLT_MIN __FLT_MIN__
+#else
+# define AG_FLT_MIN 1.17549435e-38f
+#endif
+#ifdef __FLT_MAX__
+# define AG_FLT_MAX __FLT_MAX__
+#else
+# define AG_FLT_MAX 3.40282347e+38f
+#endif
+#ifdef __DBL_MIN__
+# define AG_DBL_MIN __DBL_MIN__
+#else
+# define AG_DBL_MIN 2.2250738585072014e-308
+#endif
+#ifdef __DBL_MAX__
+# define AG_DBL_MAX __DBL_MAX__
+#else
+# define AG_DBL_MAX 1.7976931348623157e+308
+#endif
+#ifdef HAVE_LONG_DOUBLE
+# ifdef __LDBL_MIN__
+#  define AG_LDBL_MIN __LDBL_MIN__
+# else
+#  define AG_LDBL_MIN 3.36210314311209350626e-4932l
+# endif
+# ifdef __LDBL_MAX__
+#  define AG_LDBL_MAX __LDBL_MAX__
+# else
+#  define AG_LDBL_MAX 1.18973149535723176502e+4932l
+# endif
+#endif /* HAVE_LONG_DOUBLE */
 
-#endif /* _COMPAT_LIMITS_H */
+#endif /* _AGAR_CORE_LIMITS_H */
