d2a7fcf443cb1c5f2375f02404d245ccc0109a9c
Julien Nadeau Carriere <vedge@hypertriton.com>
2008-03-02 12:15:36 +0000

    set RESIZABLE
    git-svn-id: https://dev.csoft.net/agar/trunk@7207 86a84d31-9702-0410-8f1e-a60cab7bd424

--- demos/plotting/plotting.c
+++ demos/plotting/plotting.c
@@ -205,7 +205,8 @@ main(int argc, char *argv[])
 		fprintf(stderr, "%s\n", AG_GetError());
 		return (1);
 	}
-	if (AG_InitVideo(640, 480, 32, AG_VIDEO_OPENGL) == -1) {
+	if (AG_InitVideo(640, 480, 32, AG_VIDEO_OPENGL|AG_VIDEO_RESIZABLE)
+	    == -1) {
 		fprintf(stderr, "%s\n", AG_GetError());
 		return (-1);
 	}
