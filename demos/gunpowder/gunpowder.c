362a8dd5c9f643a6a01b6b122cfa4b1c4e4f126d
Julien Nadeau Carriere <vedge@hypertriton.com>
2008-03-02 12:10:23 +0000

    use sliders
    git-svn-id: https://dev.csoft.net/agar/trunk@7205 86a84d31-9702-0410-8f1e-a60cab7bd424

--- demos/gunpowder/gunpowder.c
+++ demos/gunpowder/gunpowder.c
@@ -1,7 +1,8 @@
 /*	Public domain	*/
 /*
- * This program computes the ingredient amounts for a standard black
- * powder formulation.
+ * This program computes the ingredient amounts for a standard black powder
+ * formulation. It demonstrates unit conversion and use of widget bindings
+ * to control specific variables.
  */
 
 #include <agar/core.h>
@@ -9,11 +10,16 @@
 
 #include <string.h>
 
-double kno3 = 15.0;
-double c = 3.0;
-double s = 2.0;
-double weight = 0.0, prSolid = 0.0, prWater = 0.0, prGaseous = 0.0;
+double kno3=15.0, kno3Min=0.0, kno3Max=750.0;	/* Potassium nitrate */
+double c=3.0, cMin=0.0, cMax=750.0;		/* Carbon */
+double s=2.0, sMin=0.0, sMax=750.0;		/* Sulfur */
 
+double weight = 0.0;				/* Net weight */
+double prSolid = 0.0;				/* Solid products */
+double prGaseous = 0.0;				/* Gaseous products */
+double prWater = 0.0;				/* Water product */
+
+/* Compute products of reaction. */
 static void
 UpdateProducts(void)
 {
@@ -23,6 +29,7 @@ UpdateProducts(void)
 	prGaseous = 42.98*weight/100.0;
 }
 
+/* Compute KNO3 and C from S. */
 static void
 UpdateS(AG_Event *event)
 {
@@ -31,6 +38,7 @@ UpdateS(AG_Event *event)
 	UpdateProducts();
 }
 
+/* Compute KNO3 and S from C. */
 static void
 UpdateC(AG_Event *event)
 {
@@ -39,6 +47,16 @@ UpdateC(AG_Event *event)
 	UpdateProducts();
 }
 
+/* Compute S and C from KNO3. */
+static void
+UpdateKNO3(AG_Event *event)
+{
+	c = 0.2*kno3;
+	s = (1.0/7.5)*kno3;
+	UpdateProducts();
+}
+
+/* Compute KNO3, S and C from net weight. */
 static void
 UpdateWeight(AG_Event *event)
 {
@@ -48,14 +66,7 @@ UpdateWeight(AG_Event *event)
 	UpdateProducts();
 }
 
-static void
-UpdateKNO3(AG_Event *event)
-{
-	c = 0.2*kno3;
-	s = (1.0/7.5)*kno3;
-	UpdateProducts();
-}
-
+/* Create our GUI. */
 static void
 CreateUI(void)
 {
@@ -64,34 +75,59 @@ CreateUI(void)
 	AG_Button *btn;
 	AG_Label *lbl;
 	AG_Numerical *n1, *n2, *n3, *n;
+	AG_Slider *s1, *s2, *s3;
+	AG_Box *box;
 	int i;
 
+	/* Create a window covering the entire display. */
 	win = AG_WindowNew(AG_WINDOW_PLAIN);
 	AG_WindowSetCaption(win, "Gunpowder Calculator");
 	AG_WindowSetPadding(win, 10, 10, 10, 10);
-	
-	n = AG_NumericalNew(win, AG_NUMERICAL_HFILL, "g", "Total weight: ");
-	AG_WidgetBindDouble(n, "value", &weight);
+
+	/*
+	 * Use AG_Numerical(3) to edit a double-precision floating point
+	 * variable.
+	 */
+	n = AG_NumericalNewDbl(win, AG_NUMERICAL_HFILL, "g",
+	    "Net weight: ", &weight);
+	AG_NumericalSetMinDbl(n, 0.0);
+	AG_NumericalSetMaxDbl(n, 1000.0);
 	AG_SetEvent(n, "numerical-changed", UpdateWeight, NULL);
 
 	AG_SeparatorNewHoriz(win);
 
-	n1 = AG_NumericalNew(win, AG_NUMERICAL_HFILL, "g", "KNO3: ");
-	n2 = AG_NumericalNew(win, AG_NUMERICAL_HFILL, "g", "Carbon: ");
-	n3 = AG_NumericalNew(win, AG_NUMERICAL_HFILL, "g", "Sulfur: ");
-	AG_WidgetBindDouble(n1, "value", &kno3);
-	AG_WidgetBindDouble(n2, "value", &c);
-	AG_WidgetBindDouble(n3, "value", &s);
-	AG_NumericalSizeHint(n1, "0000.00");
-	AG_NumericalSizeHint(n2, "0000.00");
-	AG_NumericalSizeHint(n3, "0000.00");
-	AG_SetEvent(n1, "numerical-changed", UpdateKNO3, NULL);
-	AG_SetEvent(n2, "numerical-changed", UpdateC, NULL);
-	AG_SetEvent(n3, "numerical-changed", UpdateS, NULL);
+	/*
+	 * Use AG_Slider(3) to edit the specific quantities.
+	 */
+	box = AG_BoxNewHoriz(win, AG_BOX_HFILL);
+	{
+		lbl = AG_LabelNew(box, 0, "KNO3: ");
+		s1 = AG_SliderNewDbl(box, AG_SLIDER_HORIZ, AG_SLIDER_HFILL,
+		    &kno3, &kno3Min, &kno3Max);
+		AG_SetEvent(s1, "slider-changed", UpdateKNO3, NULL);
+	}
+	box = AG_BoxNewHoriz(win, AG_BOX_HFILL);
+	{
+		lbl = AG_LabelNew(box, 0, "Carbon: ");
+		s2 = AG_SliderNewDbl(box, AG_SLIDER_HORIZ, AG_SLIDER_HFILL,
+		    &c, &cMin, &cMax);
+		AG_SetEvent(s2, "slider-changed", UpdateC, NULL);
+	}
+	box = AG_BoxNewHoriz(win, AG_BOX_HFILL);
+	{
+		lbl = AG_LabelNew(box, 0, "Sulfur: ");
+		s3 = AG_SliderNewDbl(box, AG_SLIDER_HORIZ, AG_SLIDER_HFILL,
+		    &s, &sMin, &sMax);
+		AG_SetEvent(s3, "slider-changed", UpdateS, NULL);
+	}
 	
 	AG_SeparatorNewHoriz(win);
 
-	AG_LabelNewStatic(win, 0, "Products of reaction:");
+	AG_LabelNewStatic(win, 0, "Ingredients:");
+	AG_LabelNewPolled(win, AG_LABEL_HFILL,
+	    "%Fg KNO3, %Fg C, %Fg S",
+	    &kno3, &c, &s);
+	AG_LabelNewStatic(win, 0, "Combustion products:");
 	AG_LabelNewPolled(win, AG_LABEL_HFILL,
 	    "%Fg solid, %Fg gaseous, %Fg H2O",
 	    &prSolid, &prGaseous, &prWater);
@@ -109,7 +145,7 @@ main(int argc, char *argv[])
 		fprintf(stderr, "%s\n", AG_GetError());
 		return (1);
 	}
-	if (AG_InitVideo(350, 170, 32, AG_VIDEO_RESIZABLE) == -1) {
+	if (AG_InitVideo(350, 220, 32, AG_VIDEO_RESIZABLE) == -1) {
 		fprintf(stderr, "%s\n", AG_GetError());
 		return (-1);
 	}
