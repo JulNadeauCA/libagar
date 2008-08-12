f3263bcf3fbd8ce7d15221b653f523019878b9c3
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2008-08-12 10:51:47 +0000

    define __BEGIN_DECLS / __END_DECLS before <threads.h>

--- trunk/core/core.h
+++ trunk/core/core.h
@@ -27,6 +27,16 @@ typedef unsigned char Uchar;
 typedef unsigned long Ulong;
 #endif
 
+#if !defined(__BEGIN_DECLS) || !defined(__END_DECLS)
+# if defined(__cplusplus)
+#  define __BEGIN_DECLS	extern "C" {
+#  define __END_DECLS	}
+# else
+#  define __BEGIN_DECLS
+#  define __END_DECLS
+# endif
+#endif
+
 #include <core/threads.h>
 
 #include <SDL.h>
