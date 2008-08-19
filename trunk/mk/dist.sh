c34566ca53e459f7624699315913b8e3fcf91de5
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2008-08-19 10:43:18 +0000

    include generated "project files" in the .zip only; leave the tar.gz free of junk.

--- trunk/mk/dist.sh
+++ trunk/mk/dist.sh
@@ -7,6 +7,7 @@ REL=`perl mk/get-release.pl`
 DISTFILE=${PROJ}-${VER}
 RHOST=resin.csoft.net
 RUSER=vedge
+MAKE=make
 
 if [ "$1" = "snapshot" ]; then
 	PHASE=beta
@@ -14,9 +15,12 @@ else
 	PHASE=stable
 fi
 
-echo "Packaging ${PROJ}-${VER} (${REL})"
-
 cd ..
+
+#
+# Build the .tar.gz package.
+#
+echo "Building tar.gz for ${PROJ}-${VER} (${REL})"
 rm -fr ${PROJ}-${VER}
 cp -fRp ${PROJ} ${PROJ}-${VER}
 rm -fR `find ${PROJ}-${VER} \( -name .svn \
@@ -27,9 +31,24 @@ rm -fR `find ${PROJ}-${VER} \( -name .svn \
     -or -name .\*.swp \
     -or -name .depend \
     -or -name .xvpics \)`
-
 tar -f ${DISTFILE}.tar -c ${PROJ}-${VER}
 gzip -f ${DISTFILE}.tar
+
+#
+# Build the .zip package.
+#
+echo "Building zip for ${PROJ}-${VER} (${REL})"
+rm -fr ${PROJ}-${VER}
+cp -fRp ${PROJ} ${PROJ}-${VER}
+rm -fR `find ${PROJ}-${VER} \( -name .svn \
+    -or -name \*~ \
+    -or -name \*.o \
+    -or -name \*.a \
+    -or -name \*.core \
+    -or -name .\*.swp \
+    -or -name .depend \
+    -or -name .xvpics \)`
+(cd ${PROJ}-${VER} && ${MAKE} proj)
 zip -r ${DISTFILE}.zip ${PROJ}-${VER}
 
 openssl md5 ${DISTFILE}.tar.gz > ${DISTFILE}.tar.gz.md5
@@ -44,14 +63,13 @@ gpg -ab ${DISTFILE}.zip
 if [ "$1" = "commit" -o "$1" = "snapshot" ]; then
 	echo "uploading"
 	scp -C ${DISTFILE}.{tar.gz,tar.gz.md5,tar.gz.asc,zip,zip.md5,zip.asc} ${RUSER}@${RHOST}:www/$PHASE.csoft.org/${PROJ}
+	echo "*********************************************************"
+	echo "TODO:"
+	echo "- Update http://en.wikipedia.org/wiki/Agar (software)"
+	echo "- Update http://fr.wikipedia.org/wiki/Agar (moteur)"
+	echo "- Update http://sourceforge.net/projects/agar/"
+	echo "- Update http://freshmeat.net/projects/agar/"
+	echo "- Announcements to agar@ and agar-fr@"
+	echo "- Announcements to agar-announce@ and agar-announce-fr@"
+	echo "*********************************************************"
 fi
-
-echo "*********************************************************"
-echo "TODO: Update http://en.wikipedia.org/wiki/Agar (software)"
-echo "TODO: Update http://fr.wikipedia.org/wiki/Agar (moteur)"
-echo "TODO: Update http://sourceforge.net/projects/agar/"
-echo "TODO: Update http://freshmeat.net/projects/agar/"
-echo "TODO: Announcements to agar@ and agar-fr@"
-echo "TODO: Announcements to agar-announce@ and agar-announce-fr@"
-echo "*********************************************************"
-
