a15105d0e5d90f81dba84dd8441775315b7177e9
Mark <mark@coreland.ath.cx>
2008-08-21 03:20:33 +0000

    init_core -> init (pedantry)
    git-svn-id: https://dev.csoft.net/agar/trunk@7491 86a84d31-9702-0410-8f1e-a60cab7bd424

--- ada-core/agar-core.adb
+++ ada-core/agar-core.adb
@@ -2,15 +2,22 @@ package body agar.core is
 
   use type c.int;
 
-  function init_core
+  function init
     (progname : string;
      flags    : init_flags_t) return boolean
   is
     ca_progname : aliased c.char_array := c.to_c (progname);
   begin
-    return init_core
+    return init
       (progname => cs.to_chars_ptr (ca_progname'unchecked_access),
        flags    => flags) = 0;
-  end init_core;
+  end init;
+
+  procedure get_version (version : out version_t) is
+    tmp_ver : aliased version_t;
+  begin
+    get_version (tmp_ver'unchecked_access);
+    version := tmp_ver;
+  end get_version;
 
 end agar.core;
