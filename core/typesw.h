5e9a29cd91a0961d2d31ffeac1e00fb6e8105842
Julien Nadeau Carriere <vedge@hypertriton.com>
2008-08-22 01:31:59 +0000

    - implement LoadClass() and UnloadClass() to automatically handle the DSO   loading/unloading and class registration needed for classes implemented in   specific modules. - allow applications to specify DSO dirs with RegisterModuleDirectory().
    git-svn-id: https://dev.csoft.net/agar/trunk@7503 86a84d31-9702-0410-8f1e-a60cab7bd424

--- core/typesw.h
+++ core/typesw.h
@@ -8,25 +8,17 @@ void AG_DestroyClassTbl(void);
 
 AG_Namespace *AG_RegisterNamespace(const char *, const char *, const char *);
 void          AG_UnregisterNamespace(const char *);
+void          AG_RegisterModuleDirectory(const char *);
+void          AG_UnregisterModuleDirectory(const char *);
 
-void AG_RegisterClass(void *);
-void AG_UnregisterClass(void *);
-int  AG_ParseClassSpec(char *, size_t, const char *)
-                       BOUNDED_ATTRIBUTE(__string__, 1, 2);
-
-/* Return description for the given object class name. */
-static __inline__ AG_ObjectClass *
-AG_FindClass(const char *name)
-{
-	int i;
-
-	for (i = 0; i < agClassCount; i++) {
-		if (strcmp(agClassTbl[i]->name, name) == 0)
-			return (agClassTbl[i]);
-	}
-	AG_SetError("No such class: %s", name);
-	return (NULL);
-}
+void            AG_RegisterClass(void *);
+void            AG_UnregisterClass(void *);
+int             AG_ParseClassSpec(char *, size_t, char *, size_t, const char *)
+                                  BOUNDED_ATTRIBUTE(__string__, 1, 2)
+                                  BOUNDED_ATTRIBUTE(__string__, 3, 4);
+AG_ObjectClass *AG_LookupClass(const char *);
+AG_ObjectClass *AG_LoadClass(const char *);
+void            AG_UnloadClass(AG_ObjectClass *);
 
 /* Return description for the given namespace. */
 static __inline__ AG_Namespace *
