22a5700f516ae72b2d8f2f5365eafefb79705675
Mark <mark@coreland.ath.cx>
2008-08-27 20:57:33 +0000

    format string audit. protect agar.core.error.
    git-svn-id: https://dev.csoft.net/agar/trunk@7540 86a84d31-9702-0410-8f1e-a60cab7bd424

--- ada-core/agar-core-error.adb
+++ ada-core/agar-core-error.adb
@@ -1,20 +1,39 @@
 package body agar.core.error is
 
+  procedure c_set_error
+    (fmt : cs.chars_ptr;
+     str : cs.chars_ptr);
+  pragma import (c, c_set_error, "AG_SetError");
+
+  procedure c_fatal_error
+    (fmt : cs.chars_ptr;
+     str : cs.chars_ptr);
+  pragma import (c, c_fatal_error, "AG_FatalError");
+
+  function c_get_error return cs.chars_ptr;
+  pragma import (c, c_get_error, "AG_GetError");
+
   function get return string is
   begin
-    return cs.value (get);
+    return cs.value (c_get_error);
   end get;
 
   procedure set (str : string) is
+    ca_fmt : aliased c.char_array := c.to_c ("%s");
     ca_str : aliased c.char_array := c.to_c (str);
   begin
-    set (cs.to_chars_ptr (ca_str'unchecked_access));
+    c_set_error
+     (fmt => cs.to_chars_ptr (ca_fmt'unchecked_access),
+      str => cs.to_chars_ptr (ca_str'unchecked_access));
   end set;
 
   procedure fatal (str : string) is
+    ca_fmt : aliased c.char_array := c.to_c ("%s");
     ca_str : aliased c.char_array := c.to_c (str);
   begin
-    fatal (cs.to_chars_ptr (ca_str'unchecked_access));
+    c_fatal_error
+      (fmt => cs.to_chars_ptr (ca_fmt'unchecked_access),
+       str => cs.to_chars_ptr (ca_str'unchecked_access));
   end fatal;
 
 end agar.core.error;
