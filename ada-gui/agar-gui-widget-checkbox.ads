dbde9a5a35198c77180d68e09b44299e1925fdf2
Mark <mark@coreland.ath.cx>
2008-08-31 08:56:27 +0000

    Added agar.gui.widget.checkbox.
    git-svn-id: https://dev.csoft.net/agar/trunk@7559 86a84d31-9702-0410-8f1e-a60cab7bd424

---
+++ ada-gui/agar-gui-widget-checkbox.ads
@@ -0,0 +1,73 @@
+with agar.core.event;
+
+package agar.gui.widget.checkbox is
+
+  use type c.unsigned;
+
+  subtype flags_t is c.unsigned;
+  CHECKBOX_HFILL  : constant flags_t := 16#01#;
+  CHECKBOX_VFILL  : constant flags_t := 16#02#;
+  CHECKBOX_EXPAND : constant flags_t := CHECKBOX_HFILL or CHECKBOX_VFILL;
+  CHECKBOX_SET    : constant flags_t := 16#04#;
+
+  type checkbox_t is record
+    widget     : widget_t;
+    flags      : flags_t;
+    state      : c.int;
+    label_text : cs.chars_ptr;
+    label      : c.int;
+    spacing    : c.int;
+  end record;
+  type checkbox_access_t is access all checkbox_t;
+  pragma convention (c, checkbox_t);
+  pragma convention (c, checkbox_access_t);
+
+  function allocate
+    (parent : widget_access_t;
+     flags  : flags_t;
+     label  : string) return checkbox_access_t;
+  pragma inline (allocate);
+
+  function allocate_function
+    (parent : widget_access_t;
+     flags  : flags_t;
+     label  : string;
+     func   : agar.core.event.callback_t) return checkbox_access_t;
+  pragma inline (allocate_function);
+
+  function allocate_integer
+    (parent : widget_access_t;
+     ptr    : access c.int;
+     label  : string) return checkbox_access_t;
+  pragma inline (allocate_integer);
+
+  function allocate_flags
+    (parent : widget_access_t;
+     ptr    : access c.unsigned;
+     mask   : c.unsigned;
+     label  : string) return checkbox_access_t;
+  pragma inline (allocate_flags);
+
+  function allocate_flags32
+    (parent : widget_access_t;
+     ptr    : access agar.core.types.uint32_t;
+     mask   : agar.core.types.uint32_t;
+     label  : string) return checkbox_access_t;
+  pragma inline (allocate_flags32);
+
+  procedure set_from_flags
+    (parent     : widget_access_t;
+     ptr        : access c.unsigned;
+     flags_desc : flag_descr_access_t);
+  pragma import (c, set_from_flags, "AG_CheckboxSetFromFlags");
+
+  procedure set_from_flags32
+    (parent     : widget_access_t;
+     ptr        : access agar.core.types.uint32_t;
+     flags_desc : flag_descr_access_t);
+  pragma import (c, set_from_flags32, "AG_CheckboxSetFromFlags");
+
+  procedure toggle (checkbox : checkbox_access_t);
+  pragma import (c, toggle, "AG_CheckboxToggle");
+
+end agar.gui.widget.checkbox;
