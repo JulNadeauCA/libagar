9d0c218b4340ca9b32ef7f9161bb65f86d9396cf
Mark <mark@coreland.ath.cx>
2008-09-12 23:26:15 +0000

    Added hbox,vbox with tests. Added missing graph.adb from previous commit.
    git-svn-id: https://dev.csoft.net/agar/trunk@7631 86a84d31-9702-0410-8f1e-a60cab7bd424

---
+++ ada-gui/agar-gui-widget-vbox.adb
@@ -0,0 +1,49 @@
+package body agar.gui.widget.vbox is
+
+  package cbinds is
+    procedure set_homogenous
+     (box        : vbox_access_t;
+      homogenous : c.int);
+    pragma import (c, set_homogenous, "AG_VBoxSetHomogenous");
+
+    procedure set_padding
+     (box     : vbox_access_t;
+      padding : c.int);
+    pragma import (c, set_padding, "AG_VBoxSetPadding");
+
+    procedure set_spacing
+     (box     : vbox_access_t;
+      spacing : c.int);
+    pragma import (c, set_spacing, "AG_VBoxSetSpacing");
+  end cbinds;
+
+  procedure set_homogenous
+    (box        : vbox_access_t;
+     homogenous : boolean := true) is
+  begin
+    if homogenous then
+      cbinds.set_homogenous (box, 1);
+    else
+      cbinds.set_homogenous (box, 0);
+    end if;
+  end set_homogenous;
+              
+  procedure set_padding
+    (box     : vbox_access_t;
+     padding : natural) is
+  begin
+    cbinds.set_padding
+      (box     => box,
+       padding => c.int (padding));
+  end set_padding;
+              
+  procedure set_spacing
+    (box     : vbox_access_t;
+     spacing : natural) is
+  begin
+    cbinds.set_spacing
+      (box     => box,
+       spacing => c.int (spacing));
+  end set_spacing;
+
+end agar.gui.widget.vbox;
