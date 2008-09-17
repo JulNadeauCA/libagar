e6dd29e11c4e20451c938825770ab449737d4daa
Mark <mark@coreland.ath.cx>
2008-09-17 22:41:55 +0000

    Added agar.gui.widget.separator API functions. Added agar.gui.widget.slider API functions.
    git-svn-id: https://dev.csoft.net/agar/trunk@7657 86a84d31-9702-0410-8f1e-a60cab7bd424

--- ada-gui/agar-gui-widget-separator.ads
+++ ada-gui/agar-gui-widget-separator.ads
@@ -5,13 +5,36 @@ package agar.gui.widget.separator is
   for type_t'size use c.unsigned'size;
   pragma convention (c, type_t);
 
+  type separator_t is limited private;
+  type separator_access_t is access all separator_t;
+  pragma convention (c, separator_access_t);
+
+  function allocate
+    (parent         : widget_access_t;
+     separator_type : type_t) return separator_access_t;
+  pragma import (c, allocate, "AG_SeparatorNew");
+
+  function allocate_spacer
+    (parent         : widget_access_t;
+     separator_type : type_t) return separator_access_t;
+  pragma import (c, allocate_spacer, "AG_SpacerNew");
+
+  procedure set_padding
+    (separator : separator_access_t;
+     pixels    : natural);
+  pragma inline (set_padding);
+
+  function widget (separator : separator_access_t) return widget_access_t;
+  pragma inline (widget);
+
+private
+
   type separator_t is record
-    widget   : widget_t;
+    widget   : aliased widget_t;
     sep_type : type_t;
     padding  : c.unsigned;
     visible  : c.int;
   end record;
-  type separator_access_t is access all separator_t;
   pragma convention (c, separator_t);
 
 end agar.gui.widget.separator;
