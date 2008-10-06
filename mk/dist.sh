0e1d35c48ada2a474c4af48b26cf31a6e7f43b07
Julien Nadeau Carriere <vedge@hypertriton.com>
2008-10-06 12:10:40 +0000

    tri:regen
    git-svn-id: https://dev.csoft.net/agar/trunk@7779 86a84d31-9702-0410-8f1e-a60cab7bd424

--- mk/dist.sh
+++ mk/dist.sh
@@ -4,73 +4,91 @@
 PROJ=agar
 VER=`perl mk/get-version.pl`
 REL=`perl mk/get-release.pl`
-DISTFILE=${PROJ}-${VER}
+DISTNAME=${PROJ}-${VER}
 RHOST=resin.csoft.net
 RUSER=vedge
 MAKE=make
+REMOTEDIR=www/${PHASE}.csoft.org/${PROJ}
 
-if [ "$1" = "snapshot" ]; then
-	PHASE=beta
+if [ "$1" != "" ]; then
+	PHASE="$1"
 else
 	PHASE=stable
 fi
 
 cd ..
 
-#
-# Build the .tar.gz package.
-#
-echo "Building tar.gz for ${PROJ}-${VER} (${REL})"
-rm -fr ${PROJ}-${VER}
-cp -fRp ${PROJ} ${PROJ}-${VER}
-rm -fR `find ${PROJ}-${VER} \( -name .svn \
+echo "*"
+echo "* Project: ${PROJ}"
+echo "* State: ${PHASE}"
+echo "* Version: ${VER}"
+echo "* Release: ${REL}"
+echo "*"
+
+# TAR: Prepare archive
+echo "Building tar.gz"
+rm -fr ${DISTNAME}
+cp -fRp ${PROJ} ${DISTNAME}
+rm -fR `find ${DISTNAME} \( -name .svn \
     -or -name \*~ \
     -or -name \*.o \
     -or -name \*.a \
     -or -name \*.core \
     -or -name .\*.swp \
     -or -name .xvpics \)`
-tar -f ${DISTFILE}.tar -c ${PROJ}-${VER}
-gzip -f ${DISTFILE}.tar
 
-#
-# Build the .zip package.
-#
-echo "Building zip for ${PROJ}-${VER} (${REL})"
-rm -fr ${PROJ}-${VER}
-cp -fRp ${PROJ} ${PROJ}-${VER}
-rm -fR `find ${PROJ}-${VER} \( -name .svn \
+# TAR: Compress archive
+tar -f ${DISTNAME}.tar -c ${DISTNAME}
+gzip -f ${DISTNAME}.tar
+
+# ZIP: Prepare archive
+echo "Building zip"
+rm -fr ${DISTNAME}
+cp -fRp ${PROJ} ${DISTNAME}
+rm -fR `find ${DISTNAME} \( -name .svn \
     -or -name \*~ \
     -or -name \*.o \
     -or -name \*.a \
     -or -name \*.core \
     -or -name .\*.swp \
     -or -name .xvpics \)`
-(cd ${PROJ}-${VER} && ${MAKE} proj)
-if [ $? != 0 ]; then
-	echo "make proj failed"
-	exit 1
-fi
-(cd ${PROJ}-${VER}/demos && ${MAKE} proj)
-if [ $? != 0 ]; then
-	echo "make proj (demos) failed"
-	exit 1
-fi
-rm -f ${PROJ}-${VER}/agar
-zip -r ${DISTFILE}.zip ${PROJ}-${VER}
 
-openssl md5 ${DISTFILE}.tar.gz > ${DISTFILE}.tar.gz.md5
-openssl rmd160 ${DISTFILE}.tar.gz >> ${DISTFILE}.tar.gz.md5
-openssl sha1 ${DISTFILE}.tar.gz >> ${DISTFILE}.tar.gz.md5
-openssl md5 ${DISTFILE}.zip > ${DISTFILE}.zip.md5
-openssl rmd160 ${DISTFILE}.zip >> ${DISTFILE}.zip.md5
-openssl sha1 ${DISTFILE}.zip >> ${DISTFILE}.zip.md5
-gpg -ab ${DISTFILE}.tar.gz
-gpg -ab ${DISTFILE}.zip
+# ZIP: Generate project files for various IDEs.
+(cd ${DISTNAME} && ${MAKE} proj)
+if [ $? != 0 ]; then
+	echo "${MAKE} proj failed"
+	exit 1
+fi
+(cd ${DISTNAME}/demos && ${MAKE} proj)
+if [ $? != 0 ]; then
+	echo "${MAKE} proj (demos) failed"
+	exit 1
+fi
+
+# ZIP: Remove include symlink.
+rm -f ${DISTNAME}/agar
+
+# ZIP: Litter header files with "DECLSPEC" as required by some compilers.
+find ${DISTNAME} -name \*.h -exec perl mk/gen-declspecs.pl {} \;
+
+# ZIP: Compress archive
+zip -r ${DISTNAME}.zip ${DISTNAME}
+
+echo "Updating checksums"
+openssl md5 ${DISTNAME}.tar.gz > ${DISTNAME}.tar.gz.md5
+openssl rmd160 ${DISTNAME}.tar.gz >> ${DISTNAME}.tar.gz.md5
+openssl sha1 ${DISTNAME}.tar.gz >> ${DISTNAME}.tar.gz.md5
+openssl md5 ${DISTNAME}.zip > ${DISTNAME}.zip.md5
+openssl rmd160 ${DISTNAME}.zip >> ${DISTNAME}.zip.md5
+openssl sha1 ${DISTNAME}.zip >> ${DISTNAME}.zip.md5
+
+echo "Updating signatures"
+gpg -ab ${DISTNAME}.tar.gz
+gpg -ab ${DISTNAME}.zip
 
 if [ "$1" = "commit" -o "$1" = "snapshot" ]; then
-	echo "uploading"
-	scp -C ${DISTFILE}.{tar.gz,tar.gz.md5,tar.gz.asc,zip,zip.md5,zip.asc} ${RUSER}@${RHOST}:www/$PHASE.csoft.org/${PROJ}
+	echo "Uploading to ${RHOST}"
+	scp -C ${DISTNAME}.{tar.gz,tar.gz.md5,tar.gz.asc,zip,zip.md5,zip.asc} ${RUSER}@${RHOST}:${REMOTEDIR}
 	echo "*********************************************************"
 	echo "TODO:"
 	echo "- Update http://en.wikipedia.org/wiki/Agar (software)"
