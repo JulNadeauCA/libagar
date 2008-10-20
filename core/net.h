6f0b8fa5203e3c07df21754aedeedd6d5cd58853
Julien Nadeau Carriere <vedge@hypertriton.com>
2008-10-20 02:52:44 +0000

    tri:missing net.h
    git-svn-id: https://dev.csoft.net/agar/trunk@7862 86a84d31-9702-0410-8f1e-a60cab7bd424

---
+++ core/net.h
@@ -0,0 +1,9 @@
+/*	Public domain	*/
+
+#ifndef _AGAR_CORE_NET_H_
+#define _AGAR_CORE_NET_H_
+#include <agar/core/net_command.h>
+#include <agar/core/net_client.h>
+#include <agar/core/net_server.h>
+#endif /* _AGAR_CORE_NET_H_ */
+
