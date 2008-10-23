3d32d244bb3fbc1fdfc7d9d42a6eb29816ccbc32
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2008-10-23 16:43:47 +0000

    remove bogus redefinition of M_ColorRGB() and M_ColorRGBA()

--- trunk/math/m_color.h
+++ trunk/math/m_color.h
@@ -1,6 +1,16 @@
 /*	Public domain	*/
 
 __BEGIN_DECLS
+void	M_WriteColor(AG_DataSource *, const M_Color *);
+M_Color	M_ReadColor(AG_DataSource *);
+M_Color	M_ColorHSVA(M_Real, M_Real, M_Real, M_Real);
+void	M_ColorTo4fv(const M_Color *, float *);
+void	M_ColorTo4dv(const M_Color *, double *);
+#define M_ColorHSV(h,s,v) M_ColorHSVA((h),(s),(v),1.0)
+#define M_ColorBlack() M_ColorRGB(0.0, 0.0, 0.0)
+#define M_ColorWhite() M_ColorRGB(1.0, 1.0, 1.0)
+#define M_ColorGray(x) M_ColorRGB((x),(x),(x))
+
 static __inline__ M_Color
 M_ColorRGB(M_Real r, M_Real g, M_Real b)
 {
@@ -24,16 +34,4 @@ M_ColorRGBA(M_Real r, M_Real g, M_Real b, M_Real a)
 	c.a = a;
 	return (c);
 }
-
-void	M_WriteColor(AG_DataSource *, const M_Color *);
-M_Color	M_ReadColor(AG_DataSource *);
-M_Color	M_ColorRGB(M_Real, M_Real, M_Real);
-M_Color	M_ColorRGBA(M_Real, M_Real, M_Real, M_Real);
-M_Color	M_ColorHSVA(M_Real, M_Real, M_Real, M_Real);
-void	M_ColorTo4fv(const M_Color *, float *);
-void	M_ColorTo4dv(const M_Color *, double *);
-#define M_ColorHSV(h,s,v) M_ColorHSVA((h),(s),(v),1.0)
-#define M_ColorBlack() M_ColorRGB(0.0, 0.0, 0.0)
-#define M_ColorWhite() M_ColorRGB(1.0, 1.0, 1.0)
-#define M_ColorGray(x) M_ColorRGB((x),(x),(x))
 __END_DECLS
