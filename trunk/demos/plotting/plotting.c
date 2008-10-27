e34b8e89d93ec01db9954e525e7ce72c6e393efe
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2008-10-27 11:48:11 +0000

    no need to force AG_VIDEO_OPENGL here.

--- trunk/demos/plotting/plotting.c
+++ trunk/demos/plotting/plotting.c
@@ -205,8 +205,7 @@ main(int argc, char *argv[])
 		fprintf(stderr, "%s\n", AG_GetError());
 		return (1);
 	}
-	if (AG_InitVideo(640, 480, 32, AG_VIDEO_OPENGL|AG_VIDEO_RESIZABLE)
-	    == -1) {
+	if (AG_InitVideo(640, 480, 32, AG_VIDEO_RESIZABLE) == -1) {
 		fprintf(stderr, "%s\n", AG_GetError());
 		return (-1);
 	}
