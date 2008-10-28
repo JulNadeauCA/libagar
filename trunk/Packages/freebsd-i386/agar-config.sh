fe4852f5e9d36b3d18af98f7d9df33674089912b
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2008-10-28 07:49:18 +0000

    emulate --threads, --network, --sdl, etc.

--- trunk/Packages/freebsd-i386/agar-config.sh
+++ trunk/Packages/freebsd-i386/agar-config.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 #
-# Static agar-config for FreeBSD binary packages
+# Static agar-config for FreeBSD/i386 binary packages.
 # Public domain
 #
 
 PREFIX="%PREFIX%"
 
-if [ "$1" = "--version" ]; then
-	echo "%INSTALLED_VERSION%"
-fi
-if [ "$1" = "--release" ]; then
-	echo "%INSTALLED_RELEASE%"
-fi
-if [ "$1" = "--prefix" ]; then
-	echo "${PREFIX}"
-fi
+if [ "$1" = "--version" ]; then echo "%INSTALLED_VERSION%"; fi
+if [ "$1" = "--release" ]; then echo "%INSTALLED_RELEASE%"; fi
+if [ "$1" = "--prefix" ]; then echo "${PREFIX}";  fi
+
+if [ "$1" = "--threads" ]; then echo "yes"; fi
+if [ "$1" = "--network" ]; then echo "yes"; fi
+if [ "$1" = "--sdl" ]; then echo "yes"; fi
+if [ "$1" = "--opengl" ]; then echo "yes"; fi
+if [ "$1" = "--freetype" ]; then echo "yes"; fi
+
 if [ "$1" = "--cflags" ]; then
 	echo "-D_REENTRANT -D_GNU_SOURCE=1 -I${PREFIX}/include/agar -I${PREFIX}/include/SDL -I${PREFIX}/include/freetype2 -I${PREFIX}/include -I/usr/local/include/SDL -I/usr/local/include/freetype2 -I/usr/local/include -I/usr/X11R6/include/SDL -I/usr/X11R6/include/freetype2 -I/usr/X11R6/include"
 fi
--- trunk/Packages/linux-i386/agar-config.sh
+++ trunk/Packages/linux-i386/agar-config.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 #
-# Static agar-config for Linux/i386 binary package.
+# Static agar-config for Linux/i386 binary packages.
 # Public domain
 #
 
 PREFIX="%PREFIX%"
 
-if [ "$1" = "--version" ]; then
-	echo "%INSTALLED_VERSION%"
-fi
-if [ "$1" = "--release" ]; then
-	echo "%INSTALLED_RELEASE%"
-fi
-if [ "$1" = "--prefix" ]; then
-	echo "${PREFIX}"
-fi
+if [ "$1" = "--version" ]; then echo "%INSTALLED_VERSION%"; fi
+if [ "$1" = "--release" ]; then echo "%INSTALLED_RELEASE%"; fi
+if [ "$1" = "--prefix" ]; then echo "${PREFIX}"; fi
+
+if [ "$1" = "--threads" ]; then echo "yes"; fi
+if [ "$1" = "--network" ]; then echo "yes"; fi
+if [ "$1" = "--sdl" ]; then echo "yes"; fi
+if [ "$1" = "--opengl" ]; then echo "yes"; fi
+if [ "$1" = "--freetype" ]; then echo "yes"; fi
+
 if [ "$1" = "--cflags" ]; then
 	echo "-I${PREFIX}/include/agar -I${PREFIX}/include/SDL -I${PREFIX}/include/freetype2 -I/usr/include/SDL -I/usr/local/include/SDL -I/usr/X11R6/include/SDL -D_GNU_SOURCE=1 -D_REENTRANT -I/usr/include/freetype2 -I/usr/local/include/freetype2 -I/usr/X11R6/include/freetype2 -I/usr/X11R6/include"
 fi
