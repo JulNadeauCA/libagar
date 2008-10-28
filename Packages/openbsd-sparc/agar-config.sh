cdcd0fca45a06d97f5c7743065614f7b6b4cbdc1
Julien Nadeau Carriere <vedge@hypertriton.com>
2008-10-28 02:23:44 +0000

    package for OpenBSD/sparc
    git-svn-id: https://dev.csoft.net/agar/trunk@7927 86a84d31-9702-0410-8f1e-a60cab7bd424

---
+++ Packages/openbsd-sparc/agar-config.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# Static agar-config for OpenBSD binary packages
+# Public domain
+#
+
+PREFIX="%PREFIX%"
+
+if [ "$1" = "--version" ]; then
+	echo "%INSTALLED_VERSION%"
+fi
+if [ "$1" = "--release" ]; then
+	echo "%INSTALLED_RELEASE%"
+fi
+if [ "$1" = "--prefix" ]; then
+	echo "${PREFIX}"
+fi
+if [ "$1" = "--cflags" ]; then
+	echo "-D_REENTRANT -I${PREFIX}/include/agar -I${PREFIX}/include/SDL -I${PREFIX}/include/freetype2 -I${PREFIX}/include -I/usr/local/include/SDL -I/usr/X11R6/include/freetype2 -I/usr/local/include/freetype2 -I/usr/X11R6/include -I/usr/local/include"
+fi
+if [ "$1" = "--libs" ]; then
+	echo "-L${PREFIX}/lib -L/usr/local/lib -L/usr/X11R6/lib -lag_gui -lag_core -lSDL -pthread -lm -lX11 -lXext -lusbhid -lfreetype -lGL -lm -ljpeg -lpthread"
+fi
