c03acb255252fe0690593e7c67f4241ecd418059
Julien Nadeau Carriere <vedge@hypertriton.com>
2008-10-28 16:14:20 +0000

    -ljpeg not needed
    git-svn-id: https://dev.csoft.net/agar/trunk@7936 86a84d31-9702-0410-8f1e-a60cab7bd424

--- Packages/openbsd-amd64/agar-config.sh
+++ Packages/openbsd-amd64/agar-config.sh
@@ -20,5 +20,5 @@ if [ "$1" = "--cflags" ]; then
 	echo "-D_REENTRANT -I${PREFIX}/include/agar -I${PREFIX}/include/SDL -I${PREFIX}/include/freetype2 -I${PREFIX}/include -I/usr/local/include/SDL -I/usr/X11R6/include/freetype2 -I/usr/local/include/freetype2 -I/usr/X11R6/include -I/usr/local/include"
 fi
 if [ "$1" = "--libs" ]; then
-	echo "-L${PREFIX}/lib -L/usr/local/lib -L/usr/X11R6/lib -lag_gui -lag_core -lSDL -pthread -lm -lX11 -lXext -lusbhid -lfreetype -lz -lGL -lm -ljpeg -lpthread"
+	echo "-L${PREFIX}/lib -L/usr/local/lib -L/usr/X11R6/lib -lag_gui -lag_core -lSDL -pthread -lm -lX11 -lXext -lusbhid -lfreetype -lz -lGL -lm -lpthread"
 fi
