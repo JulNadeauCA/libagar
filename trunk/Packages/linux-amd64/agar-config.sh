e06beb0b3a55d49c44c9b1538ddfa7ea254e894b
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2008-10-28 12:13:15 +0000

    update --libs output

--- trunk/Packages/linux-amd64/agar-config.sh
+++ trunk/Packages/linux-amd64/agar-config.sh
@@ -20,6 +20,6 @@ if [ "$1" = "--cflags" ]; then
 	echo "-I${PREFIX}/include/agar -I${PREFIX}/include/SDL -I${PREFIX}/include/freetype2 -I/usr/include/SDL -I/usr/local/include/SDL -I/usr/X11R6/include/SDL -D_GNU_SOURCE=1 -D_REENTRANT -I/usr/include/freetype2 -I/usr/local/include/freetype2 -I/usr/X11R6/include/freetype2 -I/usr/X11R6/include"
 fi
 if [ "$1" = "--libs" ]; then
-	echo "-L${PREFIX}/lib -L${PREFIX}/lib64 -L/usr/local/lib -L/usr/X11R6/lib -lag_gui -lag_core -lag_net -lSDL -lfreetype -lz -lGL -lm -lpthread"
+	echo "-L${PREFIX}/lib -L${PREFIX}/lib64 -L/usr/local/lib -L/usr/X11R6/lib -lag_gui -lag_core -lSDL -lfreetype -lz -lGL -lm -lpthread"
 fi
 
