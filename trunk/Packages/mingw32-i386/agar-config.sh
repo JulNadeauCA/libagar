aabbb734e140ac0d05d16f715f189155e684c3f7
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2008-10-30 00:32:02 +0000

    update --libs for freetype, opengl, pthreads.

--- trunk/Packages/mingw32-i386/agar-config.sh
+++ trunk/Packages/mingw32-i386/agar-config.sh
@@ -17,8 +17,8 @@ if [ "$1" = "--opengl" ]; then echo "yes"; fi
 if [ "$1" = "--freetype" ]; then echo "yes"; fi
 
 if [ "$1" = "--cflags" ]; then
-	echo "-I${PREFIX}/include/agar -I${PREFIX}/include/SDL -I${PREFIX}/include -I/usr/local/include/SDL -I/usr/local/include -I/usr/include/mingw -mno-cygwin -Dmain=SDL_main"
+	echo "-I${PREFIX}/include/agar -I${PREFIX}/include/SDL -I/usr/local/include/SDL -I${PREFIX}/include/freetype2 -I/usr/local/include/freetype2 -I${PREFIX}/include -I/usr/local/include -I/usr/include/mingw -D_GNU_SOURCE=1 -Dmain=SDL_main"
 fi
 if [ "$1" = "--libs" ]; then
-	echo "-L${PREFIX}/lib -lag_gui -lag_core -L/usr/local/lib -lmingw32 -lSDLmain -lSDL -mno-cygwin -mwindows -lm"
+	echo "-L${PREFIX}/lib -lag_gui -lag_core -L/usr/local/lib -lmingw32 -lSDLmain -lSDL -mwindows -lfreetype -lopengl32 -lm -lpthreadGCE2"
 fi
