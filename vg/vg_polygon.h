d694e170b45f3080742c830f56ecd39145fb07ac
Julien Nadeau Carriere <vedge@hypertriton.com>
2009-01-18 08:30:11 +0000

    implement PolygonDelVertex(); fix behavior of Polygon tool.
    git-svn-id: https://dev.csoft.net/agar/trunk@8046 86a84d31-9702-0410-8f1e-a60cab7bd424

--- vg/vg_polygon.h
+++ vg/vg_polygon.h
@@ -33,13 +33,31 @@ VG_PolygonSetOutline(VG_Polygon *vP, int flag)
 	VG_Unlock(VGNODE(vP)->vg);
 }
 
-static __inline__ void
+static __inline__ Uint
 VG_PolygonVertex(VG_Polygon *vP, VG_Point *pt)
 {
 	VG_Lock(VGNODE(vP)->vg);
 	vP->pts = AG_Realloc(vP->pts, (vP->nPts+1)*sizeof(VG_Point *));
-	vP->pts[vP->nPts++] = pt;
+	vP->pts[vP->nPts] = pt;
 	VG_AddRef(vP, pt);
+	VG_Unlock(VGNODE(vP)->vg);
+	return (vP->nPts++);
+}
+
+static __inline__ void
+VG_PolygonDelVertex(VG_Polygon *vP, Uint vtx)
+{
+	VG_Lock(VGNODE(vP)->vg);
+	if (vtx < 0 || vtx >= vP->nPts) {
+		goto out;
+	}
+	VG_DelRef(vP, vP->pts[vtx]);
+	if (vtx < vP->nPts-1) {
+		memmove(&vP->pts[vtx], &vP->pts[vtx+1],
+		    (vP->nPts - 1)*sizeof(VG_Point *));
+	}
+	vP->nPts--;
+out:
 	VG_Unlock(VGNODE(vP)->vg);
 }
 __END_DECLS
