c88e4a40d22b70d479987b81183ddb6fcc7fc675
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2009-04-19 02:35:13 +0000

    interface to SDL time

--- trunk/gui/gui.c
+++ trunk/gui/gui.c
@@ -138,9 +138,13 @@ int agInitedSDL = 0;		/* Video system had to initialize SDL */
 int
 AG_InitGUI(Uint flags)
 {
+	extern const AG_TimeOps agTimeOps_SDL;
 	char path[AG_PATHNAME_MAX];
 	void **ops;
 	int i, n, njoys;
+
+	/* Use SDL's time interface. */
+	AG_SetTimeOps(&agTimeOps_SDL);
 
 	/* Register the built-in widget classes. */
 	for (ops = &agGUIClasses[0]; *ops != NULL; ops++)
