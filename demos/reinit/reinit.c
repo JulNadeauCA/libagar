c567f68bccdca8c5f8296b4bdc1c80edbf8e99c6
Julien Nadeau Carriere <vedge@hypertriton.com>
2009-08-27 10:24:58 +0000

    test Init(), Destroy() loop.
    git-svn-id: https://dev.csoft.net/agar/trunk@8304 86a84d31-9702-0410-8f1e-a60cab7bd424

---
+++ demos/reinit/reinit.c
@@ -0,0 +1,37 @@
+/*	Public domain	*/
+/*
+ * This test ensures Agar can be destroyed and re-initialized.
+ */
+
+#include <agar/core.h>
+#include <agar/gui.h>
+
+int
+main(int argc, char *argv[])
+{
+	AG_Window *win;
+	int i;
+
+	for (i = 0; i < 100; i++) {
+		printf("Test %d/100:\n", i);
+		printf("\tInitCore()\n");
+		if (AG_InitCore("agar-reinit-demo", 0) == -1) {
+			printf("AG_InitCore(%d): %s\n", i, AG_GetError());
+			exit(1);
+		}
+		printf("\tInitVideo()\n");
+		if (AG_InitVideo(320, 240, 32, AG_VIDEO_RESIZABLE) == -1) {
+			printf("AG_InitVideo(%d): %s\n", i, AG_GetError());
+			exit(1);
+		}
+		printf("\tCreate Window\n");
+		win = AG_WindowNew(0);
+		AG_WindowSetCaption(win, "foo");
+		AG_WindowShow(win);
+		printf("\tDestroyVideo()\n");
+		AG_DestroyVideo();
+		printf("\tDestroy()\n");
+		AG_Destroy();
+	}
+	return (0);
+}
