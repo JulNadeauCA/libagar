ebc865c009a8bbe1088bd15f3251b361741a97b9
Julien Nadeau Carriere <vedge@hypertriton.com>
2009-08-28 11:51:51 +0000

    tri:differentiate
    git-svn-id: https://dev.csoft.net/agar/trunk@8312 86a84d31-9702-0410-8f1e-a60cab7bd424

--- demos/cplusplus/cplusplus.cpp
+++ demos/cplusplus/cplusplus.cpp
@@ -17,15 +17,12 @@ main(int argc, char *argv[])
 		cerr << AG_GetError() << endl;
 		return (1);
 	}
-	if (AG_InitVideo(640, 480, 32, AG_VIDEO_RESIZABLE) == -1) {
+	if (AG_InitVideo(320, 240, 32, AG_VIDEO_RESIZABLE) == -1) {
 		cerr << AG_GetError() << endl;
 		return (-1);
 	}
 	AG_BindGlobalKey(SDLK_ESCAPE, KMOD_NONE, AG_Quit);
-	AG_BindGlobalKey(SDLK_F8, KMOD_NONE, AG_ViewCapture);
-
-	AG_TextMsg(AG_MSG_INFO, "Hello, world!");
-
+	AG_TextMsg(AG_MSG_INFO, "Hello, C++!");
 	AG_EventLoop();
 	AG_Destroy();
 	return (0);
