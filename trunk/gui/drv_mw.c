6833be3bd93a9bfdd34cb7d973aa54547ddfc2ae
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2009-10-23 14:23:33 +0000

    new graphics driver framework for 1.4.

---
+++ trunk/gui/drv_mw.c
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2009 Hypertriton, Inc. <http://hypertriton.com/>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Multiple-window graphics driver framework. Under this framework, each
+ * Agar window has a corresponding "native" window managed by the driver.
+ */
+
+#include <config/have_sdl.h>
+#include <config/have_opengl.h>
+#include <config/have_glx.h>
+
+#include <core/core.h>
+
+#include "window.h"
+
+static void
+Init(void *obj)
+{
+	AG_DriverMw *dmw = obj;
+
+	fprintf(stderr, "AG_DriverMw(%p) INIT\n", dmw);
+	dmw->flags = 0;
+	dmw->win = NULL;
+}
+
+AG_ObjectClass agDriverMwClass = {
+	"AG_Driver:AG_DriverMw",
+	sizeof(AG_DriverMw),
+	{ 1,4 },
+	Init,
+	NULL,		/* reinit */
+	NULL,		/* destroy */
+	NULL,		/* load */
+	NULL,		/* save */
+	NULL		/* edit */
+};
---
+++ trunk/gui/drv_sw.c
@@ -0,0 +1,460 @@
+/*
+ * Copyright (c) 2009 Hypertriton, Inc. <http://hypertriton.com/>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Single-window graphics driver framework. Under this framework, the driver
+ * creates a single "native" display context and Agar emulates a window
+ * manager internally.
+ */
+
+#include <config/have_sdl.h>
+#include <config/have_opengl.h>
+#include <config/have_glx.h>
+
+#include <core/core.h>
+
+#include "gui.h"
+#include "window.h"
+#include "menu.h"
+#include "icons.h"
+
+static void (*agVideoResizeCallback)(Uint w, Uint h) = NULL;
+
+extern int agWindowXOutLimit;
+extern int agWindowBotOutLimit;
+
+static void
+Init(void *obj)
+{
+	AG_DriverSw *dsw = obj;
+
+	dsw->w = 0;
+	dsw->h = 0;
+	dsw->depth = 0;
+	dsw->flags = 0;
+	dsw->winop = AG_WINOP_NONE;
+	dsw->winSelected = NULL;
+	dsw->winToFocus = NULL;
+	dsw->winLastKeydown = NULL;
+	dsw->style = &agStyleDefault;
+	
+	if ((dsw->Lmodal = AG_ListNew()) == NULL)
+		AG_FatalError(NULL);
+}
+
+static void
+Destroy(void *obj)
+{
+	AG_DriverSw *dsw = obj;
+	
+	if (dsw->Lmodal != NULL)
+		AG_ListDestroy(dsw->Lmodal);
+}
+
+/*
+ * Generic background popup menu.
+ */
+
+static void
+UnminimizeWindow(AG_Event *event)
+{
+	AG_Window *win = AG_PTR(1);
+	AG_WindowUnminimize(win);
+}
+
+#ifdef AG_DEBUG
+static void
+OpenGuiDebugger(AG_Event *event)
+{
+	AG_Window *win;
+
+	if ((win = AG_GuiDebugger()) != NULL)
+		AG_WindowShow(win);
+}
+#endif /* AG_DEBUG */
+
+static void
+ExitApplication(AG_Event *event)
+{
+	AG_Quit();
+}
+
+/* Display a generic popup menu for the display background. */
+void
+AG_WM_BackgroundPopupMenu(AG_DriverSw *dsw)
+{
+	AG_Menu *me;
+	AG_MenuItem *mi;
+	AG_Window *win;
+	int x, y;
+	int nWindows = 0;
+
+	me = AG_MenuNew(NULL, 0);
+	mi = me->itemSel = AG_MenuAddItem(me, NULL);
+
+	AG_FOREACH_WINDOW_REVERSE(win, dsw) {
+		if (strcmp(win->caption, "win-popup") == 0) {
+			continue;
+		}
+		AG_MenuAction(mi,
+		    win->caption[0] != '\0' ? win->caption : _("Untitled"),
+		    agIconWinMaximize.s,
+		    UnminimizeWindow, "%p", win);
+		nWindows++;
+	}
+	if (nWindows > 0) {
+		AG_MenuSeparator(mi);
+	}
+#ifdef AG_DEBUG
+	AG_MenuAction(mi, _("GUI debugger"), agIconMagnifier.s,
+	    OpenGuiDebugger, NULL);
+#endif
+	AG_MenuAction(mi, _("Exit application"), agIconWinClose.s,
+	    ExitApplication, NULL);
+				
+	AG_MouseGetState(agMouse, &x, &y);
+	AG_MenuExpand(me, mi, x+4, y+4);
+}
+
+/* Resize a video display to the specified dimensions. */
+int
+AG_ResizeDisplay(int w, int h)
+{
+	AG_DriverSw *dsw = AGDRIVER_SW(agDriver);
+	AG_Window *win;
+
+	if (AGDRIVER_SINGLE(agDriver)) {
+		AG_SetError("AG_ResizeDisplay() is only applicable to "
+		            "single-window graphics drivers");
+		return (-1);
+	}
+	if (AGDRIVER_SW_CLASS(dsw)->videoResize(dsw, (Uint)w, (Uint)h) == -1)
+		return (-1);
+
+	/* Update the Agar window geometries. */
+	AG_FOREACH_WINDOW(win, agDriver) {
+		AG_SizeAlloc a;
+
+		a.x = WIDGET(win)->x;
+		a.y = WIDGET(win)->y;
+		a.w = WIDGET(win)->w;
+		a.h = WIDGET(win)->h;
+
+		AG_ObjectLock(win);
+
+		if (win->flags & AG_WINDOW_MAXIMIZED) {
+			AG_WindowSetGeometryMax(win);
+		} else {
+			if (win->flags & AG_WINDOW_HMAXIMIZE) {
+				a.x = 0;
+				a.w = dsw->w;
+			} else {
+				if (a.x+a.w > dsw->w) {
+					a.x = dsw->w - a.w;
+					if (a.x < 0) {
+						a.x = 0;
+						a.w = dsw->w;
+					}
+				}
+			}
+			if (win->flags & AG_WINDOW_VMAXIMIZE) {
+				a.y = 0;
+				a.h = dsw->h;
+			} else {
+				if (a.y+a.h > dsw->h) {
+					a.y = dsw->h - a.h;
+					if (a.y < 0) {
+						a.y = 0;
+						a.h = dsw->w;
+					}
+				}
+			}
+			AG_WidgetSizeAlloc(win, &a);
+			AG_WindowUpdate(win);
+		}
+		AG_ObjectUnlock(win);
+	}
+	if (agVideoResizeCallback != NULL) {
+		agVideoResizeCallback(w, h);
+	}
+	return (0);
+}
+
+/* Configure a video resize callback routine. */
+void
+AG_SetVideoResizeCallback(void (*fn)(Uint w, Uint h))
+{
+	agVideoResizeCallback = fn;
+}
+
+/* Process a window move initiated by the WM. */
+static void
+WM_Move(AG_Window *win, int xRel, int yRel)
+{
+	AG_Rect rPrev, rNew;
+	AG_Rect rFill1, rFill2;
+
+	rPrev.x = WIDGET(win)->x;
+	rPrev.y = WIDGET(win)->y;
+	rPrev.w = WIDTH(win);
+	rPrev.h = HEIGHT(win);
+
+	WIDGET(win)->x += xRel;
+	WIDGET(win)->y += yRel;
+	AG_WM_LimitWindowToView(win);
+
+	AG_WidgetUpdateCoords(win, WIDGET(win)->x, WIDGET(win)->y);
+
+	/* Update the background. */
+	/* XXX TODO Avoid drawing over KEEPABOVE windows */
+	rNew.x = WIDGET(win)->x;
+	rNew.y = WIDGET(win)->y;
+	rNew.w = WIDTH(win);
+	rNew.h = HEIGHT(win);
+	rFill1.w = 0;
+	rFill2.w = 0;
+	if (rNew.x > rPrev.x) {		/* Right */
+		rFill1.x = rPrev.x;
+		rFill1.y = rPrev.y;
+		rFill1.w = rNew.x - rPrev.x;
+		rFill1.h = rNew.h;
+	} else if (rNew.x < rPrev.x) {	/* Left */
+		rFill1.x = rNew.x + rNew.w;
+		rFill1.y = rNew.y;
+		rFill1.w = rPrev.x - rNew.x;
+		rFill1.h = rPrev.h;
+	}
+	if (rNew.y > rPrev.y) {		/* Downward */
+		rFill2.x = rPrev.x;
+		rFill2.y = rPrev.y;
+		rFill2.w = rNew.w;
+		rFill2.h = rNew.y - rPrev.y;
+	} else if (rNew.y < rPrev.y) {	/* Upward */
+		rFill2.x = rPrev.x;
+		rFill2.y = rNew.y + rNew.h;
+		rFill2.w = rPrev.w;
+		rFill2.h = rPrev.y - rNew.y;
+	}
+
+	if (rFill1.w > 0) {
+		agDriverOps->fillRect(agDriver, rFill1, agColors[BG_COLOR]);
+		agDriverOps->updateRegion(agDriver, rFill1);
+	}
+	if (rFill2.w > 0) {
+		agDriverOps->fillRect(agDriver, rFill2, agColors[BG_COLOR]);
+		agDriverOps->updateRegion(agDriver, rFill2);
+	}
+
+	AG_PostEvent(NULL, win, "window-user-move", "%d,%d",
+	    WIDGET(win)->x, WIDGET(win)->y);
+}
+
+/* Process a window resize operation initiated by the WM. */
+static void
+WM_Resize(int op, AG_Window *win, int xRel, int yRel)
+{
+	int x = WIDGET(win)->x;
+	int y = WIDGET(win)->y;
+	int w = WIDTH(win);
+	int h = HEIGHT(win);
+
+	switch (op) {
+	case AG_WINOP_LRESIZE:
+		if (!(win->flags & AG_WINDOW_NOHRESIZE)) {
+			if (xRel < 0) {
+				w -= xRel;
+				x += xRel;
+			} else if (xRel > 0) {
+				w -= xRel;
+				x += xRel;
+			}
+		}
+		if (!(win->flags & AG_WINDOW_NOVRESIZE)) {
+			if (yRel < 0 || yRel > 0)
+				h += yRel;
+		}
+		break;
+	case AG_WINOP_RRESIZE:
+		if (!(win->flags & AG_WINDOW_NOHRESIZE)) {
+			if (xRel < 0 || xRel > 0)
+				w += xRel;
+		}
+		if (!(win->flags & AG_WINDOW_NOVRESIZE)) {
+			if (yRel < 0 || yRel > 0)
+				h += yRel;
+		}
+		break;
+	case AG_WINOP_HRESIZE:
+		if (!(win->flags & AG_WINDOW_NOHRESIZE)) {
+			if (yRel < 0 || yRel > 0)
+				h += yRel;
+		}
+		break;
+	default:
+		break;
+	}
+	AG_WindowSetGeometry(win, x, y, w, h);
+	AG_PostEvent(NULL, win, "window-user-resize", "%d,%d", w, h);
+}
+
+
+/*
+ * Window-manager specific processing of mouse-motion events. This is
+ * called by low-level event processing code whenever a WM operation is
+ * in effect.
+ */
+void
+AG_WM_MouseMotion(AG_DriverSw *dsw, AG_Window *win, int xRel, int yRel)
+{
+	switch (dsw->winop) {
+	case AG_WINOP_MOVE:
+		WM_Move(win, xRel, yRel);
+		break;
+	case AG_WINOP_LRESIZE:
+		WM_Resize(AG_WINOP_LRESIZE, win, xRel, yRel);
+		break;
+	case AG_WINOP_RRESIZE:
+		WM_Resize(AG_WINOP_RRESIZE, win, xRel, yRel);
+		break;
+	case AG_WINOP_HRESIZE:
+		WM_Resize(AG_WINOP_HRESIZE, win, xRel, yRel);
+		break;
+	default:
+		break;
+	}
+}
+
+/*
+ * Reorder the window list and post the appropriate Window events following
+ * a change in focus (single-display drivers only).
+ */
+void
+AG_ChangeWindowFocus(void)
+{
+	AG_Window *winLast = OBJECT_LAST_CHILD(agDriver,ag_window);
+	AG_Window *winToFocus = agDriverSw->winToFocus;
+
+	if (winLast != NULL) {
+		if (winToFocus != NULL &&
+		    winToFocus == winLast) {
+			/* Nothing to do */
+			goto out;
+		}
+		AG_ObjectLock(winLast);
+		if (winLast->flags & AG_WINDOW_KEEPABOVE) {
+			AG_ObjectUnlock(winLast);
+			goto out;
+		}
+		AG_PostEvent(NULL, winLast, "window-lostfocus", NULL);
+		AG_ObjectUnlock(winLast);
+	}
+	if (winToFocus != NULL) {
+		AG_ObjectLock(winToFocus);
+		if (winToFocus->flags & AG_WINDOW_KEEPBELOW) {
+			AG_ObjectUnlock(winToFocus);
+			goto out;
+		}
+		AG_ObjectMoveToTail(winToFocus);
+		AG_PostEvent(NULL, winToFocus, "window-gainfocus", NULL);
+		AG_ObjectUnlock(winToFocus);
+	}
+out:
+	agDriverSw->winToFocus = NULL;
+}
+
+/* Limit window size to display size. */
+void
+AG_WM_LimitWindowToDisplaySize(AG_Driver *drv, AG_SizeAlloc *a)
+{
+	Uint wMax, hMax;
+
+	AG_GetDisplaySize(drv, &wMax, &hMax);
+
+	if (a->x+a->w > wMax) {
+		a->w = wMax - a->x;
+	}
+	if (a->y+a->h > hMax) {
+		a->h = hMax - a->y;
+	}
+	if (a->w < 0) {
+		a->x = 0;
+		a->w = wMax;
+	}
+	if (a->h < 0) {
+		a->y = 0;
+		a->h = hMax;
+	}
+}
+
+
+/*
+ * Limit the window geometry/coordinates to the view area.
+ * The window must be locked.
+ */
+void
+AG_WM_LimitWindowToView(AG_Window *win)
+{
+	AG_DriverSw *dsw = OBJECT(win)->parent;
+	AG_Widget *w = WIDGET(win);
+
+	if (w->x < agWindowXOutLimit - w->w) {
+		w->x = agWindowXOutLimit - w->w;
+	} else if (w->x > dsw->w - agWindowXOutLimit) {
+		w->x = dsw->w - agWindowXOutLimit;
+	}
+	if (w->y < 0) {
+		w->y = 0;
+	} else if (w->y > dsw->h - agWindowBotOutLimit) {
+		w->y = dsw->h - agWindowBotOutLimit;
+	}
+	
+#if 0
+	if (w->x + w->w > dsw->w) { w->x = dsw->w - w->w; }
+	if (w->y + w->h > dsw->h) { w->y = dsw->h - w->h; }
+	if (w->x < 0) { w->x = 0; }
+	if (w->y < 0) { w->y = 0; }
+
+	if (w->x+w->w > dsw->w) {
+		w->x = 0;
+		w->w = dsw->w - 1;
+	}
+	if (w->y+w->h > dsw->h) {
+		w->y = 0;
+		w->h = dsw->h - 1;
+	}
+#endif
+}
+
+AG_ObjectClass agDriverSwClass = {
+	"AG_Driver:AG_DriverSw",
+	sizeof(AG_DriverSw),
+	{ 1,4 },
+	Init,
+	NULL,		/* reinit */
+	Destroy,
+	NULL,		/* load */
+	NULL,		/* save */
+	NULL		/* edit */
+};
