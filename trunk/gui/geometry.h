c2f94f2e565ccdd06edc7df4ee888957e7a5c3dc
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2009-10-23 17:10:34 +0000

    implement AG_ColorCompare()

--- trunk/gui/geometry.h
+++ trunk/gui/geometry.h
@@ -207,18 +207,19 @@ AG_RectInside2(const AG_Rect2 *r, int x, int y)
 
 /* Return a Color structure for given RGB components. */
 static __inline__ AG_Color
-AG_COLOR_RGB(Uint8 r, Uint8 g, Uint8 b)
+AG_ColorRGB(Uint8 r, Uint8 g, Uint8 b)
 {
 	AG_Color c;
 	c.r = r;
 	c.g = g;
 	c.b = b;
+	c.a = 255;
 	return (c);
 }
 
 /* Return a Color structure for given RGBA components. */
 static __inline__ AG_Color
-AG_COLOR_RGBA(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+AG_ColorRGBA(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
 {
 	AG_Color c;
 	c.r = r;
@@ -226,6 +227,28 @@ AG_COLOR_RGBA(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
 	c.b = b;
 	c.a = a;
 	return (c);
+}
+
+/* Return a Color structure from 0xRRGGBBAA format. */
+static __inline__ AG_Color
+AG_ColorHex(Uint32 h)
+{
+	AG_Color c;
+
+	c.r = (h&0xff000000) >> 24;
+	c.g = (h&0x00ff0000) >> 16;
+	c.b = (h&0x0000ff00) >> 8;
+	c.a = (h&0x000000ff);
+	return (c);
+}
+
+static __inline__ int
+AG_ColorCompare(AG_Color c1, AG_Color c2)
+{
+	return (c1.r == c2.r &&
+	        c1.g == c2.g &&
+		c1.b == c2.b &&
+		c1.a == c2.a) ? 0 : 1;
 }
 __END_DECLS
 
