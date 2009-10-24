26123878c8ed9eb0ed0f19aa88ffa252e93c58c3
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2009-10-24 01:07:55 +0000

    tri:PerfMonUpdate()

--- trunk/gui/perfmon.h
+++ trunk/gui/perfmon.h
@@ -1,5 +1,7 @@
 /*	Public domain	*/
 
+#ifndef _AGAR_GUI_PERFMON_H_
+#define _AGAR_GUI_PERFMON_H_
 #include <agar/gui/begin.h>
 
 __BEGIN_DECLS
@@ -9,7 +11,8 @@ extern AG_Window *agPerfWindow;
 
 AG_Window *AG_PerfMonShow(void);
 void       AG_PerfMonInit(void);
-void       AG_PerfMonUpdate(void);
+void       AG_PerfMonUpdate(int);
 __END_DECLS
 
 #include <agar/gui/close.h>
+#endif /* _AGAR_GUI_PERFMON_H_ */
