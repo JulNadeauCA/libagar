eaf1b3180502a5ae38fa9c60aabde12870c3c425
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2009-12-08 06:30:49 +0000

    no undef if _USE_AGAR_TYPES is defined.

--- trunk/core/close_types.h
+++ trunk/core/close_types.h
@@ -1,13 +1,15 @@
 /*	Public domain	*/
 
-#undef Uint
-#undef Uchar
-#undef Ulong
-#undef Uint8
-#undef Sint8
-#undef Uint16
-#undef Sint16
-#undef Uint32
-#undef Sint32
-#undef Uint64
-#undef Sint64
+#ifndef _USE_AGAR_TYPES
+# undef Uint
+# undef Uchar
+# undef Ulong
+# undef Uint8
+# undef Sint8
+# undef Uint16
+# undef Sint16
+# undef Uint32
+# undef Sint32
+# undef Uint64
+# undef Sint64
+#endif /* _USE_AGAR_TYPES */
