2aad851614f56c7c381514e5504c65a6f41b2eef
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2009-12-15 14:08:23 +0000

    compat wrapper for getopt(3)

---
+++ trunk/core/getopt.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2009 Hypertriton, Inc. <http://hypertriton.com/>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Compatibility interface for getopt(3).
+ */
+
+#include "core.h"
+#include "getopt.h"
+
+#include <agar/config/have_getopt.h>
+
+int
+AG_Getopt(int argc, char * const argv[], const char *optstring, char **oa,
+    int *oi)
+{
+#ifdef HAVE_GETOPT
+	extern char *optarg;
+	extern int optind;
+	int rv;
+	rv = getopt(argc, argv, optstring);
+	if (oa != NULL) { *oa = optarg; }
+	if (oi != NULL) { *oi = optind; }
+	return (rv);
+#else
+	return (-1);
+#endif
+}
