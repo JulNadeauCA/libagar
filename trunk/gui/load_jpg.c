187a1c1dbe26b5cf9a70e387784d64d7b1b1e7d4
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2010-04-18 01:50:48 +0000

    implement AG_SurfaceFromJPEG(), AG_SurfaceFromPNG()

---
+++ trunk/gui/load_jpg.c
@@ -0,0 +1,311 @@
+/*
+ * Copyright (c) 2010 Hypertriton, Inc. <http://hypertriton.com/>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Support for reading image files in JPEG format via libjpeg.
+ */
+
+#include <core/core.h>
+#include "gui.h"
+
+#include <config/have_jpeg.h>
+#ifdef HAVE_JPEG
+
+#include <jpeglib.h>
+#include <errno.h>
+#include <setjmp.h>
+
+struct ag_jpg_errmgr {
+	struct jpeg_error_mgr errmgr;
+	jmp_buf escape;
+};
+
+struct ag_jpg_sourcemgr {
+	struct jpeg_source_mgr pub;
+	AG_DataSource *ds;
+	Uint8 buffer[4096];
+};
+
+int agJPEGQuality = 75;		/* Quality for JPEG export */
+
+/*
+ * Callbacks
+ */
+static void
+AG_JPG_InitSource(j_decompress_ptr cinfo)
+{
+	/* no-op */
+}
+static int
+AG_JPG_FillInputBuffer(j_decompress_ptr cinfo)
+{
+	struct ag_jpg_sourcemgr *sm = (struct ag_jpg_sourcemgr *)cinfo->src;
+	size_t rv;
+
+	rv = AG_Read(sm->ds, sm->buffer, 1, sizeof(sm->buffer));
+	if (rv <= 0) {
+		sm->buffer[0] = 0xff;
+		sm->buffer[1] = (Uint8)JPEG_EOI;
+		rv = 2;
+	}
+	sm->pub.next_input_byte = sm->buffer;
+	sm->pub.bytes_in_buffer = rv;
+	return (TRUE);
+}
+static void
+AG_JPG_SkipInputData(j_decompress_ptr cinfo, long len)
+{
+	struct ag_jpg_sourcemgr *sm = (struct ag_jpg_sourcemgr *)cinfo->src;
+
+	if (len > 0) {
+		while (len > (long)sm->pub.bytes_in_buffer) {
+			len -= (long)sm->pub.bytes_in_buffer;
+			sm->pub.fill_input_buffer(cinfo);
+		}
+		sm->pub.next_input_byte += (size_t)len;
+		sm->pub.bytes_in_buffer -= (size_t)len;
+	}
+}
+static void
+AG_JPG_TermSource(j_decompress_ptr cinfo)
+{
+	/* no-op */
+}
+static void
+AG_JPG_ErrorExit(j_common_ptr cinfo)
+{
+	struct ag_jpg_errmgr *err = (struct ag_jpg_errmgr *)cinfo->err;
+	longjmp(err->escape, 1);
+}
+static void
+AG_JPG_OutputMessage(j_common_ptr cinfo)
+{
+	/* no-op */
+}
+
+/* Load a surface from a JPEG image file. */
+AG_Surface *
+AG_SurfaceFromJPEG(const char *path)
+{
+	AG_DataSource *ds;
+	AG_Surface *s;
+
+	if ((ds = AG_OpenFile(path, "r")) == NULL) {
+		return (NULL);
+	}
+	if ((s = AG_ReadSurfaceFromJPEG(ds)) == NULL) {
+		AG_SetError("%s: %s", path, AG_GetError());
+		AG_CloseFile(ds);
+		return (NULL);
+	}
+	AG_CloseFile(ds);
+	return (s);
+}
+
+/* Configure JPEG quality for export */
+int
+AG_SetJPEGQuality(int qual)
+{
+	if (qual < 0 || qual > 100) {
+		AG_SetError("Quality must be between 0 and 100");
+		return (-1);
+	}
+	agJPEGQuality = qual;	/* Atomic */
+	return (0);
+}
+
+/* Export a surface to a JPEG image file. */
+int
+AG_SurfaceExportJPEG(const AG_Surface *su, const char *path)
+{
+	struct jpeg_error_mgr jerrmgr;
+	struct jpeg_compress_struct jcomp;
+	Uint8 *jcopybuf;
+	FILE *f;
+	JSAMPROW row[1];
+	int x;
+
+	if ((f = fopen(path, "wb")) == NULL) {
+		AG_SetError("fdopen: %s", strerror(errno));
+		return (-1);
+	}
+
+	jcomp.err = jpeg_std_error(&jerrmgr);
+
+	jpeg_create_compress(&jcomp);
+
+	jcomp.image_width = su->w;
+	jcomp.image_height = su->h;
+	jcomp.input_components = 3;
+	jcomp.in_color_space = JCS_RGB;
+
+	jpeg_set_defaults(&jcomp);
+	jpeg_set_quality(&jcomp, agJPEGQuality, TRUE);
+	jpeg_stdio_dest(&jcomp, f);
+
+	if ((jcopybuf = TryMalloc(su->w*3)) == NULL) {
+		jpeg_destroy_compress(&jcomp);
+		fclose(f);
+		return (-1);
+	}
+
+	jpeg_start_compress(&jcomp, TRUE);
+	while (jcomp.next_scanline < jcomp.image_height) {
+		Uint8 *pSrc = (Uint8 *)su->pixels +
+		    jcomp.next_scanline*su->pitch;
+		Uint8 *pDst = jcopybuf;
+		AG_Color C;
+
+		for (x = 0; x < su->w; x++) {
+			C = AG_GetColorRGB(AG_GET_PIXEL(su,pSrc), su->format);
+			*pDst++ = C.r;
+			*pDst++ = C.g;
+			*pDst++ = C.b;
+			pSrc += su->format->BytesPerPixel;
+		}
+		row[0] = jcopybuf;
+		jpeg_write_scanlines(&jcomp, row, 1);
+	}
+	jpeg_finish_compress(&jcomp);
+	jpeg_destroy_compress(&jcomp);
+
+	fclose(f);
+	Free(jcopybuf);
+	return (0);
+}
+
+/* Load surface contents from a JPEG image file. */
+AG_Surface *
+AG_ReadSurfaceFromJPEG(AG_DataSource *ds)
+{
+	struct jpeg_decompress_struct cinfo;
+	JSAMPROW rowptr[1];
+	AG_Surface *volatile su = NULL;
+	off_t start = AG_Tell(ds);
+	struct ag_jpg_errmgr jerrmgr;
+	struct ag_jpg_sourcemgr *sm;
+
+	cinfo.err = jpeg_std_error(&jerrmgr.errmgr);
+	jerrmgr.errmgr.error_exit = AG_JPG_ErrorExit;
+	jerrmgr.errmgr.output_message = AG_JPG_OutputMessage;
+	if (setjmp(jerrmgr.escape)) {
+		jpeg_destroy_decompress(&cinfo);
+		if (su != NULL) {
+			AG_SurfaceFree(su);
+		}
+		AG_SetError("Error loading JPEG file");
+		goto fail;
+	}
+
+	jpeg_create_decompress(&cinfo);
+
+	if (cinfo.src == NULL) {
+		cinfo.src = (struct jpeg_source_mgr *)
+		    (*cinfo.mem->alloc_small)((j_common_ptr)&cinfo,
+		    JPOOL_PERMANENT,
+		    sizeof(struct ag_jpg_sourcemgr));
+		sm = (struct ag_jpg_sourcemgr *)cinfo.src;
+	}
+	sm = (struct ag_jpg_sourcemgr *)cinfo.src;
+	sm->ds = ds;
+	sm->pub.init_source = AG_JPG_InitSource;
+	sm->pub.fill_input_buffer = AG_JPG_FillInputBuffer;
+	sm->pub.skip_input_data = AG_JPG_SkipInputData;
+	sm->pub.resync_to_restart = jpeg_resync_to_restart;
+	sm->pub.term_source = AG_JPG_TermSource;
+	sm->pub.bytes_in_buffer = 0;
+	sm->pub.next_input_byte = NULL;
+
+	jpeg_read_header(&cinfo, TRUE);
+
+	if (cinfo.num_components == 4) {
+		cinfo.out_color_space = JCS_CMYK;
+		cinfo.quantize_colors = FALSE;
+		jpeg_calc_output_dimensions(&cinfo);
+
+		su = AG_SurfaceRGBA(cinfo.output_width, cinfo.output_height,
+		    32, 0,
+#if AG_BYTEORDER == AG_BIG_ENDIAN
+		    0x0000ff00, 0x00ff0000, 0xff000000, 0x000000ff
+#else
+		    0x00ff0000, 0x0000ff00, 0x000000ff, 0xff000000
+#endif
+		);
+	} else {
+		cinfo.out_color_space = JCS_RGB;
+		cinfo.quantize_colors = FALSE;
+		jpeg_calc_output_dimensions(&cinfo);
+
+		su = AG_SurfaceRGB(cinfo.output_width, cinfo.output_height,
+		    24,
+#if AG_BYTEORDER == AG_BIG_ENDIAN
+		    0x00ff0000, 0x0000ff00, 0x000000ff,
+#else
+		    0x000000ff, 0x0000ff00, 0x00ff0000,
+#endif
+		    0);
+	}
+	if (su == NULL) {
+		jpeg_destroy_decompress(&cinfo);
+		AG_SetError("Out of memory");
+		goto fail;
+	}
+
+	jpeg_start_decompress(&cinfo);
+	while (cinfo.output_scanline < cinfo.output_height) {
+		rowptr[0] = (JSAMPROW)(Uint8 *)su->pixels +
+		                               cinfo.output_scanline*su->pitch;
+		jpeg_read_scanlines(&cinfo, rowptr, (JDIMENSION) 1);
+	}
+	jpeg_finish_decompress(&cinfo);
+	jpeg_destroy_decompress(&cinfo);
+	return (su);
+fail:
+	AG_Seek(ds, start, AG_SEEK_SET);
+	return (NULL);
+}
+
+#else /* !HAVE_JPEG */
+
+AG_Surface *
+AG_SurfaceFromJPEG(const char *path)
+{
+	AG_SetError(_("Agar not compiled with JPEG support"));
+	return (NULL);
+}
+int
+AG_SurfaceExportJPEG(const AG_Surface *su, const char *path)
+{
+	AG_SetError(_("Agar not compiled with JPEG support"));
+	return (-1);
+}
+AG_Surface *
+AG_ReadSurfaceFromJPEG(AG_DataSource *ds)
+{
+	AG_SetError(_("Agar not compiled with JPEG support"));
+	return (NULL);
+}
+
+#endif /* HAVE_JPEG */
---
+++ trunk/gui/load_png.c
@@ -0,0 +1,259 @@
+/*
+ * Copyright (c) 2010 Hypertriton, Inc. <http://hypertriton.com/>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Support for reading image files in PNG format via libpng.
+ */
+
+#include <core/core.h>
+#include "gui.h"
+
+#include <config/have_png.h>
+#if defined(HAVE_PNG)
+
+#ifdef macintosh
+# define MACOS
+#endif
+#include <png.h>
+
+/* Load a surface from a PNG image file. */
+AG_Surface *
+AG_SurfaceFromPNG(const char *path)
+{
+	AG_DataSource *ds;
+	AG_Surface *s;
+
+	if ((ds = AG_OpenFile(path, "r")) == NULL) {
+		return (NULL);
+	}
+	if ((s = AG_ReadSurfaceFromPNG(ds)) == NULL) {
+		AG_SetError("%s: %s", path, AG_GetError());
+		AG_CloseFile(ds);
+		return (NULL);
+	}
+	AG_CloseFile(ds);
+	return (s);
+}
+
+static void
+AG_PNG_ReadData(png_structp png, png_bytep buf, png_size_t size)
+{
+	AG_DataSource *ds = (AG_DataSource*)png_get_io_ptr(png);
+	AG_Read(ds, buf, size, 1);
+}
+
+/* Load a surface from a PNG image file. */
+AG_Surface *
+AG_ReadSurfaceFromPNG(AG_DataSource *ds)
+{
+	int start;
+	AG_Surface *volatile su = NULL;
+	png_structp png = NULL;
+	png_infop info_ptr = NULL;
+	png_uint_32 width, height;
+	int depth, colorType, intlaceType;
+	Uint32 Rmask = 0, Gmask = 0, Bmask = 0, Amask = 0;
+	AG_Palette *pal;
+	png_bytep *volatile pData = NULL;
+	int row, i, t;
+	volatile int ckey = -1;
+	png_color_16 *transv;
+
+	start = AG_Tell(ds);
+
+	if ((png = png_create_read_struct(PNG_LIBPNG_VER_STRING,
+	    NULL, NULL, NULL)) == NULL) {
+		AG_SetError("Out of memory (libpng)");
+		goto fail;
+	}
+	if ((info_ptr = png_create_info_struct(png)) == NULL) {
+		AG_SetError("png_create_info_struct() failed");
+		goto fail;
+	}
+	if (setjmp(png->jmpbuf)) {
+		AG_SetError("Error reading PNG image");
+		goto fail;
+	}
+
+	png_set_read_fn(png, ds, AG_PNG_ReadData);
+
+	/* Read PNG image information. */
+	png_read_info(png, info_ptr);
+	png_get_IHDR(png, info_ptr, &width, &height, &depth,
+	    &colorType, &intlaceType, NULL, NULL);
+
+	/* Strip the pixels with 16 bits/channel to 8 bits/channel. */
+	png_set_strip_16(png);
+
+	/*
+	 * Always expand 1/2/4-bit images to 8-bit, and expand grayscales
+	 * images of depth <8-bit to 8-bit.
+	 */
+	png_set_packing(png);
+	if (colorType == PNG_COLOR_TYPE_GRAY)
+		png_set_expand(png);
+
+	/* Figure out transparency information. */
+	if (png_get_valid(png, info_ptr, PNG_INFO_tRNS)) {
+	        int num_trans;
+		Uint8 *trans;
+
+		png_get_tRNS(png, info_ptr, &trans, &num_trans, &transv);
+		if (colorType == PNG_COLOR_TYPE_PALETTE) {
+			for (i = 0, t = -1;
+			     i < num_trans;
+			     i++) {
+				if (trans[i] == 0) {
+					if (t >= 0)
+						break;
+					t = i;
+				} else if (trans[i] != 255) {
+					break;
+				}
+			}
+			if (i == num_trans) {	/* One transparent index */
+				ckey = t;
+			} else {
+				png_set_expand(png);
+			}
+		} else {
+			ckey = 0;
+		}
+	}
+
+	/* Expand grayscale to 24-bit RGB */
+	if (colorType == PNG_COLOR_TYPE_GRAY_ALPHA)
+		png_set_gray_to_rgb(png);
+
+	/* Update png_info structure per our requirements. */
+	png_read_update_info(png, info_ptr);
+
+	png_get_IHDR(png, info_ptr, &width, &height, &depth,
+	    &colorType, &intlaceType, NULL, NULL);
+
+	if (colorType != PNG_COLOR_TYPE_PALETTE) {
+#if AG_BYTEORDER == AG_BIG_ENDIAN
+		int s = (info_ptr->channels == 4) ? 0 : 8;
+		Rmask = 0xff000000 >> s;
+		Gmask = 0x00ff0000 >> s;
+		Bmask = 0x0000ff00 >> s;
+		Amask = 0x000000ff >> s;
+#else
+		Rmask = 0x000000ff;
+		Gmask = 0x0000ff00;
+		Bmask = 0x00ff0000;
+		Amask = (info_ptr->channels == 4) ? 0xff000000 : 0;
+#endif
+	}
+	if ((su = AG_SurfaceRGBA(width, height, depth*info_ptr->channels, 0,
+	    Rmask, Gmask, Bmask, Amask)) == NULL)
+		goto fail;
+
+	if (ckey != -1) {
+	        if (colorType != PNG_COLOR_TYPE_PALETTE) {
+		        ckey = AG_MapPixelRGB(su->format,
+			    (Uint8)transv->red,
+			    (Uint8)transv->green,
+			    (Uint8)transv->blue);
+		}
+	        AG_SurfaceSetColorKey(su, AG_SRCCOLORKEY, ckey);
+	}
+
+	/* Read image data */
+	if ((pData = TryMalloc(sizeof(png_bytep)*height)) == NULL) {
+		goto fail;
+	}
+	for (row = 0; row < (int)height; row++) {
+		pData[row] = (png_bytep)(Uint8 *)su->pixels + row*su->pitch;
+	}
+	png_read_image(png, pData);
+
+	/* Read palette information */
+	if ((pal = su->format->palette) != NULL) {
+		if (colorType == PNG_COLOR_TYPE_GRAY) {
+			pal->nColors = 256;
+			for (i = 0; i < 256; i++) {
+				pal->colors[i].r = i;
+				pal->colors[i].g = i;
+				pal->colors[i].b = i;
+			}
+		} else if (info_ptr->num_palette > 0) {
+			pal->nColors = info_ptr->num_palette; 
+			for (i = 0; i < info_ptr->num_palette; i++) {
+				pal->colors[i].b = info_ptr->palette[i].blue;
+				pal->colors[i].g = info_ptr->palette[i].green;
+				pal->colors[i].r = info_ptr->palette[i].red;
+			}
+		}
+	}
+	if (png != NULL) {
+		png_destroy_read_struct(&png,
+		    info_ptr ? &info_ptr : (png_infopp)0, (png_infopp)0);
+	}
+	Free(pData);
+	return (su); 
+fail:
+	if (png != NULL) {
+		png_destroy_read_struct(&png,
+		    info_ptr ? &info_ptr : (png_infopp)0, (png_infopp)0);
+	}
+	Free(pData);
+	if (su) {
+		AG_SurfaceFree(su);
+	}
+	AG_Seek(ds, start, AG_SEEK_SET);
+	return (NULL);
+}
+
+int
+AG_SurfaceExportPNG(const AG_Surface *su, const char *path)
+{
+	/* XXX TODO */
+	AG_SetError(_("Export to PNG not yet implemented"));
+	return (-1);
+}
+
+#else /* !HAVE_PNG */
+
+AG_Surface *
+AG_SurfaceFromPNG(const char *path)
+{
+	AG_SetError(_("Agar not compiled with PNG support"));
+	return (NULL);
+}
+int
+AG_SurfaceExportPNG(const AG_Surface *su, const char *path)
+{
+	AG_SetError(_("Agar not compiled with PNG support"));
+	return (-1);
+}
+AG_Surface *
+AG_ReadSurfaceFromPNG(AG_DataSource *ds)
+{
+	AG_SetError(_("Agar not compiled with PNG support"));
+	return (NULL);
+}
+
+#endif /* HAVE_PNG */
