a23131226fbdbe0821e2012361cd77d902234c7d
Julien Nadeau Carriere <vedge@hypertriton.com>
2010-04-20 02:14:52 +0000

    remove the sdl_image stuff, use the built-in jpeg and png loaders.
    git-svn-id: https://dev.csoft.net/agar/trunk@8750 86a84d31-9702-0410-8f1e-a60cab7bd424

--- demos/loader/loader.c
+++ demos/loader/loader.c
@@ -8,59 +8,51 @@
 #include <agar/gui.h>
 
 #include <string.h>
-#ifndef _WIN32
-#include <unistd.h>
-#endif
 
-#ifndef _WIN32
-#include "config/have_sdl_image.h"
-#ifdef HAVE_SDL_IMAGE
-#include <SDL_image.h>
-#endif
-#endif
-
-/* Load a Windows bitmap using the built-in AG_SurfaceFromBMP(). */
+/* Callback routine for AG_FileDlg. */
 static void
-LoadBMP(AG_Event *event)
+LoadImage(AG_Event *event)
 {
 	AG_FileDlg *fd = AG_SELF();
 	char *file = AG_STRING(1);
 	AG_FileType *ft = AG_PTR(2);
-	AG_Surface *bmp;
+	AG_Surface *s;
 	AG_Window *win;
 	AG_Scrollview *sv;
 	Uint8 *pSrc;
-	char *title;
 	int i;
 
-	/* Load the bitmap file into a SDL surface. */
-	if ((bmp = AG_SurfaceFromBMP(file)) == NULL) {
+	if (strcmp(ft->exts[0], "*.bmp") == 0) {
+		s = AG_SurfaceFromBMP(file);
+	} else if (strcmp(ft->exts[0], "*.jpg") == 0) {
+		s = AG_SurfaceFromJPEG(file);
+	} else if (strcmp(ft->exts[0], "*.png") == 0) {
+		s = AG_SurfaceFromPNG(file);
+	} else {
+		return;
+	}
+	if (s == NULL) {
 		AG_TextMsg(AG_MSG_ERROR, "%s: %s", file, AG_GetError());
 		return;
 	}
-	pSrc = (Uint8 *)bmp->pixels;
 
-	if ((title = strrchr(file, '/')) == NULL) {
-		title = file;
-	} else {
-		title++;
-	}
 	win = AG_WindowNew(0);
-	AG_WindowSetCaption(win, "Image <%s>", title);
+	AG_WindowSetCaption(win, "Image <%s>", AG_ShortFilename(file));
 
 	/* We use AG_FileOptionFoo() to retrieve per-type options. */
-	if (AG_FileOptionBool(ft,"bmp.invert")) {
-		for (i = 0; i < bmp->w*bmp->h; i++) {
+	if (AG_FileOptionBool(ft,"invert")) {
+		pSrc = (Uint8 *)s->pixels;
+		for (i = 0; i < s->w*s->h; i++) {
 			Uint8 r, g, b;
 
-			AG_GetPixelRGB(AG_GET_PIXEL(bmp,pSrc), bmp->format,
+			AG_GetPixelRGB(AG_GET_PIXEL(s,pSrc), s->format,
 			    &r,&g,&b);
 			r = 255 - r;
 			g = 255 - g;
 			b = 255 - b;
-			AG_PUT_PIXEL(bmp, pSrc,
-			    AG_MapPixelRGB(bmp->format, r,g,b));
-			pSrc += bmp->format->BytesPerPixel;
+			AG_PUT_PIXEL(s, pSrc,
+			    AG_MapPixelRGB(s->format, r,g,b));
+			pSrc += s->format->BytesPerPixel;
 		}
 	}
 
@@ -69,56 +61,12 @@ LoadBMP(AG_Event *event)
 	 * the user can pan the view.
 	 */
 	sv = AG_ScrollviewNew(win, AG_SCROLLVIEW_BY_MOUSE|AG_SCROLLVIEW_EXPAND);
-	AG_PixmapFromSurfaceScaled(sv, 0, bmp, bmp->w, bmp->h);
+	AG_PixmapFromSurfaceScaled(sv, 0, s, s->w, s->h);
+	AG_SurfaceFree(s);
 
+	AG_WindowSetGeometry(win, -1, -1, s->w+32, s->h+64);
 	AG_WindowShow(win);
-	AG_WindowSetGeometry(win, -1, -1, bmp->w+20, bmp->h+20);
-	
-	AG_SurfaceFree(bmp);
 }
-
-#ifdef HAVE_SDL_IMAGE
-
-/* Load an image using SDL_image. */
-static void
-LoadIMG(AG_Event *event)
-{
-	AG_FileDlg *fd = AG_SELF();
-	char *file = AG_STRING(1);
-	AG_FileType *ft = AG_PTR(2);
-	SDL_Surface *img;
-	AG_Window *win;
-	AG_Scrollview *sv;
-	char *title;
-	int i;
-
-	/* Load the bitmap file into a SDL surface. */
-	if ((img = IMG_Load(file)) == NULL) {
-		AG_TextMsg(AG_MSG_ERROR, "%s: %s", file, IMG_GetError());
-		return;
-	}
-
-	if ((title = strrchr(file, '/')) == NULL) {
-		title = file;
-	} else {
-		title++;
-	}
-	win = AG_WindowNew(0);
-	AG_WindowSetCaption(win, "Image <%s>", title);
-
-	/*
-	 * Place an AG_Pixmap(3) widget inside of an AG_Scrollview(3) to
-	 * display the image.
-	 */
-	sv = AG_ScrollviewNew(win, AG_SCROLLVIEW_BY_MOUSE|AG_SCROLLVIEW_EXPAND);
-	AG_PixmapFromSurface(sv, 0, AG_SurfaceFromSDL(img));
-
-	AG_WindowShow(win);
-	AG_WindowSetGeometry(win, -1, -1, img->w+20, img->h+20);
-	
-	SDL_FreeSurface(img);
-}
-#endif /* HAVE_SDL_IMAGE */
 
 static void
 CreateWindow(void)
@@ -142,25 +90,17 @@ CreateWindow(void)
 
 	/*
 	 * Register the loader functions. We can assign a set of user
-	 * options to each type.
+	 * options to specific types as well.
 	 */
-	ft = AG_FileDlgAddType(fd, "Windows Bitmap", "*.bmp", LoadBMP, NULL);
-	AG_FileOptionNewBool(ft, "Invert bitmap", "bmp.invert", 0);
-#ifdef HAVE_SDL_IMAGE
-	AG_FileDlgAddType(fd, "JFIF format", "*.jpg,*.jpeg", LoadIMG, NULL);
-	AG_FileDlgAddType(fd, "Portable Network Graphics", "*.png", LoadIMG,
-	    NULL);
-	AG_FileDlgAddType(fd, "Graphics Interchange", "*.gif", LoadIMG, NULL);
-	AG_FileDlgAddType(fd, "GIMP Native", "*.xcf", LoadIMG, NULL);
-	AG_FileDlgAddType(fd, "Tagged Image File", "*.tif,*.tiff", LoadIMG,
-	    NULL);
-	AG_FileDlgAddType(fd, "X11 Pixmap", "*.xpm", LoadIMG, NULL);
-	AG_FileDlgAddType(fd, "Portable Anymap", "*.pnm", LoadIMG, NULL);
-	AG_FileDlgAddType(fd, "IBM PC Paintbrush", "*.pcx", LoadIMG, NULL);
-	AG_FileDlgAddType(fd, "TrueVision Targa", "*.tga", LoadIMG, NULL);
-	AG_FileDlgAddType(fd, "Interleaved Bitmap", "*.lbm,*.iff", LoadIMG,
-	    NULL);
-#endif /* HAVE_SDL_IMAGE */
+	ft = AG_FileDlgAddType(fd, "Windows Bitmap", "*.bmp", LoadImage, NULL);
+	AG_FileOptionNewBool(ft, "Invert colors", "invert", 0);
+ 
+	ft = AG_FileDlgAddType(fd, "JPEG image", "*.jpg,*.jpeg", LoadImage, NULL);
+	AG_FileOptionNewBool(ft, "Invert colors", "invert", 0);
+
+	ft = AG_FileDlgAddType(fd, "Portable Network Graphics", "*.png",
+	    LoadImage, NULL);
+	AG_FileOptionNewBool(ft, "Invert colors", "invert", 0);
 
 	/*
 	 * As different file types are selected, FileDlg will automatically
@@ -200,11 +140,6 @@ main(int argc, char *argv[])
 	AG_BindGlobalKey(AG_KEY_ESCAPE, AG_KEYMOD_ANY, AG_QuitGUI);
 	AG_BindGlobalKey(AG_KEY_F8, AG_KEYMOD_ANY, AG_ViewCapture);
 	CreateWindow();
-#ifndef HAVE_SDL_IMAGE
-	AG_TextWarning("sdl-image-warning",
-	   "The SDL_image library was not found.\n"
-	   "Without SDL_image, this demo will only load files in .bmp format.");
-#endif
 	AG_EventLoop();
 	AG_Destroy();
 	return (0);
