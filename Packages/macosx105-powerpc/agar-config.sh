e00668d62289f720ebc062dcdb52ea9c34f10a05
Julien Nadeau Carriere <vedge@hypertriton.com>
2010-04-22 11:46:21 +0000

    remove unused packages
    git-svn-id: https://dev.csoft.net/agar/trunk@8764 86a84d31-9702-0410-8f1e-a60cab7bd424

--- Packages/macosx105-powerpc/agar-config.sh
+++ Packages/macosx105-powerpc/agar-config.sh
@@ -4,14 +4,8 @@
 PREFIX="%PREFIX%"
 
 if [ "$1" = "--version" ]; then echo "%INSTALLED_VERSION%"; fi
-if [ "$1" = "--release" ]; then echo "%INSTALLED_RELEASE%"; fi
 if [ "$1" = "--prefix" ]; then echo "${PREFIX}"; fi
-
-if [ "$1" = "--threads" ]; then echo "yes"; fi
-if [ "$1" = "--network" ]; then echo "yes"; fi
-if [ "$1" = "--sdl" ]; then echo "yes"; fi
-if [ "$1" = "--opengl" ]; then echo "yes"; fi
-if [ "$1" = "--freetype" ]; then echo "yes"; fi
+if [ "$1" = "--exec-prefix" ]; then echo "${PREFIX}"; fi
 
 if [ "$1" = "--cflags" ]; then
 	echo "-D_GNU_SOURCE=1 -D_THREAD_SAFE -I${PREFIX}/include/agar -I${PREFIX}/include/SDL -I${PREFIX}/include/freetype2 -I${PREFIX}/include -I/usr/local/include/SDL -I/usr/local/include/freetype2 -I/usr/local/include -I/opt/local/include/SDL -I/opt/local/include/freetype2 -I/opt/local/include -I/usr/X11R6/include/SDL -I/usr/X11R6/include/freetype2 -I/usr/X11R6/include"
