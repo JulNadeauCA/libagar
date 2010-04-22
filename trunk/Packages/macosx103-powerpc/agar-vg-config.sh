6172b1a13e2a166c2e5505d069b4e9eb48ed2b6b
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2010-04-22 11:46:21 +0000

    remove unused packages

--- trunk/Packages/macosx103-powerpc/agar-vg-config.sh
+++ trunk/Packages/macosx103-powerpc/agar-vg-config.sh
@@ -4,10 +4,8 @@
 PREFIX="%PREFIX%"
 
 if [ "$1" = "--version" ]; then echo "%INSTALLED_VERSION%"; fi
-if [ "$1" = "--release" ]; then echo "%INSTALLED_RELEASE%"; fi
 if [ "$1" = "--prefix" ]; then echo "${PREFIX}"; fi
-
-if [ "$1" = "--threads" ]; then echo "yes"; fi
+if [ "$1" = "--exec-prefix" ]; then echo "${PREFIX}"; fi
 
 if [ "$1" = "--cflags" ]; then
 	echo "-I${PREFIX}/include/agar"
--- trunk/Packages/mingw32-i386-nothreads/agar-config.sh
+++ trunk/Packages/mingw32-i386-nothreads/agar-config.sh
@@ -4,14 +4,8 @@
 PREFIX="%PREFIX%"
 
 if [ "$1" = "--version" ]; then echo "%INSTALLED_VERSION%"; fi
-if [ "$1" = "--release" ]; then echo "%INSTALLED_RELEASE%"; fi
 if [ "$1" = "--prefix" ]; then echo "${PREFIX}"; fi
-
-if [ "$1" = "--threads" ]; then echo "no"; fi
-if [ "$1" = "--network" ]; then echo "no"; fi
-if [ "$1" = "--sdl" ]; then echo "yes"; fi
-if [ "$1" = "--opengl" ]; then echo "yes"; fi
-if [ "$1" = "--freetype" ]; then echo "yes"; fi
+if [ "$1" = "--exec-prefix" ]; then echo "${PREFIX}"; fi
 
 if [ "$1" = "--cflags" ]; then
 	echo "-I${PREFIX}/include/agar -I${PREFIX}/include/SDL -I/usr/local/include/SDL -I${PREFIX}/include/freetype2 -I/usr/local/include/freetype2 -I${PREFIX}/include -I/usr/local/include -I/usr/include/mingw -D_GNU_SOURCE=1 -Dmain=SDL_main"
