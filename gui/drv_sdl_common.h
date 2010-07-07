8952e6bc7768ac532c4d79db9631e622b2409b72
Julien Nadeau Carriere <vedge@hypertriton.com>
2010-07-07 10:26:08 +0000

    provide a public AG_SDL_TranslateEvent() function; thanks Olivier Boudeville!
    git-svn-id: https://dev.csoft.net/agar/trunk@8820 86a84d31-9702-0410-8f1e-a60cab7bd424

--- gui/drv_sdl_common.h
+++ gui/drv_sdl_common.h
@@ -45,6 +45,7 @@ int             AG_SDL_PostEventCallback(void *);
 int             AG_SDL_GetDisplaySize(Uint *, Uint *);
 void            AG_SDL_BeginEventProcessing(void *);
 int             AG_SDL_PendingEvents(void *);
+void            AG_SDL_TranslateEvent(void *, const SDL_Event *, AG_DriverEvent *);
 int             AG_SDL_GetNextEvent(void *, AG_DriverEvent *);
 int             AG_SDL_ProcessEvent(void *, AG_DriverEvent *);
 void            AG_SDL_GenericEventLoop(void *);
