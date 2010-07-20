4ab8eb52c7babebe7c0df3231c86d69cd72c4ec6
Julien Nadeau Carriere <vedge@hypertriton.com>
2010-07-20 02:19:17 +0000

    tri:regen
    git-svn-id: https://dev.csoft.net/agar/trunk@8827 86a84d31-9702-0410-8f1e-a60cab7bd424

--- mk/install-manpages.sh
+++ mk/install-manpages.sh
@@ -14,12 +14,14 @@ for F in ${MAN}; do
 	fi
 done
 
-for F in ${CATMAN} ignore; do
-	if [ "$F" = "ignore" ]; then continue; fi
-	CAT=`echo $F | sed 's/.1$$/.cat1/'`
-	echo "${INSTALL_DATA} $CAT ${CATMANDIR}"
-	${INSTALL_DATA} $CAT ${CATMANDIR}
-	if [ $? != 0 ]; then
-		exit 1;
-	fi
-done
+if [ "${NOCATMAN}" != "yes" ]; then
+	for F in ${CATMAN} ignore; do
+		if [ "$F" = "ignore" ]; then continue; fi
+		CAT=`echo $F | sed 's/.1$$/.cat1/'`
+		echo "${INSTALL_DATA} $CAT ${CATMANDIR}"
+		${INSTALL_DATA} $CAT ${CATMANDIR}
+		if [ $? != 0 ]; then
+			exit 1;
+		fi
+	done
+fi
