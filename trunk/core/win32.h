7098fd5bf18d284448985d22adc7f35767c47329
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2010-07-29 02:23:59 +0000

    fix inclusion of <windows.h> under cygwin.

--- trunk/core/win32.h
+++ trunk/core/win32.h
@@ -4,12 +4,15 @@
  */
 #include <agar/config/have_cygwin.h>
 
-#ifdef _WIN32
+#if defined(HAVE_CYGWIN)
 # include <agar/core/queue_close.h>		/* Naming conflicts */
-# ifdef HAVE_CYGWIN
-#  define USE_SYS_TYPES_FD_SET			/* Warning under Cygwin */
-# endif
+# define USE_SYS_TYPES_FD_SET			/* Warning under Cygwin */
 # include <windows.h>
-# include <agar/core/queue_close.h>
+# include <agar/core/queue_close.h>		/* Naming conflicts */
+# include <agar/core/queue.h>
+#elif defined(_WIN32)
+# include <agar/core/queue_close.h>		/* Naming conflicts */
+# include <windows.h>
+# include <agar/core/queue_close.h>		/* Naming conflicts */
 # include <agar/core/queue.h>
 #endif
