cec9e0768b298af85307a0582c7fa4cd157c51b2
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2010-09-06 02:23:21 +0000

    implement M_PolygonAddLine[23]() and M_PolygonAddVertex[23].

--- trunk/math/m_polygon.h
+++ trunk/math/m_polygon.h
@@ -12,6 +12,14 @@ void       M_PolygonWrite3(AG_DataSource *, M_Polygon3 *);
 
 M_Polygon2 M_PolygonFromLines2(Uint, M_Line2 *);
 M_Polygon3 M_PolygonFromLines3(Uint, M_Line3 *);
+
+int        M_PolygonAddLine2(M_Polygon2 *, M_Line2);
+int        M_PolygonAddLine3(M_Polygon3 *, M_Line3);
+int        M_PolygonAddVertex2(M_Polygon2 *, M_Vector2);
+int        M_PolygonAddVertex3(M_Polygon3 *, M_Vector3);
+int        M_PolygonDelLine2(M_Polygon2 *, int);
+int        M_PolygonDelLine3(M_Polygon3 *, int);
+
 int        M_PointInPolygon2(M_Polygon2 *, M_Vector2);
 int        M_PolygonIsConvex2(M_Polygon2 *);
 int        M_PolygonIsPlanar3(M_Polygon3 *);
