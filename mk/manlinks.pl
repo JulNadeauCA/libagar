40bd5b3228118d9ade2d3a17dd3f00f034779bcd
Julien Nadeau Carriere <vedge@hypertriton.com>
2011-09-21 07:49:26 +0000

    tri:regen
    git-svn-id: https://dev.csoft.net/agar/trunk@9034 86a84d31-9702-0410-8f1e-a60cab7bd424

--- mk/manlinks.pl
+++ mk/manlinks.pl
@@ -17,6 +17,10 @@ $man =~ s/([\w\-]+)\.(\d)$/$1/;
 $section = $2;
 
 while (<STDIN>) {
+	if (/^\.\\"\s*MANLINK\s*\(([\w\-]+)\)/) {
+		print "MANLINKS+=$man.${section}:$1.${section}\n";
+		print "CATLINKS+=$man.cat${section}:$1.cat${section}\n";
+	}
 	if (/^\.nr nS 1/) { $ns = 1; }
 	elsif (/^\.nr nS 0/ || /^\.\\" NOMANLINK/) { $ns = 0; }
 	next unless $ns;
