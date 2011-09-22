1f9bc9398f761e9b5ede95ed1ed96e426816a498
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2011-09-22 03:26:33 +0000

    + AG_ReadRect() / AG_WriteRect()

---
+++ trunk/gui/geometry.c
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2011 Hypertriton, Inc. <http://hypertriton.com/>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <core/core.h>
+
+#include "gui.h"
+
+AG_Rect
+AG_ReadRect(AG_DataSource *ds)
+{
+	AG_Rect r;
+
+	r.x = (int)AG_ReadSint32(ds);
+	r.y = (int)AG_ReadSint32(ds);
+	r.w = (Uint)AG_ReadUint32(ds);
+	r.h = (Uint)AG_ReadUint32(ds);
+	return (r);
+}
+
+void
+AG_WriteRect(AG_DataSource *ds, AG_Rect r)
+{
+	AG_WriteSint32(ds, (Sint32)r.x);
+	AG_WriteSint32(ds, (Sint32)r.y);
+	AG_WriteUint32(ds, (Uint32)r.w);
+	AG_WriteUint32(ds, (Uint32)r.h);
+}
