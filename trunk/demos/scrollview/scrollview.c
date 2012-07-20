cd549c6b8589a746d066cc60b9fd6629011e4137
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2012-07-20 05:44:20 +0000

    test AG_SCROLLVIEW_BY_MOUSE

--- trunk/demos/scrollview/scrollview.c
+++ trunk/demos/scrollview/scrollview.c
@@ -36,13 +36,11 @@ main(int argc, char *argv[])
 	AG_BindGlobalKey(AG_KEY_ESCAPE, AG_KEYMOD_ANY, AG_QuitGUI);
 	AG_BindGlobalKey(AG_KEY_F8, AG_KEYMOD_ANY, AG_ViewCapture);
 
+	/* Test with focusable widgets. */
 	win = AG_WindowNew(0);
-	AG_WindowSetCaption(win, "Agar scrollview demo");
-
+	AG_WindowSetCaption(win, "Scrollview (reactive widgets)");
 	AG_ButtonNew(win, 0, "Foo");
 	sv = AG_ScrollviewNew(win, AG_SCROLLVIEW_EXPAND);
-
-	/* Create a bunch of buttons under the Scrollview. */
 	for (y = 0; y < 20; y++) {
 		hBox = AG_BoxNewHoriz(sv, 0);
 		AG_BoxSetSpacing(hBox, 1);
@@ -51,8 +49,23 @@ main(int argc, char *argv[])
 			AG_ButtonNew(hBox, 0, "Foo\n%c\n%d", (char)(0x41+x), y);
 	}
 	AG_ButtonNew(win, 0, "Bar");
+	AG_WindowSetGeometryAligned(win, AG_WINDOW_MC, 400, 300);
+	AG_WindowShow(win);
 
-	AG_WindowSetGeometryAlignedPct(win, AG_WINDOW_MC, 50, 50);
+	/* Test with passive widgets. */
+	win = AG_WindowNew(0);
+	AG_WindowSetCaption(win, "Scrollview (passive widgets)");
+	AG_LabelNewS(win, 0, "Use middle mouse button to pan");
+	AG_SeparatorNewHoriz(win);
+	sv = AG_ScrollviewNew(win, AG_SCROLLVIEW_EXPAND|AG_SCROLLVIEW_BY_MOUSE);
+	for (y = 0; y < 20; y++) {
+		hBox = AG_BoxNewHoriz(sv, 0);
+		for (x = 0; x < 10; x++) {
+			AG_LabelNewS(hBox, 0, "Foo");
+			AG_LabelNewS(hBox, 0, "Bar");
+		}
+	}
+	AG_WindowSetGeometryAligned(win, AG_WINDOW_MC, 320, 240);
 	AG_WindowShow(win);
 	
 	AG_EventLoop();
