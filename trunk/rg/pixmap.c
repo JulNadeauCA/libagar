861b335305492c1296724795266dd299fd943c1a
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2012-07-30 08:14:54 +0000

    tri:conversion warnings

--- trunk/rg/pixmap.c
+++ trunk/rg/pixmap.c
@@ -483,7 +483,8 @@ RG_PixmapEdit(RG_Tileview *tv, RG_TileElement *tel)
 		};
 
 		AG_LabelNewS(ntab, 0, _("Blending method:"));
-		AG_RadioNewUint(ntab, 0, blend_modes, &px->blend_mode);
+		AG_RadioNewUint(ntab, 0, blend_modes,
+		    (Uint *)&px->blend_mode);
 		AG_SeparatorNew(ntab, AG_SEPARATOR_HORIZ);
 		AG_CheckboxNewInt(ntab, 0, _("Source pixmap only"),
 		    &pixmap_source);
