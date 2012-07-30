6493ca5808c1df7b6d11155170326dd4f106e3d7
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2012-07-30 14:24:24 +0000

    fix mousewheel behavior under wgl

--- trunk/core/win32.h
+++ trunk/core/win32.h
@@ -1,18 +1,29 @@
 /*	Public domain	*/
 /*
- * Try to safely include <windows.h>
+ * Try to safely include <windows.h> on Windows platforms.
  */
-#include <agar/config/have_cygwin.h>
 
+#if defined(_WIN32) && !defined(_XBOX)
+
+/*
+ * Needed for WM_MOUSEWHEEL to be defined. Idiotic SDK provides us with no way
+ * of testing whether WM_MOUSEHWEEL is defined, so we are forced to set this.
+ */
+#define _WIN32_WINDOWS 0x0401
+
+/* <windows.h> tries to redefine SLIST_ENTRY(), etc. */
+#include <agar/core/queue_close.h>
+
+/* Fix some warnings under Cygwin. */
+#include <agar/config/have_cygwin.h>
 #if defined(HAVE_CYGWIN)
-# include <agar/core/queue_close.h>		/* Naming conflicts */
-# define USE_SYS_TYPES_FD_SET			/* Warning under Cygwin */
-# include <windows.h>
-# include <agar/core/queue_close.h>		/* Naming conflicts */
-# include <agar/core/queue.h>
-#elif defined(_WIN32) && !defined(_XBOX)
-# include <agar/core/queue_close.h>		/* Naming conflicts */
-# include <windows.h>
-# include <agar/core/queue_close.h>		/* Naming conflicts */
-# include <agar/core/queue.h>
+# define USE_SYS_TYPES_FD_SET
 #endif
+
+#include <windows.h>
+
+/* Redefine the AG_Queue(3) macros */
+#include <agar/core/queue_close.h>
+#include <agar/core/queue.h>
+
+#endif /* _WIN32 and !_XBOX */
