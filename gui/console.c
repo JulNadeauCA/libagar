13e41a0ffe7bce906c0ed74af1725493ec7a3a1f
Julien Nadeau Carriere <vedge@hypertriton.com>
2012-08-15 04:45:27 +0000

    implement selection, copy and export to file.
    git-svn-id: https://dev.csoft.net/agar/trunk@9341 86a84d31-9702-0410-8f1e-a60cab7bd424

--- gui/console.c
+++ gui/console.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005-2007 Hypertriton, Inc. <http://hypertriton.com/>
+ * Copyright (c) 2005-2012 Hypertriton, Inc. <http://hypertriton.com/>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -24,10 +24,14 @@
  */
 
 #include <core/core.h>
+#include <core/config.h>
 
 #include "console.h"
 #include "primitive.h"
 #include "window.h"
+#include "menu.h"
+#include "editable.h"
+#include "file_dlg.h"
 
 #include <stdarg.h>
 #include <string.h>
@@ -51,41 +55,43 @@ AG_ConsoleNew(void *parent, Uint flags)
 static __inline__ void
 ClampVisible(AG_Console *cons)
 {
-	if (cons->rOffs > (cons->nLines - cons->rVisible + 2))
-		cons->rOffs = cons->nLines - cons->rVisible + 2;
-	if (cons->rOffs < 0)
-		cons->rOffs = 0;
+	int v = (int)cons->nLines - (int)cons->rVisible;
+
+	cons->rOffs = MAX(0,v);
 }
 
 static void
 ScrollUp(AG_Event *event)
 {
 	AG_Console *cons = AG_SELF();
-	int n = AG_INT(1);
 
-	cons->rOffs -= n;
-	ClampVisible(cons);
-	AG_Redraw(cons);
+	if (((int)cons->rOffs - 1) >= 0) {
+		cons->rOffs--;
+		AG_Redraw(cons);
+	}
 }
 
 static void
 ScrollDown(AG_Event *event)
 {
 	AG_Console *cons = AG_SELF();
-	int n = AG_INT(1);
 
-	cons->rOffs += n;
-	ClampVisible(cons);
-	AG_Redraw(cons);
+	if ((cons->rOffs + 1) < (cons->nLines - cons->rVisible + 2)) {
+		cons->rOffs++;
+		AG_Redraw(cons);
+	}
 }
 
 static void
 PageUp(AG_Event *event)
 {
 	AG_Console *cons = AG_SELF();
-
-	cons->rOffs -= cons->rVisible;
-	ClampVisible(cons);
+	
+	if (((int)cons->rOffs - (int)cons->rVisible) >= 0) {
+		cons->rOffs -= cons->rVisible;
+	} else {
+		cons->rOffs = 0;
+	}
 	AG_Redraw(cons);
 }
 
@@ -94,9 +100,237 @@ PageDown(AG_Event *event)
 {
 	AG_Console *cons = AG_SELF();
 
-	cons->rOffs += cons->rVisible;
-	ClampVisible(cons);
+	if ((cons->rOffs + cons->rVisible) <
+	    (cons->nLines - cons->rVisible + 2)) {
+		cons->rOffs += cons->rVisible;
+	} else {
+		int v;
+		v = cons->nLines - cons->rVisible;
+		if (v < 0) { v = 0; }
+		cons->rOffs = v;
+	}
 	AG_Redraw(cons);
+}
+
+static int
+MapLine(AG_Console *cons, int yMouse, int *nLine)
+{
+	Uint sel;
+
+	if (cons->nLines == 0) {
+		return (-1);
+	}
+	if (yMouse < cons->padding) {
+		*nLine = cons->rOffs;
+	} else if (yMouse > WIDGET(cons)->h) {
+		*nLine = (int)cons->nLines - 1;
+	} else {
+		sel = (yMouse - cons->padding) / cons->lineskip;
+		if ((cons->rOffs + sel) >= cons->nLines) {
+			*nLine = (int)cons->nLines - 1;
+		} else {
+			*nLine = (int)(cons->rOffs + sel);
+		}
+	}
+	return (0);
+}
+
+/* Export selected lines to a single C string. */
+char *
+AG_ConsoleExportText(AG_Console *cons, int nativeNL)
+{
+	char *s, *ps;
+	size_t sizeReq = 1;
+	int i, dir;
+
+	if (cons->pos == -1) {
+		return (NULL);
+	}
+	dir = (cons->sel < 0) ? -1 : +1;
+	for (i = cons->pos; i >= 0 && i < cons->nLines; i += dir) {
+		AG_ConsoleLine *ln = &cons->lines[i];
+		sizeReq += ln->len + 2; /* \r\n */
+	}
+	if ((s = TryMalloc(sizeReq)) == NULL) {
+		return (NULL);
+	}
+	ps = &s[0];
+	*ps = '\0';
+	for (i = cons->pos; i >= 0 && i < cons->nLines; i += dir) {
+		AG_ConsoleLine *ln = &cons->lines[i];
+		memcpy(ps, ln->text, ln->len);
+#ifdef _WIN32
+		if (nativeNL) {
+			ps[ln->len] = '\r';
+			ps[ln->len+1] = '\n';
+			ps[ln->len+2] = '\0';
+			ps += ln->len + 2;
+		} else
+#endif
+		{
+			ps[ln->len] = '\n';
+			ps[ln->len+1] = '\0';
+			ps += ln->len + 1;
+		}
+	}
+	return (s);
+}
+static void
+MenuCopy(AG_Event *event)
+{
+	AG_Console *cons = AG_PTR(1);
+	AG_EditableClipboard *cb = &agEditableClipbrd;
+	char *s;
+
+	if ((s = AG_ConsoleExportText(cons, 0)) == NULL) {
+		return;
+	}
+	Strlcpy(cb->encoding, "UTF-8", sizeof(cb->encoding));
+	Free(cb->s);
+	if ((cb->s = AG_ImportUnicode("UTF-8", s, &cb->len, NULL)) == NULL) {
+		cb->len = 0;
+	}
+	free(s);
+}
+static void
+MenuExportToFileTXT(AG_Event *event)
+{
+	AG_Console *cons = AG_PTR(1);
+	char *path = AG_STRING(2);
+	char *s;
+	FILE *f;
+
+	if ((s = AG_ConsoleExportText(cons, 1)) == NULL) {
+		return;
+	}
+	if ((f = fopen(path, "wb")) == NULL) {
+		AG_TextMsg(AG_MSG_ERROR, _("Unable to open %s"), path);
+		goto out;
+	}
+	if (fwrite(s, strlen(s), 1, f) != 1) {
+		AG_TextMsg(AG_MSG_ERROR, _("Write error"));
+	} else {
+		AG_TextTmsg(AG_MSG_INFO, 1000, _("Successfully saved to %s"),
+		    path);
+	}
+	fclose(f);
+out:
+	free(s);
+}
+static void
+MenuExportToFileDlg(AG_Event *event)
+{
+	char path[AG_PATHNAME_MAX];
+	AG_Console *cons = AG_PTR(1);
+	AG_Window *win;
+	AG_FileDlg *fd;
+
+	if ((win = AG_WindowNew(0)) == NULL) {
+		return;
+	}
+	AG_WindowSetCaptionS(win, _("Export to text file..."));
+
+	fd = AG_FileDlgNew(win, AG_FILEDLG_SAVE|AG_FILEDLG_CLOSEWIN|
+	                        AG_FILEDLG_EXPAND);
+	AG_FileDlgSetOptionContainer(fd, AG_BoxNewVert(win, AG_BOX_HFILL));
+	AG_GetString(agConfig, "save-path", path, sizeof(path));
+	AG_FileDlgSetDirectoryMRU(fd, "agar.console.export-dir", path);
+	
+	AG_FileDlgAddType(fd, _("Text file"), "*.txt",
+	    MenuExportToFileTXT, "%p", cons);
+	AG_WindowShow(win);
+}
+
+static void
+MenuSelectAll(AG_Event *event)
+{
+	AG_Console *cons = AG_PTR(1);
+
+	cons->pos = 0;
+	cons->sel = cons->nLines-1;
+}
+
+static AG_PopupMenu *
+PopupMenu(AG_Console *cons)
+{
+	AG_PopupMenu *pm;
+	AG_MenuItem *m;
+
+	if ((pm = AG_PopupNew(cons)) == NULL) {
+		return (NULL);
+	}
+	m = AG_MenuAction(pm->item, _("Copy"), NULL,
+	    MenuCopy, "%p", cons);
+	m->state = (cons->pos != -1);
+	m = AG_MenuAction(pm->item, _("Export to file..."), NULL,
+	    MenuExportToFileDlg, "%p", cons);
+	m->state = (cons->pos != -1);
+	AG_MenuSeparator(pm->item);
+	AG_MenuAction(pm->item, _("Select All"), NULL,
+	    MenuSelectAll, "%p", cons);
+	return (pm);
+}
+
+static void
+Select(AG_Event *event)
+{
+	AG_Console *cons = AG_SELF();
+/*	int x = AG_INT(2); */
+	int y = AG_INT(3);
+
+	if (AG_WidgetDisabled(cons)) {
+		return;
+	}
+	AG_WidgetFocus(cons);
+
+	if (cons->pm != NULL) {
+		AG_PopupDestroy(cons, cons->pm);
+		cons->pm = NULL;
+	}
+	MapLine(cons, y, &cons->pos);
+	cons->sel = 0;
+	AG_Redraw(cons);
+}
+
+static void
+ShowPopup(AG_Event *event)
+{
+	AG_Console *cons = AG_SELF();
+	int x = AG_INT(2);
+	int y = AG_INT(3);
+		
+	if ((cons->flags & AG_CONSOLE_NOPOPUP) == 0) {
+		if (cons->pm != NULL) {
+			AG_PopupDestroy(cons, cons->pm);
+		}
+		if ((cons->pm = PopupMenu(cons)) != NULL)
+			AG_PopupShowAt(cons->pm, x, y);
+	}
+}
+
+static void
+MouseMotion(AG_Event *event)
+{
+	AG_Console *cons = AG_SELF();
+	int x = AG_INT(1);
+	int y = AG_INT(2);
+	int buttons = AG_INT(5);
+	int newPos, newSel;
+
+	if ((buttons & AG_MOUSE_LEFT) == 0) {
+		return;
+	}
+	if (x < cons->r.x || x > cons->r.x+cons->r.w) {
+		return;
+	}
+	if (MapLine(cons, y, &newPos) == -1) {
+		return;
+	}
+	newSel = newPos - cons->pos;
+	if (newSel != cons->sel) {
+		cons->sel = newSel;
+		AG_Redraw(cons);
+	}
 }
 
 static void
@@ -109,33 +343,52 @@ Init(void *obj)
 	cons->flags = 0;
 	cons->padding = 4;
 	cons->lines = NULL;
-	cons->lineskip = agTextFontLineSkip - agTextFontHeight;
+	cons->lineskip = agTextFontLineSkip;
 	cons->nLines = 0;
 	cons->rOffs = 0;
 	cons->rVisible = 0;
 	cons->cBg = AG_ColorRGB(0,0,0);
 	cons->vBar = AG_ScrollbarNew(cons, AG_SCROLLBAR_VERT, AG_SCROLLBAR_AUTOSIZE);
 	cons->vBar->maxOffs = +2;
+	cons->pm = NULL;
+
+	cons->pos = -1;
+	cons->sel = 0;
 
 	cons->r = AG_RECT(0,0,0,0);
 	cons->font = agDefaultFont;
 	cons->scrollTo = NULL;
 
-	AG_BindInt(cons->vBar, "value", &cons->rOffs);
-	AG_BindInt(cons->vBar, "max", &cons->nLines);
-	AG_BindInt(cons->vBar, "visible", &cons->rVisible);
+	AG_BindUint(cons->vBar, "value", &cons->rOffs);
+	AG_BindUint(cons->vBar, "max", &cons->nLines);
+	AG_BindUint(cons->vBar, "visible", &cons->rVisible);
 
-	AG_ActionFn(cons, "ScrollUp",	ScrollUp, "%i", 1);
-	AG_ActionFn(cons, "ScrollDown",	ScrollDown, "%i", 1);
+	AG_ActionFn(cons, "Select",	Select, NULL);
+	AG_ActionFn(cons, "ShowPopup",	ShowPopup, NULL);
+	AG_ActionFn(cons, "ScrollUp",	ScrollUp, NULL);
+	AG_ActionFn(cons, "ScrollDown",	ScrollDown, NULL);
 	AG_ActionFn(cons, "PageUp",	PageUp, NULL);
 	AG_ActionFn(cons, "PageDown",   PageDown, NULL);
 
+	AG_ActionOnButtonDown(cons, AG_MOUSE_LEFT, "Select");
+	AG_ActionOnButtonDown(cons, AG_MOUSE_RIGHT, "ShowPopup");
 	AG_ActionOnButtonDown(cons, AG_MOUSE_WHEELUP, "ScrollUp");
 	AG_ActionOnButtonDown(cons, AG_MOUSE_WHEELDOWN, "ScrollDown");
+
 	AG_ActionOnKey(cons, AG_KEY_UP, AG_KEYMOD_ANY, "ScrollUp");
 	AG_ActionOnKey(cons, AG_KEY_DOWN, AG_KEYMOD_ANY, "ScrollDown");
 	AG_ActionOnKey(cons, AG_KEY_PAGEUP, AG_KEYMOD_ANY, "PageUp");
 	AG_ActionOnKey(cons, AG_KEY_PAGEDOWN, AG_KEYMOD_ANY, "PageDown");
+	
+	AG_SetEvent(cons, "mouse-motion", MouseMotion, NULL);
+
+#ifdef AG_DEBUG
+	AG_BindUint(cons, "nLines", &cons->nLines);
+	AG_BindUint(cons, "rOffs", &cons->rOffs);
+	AG_BindUint(cons, "rVisible", &cons->rVisible);
+	AG_BindInt(cons, "pos", &cons->pos);
+	AG_BindInt(cons, "sel", &cons->sel);
+#endif
 }
 
 static void
@@ -174,9 +427,12 @@ Draw(void *p)
 	AG_Console *cons = p;
 	AG_Surface *su;
 	AG_Rect rDst;
-	Uint r;
+	Uint lnIdx;
 
 	STYLE(cons)->ConsoleBackground(cons, cons->cBg);
+	
+	if (cons->nLines == 0)
+		goto out;
 
 	AG_PushTextState();
 	AG_TextFont(cons->font);
@@ -186,36 +442,45 @@ Draw(void *p)
 		cons->scrollTo = NULL;
 		ClampVisible(cons);
 	}
-	if (cons->nLines > 0) {
-		AG_PushClipRect(cons, cons->r);
-		rDst = cons->r;
-		for (r = cons->rOffs, rDst.y = cons->padding;
-		     r < cons->nLines && rDst.y < WIDGET(cons)->h;
-		     r++) {
-			AG_ConsoleLine *ln = &cons->lines[r];
-	
-			if (ln->surface == -1) {
-				AG_TextColor(ln->cFg);
-				AG_TextBGColor(ln->cBg);
-				su = AG_TextRender(ln->text);
-				ln->surface = AG_WidgetMapSurface(cons, su);
-			}
-			AG_WidgetBlitSurface(cons, ln->surface, cons->padding,
-			    rDst.y);
-			rDst.y += WSURFACE(cons,ln->surface)->h + cons->lineskip;
-		}
-		AG_PopClipRect(cons);
-	}
-	
-	AG_PopTextState();
+	AG_PushClipRect(cons, cons->r);
+	rDst = cons->r;
+	rDst.x = cons->padding;
+	rDst.y = cons->padding;
+	for (lnIdx = cons->rOffs;
+	     lnIdx < cons->nLines && rDst.y < WIDGET(cons)->h;
+	     lnIdx++) {
+		AG_ConsoleLine *ln = &cons->lines[lnIdx];
 
+		if (ln->surface == -1) {
+			AG_TextColor(ln->cFg);
+			AG_TextBGColor(ln->cBg);
+			su = AG_TextRender(ln->text);
+			ln->surface = AG_WidgetMapSurface(cons, su);
+		}
+		if (cons->pos != -1) {
+			if ((lnIdx == cons->pos) ||
+			    ((cons->sel > 0 && lnIdx > cons->pos && lnIdx < cons->pos + cons->sel + 1) ||
+			     (cons->sel < 0 && lnIdx < cons->pos && lnIdx > cons->pos + cons->sel - 1))) {
+				AG_DrawRectFilled(cons,
+				    AG_RECT(rDst.x, rDst.y,
+				            WIDGET(cons)->w - cons->padding*2,
+				            cons->lineskip+1),
+				    agColors[TEXT_SEL_COLOR]);
+			}
+		}
+		AG_WidgetBlitSurface(cons, ln->surface, rDst.x, rDst.y);
+		rDst.y += cons->lineskip;
+	}
+	AG_PopClipRect(cons);
+	AG_PopTextState();
+out:
 	AG_WidgetDraw(cons->vBar);
 }
 
 static void
 FreeLines(AG_Console *cons)
 {
-	int i;
+	Uint i;
 	
 	for (i = 0; i < cons->nLines; i++) {
 		Free(cons->lines[i].text);
@@ -246,11 +511,13 @@ AG_ConsoleSetPadding(AG_Console *cons, int padding)
 void
 AG_ConsoleSetFont(AG_Console *cons, AG_Font *font)
 {
-	int i;
+	Uint i;
 
 	AG_ObjectLock(cons);
 	cons->font = (font != NULL) ? font : agDefaultFont;
 	cons->rVisible = HEIGHT(cons) / cons->font->height;
+	cons->lineskip = cons->font->lineskip;
+	cons->rOffs = 0;
 
 	for (i = 0; i < cons->nLines; i++) {
 		AG_ConsoleLine *ln = &cons->lines[i];
@@ -281,7 +548,6 @@ AG_ConsoleAppendLine(AG_Console *cons, const char *s)
 		ln->len = 0;
 	}
 	ln->cons = cons;
-	ln->selected = 0;
 	ln->p = NULL;
 	ln->surface = -1;
 	ln->cBg = AG_ColorRGBA(0,0,0,0);
@@ -419,6 +685,8 @@ AG_ConsoleClear(AG_Console *cons)
 {
 	FreeLines(cons);
 	cons->rOffs = 0;
+	cons->pos = -1;
+	cons->sel = 0;
 	AG_Redraw(cons);
 }
 
