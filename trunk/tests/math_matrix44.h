ac94f6307ef5912d8a4980a521c417a16aaf8001
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2012-08-18 06:50:47 +0000

    microbenchmarks for M_Matrix44 and M_Vector3

---
+++ trunk/tests/math_matrix44.h
@@ -0,0 +1,307 @@
+/*	Public domain	*/
+/*
+ * Microbenchmarks for comparing the performance of M_Vector3 operations
+ * on random vector sets, under different backends.
+ */
+
+static __inline__ void
+UseMatrix(const M_Matrix44 *M)
+{
+	if (M->m[0][0] > M->m[3][3]) { realJunk = M->m[1][1]+M->m[2][2]; }
+}
+
+static void
+MatrixZero44(void *ti)
+{
+	M_Matrix44 M[10];
+	int i;
+	for (i = 0; i < 10; i++) { M[i] = M_MatZero44(); }
+	for (i = 0; i < 10; i++) { UseMatrix(&M[i]); }
+}
+static void
+MatrixZero44v(void *ti)
+{
+	M_Matrix44 M[10];
+	int i;
+	for (i = 0; i < 10; i++) { M_MatZero44v(&M[i]); }
+	for (i = 0; i < 10; i++) { UseMatrix(&M[i]); }
+}
+
+static void
+MatrixIdentity44(void *ti)
+{
+	M_Matrix44 M[10];
+	int i;
+	for (i = 0; i < 10; i++) { M[i] = M_MatIdentity44(); }
+	for (i = 0; i < 10; i++) { UseMatrix(&M[i]); }
+}
+static void
+MatrixIdentity44v(void *ti)
+{
+	M_Matrix44 M[10];
+	int i;
+	for (i = 0; i < 10; i++) { M_MatIdentity44v(&M[i]); }
+	for (i = 0; i < 10; i++) { UseMatrix(&M[i]); }
+}
+
+static void
+MatrixInvert44(void *ti)
+{
+	M_Matrix44 M[2], Minv[2];
+	int i;
+	for (i = 0; i < 2; i++) { M[i] = RandomMatrix44(ti); }
+	for (i = 0; i < 2; i++) { Minv[i] = M_MatInvert44(M[i]); }
+	for (i = 0; i < 2; i++) { UseMatrix(&Minv[i]); }
+}
+static void
+MatrixInvert44p(void *ti)
+{
+	M_Matrix44 M[2], Minv[2];
+	int i;
+	for (i = 0; i < 2; i++) { M[i] = RandomMatrix44(ti); }
+	for (i = 0; i < 2; i++) { Minv[i] = M_MatInvert44p(&M[i]); }
+	for (i = 0; i < 2; i++) { UseMatrix(&Minv[i]); }
+}
+
+static void
+MatrixTranspose44(void *ti)
+{
+	M_Matrix44 M[6], Mt[6];
+	int i;
+	for (i = 0; i < 6; i++) { M[i] = RandomMatrix44(ti); }
+	for (i = 0; i < 6; i++) { Mt[i] = M_MatTranspose44(M[i]); }
+	for (i = 0; i < 6; i++) { UseMatrix(&Mt[i]); }
+}
+static void
+MatrixTranspose44p(void *ti)
+{
+	M_Matrix44 M[6], Mt[6];
+	int i;
+	for (i = 0; i < 6; i++) { M[i] = RandomMatrix44(ti); }
+	for (i = 0; i < 6; i++) { Mt[i] = M_MatTranspose44p(&M[i]); }
+	for (i = 0; i < 6; i++) { UseMatrix(&Mt[i]); }
+}
+static void
+MatrixTranspose44v(void *ti)
+{
+	M_Matrix44 M[6];
+	int i;
+	for (i = 0; i < 6; i++) { M[i] = RandomMatrix44(ti); }
+	for (i = 0; i < 6; i++) { M_MatTranspose44v(&M[i]); }
+	for (i = 0; i < 6; i++) { UseMatrix(&M[i]); }
+}
+
+static void
+MatrixMult44(void *ti)
+{
+	M_Matrix44 A[3], B[3], AB[3];
+	int i;
+	for (i = 0; i < 3; i++) { A[i] = RandomMatrix44(ti); B[i] = RandomMatrix44(ti); }
+	for (i = 0; i < 3; i++) { AB[i] = M_MatMult44(A[i], B[i]); }
+	for (i = 0; i < 3; i++) { UseMatrix(&AB[i]); }
+}
+static void
+MatrixMult44v(void *ti)
+{
+	M_Matrix44 A[3], B[3];
+	int i;
+	for (i = 0; i < 3; i++) { A[i] = RandomMatrix44(ti); B[i] = RandomMatrix44(ti); }
+	for (i = 0; i < 3; i++) { M_MatMult44v(&A[i], &B[i]); }
+	for (i = 0; i < 3; i++) { UseMatrix(&A[i]); }
+}
+static void
+MatrixMult44pv(void *ti)
+{
+	M_Matrix44 A[3], B[3], AB[3];
+	int i;
+	for (i = 0; i < 3; i++) { A[i] = RandomMatrix44(ti); B[i] = RandomMatrix44(ti); }
+	for (i = 0; i < 3; i++) { M_MatMult44pv(&AB[i], &A[i], &B[i]); }
+	for (i = 0; i < 3; i++) { UseMatrix(&AB[i]); }
+}
+
+static void
+MatrixMult44Vector3(void *ti)
+{
+	M_Matrix44 A[6];
+	M_Vector3 b[6], x[6];
+	int i;
+	for (i = 0; i < 6; i++) { A[i] = RandomMatrix44(ti); b[i] = RandomVector3(ti); }
+	for (i = 0; i < 6; i++) { x[i] = M_MatMult44Vector3(A[i], b[i]); }
+	for (i = 0; i < 6; i++) { realJunk = x[i].x; }
+}
+static void
+MatrixMult44Vector3p(void *ti)
+{
+	M_Matrix44 A[6];
+	M_Vector3 b[6], x[6];
+	int i;
+	for (i = 0; i < 6; i++) { A[i] = RandomMatrix44(ti); b[i] = RandomVector3(ti); }
+	for (i = 0; i < 6; i++) { x[i] = M_MatMult44Vector3p(&A[i], &b[i]); }
+	for (i = 0; i < 6; i++) { realJunk = x[i].x; }
+}
+static void
+MatrixMult44Vector3v(void *ti)
+{
+	M_Matrix44 A[6];
+	M_Vector3 b[6];
+	int i;
+	for (i = 0; i < 6; i++) { A[i] = RandomMatrix44(ti); b[i] = RandomVector3(ti); }
+	for (i = 0; i < 6; i++) { M_MatMult44Vector3v(&b[i], &A[i]); }
+	for (i = 0; i < 6; i++) { realJunk = b[i].x; }
+}
+
+static void
+MatrixMult44Vector4(void *ti)
+{
+	M_Matrix44 A[6];
+	M_Vector4 b[6], x[6];
+	int i;
+	for (i = 0; i < 6; i++) { A[i] = RandomMatrix44(ti); b[i] = RandomVector4(ti); }
+	for (i = 0; i < 6; i++) { x[i] = M_MatMult44Vector4(A[i], b[i]); }
+	for (i = 0; i < 6; i++) { realJunk = x[i].x; }
+}
+static void
+MatrixMult44Vector4p(void *ti)
+{
+	M_Matrix44 A[6];
+	M_Vector4 b[6], x[6];
+	int i;
+	for (i = 0; i < 6; i++) { A[i] = RandomMatrix44(ti); b[i] = RandomVector4(ti); }
+	for (i = 0; i < 6; i++) { x[i] = M_MatMult44Vector4p(&A[i], &b[i]); }
+	for (i = 0; i < 6; i++) { realJunk = x[i].x; }
+}
+static void
+MatrixMult44Vector4v(void *ti)
+{
+	M_Matrix44 A[6];
+	M_Vector4 b[6];
+	int i;
+	for (i = 0; i < 6; i++) { A[i] = RandomMatrix44(ti); b[i] = RandomVector4(ti); }
+	for (i = 0; i < 6; i++) { M_MatMult44Vector4v(&b[i], &A[i]); }
+	for (i = 0; i < 6; i++) { realJunk = b[i].x; }
+}
+
+static void
+MatrixCopy44(void *ti)
+{
+	M_Matrix44 A[6], B[6];
+	int i;
+	for (i = 0; i < 6; i++) { A[i] = RandomMatrix44(ti); }
+	for (i = 0; i < 6; i++) { M_MatCopy44(&B[i], &A[i]); }
+	for (i = 0; i < 6; i++) { realJunk = B[i].m[0][0]; }
+}
+
+static void
+MatrixRotateAxis44(void *ti)
+{
+	M_Matrix44 A[6];
+	M_Vector3 b[6];
+	M_Real theta[6];
+	int i;
+	for (i = 0; i < 6; i++) {
+		A[i] = RandomMatrix44(ti);
+		b[i] = RandomVector3(ti);
+		theta[i] = RandomReal(ti);
+	}
+	for (i = 0; i < 6; i++) { M_MatRotateAxis44(&A[i], theta[i], b[i]); }
+	for (i = 0; i < 6; i++) { realJunk = A[i].m[0][0]; }
+}
+static void
+MatrixRotate44I(void *ti)
+{
+	M_Matrix44 A[6];
+	M_Real theta[6];
+	int i;
+	for (i = 0; i < 6; i++) {
+		A[i] = RandomMatrix44(ti);
+		theta[i] = RandomReal(ti);
+	}
+	for (i = 0; i < 6; i++) { M_MatRotate44I(&A[i], theta[i]); }
+	for (i = 0; i < 6; i++) { realJunk = A[i].m[0][0]; }
+}
+static void
+MatrixTranslate44(void *ti)
+{
+	M_Matrix44 A[6];
+	M_Vector3 b[6];
+	int i;
+	for (i = 0; i < 6; i++) {
+		A[i] = RandomMatrix44(ti);
+		b[i] = RandomVector3(ti);
+	}
+	for (i = 0; i < 6; i++) { M_MatTranslate44v(&A[i], b[i]); }
+	for (i = 0; i < 6; i++) { realJunk = A[i].m[0][0]; }
+}
+static void
+MatrixTranslateX44(void *ti)
+{
+	M_Matrix44 A[6];
+	M_Real t[6];
+	int i;
+	for (i = 0; i < 6; i++) {
+		A[i] = RandomMatrix44(ti);
+		t[i] = RandomReal(ti);
+	}
+	for (i = 0; i < 6; i++) { M_MatTranslate44X(&A[i], t[i]); }
+	for (i = 0; i < 6; i++) { realJunk = A[i].m[0][0]; }
+}
+static void
+MatrixScale44(void *ti)
+{
+	M_Matrix44 A[6];
+	M_Vector4 b[6];
+	int i;
+	for (i = 0; i < 6; i++) {
+		A[i] = RandomMatrix44(ti);
+		b[i] = RandomVector4(ti);
+	}
+	for (i = 0; i < 6; i++) { M_MatScale44(&A[i], b[i].x, b[i].y, b[i].z, b[i].w); }
+	for (i = 0; i < 6; i++) { realJunk = A[i].m[0][0]; }
+}
+static void
+MatrixUniScale44(void *ti)
+{
+	M_Matrix44 A[6];
+	M_Real s[6];
+	int i;
+	for (i = 0; i < 6; i++) {
+		A[i] = RandomMatrix44(ti);
+		s[i] = RandomReal(ti);
+	}
+	for (i = 0; i < 6; i++) { M_MatUniScale44(&A[i], s[i]); }
+	for (i = 0; i < 6; i++) { realJunk = A[i].m[0][0]; }
+}
+
+static struct ag_benchmark_fn mathBenchMatrix44Fns[] = {
+	{ "Zero44()", 		MatrixZero44		},
+	{ "Zero44v()", 		MatrixZero44v		},
+	{ "Identity44()", 	MatrixIdentity44	},
+	{ "Identity44v()", 	MatrixIdentity44v	},
+	{ "Transpose44()", 	MatrixTranspose44	},
+	{ "Transpose44p()", 	MatrixTranspose44p	},
+	{ "Transpose44v()", 	MatrixTranspose44v	},
+	{ "Invert44()", 	MatrixInvert44		},
+	{ "Invert44p()", 	MatrixInvert44p		},
+	{ "Mult44()", 		MatrixMult44		},
+	{ "Mult44v()", 		MatrixMult44v		},
+	{ "Mult44pv()", 	MatrixMult44pv		},
+	{ "Mult44Vector3()", 	MatrixMult44Vector3	},
+	{ "Mult44Vector3p()", 	MatrixMult44Vector3p	},
+	{ "Mult44Vector3v()", 	MatrixMult44Vector3v	},
+	{ "Mult44Vector4()", 	MatrixMult44Vector4	},
+	{ "Mult44Vector4p()", 	MatrixMult44Vector4p	},
+	{ "Mult44Vector4v()", 	MatrixMult44Vector4v	},
+	{ "Copy()", 		MatrixCopy44		},
+	{ "RotateAxis44()",	MatrixRotateAxis44	},
+	{ "Rotate44I()",	MatrixRotate44I		},
+	{ "Translate44()",	MatrixTranslate44	},
+	{ "TranslateX44()",	MatrixTranslateX44	},
+	{ "Scale44()",		MatrixScale44		},
+	{ "UniScale44()",	MatrixUniScale44	},
+};
+struct ag_benchmark mathBenchMatrix44 = {
+	"M_Matrix44",
+	&mathBenchMatrix44Fns[0],
+	sizeof(mathBenchMatrix44Fns) / sizeof(mathBenchMatrix44Fns[0]),
+	10, 50000, 10000
+};
---
+++ trunk/tests/math_vector3.h
@@ -0,0 +1,371 @@
+/*	Public domain	*/
+/*
+ * Microbenchmarks for comparing the performance of M_Vector3 operations
+ * on random vector sets, under different backends.
+ */
+
+static void
+VectorZero3(void *ti)
+{
+	M_Vector3 v[4];
+	int i;
+
+	for (i = 0; i < 4; i++) {
+		v[i] = M_VecZero3();
+		if (i > 1) {
+			if (v[i].x > v[i-1].x) { v[i].x = v[i-1].x; }
+			if (v[i].y > v[i-1].y) { v[i].y = v[i-1].y; }
+			if (v[i].y > v[i-1].z) { v[i].z = v[i-1].z; }
+		}
+	}
+}
+static void
+VectorGet3(void *ti)
+{
+	M_Real f[3];
+	M_Vector3 v;
+
+	f[0] = RandomReal(ti);
+	f[1] = RandomReal(ti);
+	f[2] = RandomReal(ti);
+	v = M_VecGet3(f[0], f[1], f[2]);
+	if (v.x > RandomReal(ti)) { realJunk = v.x; }
+}
+static void
+VectorSet3(void *ti)
+{
+	M_Real f[3];
+	M_Vector3 v;
+	
+	f[0] = RandomReal(ti);
+	f[1] = RandomReal(ti);
+	f[2] = RandomReal(ti);
+	M_VecSet3(&v, f[0], f[1], f[2]);
+	if (v.x > RandomReal(ti)) { realJunk = v.x; }
+}
+static void
+VectorCopy3(void *ti)
+{
+	M_Vector3 rOrig = RandomVector3(ti);
+	M_Vector3 vDup;
+
+	M_VecCopy3(&vDup, &rOrig);
+	if (vDup.x > RandomReal(ti)) { realJunk = vDup.x; }
+}
+static void
+VectorFlip3(void *ti)
+{
+	M_Vector3 vMir;
+
+	vMir = M_VecFlip3(RandomVector3(ti));
+	if (vMir.x > RandomReal(ti)) { realJunk = vMir.x; }
+}
+static void
+VectorLen3(void *ti)
+{
+	M_Real len1, len2;
+
+	len1 = M_VecLen3(RandomVector3(ti));
+	len2 = M_VecLen3(RandomVector3(ti));
+	if (len1 > len2) { realJunk = len1+len2; }
+}
+static void
+VectorLen3p(void *ti)
+{
+	M_Vector3 v = RandomVector3(ti);
+	M_Real len1, len2;
+
+	len1 = M_VecLen3p(&v);
+	len2 = M_VecLen3p(&v);
+	if (len1 > len2) { realJunk = len1+len2; }
+}
+static void
+VectorDot3(void *ti)
+{
+	M_Vector3 v[8];
+	M_Real dots[6];
+	int i;
+
+	for (i = 0; i < 8; i++) { v[i] = RandomVector3(ti); }
+	dots[0] = M_VecDot3(v[0], v[1]);
+	dots[1] = M_VecDot3(v[2], v[1]);
+	dots[2] = M_VecDot3(v[2], v[3]);
+	dots[5] = M_VecDot3(v[7], v[6]);
+	dots[4] = M_VecDot3(v[5], v[6]);
+	dots[3] = M_VecDot3(v[4], v[3]);
+	for (i = 0; i < 6; i++) { realJunk += dots[i]; }
+}
+static void
+VectorDot3p(void *ti)
+{
+	M_Vector3 v[8];
+	M_Real dots[6];
+	int i;
+
+	for (i = 0; i < 8; i++) { v[i] = RandomVector3(ti); }
+	dots[0] = M_VecDot3p(&v[0], &v[1]);
+	dots[1] = M_VecDot3p(&v[2], &v[1]);
+	dots[2] = M_VecDot3p(&v[2], &v[3]);
+	dots[5] = M_VecDot3p(&v[7], &v[6]);
+	dots[4] = M_VecDot3p(&v[5], &v[6]);
+	dots[3] = M_VecDot3p(&v[4], &v[3]);
+	for (i = 0; i < 6; i++) { realJunk += dots[i]; }
+}
+static void
+VectorDistance3(void *ti)
+{
+	M_Vector3 v[8];
+	M_Real dots[6];
+	int i;
+
+	for (i = 0; i < 8; i++) { v[i] = RandomVector3(ti); }
+	dots[0] = M_VecDistance3(v[0], v[1]);
+	dots[1] = M_VecDistance3(v[2], v[1]);
+	dots[2] = M_VecDistance3(v[2], v[3]);
+	dots[5] = M_VecDistance3(v[7], v[6]);
+	dots[4] = M_VecDistance3(v[5], v[6]);
+	dots[3] = M_VecDistance3(v[4], v[3]);
+	for (i = 0; i < 6; i++) { realJunk += dots[i]; }
+}
+static void
+VectorDistance3p(void *ti)
+{
+	M_Vector3 v[8];
+	M_Real dots[6];
+	int i;
+
+	for (i = 0; i < 8; i++) { v[i] = RandomVector3(ti); }
+	dots[0] = M_VecDistance3p(&v[0], &v[1]);
+	dots[1] = M_VecDistance3p(&v[2], &v[1]);
+	dots[2] = M_VecDistance3p(&v[2], &v[3]);
+	dots[5] = M_VecDistance3p(&v[7], &v[6]);
+	dots[4] = M_VecDistance3p(&v[5], &v[6]);
+	dots[3] = M_VecDistance3p(&v[4], &v[3]);
+	for (i = 0; i < 6; i++) { realJunk += dots[i]; }
+}
+static void
+VectorNorm3(void *ti)
+{
+	M_Vector3 v[6], vNorm[6];
+	int i;
+
+	for (i = 0; i < 6; i++) { v[i] = RandomVector3(ti); }
+	for (i = 0; i < 6; i++) { vNorm[i] = M_VecNorm3(v[i]); }
+	for (i = 0; i < 6; i++) { realJunk += vNorm[i].x; }
+}
+static void
+VectorNorm3p(void *ti)
+{
+	M_Vector3 v[6], vNorm[6];
+	int i;
+
+	for (i = 0; i < 6; i++) { v[i] = RandomVector3(ti); }
+	for (i = 0; i < 6; i++) { vNorm[i] = M_VecNorm3p(&v[i]); }
+	for (i = 0; i < 6; i++) { realJunk += vNorm[i].x; }
+}
+static void
+VectorNorm3v(void *ti)
+{
+	M_Vector3 v[6];
+	int i;
+
+	for (i = 0; i < 6; i++) { v[i] = RandomVector3(ti); }
+	for (i = 0; i < 6; i++) { M_VecNorm3v(&v[i]); }
+}
+static void
+VectorCross3(void *ti)
+{
+	M_Vector3 v[8], x[6];
+	int i;
+
+	for (i = 0; i < 8; i++) { v[i] = RandomVector3(ti); }
+	x[0] = M_VecCross3(v[0], v[1]);
+	x[1] = M_VecCross3(v[2], v[1]);
+	x[2] = M_VecCross3(v[2], v[3]);
+	x[5] = M_VecCross3(v[7], v[6]);
+	x[4] = M_VecCross3(v[5], v[6]);
+	x[3] = M_VecCross3(v[4], v[3]);
+	for (i = 0; i < 6; i++) { realJunk += x[i].x; }
+}
+static void
+VectorCross3p(void *ti)
+{
+	M_Vector3 v[8], x[6];
+	int i;
+
+	for (i = 0; i < 8; i++) { v[i] = RandomVector3(ti); }
+	x[0] = M_VecCross3p(&v[0], &v[1]);
+	x[1] = M_VecCross3p(&v[2], &v[1]);
+	x[2] = M_VecCross3p(&v[2], &v[3]);
+	x[5] = M_VecCross3p(&v[7], &v[6]);
+	x[4] = M_VecCross3p(&v[5], &v[6]);
+	x[3] = M_VecCross3p(&v[4], &v[3]);
+	for (i = 0; i < 6; i++) { realJunk += x[i].x; }
+}
+
+static void
+VectorNormCross3(void *ti)
+{
+	M_Vector3 v[4], x[3];
+	int i;
+
+	for (i = 0; i < 4; i++) { v[i] = RandomVector3(ti); }
+	x[0] = M_VecNormCross3(v[0], v[1]);
+	x[1] = M_VecNormCross3(v[1], v[2]);
+	x[2] = M_VecNormCross3(v[2], v[3]);
+	for (i = 0; i < 3; i++) { realJunk += x[i].x; }
+}
+static void
+VectorNormCross3p(void *ti)
+{
+	M_Vector3 v[4], x[3];
+	int i;
+
+	for (i = 0; i < 8; i++) { v[i] = RandomVector3(ti); }
+	x[0] = M_VecNormCross3p(&v[0], &v[1]);
+	x[1] = M_VecNormCross3p(&v[1], &v[2]);
+	x[2] = M_VecNormCross3p(&v[2], &v[3]);
+	for (i = 0; i < 3; i++) { realJunk += x[i].x; }
+}
+
+static void
+VectorScale3(void *ti)
+{
+	M_Vector3 a[6], b[6];
+	int i;
+
+	for (i = 0; i < 6; i++) { a[i] = RandomVector3(ti); }
+	for (i = 0; i < 6; i++) { b[i] = M_VecScale3(a[i], RandomReal(ti)); }
+	for (i = 0; i < 6; i++) { realJunk += b[i].x; }
+}
+static void
+VectorScale3p(void *ti)
+{
+	M_Vector3 a[6], b[6];
+	int i;
+
+	for (i = 0; i < 6; i++) { a[i] = RandomVector3(ti); }
+	for (i = 0; i < 6; i++) { b[i] = M_VecScale3p(&a[i], RandomReal(ti)); }
+	for (i = 0; i < 6; i++) { realJunk += b[i].x; }
+}
+static void
+VectorScale3v(void *ti)
+{
+	M_Vector3 v[6];
+	int i;
+
+	for (i = 0; i < 6; i++) { v[i] = RandomVector3(ti); }
+	for (i = 0; i < 6; i++) { M_VecScale3v(&v[i], RandomReal(ti)); }
+	for (i = 0; i < 6; i++) { realJunk += v[i].x; }
+}
+static void
+VectorAdd3(void *ti)
+{
+	M_Vector3 a[6], b[6], c[6];
+	int i;
+
+	for (i = 0; i < 6; i++) { a[i] = RandomVector3(ti); b[i] = RandomVector3(ti); }
+	for (i = 0; i < 6; i++) { c[i] = M_VecAdd3(a[i], b[i]); }
+	for (i = 0; i < 6; i++) { realJunk += c[i].x; }
+}
+static void
+VectorAdd3p(void *ti)
+{
+	M_Vector3 a[6], b[6], c[6];
+	int i;
+
+	for (i = 0; i < 6; i++) { a[i] = RandomVector3(ti); b[i] = RandomVector3(ti); }
+	for (i = 0; i < 6; i++) { c[i] = M_VecAdd3p(&a[i], &b[i]); }
+	for (i = 0; i < 6; i++) { realJunk += c[i].x; }
+}
+static void
+VectorAdd3v(void *ti)
+{
+	M_Vector3 a[6], b[6];
+	int i;
+
+	for (i = 0; i < 6; i++) { a[i] = RandomVector3(ti); b[i] = RandomVector3(ti); }
+	for (i = 0; i < 6; i++) { M_VecAdd3v(&a[i], &b[i]); }
+	for (i = 0; i < 6; i++) { realJunk += a[i].x; }
+}
+
+static void
+VectorAvg3(void *ti)
+{
+	M_Vector3 a[6], b[6], c[6];
+	int i;
+
+	for (i = 0; i < 6; i++) { a[i] = RandomVector3(ti); b[i] = RandomVector3(ti); }
+	for (i = 0; i < 6; i++) { c[i] = M_VecAvg3(a[i], b[i]); }
+	for (i = 0; i < 6; i++) { realJunk += c[i].x; }
+}
+
+static void
+VectorAvg3p(void *ti)
+{
+	M_Vector3 a[6], b[6], c[6];
+	int i;
+
+	for (i = 0; i < 6; i++) { a[i] = RandomVector3(ti); b[i] = RandomVector3(ti); }
+	for (i = 0; i < 6; i++) { c[i] = M_VecAvg3p(&a[i], &b[i]); }
+	for (i = 0; i < 6; i++) { realJunk += c[i].x; }
+}
+
+static void
+VectorLERP3(void *ti)
+{
+	M_Vector3 a[6], b[6], c[6];
+	int i;
+
+	for (i = 0; i < 6; i++) { a[i] = RandomVector3(ti); b[i] = RandomVector3(ti); }
+	for (i = 0; i < 6; i++) { c[i] = M_VecLERP3(a[i], b[i], RandomReal(ti)); }
+	for (i = 0; i < 6; i++) { realJunk += c[i].x; }
+}
+
+static void
+VectorSum3(void *ti)
+{
+	M_Vector3 v[12], vOut;
+	int i;
+
+	for (i = 0; i < 12; i++) { v[i] = RandomVector3(ti); }
+	vOut = M_VecSum3(v, 12);
+	for (i = 0; i < 12; i++) { realJunk += vOut.x; }
+}
+
+static struct ag_benchmark_fn mathBenchVector3Fns[] = {
+	{ "Zero3()", 		VectorZero3		},
+	{ "Get3()",		VectorGet3		},
+	{ "Set3()", 		VectorSet3		},
+	{ "Copy3()",		VectorCopy3		},
+	{ "Flip3()",		VectorFlip3		},
+	{ "Len3()", 		VectorLen3		},
+	{ "Len3p()", 		VectorLen3p		},
+	{ "Dot()", 		VectorDot3		},
+	{ "Dot3p()",		VectorDot3p		},
+	{ "Distance3()", 	VectorDistance3		},
+	{ "Distance3p()", 	VectorDistance3p	},
+	{ "Norm3()", 		VectorNorm3		},
+	{ "Norm3p()", 		VectorNorm3p		},
+	{ "Norm3v()", 		VectorNorm3v		},
+	{ "Cross3()", 		VectorCross3		},
+	{ "Cross3p()",	 	VectorCross3p		},
+	{ "NormCross3()", 	VectorNormCross3	},
+	{ "NormCross3p()",	VectorNormCross3p	},
+	{ "Scale3()", 		VectorScale3		},
+	{ "Scale3p()",	 	VectorScale3p		},
+	{ "Scale3v()",	 	VectorScale3v		},
+	{ "Add3()", 		VectorAdd3		},
+	{ "Add3p()", 		VectorAdd3p		},
+	{ "Add3v()", 		VectorAdd3v		},
+	{ "Avg3()", 		VectorAvg3		},
+	{ "Avg3p()", 		VectorAvg3p		},
+	{ "LERP3()", 		VectorLERP3		},
+	{ "Sum3()", 		VectorSum3		},
+};
+struct ag_benchmark mathBenchVector3 = {
+	"M_Vector3",
+	&mathBenchVector3Fns[0],
+	sizeof(mathBenchVector3Fns) / sizeof(mathBenchVector3Fns[0]),
+	20, 10000, 100000
+};
