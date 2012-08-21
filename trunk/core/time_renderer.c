f0a113164064e57b3bdb3c97e16b32422b61ef05
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2012-08-21 09:10:32 +0000

    rename agTimeOps_condwait -> agTimeOps_renderer; use CLOCK_MONOTONIC.

---
+++ trunk/core/time_renderer.c
@@ -0,0 +1,79 @@
+/*	Public domain	*/
+/*
+ * Rendering-aware time backend. This is useful for applications performing
+ * offline rendering, where the outcome of the rendering may be influenced by
+ * different threads, each using AG_Delay() calls or Agar timers.
+ *
+ * Using this backend, the start of a rendering cycle causes AG_Delay() to
+ * block until the rendering cycle is complete.
+ */
+
+#include <sys/types.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "core.h"
+
+AG_Cond agCondBeginRender;
+AG_Cond agCondEndRender;
+AG_Mutex agCondRenderLock;
+
+static struct timespec t0;
+static int inited = 0;
+
+static void
+Init(void)
+{
+	if (!inited) {
+		AG_CondInit(&agCondBeginRender);
+		AG_CondInit(&agCondEndRender);
+		AG_MutexInit(&agCondRenderLock);
+		inited = 1;
+	}
+	clock_gettime(CLOCK_MONOTONIC, &t0);
+}
+
+static Uint32
+GetTicks(void)
+{
+	struct timespec t;
+
+	clock_gettime(CLOCK_MONOTONIC, &t);
+	return (Uint32)(t.tv_sec - t0.tv_sec)*1000 +
+	               (t.tv_nsec - t0.tv_nsec)/1000000;
+}
+
+static void
+Delay(Uint32 Tdelay)
+{
+	struct timespec ts, tsNow;
+	int rv;
+
+	AG_MutexLock(&agCondRenderLock);
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+	ts.tv_sec += (Tdelay/1000);
+	ts.tv_nsec += (Tdelay % 1000)*1000000;
+
+	for (;;) {
+		rv = AG_CondTimedWait(&agCondBeginRender, &agCondRenderLock, &ts);
+		if (rv != 0) {
+			clock_gettime(CLOCK_MONOTONIC, &tsNow);
+			if (tsNow.tv_sec < ts.tv_sec) {
+				continue;
+			}
+			if (tsNow.tv_sec > ts.tv_sec ||
+			    tsNow.tv_nsec >= ts.tv_nsec)
+				break;
+		}
+	}
+	AG_MutexUnlock(&agCondRenderLock);
+}
+
+const AG_TimeOps agTimeOps_renderer = {
+	"renderer",
+	Init,
+	NULL,
+	GetTicks,
+	Delay
+};
