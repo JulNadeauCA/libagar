ec2fa451fa9c4a2597f910d415420aeeed5edb18
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2012-11-05 01:50:16 +0000

    use _AGAR_CORE_DEFINED_FOO to avoid undefining potentially externally defined types like Uint.

--- trunk/core/close_types.h
+++ trunk/core/close_types.h
@@ -1,15 +1,37 @@
 /*	Public domain	*/
 
 #ifndef _USE_AGAR_TYPES
-# undef Uint
-# undef Uchar
-# undef Ulong
-# undef Uint8
-# undef Sint8
-# undef Uint16
-# undef Sint16
-# undef Uint32
-# undef Sint32
-# undef Uint64
-# undef Sint64
+# ifdef _AGAR_CORE_DEFINED_UINT
+#  undef Uint
+# endif
+# ifdef _AGAR_CORE_DEFINED_UCHAR
+#  undef Uchar
+# endif
+# ifdef _AGAR_CORE_DEFINED_ULONG
+#  undef Ulong
+# endif
+# ifdef _AGAR_CORE_DEFINED_UINT8
+#  undef Uint8
+# endif
+# ifdef _AGAR_CORE_DEFINED_SINT8
+#  undef Sint8
+# endif
+# ifdef _AGAR_CORE_DEFINED_UINT16
+#  undef Uint16
+# endif
+# ifdef _AGAR_CORE_DEFINED_SINT16
+#  undef Sint16
+# endif
+# ifdef _AGAR_CORE_DEFINED_UINT32
+#  undef Uint32
+# endif
+# ifdef _AGAR_CORE_DEFINED_SINT32
+#  undef Sint32
+# endif
+# ifdef _AGAR_CORE_DEFINED_UINT64
+#  undef Uint64
+# endif
+# ifdef _AGAR_CORE_DEFINED_SINT64
+#  undef Sint64
+# endif
 #endif /* _USE_AGAR_TYPES */
--- trunk/core/types.h
+++ trunk/core/types.h
@@ -36,73 +36,94 @@
 #endif
 
 #ifndef Uint
-#define Uint unsigned int
+# define Uint unsigned int
+# define _AGAR_CORE_DEFINED_UINT
 #endif
 #ifndef Uchar
-#define Uchar unsigned char
+# define Uchar unsigned char
+# define _AGAR_CORE_DEFINED_UCHAR
 #endif
 #ifndef Ulong
-#define Ulong unsigned long
+# define Ulong unsigned long
+# define _AGAR_CORE_DEFINED_ULONG
 #endif
 
 #if defined(_WIN32)
 # ifndef Sint8
-# define Sint8 __int8
+#  define Sint8 __int8
+#  define _AGAR_CORE_DEFINED_SINT8
 # endif
 # ifndef Uint8
-# define Uint8 unsigned __int8
+#  define Uint8 unsigned __int8
+#  define _AGAR_CORE_DEFINED_UINT8
 # endif
 # ifndef Sint16
-# define Sint16 __int16
+#  define Sint16 __int16
+#  define _AGAR_CORE_DEFINED_SINT16
 # endif
 # ifndef Uint16
-# define Uint16 unsigned __int16
+#  define Uint16 unsigned __int16
+#  define _AGAR_CORE_DEFINED_UINT16
 # endif
 # ifndef Sint32
-# define Sint32 __int32
+#  define Sint32 __int32
+#  define _AGAR_CORE_DEFINED_SINT32
 # endif
 # ifndef Uint32
-# define Uint32 unsigned __int32
+#  define Uint32 unsigned __int32
+#  define _AGAR_CORE_DEFINED_UINT32
 # endif
 # ifndef Sint64
-# define Sint64 __int64
+#  define Sint64 __int64
+#  define _AGAR_CORE_DEFINED_SINT64
 # endif
 # ifndef Uint64
-# define Uint64 unsigned __int64
+#  define Uint64 unsigned __int64
+#  define _AGAR_CORE_DEFINED_UINT64
 # endif
 #else /* !_WIN32 */
 # ifndef Sint8
-# define Sint8 int8_t
+#  define Sint8 int8_t
+#  define _AGAR_CORE_DEFINED_SINT8
 # endif
 # ifndef Uint8
-# define Uint8 u_int8_t
+#  define Uint8 u_int8_t
+#  define _AGAR_CORE_DEFINED_UINT8
 # endif
 # ifndef Sint16
-# define Sint16 int16_t
+#  define Sint16 int16_t
+#  define _AGAR_CORE_DEFINED_SINT16
 # endif
 # ifndef Uint16
-# define Uint16 u_int16_t
+#  define Uint16 u_int16_t
+#  define _AGAR_CORE_DEFINED_UINT16
 # endif
 # ifndef Sint32
-# define Sint32 int32_t
+#  define Sint32 int32_t
+#  define _AGAR_CORE_DEFINED_SINT32
 # endif
 # ifndef Uint32
-# define Uint32 u_int32_t
+#  define Uint32 u_int32_t
+#  define _AGAR_CORE_DEFINED_UINT32
 # endif
 # ifdef AG_HAVE_64BIT
 #  ifndef Sint64
-#  define Sint64 int64_t
+#   define Sint64 int64_t
+#   define _AGAR_CORE_DEFINED_SINT64
 #  endif
 #  ifndef Uint64
-#  define Uint64 u_int64_t
+#   define Uint64 u_int64_t
+#   define _AGAR_CORE_DEFINED_UINT64
 #  endif
 # else /* !AG_HAVE_64BIT */
 typedef struct ag_fake_int64 { Uint32 _pad1; Uint32 _pad2; };
 #  ifndef Sint64
-#  define Sint64 struct ag_fake_int64
+#   define Sint64 struct ag_fake_int64
+#   define _AGAR_CORE_DEFINED_SINT64
 #  endif
 #  ifndef Uint64
-#  define Uint64 struct ag_fake_int64
+#   define Uint64 struct ag_fake_int64
+#   define _AGAR_CORE_DEFINED_UINT64
 #  endif
 # endif /* AG_HAVE_64BIT */
 #endif /* !WIN32 */
