2f0d4c98babeb18e3530ead7d2c9cc0a34af036f
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2013-03-12 05:44:32 +0000

    don't try to use execvp() on _WIN32

--- trunk/core/exec.h
+++ trunk/core/exec.h
@@ -6,10 +6,10 @@
 
 #include <agar/config/have_execvp.h>
 
-#ifdef HAVE_EXECVP
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#if defined(HAVE_EXECVP) && !defined(_WIN32)
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/wait.h>
 #endif
 
 enum ag_exec_wait_type {
@@ -17,7 +17,7 @@ enum ag_exec_wait_type {
 	AG_EXEC_WAIT_INFINITE
 };
 
-#ifdef HAVE_EXECVP
+#if defined(HAVE_EXECVP) && !defined(_WIN32)
 typedef pid_t AG_ProcessID;
 #else
 typedef int AG_ProcessID;
