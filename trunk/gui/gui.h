1790b10cad3201e3bf3c02edcf73f717b55c7ae2
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2013-04-22 01:07:54 +0000

    don't mark the old AG_VIDEO_* flags deprecated, AG_InitVideoSDL() still uses them.

--- trunk/gui/gui.h
+++ trunk/gui/gui.h
@@ -9,23 +9,21 @@
 #define AG_ZOOM_RANGE	14		/* Number of default zoom settings */
 #define AG_ZOOM_DEFAULT	5		/* Initial zoom setting (1.0) */
 
-#ifdef AG_LEGACY
 /* For old AG_InitVideo() interface */
-# define AG_VIDEO_HWSURFACE     0x0001
-# define AG_VIDEO_ASYNCBLIT     0x0002
-# define AG_VIDEO_ANYFORMAT     0x0004
-# define AG_VIDEO_HWPALETTE     0x0008
-# define AG_VIDEO_DOUBLEBUF     0x0010
-# define AG_VIDEO_FULLSCREEN    0x0020
-# define AG_VIDEO_RESIZABLE     0x0040
-# define AG_VIDEO_NOFRAME       0x0080
-# define AG_VIDEO_BGPOPUPMENU   0x0100
-# define AG_VIDEO_OPENGL	0x0200
-# define AG_VIDEO_OPENGL_OR_SDL 0x0400
-# define AG_VIDEO_NOBGCLEAR     0x0800
-# define AG_VIDEO_OVERLAY       0x1000
-# define AG_VIDEO_SDL           0x2000
-#endif /* AG_LEGACY */
+#define AG_VIDEO_HWSURFACE     0x0001
+#define AG_VIDEO_ASYNCBLIT     0x0002
+#define AG_VIDEO_ANYFORMAT     0x0004
+#define AG_VIDEO_HWPALETTE     0x0008
+#define AG_VIDEO_DOUBLEBUF     0x0010
+#define AG_VIDEO_FULLSCREEN    0x0020
+#define AG_VIDEO_RESIZABLE     0x0040
+#define AG_VIDEO_NOFRAME       0x0080
+#define AG_VIDEO_BGPOPUPMENU   0x0100
+#define AG_VIDEO_OPENGL	       0x0200
+#define AG_VIDEO_OPENGL_OR_SDL 0x0400
+#define AG_VIDEO_NOBGCLEAR     0x0800
+#define AG_VIDEO_OVERLAY       0x1000
+#define AG_VIDEO_SDL           0x2000
 
 __BEGIN_DECLS
 /* Global configuration settings (bound to agConfig) */
