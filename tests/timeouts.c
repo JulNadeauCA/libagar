a3a8c09ec43fea4c7145dede5f518fbd3c688fe8
Julien Nadeau Carriere <vedge@hypertriton.com>
2014-07-28 03:10:05 +0000

    - allow testing different periods. - associate test timer instances with the test window.
    git-svn-id: https://dev.csoft.net/agar/trunk@9706 86a84d31-9702-0410-8f1e-a60cab7bd424

--- tests/timeouts.c
+++ tests/timeouts.c
@@ -8,6 +8,7 @@ typedef struct {
 	AG_TestInstance _inherit;
 	AG_Timer to[3], toReg;
 	AG_Window *win;
+	Uint tick, period;
 } MyTestInstance;
 
 static Uint32
@@ -38,7 +39,7 @@ static Uint32
 TimeoutRegular(AG_Timer *to, AG_Event *event)
 {
 	MyTestInstance *ti = AG_PTR(1);
-	TestMsg(ti, "Tick");
+	TestMsg(ti, "Tick %u", ti->tick++);
 	return (to->ival);
 }
 
@@ -49,31 +50,33 @@ TestOneShot(AG_Event *event)
 	MyTestInstance *ti = AG_PTR(1);
 
 	TestMsg(ti, "Testing 3 one-shot timers");
-	if (AG_AddTimer(NULL, &ti->to[0], 1000, Timeout1, "%p", ti) == -1 ||
-	    AG_AddTimer(NULL, &ti->to[1], 2000, Timeout2, "%p", ti) == -1 ||
-	    AG_AddTimer(NULL, &ti->to[2], 2100, Timeout3, "%p", ti) == -1) {
+	if (AG_AddTimer(ti->win, &ti->to[0], 1000, Timeout1, "%p", ti) == -1 ||
+	    AG_AddTimer(ti->win, &ti->to[1], 2000, Timeout2, "%p", ti) == -1 ||
+	    AG_AddTimer(ti->win, &ti->to[2], 2100, Timeout3, "%p", ti) == -1) {
 		TestMsg(ti, "AddTimer: %s", AG_GetError());
 	}
 }
 
 static void
-StartRegular(AG_Event *event)
+StartPeriodic(AG_Event *event)
 {
 	MyTestInstance *ti = AG_PTR(1);
 
-	TestMsg(ti, "Starting regular timer");
-	if (AG_AddTimer(NULL, &ti->toReg, 1000, TimeoutRegular, "%p", ti) == -1) {
+	ti->tick = 0;
+	TestMsg(ti, "Testing periodic timer at %u ms", ti->period);
+	if (AG_AddTimer(ti->win, &ti->toReg, ti->period,
+	    TimeoutRegular, "%p", ti) == -1) {
 		TestMsg(ti, "AddTimer: %s", AG_GetError());
 	}
 }
 
 static void
-StopRegular(AG_Event *event)
+StopPeriodic(AG_Event *event)
 {
 	MyTestInstance *ti = AG_PTR(1);
 
-	TestMsg(ti, "Stopping regular timer");
-	AG_DelTimer(NULL, &ti->toReg);
+	TestMsg(ti, "Stopping periodic timer");
+	AG_DelTimer(ti->win, &ti->toReg);
 }
 
 static int
@@ -82,6 +85,8 @@ Init(void *obj)
 	MyTestInstance *ti = obj;
 
 	ti->win = NULL;
+	ti->tick = 0;
+	ti->period = 1000;
 	AG_InitTimer(&ti->to[0], "testTimer1", 0);
 	AG_InitTimer(&ti->to[1], "testTimer2", 0);
 	AG_InitTimer(&ti->to[2], "testTimer3", 0);
@@ -106,14 +111,24 @@ static int
 TestGUI(void *obj, AG_Window *win)
 {
 	MyTestInstance *ti = obj;
+	AG_Box *hBox;
+	AG_Numerical *num;
 
 	ti->win = win;
 	AG_LabelNewS(win, 0, "Test for AG_Timer(3) facility");
 	AG_LabelNew(win, 0, "timeOps: %s", agTimeOps->name);
-	AG_ButtonNewFn(win, 0, "Timer Inspector", StartTimerInspector, "%p", ti);
-	AG_ButtonNewFn(win, 0, "Test One-Shot", TestOneShot, "%p", ti);
-	AG_ButtonNewFn(win, 0, "Start Regular", StartRegular, "%p", ti);
-	AG_ButtonNewFn(win, 0, "Stop Regular", StopRegular, "%p", ti);
+	AG_ButtonNewFn(win, AG_BUTTON_HFILL, "Open Timer Inspector",
+	    StartTimerInspector, "%p", ti);
+	AG_SeparatorNewHoriz(win);
+
+	hBox = AG_BoxNewHoriz(win, 0);
+	{
+		AG_ButtonNewFn(hBox, 0, "Test One-Shot", TestOneShot, "%p", ti);
+		AG_ButtonNewFn(hBox, 0, "Start Periodic", StartPeriodic, "%p", ti);
+		AG_ButtonNewFn(hBox, 0, "Stop Periodic", StopPeriodic, "%p", ti);
+	}
+	num = AG_NumericalNew(win, 0, "ms", "Period: ");
+	AG_BindUint(num, "value", &ti->period);
 	return (0);
 }
 
@@ -124,7 +139,7 @@ const AG_TestCase timeoutsTest = {
 	0,
 	sizeof(MyTestInstance),
 	Init,
-	NULL,		/* destroy */
+	NULL,
 	NULL,		/* test */
 	TestGUI,
 	NULL		/* bench */
