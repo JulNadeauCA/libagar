67a873172c38164b9208e50a031401df47fb7d29
Julien Nadeau Carriere <vedge@hypertriton.com>
2014-08-05 11:50:37 +0000

    make test more useful
    git-svn-id: https://dev.csoft.net/agar/trunk@9716 86a84d31-9702-0410-8f1e-a60cab7bd424

--- tests/charsets.c
+++ tests/charsets.c
@@ -22,31 +22,47 @@ TestGUI(void *obj, AG_Window *win)
 
 	/* Bind to a C string in US-ASCII */
 	AG_Strlcpy(myASCII, "ASCII!", sizeof(myASCII));
-	tb = AG_TextboxNew(win, AG_TEXTBOX_HFILL|AG_TEXTBOX_EXCL, "ASCII Buffer: ");
+	tb = AG_TextboxNew(win, AG_TEXTBOX_HFILL|AG_TEXTBOX_EXCL,
+	    "ASCII Buffer: \n(%lu bytes)", sizeof(myASCII));
 	AG_TextboxBindASCII(tb, myASCII, sizeof(myASCII));
-	
+	AG_TextboxSizeHintLines(tb, 2);
+
 	/* Bind to a C string in UTF-8 */
 	AG_Strlcpy(myUTF, "\xc3\x85ngstrom!", sizeof(myUTF));
-	tb = AG_TextboxNew(win, AG_TEXTBOX_HFILL|AG_TEXTBOX_EXCL, "UTF-8 Buffer: ");
+	tb = AG_TextboxNew(win, AG_TEXTBOX_HFILL|AG_TEXTBOX_EXCL,
+	    "UTF-8 Buffer: \n(%lu bytes)", sizeof(myUTF));
 	AG_TextboxBindUTF8(tb, myUTF, sizeof(myUTF));
+	AG_TextboxSizeHintLines(tb, 2);
 
 	/* Bind to a C string in any iconv-supported encoding */
 #ifdef HAVE_ICONV
 	AG_Strlcpy(myLat1, "Overv\xE5knign for feils\xF8king!", sizeof(myLat1));
-	tb = AG_TextboxNew(win, AG_TEXTBOX_HFILL|AG_TEXTBOX_EXCL, "LATIN-1 Buffer: ");
+	tb = AG_TextboxNew(win, AG_TEXTBOX_HFILL|AG_TEXTBOX_EXCL,
+	    "LATIN-1 Buffer: \n(%lu bytes)", sizeof(myLat1));
 	AG_TextboxBindEncoded(tb, "ISO-8859-1", myLat1, sizeof(myLat1));
+	AG_TextboxSizeHintLines(tb, 2);
+	AG_LabelNewS(win, 0, "iconv support: YES");
 #else
-	AG_LabelNewS(win, 0, "(Agar was not compiled with iconv support)");
+	AG_LabelNewS(win, 0,
+	    "iconv support: NO.\n"
+	    "Available encodings: ASCII, UTF-8, UCS-4");
 #endif
+	AG_SeparatorNewHoriz(win);
 
 	/* Bind to a multilingual AG_Text(3) element. */
 	myTxt = AG_TextNew(0);
 	AG_TextSetEnt(myTxt, AG_LANG_FR, "Fran\xc3\xa7\x61is!");
 	AG_TextSetEnt(myTxt, AG_LANG_EN, "English!");
 	AG_TextSetEnt(myTxt, AG_LANG_NO, "Norsk!");
-	AG_TextSetLangISO(myTxt, "fr");
-	tb = AG_TextboxNew(win, AG_TEXTBOX_HFILL, "AG_Text Buffer: ");
+	AG_LabelNewS(win, 0, "Multilingual AG_Text(3) buffer:");
+	tb = AG_TextboxNewS(win,
+	    AG_TEXTBOX_MULTILINGUAL|AG_TEXTBOX_EXPAND|AG_TEXTBOX_MULTILINE,
+	    NULL);
 	AG_TextboxBindText(tb, myTxt);
+	AG_TextboxSetLang(tb, AG_LANG_FR);
+	AG_TextboxSizeHint(tb, "XXXXXXXXXXXXXXXXXXXXXXXXX");
+	AG_TextboxSizeHintLines(tb, 5);
+	AG_TextboxSetCursorPos(tb, -1);		/* End of string */
 	return (0);
 }
 
