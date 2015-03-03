f1cb0c09109645d73686d8b5854b42dced5908ce
Julien Nadeau Carriere <vedge@hypertriton.com>
2015-03-03 10:20:58 +0000

    prevent unsafe operation in the threads test
    git-svn-id: https://dev.csoft.net/agar/trunk@9780 86a84d31-9702-0410-8f1e-a60cab7bd424

--- tests/threads.c
+++ tests/threads.c
@@ -32,6 +32,7 @@ typedef struct {
 		int value;
 	} spinning;
 	int closeTest;
+	AG_Mutex lock;
 } MyTestInstance;
 
 static void *
@@ -60,21 +61,33 @@ SleepingWorker(void *arg)
 
 	TestMsg(ti, "Sleeping worker created");
 	for (;;) {
+		AG_MutexLock(&ti->lock);
 		if (ti->closeTest) {
-			break;
+			TestMsg(ti, "Task aborted");
+			AG_MutexUnlock(&ti->lock);
+			goto out;
 		}
 		TestMsg(ti, "Sleeping worker: %d/%d", ti->sleeping.value, ti->sleeping.max);
 		if (++ti->sleeping.value == ti->sleeping.max) {
-			TestMsg(ti, "Sleeping worker thread exiting");
-			AG_ObjectDetach(ti->sleeping.win);
-			ti->sleeping.win = NULL;
-			AG_ThreadExit(NULL);
+			TestMsg(ti, "Task completed");
+			AG_MutexUnlock(&ti->lock);
+			goto out;
 		}
 		AG_LabelText(ti->sleeping.lbl,
 		    "Sleeping worker progress: %d/%d", ti->sleeping.value, ti->sleeping.max);
-		AG_Delay(1000);
+
+		AG_MutexUnlock(&ti->lock);
+		AG_Delay(100);
 	}
-	ti->sleeping.running = 0;
+out:
+	AG_MutexLock(&ti->lock);
+	ti->closeTest = 0;
+	if (ti->sleeping.win != NULL) {
+		AG_ObjectDetach(ti->sleeping.win);
+		ti->sleeping.win = NULL;
+		ti->sleeping.running = 0;
+	}
+	AG_MutexUnlock(&ti->lock);
 	return (NULL);
 }
 
@@ -87,16 +100,18 @@ SpinningWorker(void *arg)
 	TestMsg(ti, "Spinning worker created");
 	for (;;) {
 		int i;
-
+	
+		AG_MutexLock(&ti->lock);
 		if (ti->closeTest) {
-			break;
+			TestMsg(ti, "Task aborted. x=%f, y=%f", x, y);
+			AG_MutexUnlock(&ti->lock);
+			goto out;
 		}
 		TestMsg(ti, "Spinning worker: %d/%d", ti->spinning.value, ti->spinning.max);
 		if (++ti->spinning.value == ti->spinning.max) {
-			TestMsg(ti, "Spinning worker thread exiting. x=%f, y=%f", x, y);
-			AG_ObjectDetach(ti->spinning.win);
-			ti->spinning.win = NULL;
-			AG_ThreadExit(NULL);
+			TestMsg(ti, "Task completed. x=%f, y=%f", x, y);
+			AG_MutexUnlock(&ti->lock);
+			goto out;
 		}
 		AG_LabelText(ti->spinning.lbl,
 		    "Spinning worker progress: %d/%d", ti->spinning.value, ti->spinning.max);
@@ -105,8 +120,17 @@ SpinningWorker(void *arg)
 			x = sin(cos(y)) + tan((double)i);
 			if (x != 12.345) { x = 1.0; }
 		}
+		AG_MutexUnlock(&ti->lock);
+		AG_Delay(1);
 	}
-	ti->spinning.running = 0;
+out:
+	AG_MutexLock(&ti->lock);
+	if (ti->spinning.win != NULL) {
+		AG_ObjectDetach(ti->spinning.win);
+		ti->spinning.win = NULL;
+		ti->spinning.running = 0;
+	}
+	AG_MutexUnlock(&ti->lock);
 	return (NULL);
 }
 
@@ -128,8 +152,11 @@ CreateSleepingWorker(AG_Event *event)
 	AG_Window *win;
 	AG_ProgressBar *pb;
 
+	AG_MutexLock(&ti->lock);
+
 	if (ti->sleeping.win != NULL ||
 	    (win = AG_WindowNew(AG_WINDOW_NOCLOSE)) == NULL) {
+		AG_MutexUnlock(&ti->lock);
 		return;
 	}
 	ti->sleeping.win = win;
@@ -144,11 +171,13 @@ CreateSleepingWorker(AG_Event *event)
 	AG_BindInt(pb, "min", &ti->sleeping.min);
 	AG_BindInt(pb, "max", &ti->sleeping.max);
 	ti->sleeping.lbl = AG_LabelNew(win, AG_LABEL_HFILL, "...");
-	AG_WindowAttach(ti->winParent, win);
+/*	AG_WindowAttach(ti->winParent, win); */
 	AG_WindowShow(win);
 
 	AG_ThreadCreate(&ti->sleeping.th, SleepingWorker, ti);
 	ti->sleeping.running = 1;
+	
+	AG_MutexUnlock(&ti->lock);
 }
 
 static void
@@ -157,9 +186,12 @@ CreateSpinningWorker(AG_Event *event)
 	MyTestInstance *ti = AG_PTR(1);
 	AG_Window *win;
 	AG_ProgressBar *pb;
+	
+	AG_MutexLock(&ti->lock);
 
 	if (ti->spinning.win != NULL ||
 	    (win = AG_WindowNew(AG_WINDOW_NOCLOSE)) == NULL) {
+		AG_MutexUnlock(&ti->lock);
 		return;
 	}
 	ti->spinning.win = win;
@@ -174,11 +206,13 @@ CreateSpinningWorker(AG_Event *event)
 	AG_BindInt(pb, "min", &ti->spinning.min);
 	AG_BindInt(pb, "max", &ti->spinning.max);
 	ti->spinning.lbl = AG_LabelNew(win, AG_LABEL_HFILL, "...");
-	AG_WindowAttach(ti->winParent, win);
+/*	AG_WindowAttach(ti->winParent, win); */
 	AG_WindowShow(win);
 
 	AG_ThreadCreate(&ti->spinning.th, SpinningWorker, ti);
 	ti->spinning.running = 1;
+	
+	AG_MutexUnlock(&ti->lock);
 }
 
 static int
@@ -194,6 +228,7 @@ Init(void *obj)
 	ti->spinning.win = NULL;
 	ti->spinning.running = 0;
 	ti->closeTest = 0;
+	AG_MutexInit(&ti->lock);
 	return (0);
 }
 
@@ -202,6 +237,7 @@ Destroy(void *obj)
 {
 	MyTestInstance *ti = obj;
 
+	AG_MutexDestroy(&ti->lock);
 	AG_ObjectDestroy(&ti->workerMgr);
 }
 
@@ -210,14 +246,19 @@ CloseTest(AG_Event *event)
 {
 	MyTestInstance *ti = AG_PTR(1);
 	
-	ti->closeTest = 1;
+	AG_MutexLock(&ti->lock);
+	if (ti->sleeping.running) {
+		TestMsg(ti, "Sleeping worker is not finished");
+		AG_MutexUnlock(&ti->lock);
+		return;
+	}
+	if (ti->spinning.running) {
+		TestMsg(ti, "Spinning worker is not finished");
+		AG_MutexUnlock(&ti->lock);
+		return;
+	}
+	AG_MutexUnlock(&ti->lock);
 
-	while (ti->sleeping.running) {
-		AG_Delay(50);
-	}
-	while (ti->spinning.running) {
-		AG_Delay(50);
-	}
 	TestWindowClose(event);
 }
 
