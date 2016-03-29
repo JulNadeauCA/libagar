7ccb36ac424fb978c1057484ad57d2d5f7ff9f58
vedge <vedge@86a84d31-9702-0410-8f1e-a60cab7bd424>
2016-03-29 07:17:45 +0000

    fix compilation under --disable-gui

--- trunk/math/m_math.c
+++ trunk/math/m_math.c
@@ -61,7 +61,14 @@ static size_t
 PrintTime(AG_FmtString *fs, char *dst, size_t dstSize)
 {
 	M_Time *t = AG_FMTSTRING_ARG(fs);
-	return AG_UnitFormat((double)(*t), agTimeUnits, dst, dstSize);
+#ifdef ENABLE_GUI
+	if (agGUI) {
+		return AG_UnitFormat((double)(*t), agTimeUnits, dst, dstSize);
+	} else
+#endif
+	{
+		return Snprintf(dst, dstSize, "%f", (double)(*t));
+	}
 }
 static size_t
 PrintComplex(AG_FmtString *fs, char *dst, size_t dstSize)
