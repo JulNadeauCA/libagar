eb8c3f409e0a77b6f5f1d6d7e3da4826fa42c2da
Julien Nadeau Carriere <vedge@hypertriton.com>
2018-11-16 09:49:42 +0000

    define the memory model (AG_MODEL) to be AG_SMALL, AG_MEDIUM or AG_LARGE
    git-svn-id: https://dev.csoft.net/agar/trunk@9990 86a84d31-9702-0410-8f1e-a60cab7bd424

--- core/limits.h
+++ core/limits.h
@@ -14,110 +14,150 @@
 #if defined(MAXPATHLEN)
 # define AG_PATHNAME_MAX MAXPATHLEN
 #else
-# define AG_PATHNAME_MAX 1024
+# if AG_MODEL == AG_SMALL
+#  define AG_PATHNAME_MAX 64
+# else
+#  define AG_PATHNAME_MAX 1024
+# endif
 #endif
+
 #if defined(FILENAME_MAX)
 # define AG_FILENAME_MAX FILENAME_MAX
 #else
-# define AG_FILENAME_MAX 256
+# if AG_MODEL == AG_SMALL
+#  define AG_FILENAME_MAX (16+1)
+# elif AG_MODEL == AG_MEDIUM
+#  define AG_FILENAME_MAX 128
+# else
+#  define AG_FILENAME_MAX 256
+# endif
 #endif
-#if defined (_XBOX)
-#define AG_ARG_MAX MAX_LAUNCH_DATA_SIZE - 520
+
+#if defined(_XBOX)
+# define AG_ARG_MAX (MAX_LAUNCH_DATA_SIZE - 520)
 #elif defined(ARG_MAX)
-#define AG_ARG_MAX ARG_MAX
+# define AG_ARG_MAX ARG_MAX
 #else
-#define AG_ARG_MAX 4096
+# if AG_MODEL == AG_SMALL
+#  define AG_ARG_MAX 256
+# else
+#  define AG_ARG_MAX 4096
+# endif
 #endif
-#define AG_SEARCHPATH_MAX (AG_PATHNAME_MAX*4)
 
 #if defined(BUFSIZ)
 # define AG_BUFFER_MIN BUFSIZ
 # define AG_BUFFER_MAX BUFSIZ
 #else
-# define AG_BUFFER_MIN 1024
-# define AG_BUFFER_MAX 8192
+# if AG_MODEL == AG_SMALL
+#  define AG_BUFFER_MIN 256
+#  define AG_BUFFER_MAX 1024
+# else
+#  define AG_BUFFER_MIN 1024
+#  define AG_BUFFER_MAX 8192
+# endif
 #endif
 
 /*
  * Integer limits
  */
 #ifdef _MK_HAVE_LIMITS_H
+
 # include <limits.h>
+
 # define AG_INT_MIN	INT_MIN
 # define AG_INT_MAX	INT_MAX
 # define AG_UINT_MAX	UINT_MAX
 # define AG_LONG_MIN	LONG_MIN
 # define AG_LONG_MAX	LONG_MAX
 # define AG_ULONG_MAX	ULONG_MAX
-#else
-# define AG_INT_MIN	(-0x7fffffff-1)
-# define AG_INT_MAX	0x7fffffff
-# define AG_UINT_MAX	0xffffffffU
-# ifdef __LP64__
-#  define AG_ULONG_MAX	0xffffffffffffffffUL
-#  define AG_LONG_MAX	0x7fffffffffffffffL	
-#  define AG_LONG_MIN	(-0x7fffffffffffffffL-1)	
-# else
-#  define AG_ULONG_MAX	0xffffffffUL
-#  define AG_LONG_MAX	0x7fffffffL
-#  define AG_LONG_MIN	(-0x7fffffffL-1)
-# endif
-#endif
-#ifdef HAVE_LONG_LONG
-# define AG_ULLONG_MAX	0xffffffffffffffffULL	
-# define AG_LLONG_MIN	(-0x7fffffffffffffffLL-1)	
-# define AG_LLONG_MAX	0x7fffffffffffffffLL	
-#endif
+
+#else /* !HAVE_LIMITS_H */
+
+# if AG_MODEL == AG_SMALL
+
+#  define AG_INT_MIN   (-0x7ffe)
+#  define AG_INT_MAX     0x7fff
+#  define AG_UINT_MAX    0xffff
+#  define AG_LONG_MIN  (-0x7ffffffe)
+#  define AG_LONG_MAX    0x7fffffff
+#  define AG_ULONG_MAX   0xffffffffU
+
+# else /* MEDIUM or LARGE */
+
+#  define AG_INT_MIN   (-0x7ffffffe)
+#  define AG_INT_MAX     0x7fffffff
+#  define AG_UINT_MAX    0xffffffffU
+#  ifdef __LP64__
+#   define AG_ULONG_MAX  0xffffffffffffffffUL
+#   define AG_LONG_MAX   0x7fffffffffffffffL	
+#   define AG_LONG_MIN (-0x7ffffffffffffffeL)	
+#  else
+#   define AG_ULONG_MAX  0xffffffffUL
+#   define AG_LONG_MAX	 0x7fffffffL
+#   define AG_LONG_MIN (-0x7ffffffeL)
+#  endif
+#  ifdef HAVE_LONG_LONG
+#   define AG_ULLONG_MAX  0xffffffffffffffffULL	
+#   define AG_LLONG_MIN (-0x7ffffffffffffffeLL)	
+#   define AG_LLONG_MAX   0x7fffffffffffffffLL	
+#  endif
+# endif /* MEDIUM or LARGE */
+
+#endif /* !HAVE_LIMITS_H */
 
 /*
  * Floating-point number limits
  */
-#ifdef __FLT_MIN__
-# define AG_FLT_MIN __FLT_MIN__
-#else
-# define AG_FLT_MIN 1.175494351e-38f
-#endif
-#ifdef __FLT_MAX__
-# define AG_FLT_MAX __FLT_MAX__
-#else
-# define AG_FLT_MAX 3.402823466e+38f
-#endif
-#ifdef __DBL_MIN__
-# define AG_DBL_MIN __DBL_MIN__
-#else
-# define AG_DBL_MIN 2.2250738585072014e-308
-#endif
-#ifdef __DBL_MAX__
-# define AG_DBL_MAX __DBL_MAX__
-#else
-# define AG_DBL_MAX 1.7976931348623158e+308
-#endif
-#ifdef AG_HAVE_LONG_DOUBLE
-# ifdef __LDBL_MIN__
-#  define AG_LDBL_MIN __LDBL_MIN__
+#ifdef AG_HAVE_FLOAT
+# ifdef __FLT_MIN__
+#  define AG_FLT_MIN __FLT_MIN__
 # else
-#  define AG_LDBL_MIN 3.36210314311209350626e-4932l
+#  define AG_FLT_MIN 1.175494351e-38f
 # endif
-# ifdef __LDBL_MAX__
-#  define AG_LDBL_MAX __LDBL_MAX__
+# ifdef __FLT_MAX__
+#  define AG_FLT_MAX __FLT_MAX__
 # else
-#  define AG_LDBL_MAX 1.18973149535723176502e+4932l
+#  define AG_FLT_MAX 3.402823466e+38f
 # endif
-#endif /* AG_HAVE_LONG_DOUBLE */
+# ifdef __DBL_MIN__
+#  define AG_DBL_MIN __DBL_MIN__
+# else
+#  define AG_DBL_MIN 2.2250738585072014e-308
+# endif
+# ifdef __DBL_MAX__
+#  define AG_DBL_MAX __DBL_MAX__
+# else
+#  define AG_DBL_MAX 1.7976931348623158e+308
+# endif
+# ifdef AG_HAVE_LONG_DOUBLE
+#  ifdef __LDBL_MIN__
+#   define AG_LDBL_MIN __LDBL_MIN__
+#  else
+#   define AG_LDBL_MIN 3.36210314311209350626e-4932l
+#  endif
+#  ifdef __LDBL_MAX__
+#   define AG_LDBL_MAX __LDBL_MAX__
+#  else
+#   define AG_LDBL_MAX 1.18973149535723176502e+4932l
+#  endif
+# endif /* AG_HAVE_LONG_DOUBLE */
 
-#ifdef _MK_HAVE_FLOAT_H
-# include <float.h>
-# define AG_FLT_EPSILON FLT_EPSILON
-# define AG_DBL_EPSILON DBL_EPSILON
-# ifdef AG_HAVE_LONG_DOUBLE
-#  define AG_LDBL_EPSILON LDBL_EPSILON
-# endif
-#else /* !_MK_HAVE_FLOAT_H */
-# define AG_FLT_EPSILON 1.192092896e-7f
-# define AG_DBL_EPSILON 2.2204460492503131e-16
-# ifdef AG_HAVE_LONG_DOUBLE
-#  define AG_LDBL_EPSILON 1.08420217248550443e-19l
-# endif
-#endif /* _MK_HAVE_FLOAT_H */
+# ifdef _MK_HAVE_FLOAT_H
+#  include <float.h>
+#  define AG_FLT_EPSILON FLT_EPSILON
+#  define AG_DBL_EPSILON DBL_EPSILON
+#  ifdef AG_HAVE_LONG_DOUBLE
+#   define AG_LDBL_EPSILON LDBL_EPSILON
+#  endif
+# else /* !HAVE_FLOAT_H */
+#  define AG_FLT_EPSILON 1.192092896e-7f
+#  define AG_DBL_EPSILON 2.2204460492503131e-16
+#  ifdef AG_HAVE_LONG_DOUBLE
+#   define AG_LDBL_EPSILON 1.08420217248550443e-19l
+#  endif
+# endif /* !HAVE_FLOAT_H */
+
+#endif /* AG_HAVE_FLOAT */
 
 #endif /* _AGAR_CORE_LIMITS_H_ */
--- core/types.h
+++ core/types.h
@@ -1,42 +1,55 @@
 /*	Public domain	*/
 
 /*
- * Definitions of primitive integer types used throughout Agar. Also
- * defines AG_HAVE_64BIT and AG_HAVE_LONG_DOUBLE.
+ * Primitive integer data types used by Agar.
+ *
+ * Defines public AG_HAVE_64BIT, AG_HAVE_FLOAT and AG_HAVE_LONG_DOUBLE.
  */
 
-#include <agar/config/have_cygwin.h>
+/* Agar memory model */
+#define AG_SMALL  16  /* 8- and 16-bit CPU with KBs of RAM, 4- or 8-bit color */
+#define AG_MEDIUM 32  /* 32- or 64-bit CPU with MBs of RAM, 8-bit color */
+#define AG_LARGE  64  /* 32- or 64-bit CPU with GBs of RAM, 16-bit color */
+#include <agar/config/ag_model.h>
 
+#include <agar/config/have_cygwin.h>
 #if defined(HAVE_CYGWIN)
 # include <basetyps.h>
 #else
 # if !defined(_WIN32)
 #  include <agar/config/_mk_have_sys_types_h.h>
-#  ifdef _MK_HAVE_SYS_TYPES_H
+#  include <agar/config/_mk_have_stdint_h.h>
+#  if defined(_MK_HAVE_SYS_TYPES_H)
 #   undef _MK_HAVE_SYS_TYPES_H
 #   include <sys/types.h>
+#  elif defined(_MK_HAVE_STDINT_H)
+#   include <stdint.h>
 #  endif
 # endif /* !_WIN32 */
 #endif /* HAVE_CYGWIN */
 
-#ifndef _AGAR_HAVE_64BIT_H
-# include <agar/config/have_64bit.h>
-# define _AGAR_HAVE_64BIT_H_
-# ifdef HAVE_64BIT
-#  include <agar/config/have_int64_t.h>
-#  include <agar/config/have___int64.h>
-#  define AG_HAVE_64BIT
-# endif
+#include <agar/config/have_64bit.h>
+#ifdef HAVE_64BIT
+# include <agar/config/have_int64_t.h>
+# include <agar/config/have___int64.h>
+# define AG_HAVE_64BIT "yes"
 #endif
 
-#ifndef _AGAR_HAVE_LONG_DOUBLE_H_
-# include <agar/config/have_long_double.h>
-# define _AGAR_HAVE_LONG_DOUBLE_H_
-# ifdef HAVE_LONG_DOUBLE
-#  define AG_HAVE_LONG_DOUBLE
-# endif
+/*
+ * Floating Point Types
+ */
+#include <agar/config/have_float.h>
+#ifdef HAVE_FLOAT
+# define AG_HAVE_FLOAT "yes"
+#endif
+#include <agar/config/have_long_double.h>
+#ifdef HAVE_LONG_DOUBLE
+# define AG_HAVE_LONG_DOUBLE "yes"
 #endif
 
+/*
+ * C Integer Types
+ */
 #ifndef Uint
 # define Uint unsigned int
 # define _AGAR_CORE_DEFINED_UINT
@@ -50,46 +63,16 @@
 # define _AGAR_CORE_DEFINED_ULONG
 #endif
 
-#if defined(_WIN32)
-# ifndef Sint8
-#  define Sint8 __int8
-#  define _AGAR_CORE_DEFINED_SINT8
-# endif
-# ifndef Uint8
-#  define Uint8 unsigned __int8
-#  define _AGAR_CORE_DEFINED_UINT8
-# endif
-# ifndef Sint16
-#  define Sint16 __int16
-#  define _AGAR_CORE_DEFINED_SINT16
-# endif
-# ifndef Uint16
-#  define Uint16 unsigned __int16
-#  define _AGAR_CORE_DEFINED_UINT16
-# endif
-# ifndef Sint32
-#  define Sint32 __int32
-#  define _AGAR_CORE_DEFINED_SINT32
-# endif
-# ifndef Uint32
-#  define Uint32 unsigned __int32
-#  define _AGAR_CORE_DEFINED_UINT32
-# endif
-# ifndef Sint64
-#  define Sint64 __int64
-#  define _AGAR_CORE_DEFINED_SINT64
-# endif
-# ifndef Uint64
-#  define Uint64 unsigned __int64
-#  define _AGAR_CORE_DEFINED_UINT64
-# endif
-#else /* !_WIN32 */
+/*
+ * Fixed-Size Integer Types
+ */
+#if (AG_MODEL == AG_SMALL)
 # ifndef Sint8
 #  define Sint8 int8_t
 #  define _AGAR_CORE_DEFINED_SINT8
 # endif
 # ifndef Uint8
-#  define Uint8 u_int8_t
+#  define Uint8 uint8_t
 #  define _AGAR_CORE_DEFINED_UINT8
 # endif
 # ifndef Sint16
@@ -97,7 +80,7 @@
 #  define _AGAR_CORE_DEFINED_SINT16
 # endif
 # ifndef Uint16
-#  define Uint16 u_int16_t
+#  define Uint16 uint16_t
 #  define _AGAR_CORE_DEFINED_UINT16
 # endif
 # ifndef Sint32
@@ -105,35 +88,124 @@
 #  define _AGAR_CORE_DEFINED_SINT32
 # endif
 # ifndef Uint32
-#  define Uint32 u_int32_t
+#  define Uint32 uint32_t
 #  define _AGAR_CORE_DEFINED_UINT32
 # endif
-# ifdef AG_HAVE_64BIT
+
+#else /* MEDIUM or LARGE */
+
+# if defined(_WIN32)
+#  ifndef Sint8
+#   define Sint8 __int8
+#   define _AGAR_CORE_DEFINED_SINT8
+#  endif
+#  ifndef Uint8
+#   define Uint8 unsigned __int8
+#   define _AGAR_CORE_DEFINED_UINT8
+#  endif
+#  ifndef Sint16
+#   define Sint16 __int16
+#   define _AGAR_CORE_DEFINED_SINT16
+#  endif
+#  ifndef Uint16
+#   define Uint16 unsigned __int16
+#   define _AGAR_CORE_DEFINED_UINT16
+#  endif
+#  ifndef Sint32
+#   define Sint32 __int32
+#   define _AGAR_CORE_DEFINED_SINT32
+#  endif
+#  ifndef Uint32
+#   define Uint32 unsigned __int32
+#   define _AGAR_CORE_DEFINED_UINT32
+#  endif
 #  ifndef Sint64
-#   if defined(HAVE_INT64_T)
-#    define Sint64 int64_t
-#   elif defined(HAVE___INT64)
-#    define Sint64 __int64
-#   endif
+#   define Sint64 __int64
 #   define _AGAR_CORE_DEFINED_SINT64
 #  endif
 #  ifndef Uint64
-#   if defined(HAVE_INT64_T)
-#    define Uint64 u_int64_t
-#   elif defined(HAVE___INT64)
-#    define Uint64 __int64
+#   define Uint64 unsigned __int64
+#   define _AGAR_CORE_DEFINED_UINT64
+#  endif
+#  define HAVE_64BIT "yes"
+#  define AG_HAVE_64BIT "yes"
+
+# else /* !_WIN32 */
+
+#  ifndef Sint8
+#   define Sint8 int8_t
+#   define _AGAR_CORE_DEFINED_SINT8
+#  endif
+#  ifndef Uint8
+#   define Uint8 u_int8_t
+#   define _AGAR_CORE_DEFINED_UINT8
+#  endif
+#  ifndef Sint16
+#   define Sint16 int16_t
+#   define _AGAR_CORE_DEFINED_SINT16
+#  endif
+#  ifndef Uint16
+#   define Uint16 u_int16_t
+#   define _AGAR_CORE_DEFINED_UINT16
+#  endif
+#  ifndef Sint32
+#   define Sint32 int32_t
+#   define _AGAR_CORE_DEFINED_SINT32
+#  endif
+#  ifndef Uint32
+#   define Uint32 u_int32_t
+#   define _AGAR_CORE_DEFINED_UINT32
+#  endif
+#  ifdef AG_HAVE_64BIT
+#   ifndef Sint64
+#    if defined(HAVE_INT64_T)
+#     define Sint64 int64_t
+#     define _AGAR_CORE_DEFINED_SINT64
+#    elif defined(HAVE___INT64)
+#     define Sint64 __int64
+#     define _AGAR_CORE_DEFINED_SINT64
+#    else
+#     error "No Sint64 type"
+#    endif
 #   endif
-#   define _AGAR_CORE_DEFINED_UINT64
-#  endif
-# else /* !AG_HAVE_64BIT */
-typedef struct ag_fake_int64 { Uint32 _pad1; Uint32 _pad2; };
-#  ifndef Sint64
-#   define Sint64 struct ag_fake_int64
-#   define _AGAR_CORE_DEFINED_SINT64
-#  endif
-#  ifndef Uint64
-#   define Uint64 struct ag_fake_int64
-#   define _AGAR_CORE_DEFINED_UINT64
-#  endif
-# endif /* AG_HAVE_64BIT */
-#endif /* !WIN32 */
+#   ifndef Uint64
+#    if defined(HAVE_INT64_T)
+#     define Uint64 u_int64_t
+#     define _AGAR_CORE_DEFINED_UINT64
+#    elif defined(HAVE___INT64)
+#     define Uint64 __int64
+#     define _AGAR_CORE_DEFINED_UINT64
+#    else
+#     error "No Uint64 type"
+#    endif
+#   endif
+#  else
+#   warning "64-bit types not supported"
+#  endif /* AG_HAVE_64BIT */
+
+# endif /* !WIN32 */
+
+#endif /* MEDIUM or LARGE */
+
+/*
+ * Size and Offset Types
+ */
+#if AG_MODEL == AG_SMALL
+# define AG_Size     Uint16
+# define AG_Offset   Sint16
+# define AG_SIZE_MAX 0xffff
+# define AG_OFFS_MAX 0x7fff
+#elif AG_MODEL == AG_MEDIUM
+# define AG_Size     Uint32
+# define AG_Offset   Sint32
+# define AG_SIZE_MAX 0xffffffff
+# define AG_OFFS_MAX 0x7fffffff
+#elif AG_MODEL == AG_LARGE
+# define AG_Size     Uint64
+# define AG_Offset   Sint64
+# define AG_SIZE_MAX 0xffffffffffffffff
+# define AG_OFFS_MAX 0x7fffffffffffffff
+#else
+# error "Invalid AG_MODEL"
+#endif
+
