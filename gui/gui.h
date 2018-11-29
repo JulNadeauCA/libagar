31735a423db11278751131cc1b6da88cdc033b56
Julien Nadeau Carriere <vedge@hypertriton.com>
2018-11-29 04:09:37 +0000

    move old AG_InitVideo() flags -> AG_LEGACY section.
    git-svn-id: https://dev.csoft.net/agar/trunk@10056 86a84d31-9702-0410-8f1e-a60cab7bd424

--- gui/gui.h
+++ gui/gui.h
@@ -9,24 +9,7 @@
 #define AG_ZOOM_RANGE	14		/* Number of default zoom settings */
 #define AG_ZOOM_DEFAULT	5		/* Initial zoom setting (1.0) */
 
-/* For old AG_InitVideo() interface */
-#define AG_VIDEO_HWSURFACE     0x0001
-#define AG_VIDEO_ASYNCBLIT     0x0002
-#define AG_VIDEO_ANYFORMAT     0x0004
-#define AG_VIDEO_HWPALETTE     0x0008
-#define AG_VIDEO_DOUBLEBUF     0x0010
-#define AG_VIDEO_FULLSCREEN    0x0020
-#define AG_VIDEO_RESIZABLE     0x0040
-#define AG_VIDEO_NOFRAME       0x0080
-#define AG_VIDEO_BGPOPUPMENU   0x0100
-#define AG_VIDEO_OPENGL	       0x0200
-#define AG_VIDEO_OPENGL_OR_SDL 0x0400
-#define AG_VIDEO_NOBGCLEAR     0x0800
-#define AG_VIDEO_OVERLAY       0x1000
-#define AG_VIDEO_SDL           0x2000
-
 __BEGIN_DECLS
-/* Global configuration settings (bound to agConfig) */
 extern int agGUI, agRenderingContext, agStereo, agXsync;
 extern int agKbdDelay, agKbdRepeat;
 extern int agMouseDblclickDelay, agMouseSpinDelay, agMouseSpinIval,
@@ -57,6 +40,20 @@ int   AG_SetVideoSurfaceSDL(void *_Nonnull);
 #ifdef AG_LEGACY
 int  AG_InitVideo(int,int, int, Uint) DEPRECATED_ATTRIBUTE;
 void AG_DestroyVideo(void) DEPRECATED_ATTRIBUTE;
+#define AG_VIDEO_HWSURFACE     0x0001
+#define AG_VIDEO_ASYNCBLIT     0x0002
+#define AG_VIDEO_ANYFORMAT     0x0004
+#define AG_VIDEO_HWPALETTE     0x0008
+#define AG_VIDEO_DOUBLEBUF     0x0010
+#define AG_VIDEO_FULLSCREEN    0x0020
+#define AG_VIDEO_RESIZABLE     0x0040
+#define AG_VIDEO_NOFRAME       0x0080
+#define AG_VIDEO_BGPOPUPMENU   0x0100
+#define AG_VIDEO_OPENGL	       0x0200
+#define AG_VIDEO_OPENGL_OR_SDL 0x0400
+#define AG_VIDEO_NOBGCLEAR     0x0800
+#define AG_VIDEO_OVERLAY       0x1000
+#define AG_VIDEO_SDL           0x2000
 #endif
 __END_DECLS
 
