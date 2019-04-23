f0c8520fe4207e8aea72ec8bf962f2c76a196999
Julien Nadeau Carriere <vedge@hypertriton.com>
2019-04-23 12:29:10 +0000

    Work around compilers with preprocessors that can't handle __has_feature()
    git-svn-id: https://dev.csoft.net/agar/trunk@10105 86a84d31-9702-0410-8f1e-a60cab7bd424

--- gui/begin.h
+++ gui/begin.h
@@ -148,22 +148,12 @@
 # endif
 #endif
 #if defined(__GNUC__) || defined(__CC65__)
-# if !defined(_Nonnull) || !defined(_Nullable) || !defined(_Null_unspecified)
-#  define _Nonnull
-#  define _Nullable
-#  define _Null_unspecified
-#  define _AGAR_GUI_DEFINED_NULLABILITY
-# endif
+# define _Nonnull
+# define _Nullable
+# define _Null_unspecified
+# define _AGAR_GUI_DEFINED_NULLABILITY
 #else
-# ifndef __has_feature
-# define __has_feature(x) 0
-# endif
-# if !(defined(__clang__) && __has_feature(nullability))
-#  define _Nonnull
-#  define _Nullable
-#  define _Null_unspecified
-#  define _AGAR_GUI_DEFINED_NULLABILITY
-# endif
+# include <agar/gui/nullability.h>
 #endif
 
 /* Restrict */
