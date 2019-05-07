7fd14ad851d17c6d0ba9337a8ef5924cd68d581e
Julien Nadeau Carriere <vedge@hypertriton.com>
2019-05-07 05:50:54 +0000

    Make this example more useful
    git-svn-id: https://dev.csoft.net/agar/trunk@10125 86a84d31-9702-0410-8f1e-a60cab7bd424

--- core/dummy_object.h
+++ core/dummy_object.h
@@ -3,12 +3,21 @@
  * Definition of an example Agar object class.
  */
 
+/* Dummy object instance */
 typedef struct ag_dummy {
-	struct ag_object _inherit;
-	int foo;
+	struct ag_object _inherit;	/* AG_Object -> AG_Dummy */
+
+	Uint flags;
+#define AG_DUMMY_OPTION1 0x01		/* Some option */
+#define AG_DUMMY_OPTION2 0x02		/* Some other option */
+#define AG_DUMMY_TMPFLAG 0x04		/* Some non-persistent flag */
+#define AG_DUMMY_SAVED   (AG_DUMMY_OPTION1 | AG_DUMMY_OPTION2)
+
+	int x,y;			/* Some integers */
+	void *myData;			/* Some dynamically allocated data */
+	/* ... */
 } AG_Dummy;
 
-extern AG_ObjectClass agDummyClass;
+extern AG_ObjectClass agDummyClass;	/* Class description */
 
-AG_Dummy *_Nullable AG_DummyNew(void);
-
+AG_Dummy *_Nullable AG_DummyNew(int,int, Uint);		/* Optional */
