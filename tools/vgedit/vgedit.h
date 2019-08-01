eb308078a84a1eaab15677c01ef9bc280c57de09
Julien Nadeau Carriere <vedge@hypertriton.com>
2019-08-01 11:01:39 +0000

    Introduce vgedit, a very basic editor for VG(3) object files.
    The tool is also a good demonstration of VG_View(3) usage.
    
    TODO: Compile VG drawings into display lists (AG_VectorElement arrays).
    TODO: Subclass VG for proper native handling of SVG and DXF.
    
    
    
    git-svn-id: https://dev.csoft.net/agar/trunk@10355 86a84d31-9702-0410-8f1e-a60cab7bd424

---
+++ tools/vgedit/vgedit.h
@@ -0,0 +1,19 @@
+/*	Public domain	*/
+
+#ifndef _VGEDIT_H_
+#define _VGEDIT_H_
+
+#include "config/enable_nls.h"
+#ifdef ENABLE_NLS
+# include <libintl.h>
+# define _(String) gettext(String)
+# define gettext_noop(String) (String)
+# define N_(String) gettext_noop(String)
+#else
+# undef _
+# undef N_
+# define _(s) (s)
+# define N_(s) (s)
+#endif
+
+#endif /* _VGEDIT_H_ */
