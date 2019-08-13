e73f9da3d047c9db151c70991be4aa866a34f175
Julien Nadeau Carriere <vedge@hypertriton.com>
2019-08-13 18:28:45 +0000

    Try the object system and event delivery.
    git-svn-id: https://dev.csoft.net/agar/trunk@10397 86a84d31-9702-0410-8f1e-a60cab7bd424

--- tests/c64hello/hello.c
+++ tests/c64hello/hello.c
@@ -1,8 +1,26 @@
 /*
- * Fancy hello world program using cc65.
+ * Copyright (c) 2019 Julien Nadeau Carriere <vedge@csoft.net>
+ * All rights reserved.
  *
- * Ullrich von Bassewitz (ullrich@von-bassewitz.de)
- *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <stdlib.h>
@@ -12,77 +30,86 @@
 
 #include <agar/core.h>
 
-
-/*****************************************************************************/
-/*     	      	    	  	     Data	     			     */
-/*****************************************************************************/
-
-static const char WebsiteURL [] = "http://libagar.org";
-static const char HelloText [] = "Hello world!";
-
-/*****************************************************************************/
-/*     	      	    	  	     Code	     			     */
-/*****************************************************************************/
-
-
-
-int main (void)
+/*
+ * Register an example Agar object class.
+ */
+static void
+MyClass_SayHello(AG_Event *event)
 {
-    unsigned char XSize, YSize;
-    AG_AgarVersion v;
+	AG_Object *obj = AG_OBJECT_SELF();
+	const char *thing = AG_STRING(1);
 
-    AG_GetVersion(&v);
-
-    /* Set screen colors, hide the cursor */
-    textcolor (COLOR_WHITE);
-    bordercolor (COLOR_BLACK);
-    bgcolor (COLOR_BLACK);
-    cursor (0);
-
-    /* Clear the screen, put cursor in upper left corner */
-    clrscr ();
-
-    /* Ask for the screen size */
-    screensize (&XSize, &YSize);
-
-    /* Draw a border around the screen */
-
-    /* Top line */
-    cputc (CH_ULCORNER);
-    chline (XSize - 2);
-    cputc (CH_URCORNER);
-
-    /* Vertical line, left side */
-    cvlinexy (0, 1, YSize - 2);
-
-    /* Bottom line */
-    cputc (CH_LLCORNER);
-    chline (XSize - 2);
-    cputc (CH_LRCORNER);
-
-    /* Vertical line, right side */
-    cvlinexy (XSize - 1, 1, YSize - 2);
-
-    /* Write the Agar version in the mid of the screen */
-    gotoxy ((XSize - strlen ("Agar X.X.X")) / 2, YSize/2 - 1);
-    textcolor(5);
-    cprintf ("Agar %d.%d.%d", v.major, v.minor, v.patch);
-    gotoxy ((XSize - strlen (v.release) - 1) / 2, wherey()+1);
-    textcolor(2);
-    cprintf ("\"%s\"", v.release);
-    gotoxy ((XSize - strlen (WebsiteURL)) / 2, wherey()+1);
-    textcolor(3);
-    cprintf ("%s", WebsiteURL);
-    gotoxy ((XSize - strlen (HelloText)) / 2, wherey()+1);
-    textcolor(4);
-    cprintf ("%s", HelloText);
-
-    /* Wait for the user to press a key */
-    (void) cgetc ();
-
-    /* Clear the screen again */
-    clrscr ();
-
-    /* Done */
-    return EXIT_SUCCESS;
+	AG_Debug(obj, "Hello, %s!\n", thing);
 }
+static void
+MyClass_Init(void *pObj)
+{
+	AG_Object *obj = pObj;
+	int mag;
+
+	AG_SetInt(obj, "magic", 30000);
+	if ((mag = AG_GetInt(obj,"magic")) == 30000) {
+		AG_Verbose("Variables OK! (magic=%d)", mag);
+	} else {
+		AG_Verbose("Variables don't work (%d!=30000)", mag);
+	}
+	AG_SetEvent(obj, "greet", MyClass_SayHello, NULL);
+}
+static void
+MyClass_Destroy(void *pObj)
+{
+	AG_Object *obj = pObj;
+
+	AG_Verbose("Instance of MyClass finalizing");
+}
+static AG_ObjectClass myClass = {
+	"My_Class",
+	sizeof(AG_Object),
+	{ 1,0 },
+	MyClass_Init,
+	NULL,			/* reset */
+	MyClass_Destroy,
+	NULL,			/* load */
+	NULL,			/* save */
+	NULL			/* edit */
+};
+
+int
+main(void)
+{
+	unsigned char XSize, YSize;
+	AG_Object *myObj;
+	
+	textcolor(COLOR_WHITE);
+	bordercolor(COLOR_BLACK);
+	bgcolor(COLOR_BLACK);
+	cursor(0);
+	clrscr();
+
+	_heapadd((void *)0x0803, 0x17fd);
+/*	_heapadd((void *)0x0400, 0x0400); */
+
+	if (AG_InitCore(NULL, AG_VERBOSE) == -1) {
+		return EXIT_FAILURE;
+	}
+	AG_Verbose("Agar OK (%d bytes free)", _heapmemavail());
+
+	/* Register a new Agar object class. */
+	AG_RegisterClass(&myClass);
+
+	/* Create an instance of our new class. */
+	if ((myObj = AG_ObjectNew(NULL, "myobj1", &myClass)) == NULL) {
+		AG_FatalError("ObjectNew");
+	}
+	AG_Verbose("%s lives at %p", myObj->name, myObj);
+
+	/* Test event delivery. */
+	AG_PostEvent(NULL, myObj, "greet", "%s", "world");
+
+ 	/* Clear the screen again */
+//	clrscr ();
+
+	/* Done */
+	return EXIT_SUCCESS;
+}
+
