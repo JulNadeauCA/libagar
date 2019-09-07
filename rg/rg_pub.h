793913a11842461afb844cfd863b8292a0bb00ff
Julien Nadeau Carriere <vedge@hypertriton.com>
2019-09-07 12:12:45 +0000

    core_begin.h is no longer needed.
    git-svn-id: https://dev.csoft.net/agar/trunk@10480 86a84d31-9702-0410-8f1e-a60cab7bd424

--- rg/rg_pub.h
+++ rg/rg_pub.h
@@ -3,9 +3,11 @@
 #ifndef _AGAR_RG_PUBLIC_H_
 #define _AGAR_RG_PUBLIC_H_
 #define _AGAR_RG_PUBLIC
-#include <agar/core/core_begin.h>
+
+#include <agar/core/core.h>
+
 #include <agar/rg/tileset.h>
 #include <agar/rg/tileview.h>
 #include <agar/rg/icons.h>
-#include <agar/core/core_close.h>
-#endif
+
+#endif /* _AGAR_RG_PUBLIC_H_ */
