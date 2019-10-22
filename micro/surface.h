4ed8c5cf9787b12c5af47d9901dfb33395203dde
Julien Nadeau Carriere <vedge@hypertriton.com>
2019-10-22 09:06:02 +0000

    Fix #defines
    git-svn-id: https://dev.csoft.net/agar/trunk@10534 86a84d31-9702-0410-8f1e-a60cab7bd424

--- micro/surface.h
+++ micro/surface.h
@@ -81,49 +81,31 @@ MA_Surface *_Nonnull MA_SurfaceDup(const MA_Surface *_Nonnull)
 void                 MA_SurfaceCopy(MA_Surface *_Nonnull,
                                     const MA_Surface *_Nonnull);
 
-void MA_SurfaceBlit(const MA_Surface *_Nonnull, const MA_Rect *_Nullable,
-		    MA_Surface *_Nonnull, Uint8,Uint8);
+void   MA_SurfaceBlit(const MA_Surface *_Nonnull, const MA_Rect *_Nullable,
+                      MA_Surface *_Nonnull, Uint8,Uint8);
 
-void MA_SurfaceFree(MA_Surface *_Nonnull);
+void   MA_SurfaceFree(MA_Surface *_Nonnull);
 
-void MA_FillRect(MA_Surface *_Nonnull, const MA_Rect *_Nullable,
-                 const MA_Color *_Nonnull);
+void   MA_FillRect(MA_Surface *_Nonnull, const MA_Rect *_Nullable,
+                   const MA_Color *_Nonnull);
 
 Uint8  MA_SurfaceGet8(const MA_Surface *_Nonnull, Uint8,Uint8)
                      _Pure_Attribute;
 void   MA_SurfacePut8(MA_Surface *_Nonnull, Uint8,Uint8, Uint8);
 
-Uint16 AG_MapPixel16_RGBA4(const MA_PixelFormat *_Nonnull, Uint8,Uint8,Uint8,Uint8)
+Uint16 MA_MapPixel16_RGBA4(const MA_PixelFormat *_Nonnull, Uint8,Uint8,Uint8,Uint8)
                           _Pure_Attribute;
-void   AG_GetColor16_RGBA4(Uint16, const MA_PixelFormat *_Nonnull,
+void   MA_GetColor16_RGBA4(Uint16, const MA_PixelFormat *_Nonnull,
                            Uint8 *_Nonnull, Uint8 *_Nonnull,
                            Uint8 *_Nonnull, Uint8 *_Nonnull);
-void   AG_GetColor16(MA_Color *_Nonnull, Uint16, const MA_PixelFormat *_Nonnull);
+void   MA_GetColor16(MA_Color *_Nonnull, Uint16, const MA_PixelFormat *_Nonnull);
 
-# define MA_SurfaceGet(S,x,y)                 MA_SurfaceGet32((S),(x),(y))
-# define MA_SurfaceGet_At(S,p)                MA_SurfaceGet32_At((S),(p))
-# define MA_GetColor(c,px,pf)                 MA_GetColor32((c),(px),(pf))
-# define MA_SurfacePut(S,x,y,px)              MA_SurfacePut32((S),(x),(y),(px))
-# define MA_SurfacePut_At(S,p,px)             MA_SurfacePut32_At((S),(p),(px))
-# define MA_MapPixel(pf,px)                   MA_MapPixel32((pf),(px))
-# define MA_MapPixel_RGB(pf,r,g,b)            MA_MapPixel32_RGB8((pf),(r),(g),(b))
-# define MA_MapPixel_RGBA(pf,r,g,b,a)         MA_MapPixel32_RGBA8((pf),(r),(g),(b),(a))
-# define MA_MapPixel_RGB8(pf,r,g,b)           MA_MapPixel32_RGB8((pf),(r),(g),(b))
-# define MA_MapPixel_RGBA8(pf,r,g,b,a)        MA_MapPixel32_RGBA8((pf),(r),(g),(b),(a))
-# define MA_MapPixel_RGB16(pf,r,g,b)          MA_MapPixel32_RGB16((pf),(r),(g),(b))
-# define MA_MapPixel_RGBA16(pf,r,g,b,a)       MA_MapPixel32_RGBA16((pf),(r),(g),(b),(a))
-# define MA_SurfaceBlendRGB(s,x,y,r,g,b,a,fn) MA_SurfaceBlendRGB8((s),(x),(y),(r),(g),(b),(a),(fn))
-# define MA_GetColor_RGB8(px,pf,r,g,b)        MA_GetColor32_RGB8((px),(pf),(r),(g),(b))
-# define MA_GetColor_RGBA8(px,pf,r,g,b,a)     MA_GetColor32_RGBA8((px),(pf),(r),(g),(b),(a))
-# define MA_GetColor_RGB16(px,pf,r,g,b)       MA_GetColor32_RGB16((px),(pf),(r),(g),(b))
-# define MA_GetColor_RGBA16(px,pf,r,g,b,a)    MA_GetColor32_RGBA16((px),(pf),(r),(g),(b),(a))
+Sint8  MA_PixelFormatCompare(const MA_PixelFormat *_Nonnull,
+                             const MA_PixelFormat *_Nonnull)
+                            _Pure_Attribute;
 
-Sint8 MA_PixelFormatCompare(const MA_PixelFormat *_Nonnull,
-                            const MA_PixelFormat *_Nonnull)
-			   _Pure_Attribute;
-
-Uint32 ag_map_pixel32(const MA_PixelFormat *_Nonnull, const MA_Color *_Nonnull)
-                     _Pure_Attribute;
+Uint16 MA_MapPixel16(const MA_PixelFormat *_Nonnull, const MA_Color *_Nonnull)
+                    _Pure_Attribute;
 
 Uint16 MA_SurfaceGet16_At(const MA_Surface *_Nonnull, const Uint8 *_Nonnull)
                          _Pure_Attribute;
@@ -133,10 +115,23 @@ Uint16 MA_SurfaceGet16(const MA_Surface *_Nonnull, Uint8,Uint8)
 void   MA_SurfacePut16(MA_Surface *_Nonnull, Uint8,Uint8, Uint16);
 void   MA_SurfacePut16_At(MA_Surface *_Nonnull, Uint8 *_Nonnull, Uint16);
 
-void MA_SurfaceBlend(MA_Surface *_Nonnull, Uint8,Uint8, const MA_Color *_Nonnull);
-void MA_SurfaceBlendAt(MA_Surface *_Nonnull, Uint8 *_Nonnull,
-                       const MA_Color *_Nonnull);
+void   MA_SurfaceBlend(MA_Surface *_Nonnull, Uint8,Uint8, const MA_Color *_Nonnull);
+void   MA_SurfaceBlendAt(MA_Surface *_Nonnull, Uint8 *_Nonnull,
+                         const MA_Color *_Nonnull);
 
+#define MA_SurfaceGet(S,x,y)              MA_SurfaceGet16((S),(x),(y))
+#define MA_SurfaceGet_At(S,p)             MA_SurfaceGet16_At((S),(p))
+#define MA_GetColor(c,px,pf)              MA_GetColor16((c),(px),(pf))
+#define MA_SurfacePut(S,x,y,px)           MA_SurfacePut16((S),(x),(y),(px))
+#define MA_SurfacePut_At(S,p,px)          MA_SurfacePut16_At((S),(p),(px))
+#define MA_MapPixel(pf,px)                MA_MapPixel16((pf),(px))
+#define MA_MapPixel_RGB(pf,r,g,b)         MA_MapPixel16_RGBA4((pf),(r),(g),(b),0xf)
+#define MA_MapPixel_RGBA(pf,r,g,b,a)      MA_MapPixel16_RGBA4((pf),(r),(g),(b),(a))
+#define MA_MapPixel_RGB4(pf,r,g,b)        MA_MapPixel16_RGBA4((pf),(r),(g),(b),0xf)
+#define MA_MapPixel_RGBA4(pf,r,g,b,a)     MA_MapPixel16_RGBA4((pf),(r),(g),(b),(a))
+#define MA_SurfaceBlendRGB(s,x,y,r,g,b,a) MA_SurfaceBlendRGB4((s),(x),(y),(r),(g),(b),(a))
+#define MA_GetColor_RGB8(px,pf,r,g,b)     MA_GetColor16_RGB4((px),(pf),(r),(g),(b))
+#define MA_GetColor_RGBA8(px,pf,r,g,b,a)  MA_GetColor16_RGBA4((px),(pf),(r),(g),(b),(a))
 __END_DECLS
 
 #include <agar/micro/close.h>
