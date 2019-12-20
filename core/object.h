91782c49d9fa2ae178b0fbb0f32c46a78e2e6375
Julien Nadeau Carriere <vedge@hypertriton.com>
2019-12-20 15:49:39 +0000

    Merge legacy_object.h into object.h.
    git-svn-id: https://dev.csoft.net/agar/trunk@10610 86a84d31-9702-0410-8f1e-a60cab7bd424

--- core/object.h
+++ core/object.h
@@ -531,6 +531,27 @@ void ag_unlock_timers(void *_Nullable);
 #endif /* !AG_INLINE_OBJECT */
 
 #ifdef AG_LEGACY
+/* Calls renamed for consistency in 1.6.0 */
+# define AG_ObjectFreeDataset(o)     AG_ObjectReset(o)
+# define AG_ObjectIsClass(o,c)       AG_OfClass((o),(c))
+# define AG_GetStringCopy(o,n,b,s)   AG_GetString((o),(n),(b),(s))
+# define AG_PrtString                AG_SetStringF
+/* Redundant const pointer types removed from AG_Variable in 1.6.0 */
+# define AG_VARIABLE_CONST_STRING	AG_VARIABLE_STRING
+# define AG_VARIABLE_P_CONST_STRING	AG_VARIABLE_P_STRING
+# define AG_VARIABLE_CONST_POINTER	AG_VARIABLE_POINTER
+# define AG_VARIABLE_P_CONST_POINTER	AG_VARIABLE_P_POINTER
+# define AG_SetConstString(o,n,v)	AG_SetString((o),(n),(char *)(v))
+# define AG_BindConstString		AG_BindString
+# define AG_BindConstStringMp		AG_BindStringMp
+# define AG_BindConstStringFn		AG_BindStringFn
+# define AG_GetConstPointer(o,x)	((const void *)AG_GetPointer((o),(x)))
+# define AG_SetConstPointer(o,n,v)	AG_SetPointer((o),(n),(void *)(v))
+# define AG_InitConstPointer(var,v)	AG_InitPointer((var),(void *)(v))
+# define AG_BindConstPointer		AG_BindPointer
+# define AG_BindConstPointerFn		AG_BindPointerFn
+# define AG_BindConstPointerMp		AG_BindPointerMp
+
 void AG_ObjectInitStatic(void *_Nonnull _Restrict, void *_Nullable _Restrict)
                         DEPRECATED_ATTRIBUTE;
 void AG_ObjectInitNamed(void *_Nonnull _Restrict, void *_Nonnull _Restrict,
@@ -538,8 +559,6 @@ void AG_ObjectInitNamed(void *_Nonnull _Restrict, void *_Nonnull _Restrict,
                        DEPRECATED_ATTRIBUTE;
 #endif /* AG_LEGACY */
 __END_DECLS
-#ifdef AG_LEGACY
-#include <agar/core/legacy_object.h>
-#endif
+
 #include <agar/core/close.h>
 #endif /* _AGAR_CORE_OBJECT_H_ */
