eeb4eaf6afaa142a995e3e5c4f8373bb0e56d463
Julien Nadeau Carriere <vedge@hypertriton.com>
2020-04-22 06:33:33 +0000

    tri:regen
    git-svn-id: https://dev.csoft.net/agar/trunk@10874 86a84d31-9702-0410-8f1e-a60cab7bd424

--- mk/build.lib.mk
+++ mk/build.lib.mk
@@ -377,13 +377,8 @@ lib${LIB}.so: ${SRCS_GENERATED} _lib_objs ${OBJS}
 	    \
 	    case "${HOST}" in \
 	    *-darwin*) \
-	        if [ "${HAVE_CC_CLANG}" = "yes" ]; then \
-	            echo "${CC} -shared -o $$_libout -Wl,-rpath,${PREFIX}/lib ${LDFLAGS} -dynamiclib -install_name lib${LIB}.dylib $$_objs ${LIBS}"; \
-	            ${CC} -shared -o $$_libout -Wl,-rpath ${PREFIX}/lib ${LDFLAGS} -dynamiclib -install_name lib${LIB}.dylib $$_objs ${LIBS}; \
-	        else \
-	            echo "${CC} -shared -o $$_libout -Wl,-rpath,${PREFIX}/lib ${LDFLAGS} -dynamiclib -install_name lib${LIB}.dylib $$_objs"; \
-	            ${CC} -shared -o $$_libout -Wl,-rpath ${PREFIX}/lib ${LDFLAGS} -dynamiclib -install_name lib${LIB}.dylib $$_objs; \
-	        fi; \
+	        echo "${CC} -shared -o $$_libout -Wl,-rpath,${PREFIX}/lib ${LDFLAGS} -dynamiclib -install_name lib${LIB}.dylib $$_objs ${LIBS}"; \
+	        ${CC} -shared -o $$_libout -Wl,-rpath ${PREFIX}/lib ${LDFLAGS} -dynamiclib -install_name lib${LIB}.dylib $$_objs ${LIBS}; \
 	        ;; \
 	    *-mingw*) \
 	        echo "${CC} -shared -o $$_libout -Wl,--out-implib,lib${LIB}_dll.lib -Wl,-rpath ${PREFIX}/lib ${LDFLAGS} $$_objs ${LIBS}"; \
