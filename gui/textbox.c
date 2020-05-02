98473b02705f9e2d8c17576be7152c4cd941c346
Julien Nadeau Carriere <vedge@hypertriton.com>
2020-05-02 08:10:34 +0000

    In MULTILINE (and !WORDWRAP) mode create the horizontal scrollbar in constructor.
    git-svn-id: https://dev.csoft.net/agar/trunk@10888 86a84d31-9702-0410-8f1e-a60cab7bd424

--- gui/textbox.c
+++ gui/textbox.c
@@ -102,6 +102,15 @@ AG_TextboxNewS(void *parent, Uint flags, const char *label)
 		AG_BindInt(sb, "max",     &tb->ed->yMax);
 		AG_BindInt(sb, "visible", &tb->ed->yVis);
 		AG_BindInt(sb, "value",   &tb->ed->y);
+
+		if ((flags & AG_TEXTBOX_WORDWRAP) == 0) {
+			sb = tb->hBar = AG_ScrollbarNew(tb, AG_SCROLLBAR_HORIZ,
+			                                    AG_SCROLLBAR_EXCL);
+			AG_SetInt(sb,  "min",     0);
+			AG_BindInt(sb, "max",     &tb->ed->xMax);
+			AG_BindInt(sb, "visible", &WIDTH(tb->ed));
+			AG_BindInt(sb, "value",   &tb->ed->x);
+		}
 	}
 	if (flags & AG_TEXTBOX_RETURN_BUTTON) {
 		tb->btnRet = AG_ButtonNewInt(tb, 0,
