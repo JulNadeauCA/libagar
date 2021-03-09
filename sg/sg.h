b2025c7e86a90d8bdd7b8da4e89e21416572e50a
Julien Nadeau Carriere <vedge@hypertriton.com>
2021-03-09 15:06:23 +0000

    On Apple reference <OpenGL/glu.h> not <GL/glu.h>. Thanks @WallyZ21!
    git-svn-id: https://dev.csoft.net/agar/trunk@10939 86a84d31-9702-0410-8f1e-a60cab7bd424

--- sg/sg.h
+++ sg/sg.h
@@ -10,7 +10,11 @@
 #include <agar/gui/opengl.h>
 
 #if defined(_AGAR_SG_INTERNAL) || defined(_USE_SG_GL)
-# include <GL/glu.h>
+# if defined(__APPLE__)
+#  include <OpenGL/glu.h>
+# else
+#  include <GL/glu.h>
+# endif
 #endif
 
 /* Definitions internal to the SG implementation */
