1ffa70463a62bda5050c4b6bf6b0af5b947d9ac6
Julien Nadeau Carriere <vedge@hypertriton.com>
2022-11-30 05:07:29 +0000

    Use the `CHECK_MARK` from Algue (character is not available in every font).
    git-svn-id: https://dev.csoft.net/agar/trunk@11025 86a84d31-9702-0410-8f1e-a60cab7bd424

--- gui/checkbox.c
+++ gui/checkbox.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-2020 Julien Nadeau Carriere <vedge@csoft.net>
+ * Copyright (c) 2002-2022 Julien Nadeau Carriere <vedge@csoft.net>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -331,7 +331,7 @@ Draw(void *_Nonnull obj)
 
 		if (cb->suCheckmark == -1) {
 			cb->suCheckmark = AG_WidgetMapSurface(cb,
-			    AG_TextRender(AGSI_CHECK_MARK));
+			    AG_TextRender(AGSI_ALGUE AGSI_CHECK_MARK));
 		}
 		S = WSURFACE(cb, cb->suCheckmark);
 		AG_WidgetBlitFrom(cb, cb->suCheckmark, NULL,
