eec00294ab469a42d4de4da7b1a9dba04e1b65eb
Julien Nadeau Carriere <vedge@hypertriton.com>
2024-02-02 07:09:41 +0000

    Fix cmake shared library versioning and header file installation.
    Provide a package `agarConfig.cmake` for including Agar in external projects.
    
    
    
    git-svn-id: https://dev.csoft.net/agar/trunk@11209 86a84d31-9702-0410-8f1e-a60cab7bd424

---
+++ agarConfig.cmake
@@ -0,0 +1,81 @@
+#
+# Cmake package for including the Agar library in a cmake project.
+#
+if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/ag_coreTargets.cmake")
+  include("${CMAKE_CURRENT_LIST_DIR}/ag_coreTargets.cmake")
+endif()
+if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/ag_coreStaticTargets.cmake")
+  include("${CMAKE_CURRENT_LIST_DIR}/ag_coreStaticTargets.cmake")
+endif()
+
+if(NOT TARGET ag_core::ag_core AND TARGET ag_core::ag_core-static)
+  if(CMAKE_VERSION VERSION_LESS "3.18")
+      set_target_properties(ag_core::ag_core-static PROPERTIES IMPORTED_GLOBAL TRUE)
+  endif()
+  add_library(ag_core::ag_core ALIAS ag_core::ag_core-static)
+endif()
+
+get_target_property(AGAR_CORE_INCLUDE_DIRS ag_core::ag_core INTERFACE_INCLUDE_DIRECTORIES)
+
+set(relprops
+    IMPORTED_IMPLIB_RELEASE IMPORTED_IMPLIB_NOCONFIG IMPORTED_IMPLIB IMPORTED_IMPLIB_MINSIZEREL
+    IMPORTED_IMPLIB_RELWITHDEBINFO IMPORTED_LOCATION_RELEASE IMPORTED_LOCATION_NOCONFIG
+    IMPORTED_LOCATION IMPORTED_LOCATION_MINSIZEREL IMPORTED_LOCATION_RELWITHDEBINFO)
+
+set(dbgprops IMPORTED_IMPLIB_DEBUG IMPORTED_LOCATION_DEBUG)
+
+foreach(prop ${relprops})
+	get_target_property(ag_coreimplib ag_core::ag_core ${prop})
+	if(ag_coreimplib)
+		message("set ag_coreimplib from ${prop}")
+		break()
+	endif()
+endforeach()
+
+#foreach(prop ${relprops})
+#	get_target_property(ag_guiimplib ag_gui::ag_gui ${prop})
+#	if(ag_guiimplib)
+#		#message("set ag_guiimplib from ${prop}")
+#		break()
+#	endif()
+#endforeach()
+
+foreach(prop ${dbgprops})
+	get_target_property(ag_coreimplibdbg ag_core::ag_core ${prop})
+	if(ag_coreimplibdbg)
+		message("set ag_coreimplibdbg from ${prop}")
+		break()
+	endif()
+endforeach()
+
+#foreach(prop ${dbgprops})
+#	get_target_property(ag_guiimplibdbg ag_gui::ag_gui ${prop})
+#	if(ag_guiimplibdbg)
+#		#message("set ag_guiimplibdbg from ${prop}")
+#		break()
+#	endif()
+#endforeach()
+
+if(ag_coreimplib AND ag_coreimplibdbg)
+	set(AGAR_CORE_LIBRARIES $<IF:$<CONFIG:Debug>,${ag_coreimplibdbg},${ag_coreimplib}>)
+else()
+	if((NOT ag_coreimplib) AND ag_coreimplibdbg)
+		set(ag_coreimplib ${ag_coreimplibdbg})
+	endif()
+	if(ag_coreimplib)
+		set(AGAR_CORE_LIBRARIES ${ag_coreimplib})
+	else()
+		message(FATAL_ERROR, "ag_core::ag_core has no lib to link against in IMPORTED_IMPLIB* or IMPORTED_LOCATION*")
+	endif()
+endif()
+
+get_filename_component(AGAR_CORE_LIBDIR ${ag_coreimplib} PATH)
+
+# XXX
+get_filename_component(AGAR_CORE_EXEC_PREFIX ${AGAR_CORE_LIBDIR} PATH)
+set(AGAR_CORE_PREFIX ${AGAR_CORE_EXEC_PREFIX})
+
+unset(ag_coreimplib)
+unset(ag_coreimplibdbg)
+unset(relprops)
+unset(dbgprops)
