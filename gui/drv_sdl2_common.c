46e5cd65939dab437f59a3325a0fc0e38fcb36a7
Julien Nadeau Carriere <vedge@hypertriton.com>
2026-05-08 01:31:43 +0000

    Remove unused `SDL_OPENGL` code in `AG_InitVideoSDL2()`.
    git-svn-id: file:///home/dev/agar/trunk@11295 86a84d31-9702-0410-8f1e-a60cab7bd424

--- gui/drv_sdl2_common.c
+++ gui/drv_sdl2_common.c
@@ -51,9 +51,7 @@ static void AG_SDL2_SoftBlit_NoColorkey(const AG_Surface *_Nonnull, AG_Rect,
                                         SDL_Surface *_Nonnull, AG_Rect);
 
 /*
- * Initialize Agar with an existing SDL2 display. If the display surface has
- * flags SDL_OPENGL / SDL_OPENGLBLIT set, the "sdl2gl" driver is selected;
- * otherwise, the "sdl2fb" driver is used.
+ * Initialize Agar with an existing SDL2 display surface.
  */
 int
 AG_InitVideoSDL2(void *pDisplay, Uint flags)
@@ -61,51 +59,27 @@ AG_InitVideoSDL2(void *pDisplay, Uint flags)
 	SDL_Surface *display = pDisplay;
 	AG_Driver *drv = NULL;
 	AG_DriverClass *dc = NULL, **pd;
-	int useGL = 0;
 
 	if (AG_InitGUIGlobals() == -1)
 		return (-1);
-#if 0
-	/* Enable OpenGL mode if the surface has SDL_OPENGL set. */
-	if ((display->flags & SDL_OPENGL)
-#ifdef SDL_OPENGLBLIT
-	    || (display->flags & SDL_OPENGLBLIT)
-#endif
-	    ) {
-		useGL = 1;
-	} else {
-		if (flags & AG_VIDEO_OPENGL) {
-			AG_SetErrorS(_("AG_VIDEO_OPENGL flag requested and "
-			               "display surface is !SDL_OPENGL"));
-			goto fail;
-		}
-	}
-#endif
+
 	for (pd = &agDriverList[0]; *pd != NULL; pd++) {
 		if (((*pd)->wm == AG_WM_SINGLE) &&
-		    ((*pd)->flags & AG_DRIVER_SDL2)) {
-			if (useGL) {
-				if (!((*pd)->flags & AG_DRIVER_OPENGL))
-					continue;
-			} else {
-				if ((*pd)->flags & AG_DRIVER_OPENGL)
-					continue;
-			}
+		    ((*pd)->flags & AG_DRIVER_SDL2) &&
+		    ((*pd)->flags & AG_DRIVER_OPENGL) == 0) {
 			dc = *pd;
 			break;
 		}
 	}
 	if (dc == NULL) {
-		AG_SetError(_("No compatible %s driver is available"),
-		    useGL ? "SDL2/OpenGL" : "SDL2");
+		AG_SetErrorS(_("No compatible SDL2 driver is available"));
 		goto fail;
 	}
 	if ((drv = AG_DriverOpen(dc, NULL)) == NULL)
 		goto fail;
 
 	/* Open a video display. */
-	if (AGDRIVER_SW_CLASS(drv)->openVideoContext(drv, (void *)display,
-	    flags) == -1) {
+	if (AGDRIVER_SW_CLASS(drv)->openVideoContext(drv, (void *)display, flags) == -1) {
 		AG_DriverClose(drv);
 		goto fail;
 	}
@@ -137,8 +111,7 @@ AG_SetVideoSurfaceSDL2(void *pDisplay)
 		AG_SetErrorS(_("Current driver is not an SDL2 driver"));
 		return (-1);
 	}
-	if (AGDRIVER_SW_CLASS(agDriverSw)->setVideoContext(agDriverSw,
-	    pDisplay) == -1) {
+	if (AGDRIVER_SW_CLASS(agDriverSw)->setVideoContext(agDriverSw, pDisplay) == -1) {
 		return (-1);
 	}
 	AG_PostResizeDisplay(agDriverSw);
